3b75258259fc903022822e3154064aaa5d42bb13
theTwister <6237734+twist84@users.noreply.github.com>
2022-08-22 20:56:16 +0100

    Hooks

--- game/source/memory/module.cpp
+++ game/source/memory/module.cpp
@@ -1,5 +1,9 @@
 #include "memory/module.hpp"
 
+#include "camera/director.hpp"
+#include "game/player_control.hpp"
+#include "main/main_game_launch.hpp"
+
 #include <windows.h>
 #include <stdio.h>
 
@@ -16,4 +20,40 @@ inline c_hook_call<address>::c_hook_call(module_address const function, bool rem
 
     memcpy(m_addr.pointer, &m_call, sizeof(call_instruction));
     VirtualProtect(m_addr.pointer, sizeof(call_instruction), protect, &protect);
+}
+
+void main_loop_body_begin();
+void main_loop_body_end();
+
+c_hook_call<0x00505C2B> main_loop_body_begin_call({ .pointer = main_loop_body_begin });
+c_hook_call<0x0050605C> main_loop_body_end_call({ .pointer = main_loop_body_end });
+
+void main_loop_body_begin()
+{
+    if (GetKeyState(VK_PRIOR) & 0x8000)
+    {
+        player_control_toggle_machinima_camera_enabled();
+        player_control_toggle_machinima_camera_debug();
+        Sleep(75);
+    }
+    else if (GetKeyState(VK_PRIOR) & 0x8000)
+    {
+        player_control_toggle_machinima_camera_use_old_controls();
+        Sleep(75);
+    }
+}
+
+void main_loop_body_end()
+{
+    if (GetKeyState(VK_HOME) & 0x8000)
+    {
+        main_game_launch_set_multiplayer_splitscreen_count(4);
+        //main_game_launch_set_coop_player_count(4);
+        main_game_launch("maps\\riverworld");
+    }
+    else if (GetKeyState(VK_END) & 0x8000)
+    {
+        director_toggle(0, _director_mode_debug);
+        Sleep(75);
+    }
 }
