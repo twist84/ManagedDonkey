aeb1aa64c4868bc540a76a2bce0074143bc1dfdb
theTwister <6237734+twist84@users.noreply.github.com>
2022-08-24 22:21:51 +0100

    Added transport_security

---
+++ game/source/networking/transport/transport_security.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+struct s_transport_secure_identifier
+{
+	long parts[4];
+};
+
+struct s_transport_secure_address
+{
+	long parts[4];
+};
+
+struct s_transport_secure_key
+{
+	long parts[4];
+};
