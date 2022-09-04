8872fa40fedb81946269478cbb7be965613e3c2a
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-04 07:52:30 +0100

    Added `player_action`

--- game/source/game/player_control.hpp
+++ game/source/game/player_control.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "units/units.hpp"
 
 struct s_player_control_non_deterministic_input_user_state
 {
-	char __data0[6];
+	byte __data0[0x6];
 	bool player_input_locked;
-	char __data7[0x30 - 7];
+	byte __data7[0x29];
 };
 static_assert(sizeof(s_player_control_non_deterministic_input_user_state) == 0x30);
 
@@ -46,19 +47,19 @@ struct s_player_control_internal_state
 	real_point2d throttle;
 	real primary_trigger;
 	real secondary_trigger;
-	word weapon_set_identifier;
-	byte primary_weapon_index;
-	byte secondary_weapon_index;
+	s_unit_weapon_set weapon_set;
 	word grenade_index;
 	word zoom_level;
 	s_player_action_context action_context;
 	s_aim_assist_targeting_result targeting;
 	bool map_editor_rotation_valid;
 	bool map_editor_player_locked_for_manipulation;
-	char __data66[2];
+	byte : 8;
+	byte : 8;
 	real_euler_angles2d map_editor_rotation;
 	word_flags map_editor_flags;
-	char __data6A[2];
+	byte : 8;
+	byte : 8;
 };
 static_assert(sizeof(s_player_control_internal_state) == 0x6C);
 
@@ -72,11 +73,47 @@ static_assert(sizeof(s_player_control_state) == 0x70);
 struct s_player_control
 {
 	s_player_control_state state;
-	char __data70[120];
+	byte __data70[0x78];
 	real_point3d __positionE8;
-	char __dataF4[4];
+	byte __dataF4[0x4];
 };
 static_assert(sizeof(s_player_control) == 0xF8);
+
+struct player_action
+{
+	dword control_context;
+	byte control_context_identifier;
+	byte : 8;
+	byte : 8;
+	byte : 8;
+	dword_flags control_flags;
+	real_euler_angles2d angles;
+	real_point2d throttle;
+	bool map_editor_rotation_valid;
+	bool map_editor_player_locked_for_manipulation;
+	byte : 8;
+	byte : 8;
+	real_euler_angles2d map_editor_rotation;
+	word_flags map_editor_flags;
+	byte : 8;
+	byte : 8;
+	real trigger;
+	real secondary_trigger;
+	word_flags action_flags;
+	s_unit_weapon_set weapon_set;
+	word grenade_index;
+	word zoom_level;
+	byte : 8;
+	byte : 8;
+	s_player_action_context action_context;
+	s_aim_assist_targeting_result targeting;
+	bool velocity_exceeds_motion_tracker_threshold;
+	byte : 8;
+	byte : 8;
+	byte : 8;
+	qword action_test_flags;
+};
+static_assert(sizeof(player_action) == 0x80);
 
 struct s_player_control_globals
 {
@@ -89,7 +126,7 @@ struct s_player_control_globals
 	bool machinima_camera_use_old_controls;
 	bool machinima_camera_debug;
 	bool __unknown8AB;
-	char __data8AC[4];
+	byte __data8AC[0x4];
 };
 static_assert(sizeof(s_player_control_globals) == 0x8B0);
 
---
+++ game/source/units/units.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+struct s_unit_weapon_set
+{
+	word set_identifier;
+	byte weapon_indices[2];
+};
