38cb5fb6e61ceca937e40c867e1610268610d540
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-04 08:00:28 +0100

    Updated `s_network_base_memory_globals`

--- game/source/networking/network_memory.hpp
+++ game/source/networking/network_memory.hpp
@@ -1,43 +1,54 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+
+#include "networking/delivery/network_link.hpp"
+#include "networking/messages/network_message_handler.hpp"
+#include "networking/messages/network_message_type_collection.hpp"
+#include "networking/session/network_observer.hpp"
+#include "networking/session/network_session.hpp"
+#include "networking/session/network_session_manager.hpp"
 #include "networking/session/network_session_parameter_type_collection.hpp"
+#include "networking/transport/transport_address.hpp"
+#include "simulation/simulation_type_collection.hpp"
+#include "simulation/simulation_watcher.hpp"
+#include "simulation/simulation_world.hpp"
+
+struct c_network_out_of_band_consumer
+{
+public:
+	virtual bool __cdecl receive_out_of_band_packet(transport_address const* address, c_bitstream* packet);
+};
+static_assert(sizeof(c_network_out_of_band_consumer) == 0x4);
+
+struct c_network_message_gateway : c_network_out_of_band_consumer
+{
+	bool m_initialized;
+	c_network_link* m_link;
+	c_network_message_type_collection* m_message_types;
+	c_network_message_handler* m_message_handler;
+	byte __data14[0x1];
+	byte m_stream_data[0x5BE];
+	byte __data5D4[0x15];
+	c_bitstream m_stream;
+};
+static_assert(sizeof(c_network_message_gateway) == 0x688);
 
 struct s_network_base_memory_globals
 {
-	//c_network_link link;
-	byte link[0x378];
-
-	//c_network_message_type_collection message_types;
-	byte message_types[0x57C];
+	c_network_link link;
+	c_network_message_type_collection message_types;
 	dword : 32;
-
-	//c_network_message_gateway message_gateway;
-	byte message_gateway[0x688];
-
-	//c_network_message_handler message_handler;
-	byte message_handler[0x18];
-
-	//c_network_observer observer;
-	byte observer[0x23F20];
-
-	//c_network_session_manager session_manager;
-	byte session_manager[0xC];
+	c_network_message_gateway message_gateway;
+	c_network_message_handler message_handler;
+	c_network_observer observer;
+	c_network_session_manager session_manager;
 	dword : 32;
-
-	//c_network_session sessions[3];
-	byte sessions[0x25BC40 * 3];
-
+	c_network_session sessions[3];
 	c_network_session_parameter_type_collection sesssion_parameter_types;
-
-	//c_simulation_world simulation_world;
-	byte simulation_world[0x1540];
-
-	//c_simulation_watcher simulation_watcher;
-	byte simulation_watcher[0x16658];
-
-	//c_simulation_type_collection simulation_types;
-	byte simulation_types[0x188];
+	c_simulation_world simulation_world;
+	c_simulation_watcher simulation_watcher;
+	c_simulation_type_collection simulation_types;
 };
 static_assert(sizeof(s_network_base_memory_globals) == 0x7506C8);
 
