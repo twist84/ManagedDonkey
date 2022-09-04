104802980ce3151a5e3c86d31d5d23fb0328c712
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-04 08:40:58 +0100

    Updated `session_parameter_type_collection`

--- game/source/networking/session/network_session_parameter_type_collection.cpp
+++ game/source/networking/session/network_session_parameter_type_collection.cpp
@@ -8,15 +8,14 @@
 void c_network_session_parameter_type_collection::register_session_parameter_type(
 	e_network_session_parameter_type session_parameter_type,
 	char const* session_parameter_name,
-	s_network_session_parameter_type::encode_t* update_encode,
-	s_network_session_parameter_type::decode_t* update_decode,
+	encode_t* update_encode,
+	decode_t* update_decode,
 	long update_size,
-	s_network_session_parameter_type::encode_t* change_request_encode,
-	s_network_session_parameter_type::decode_t* change_request_decode,
+	encode_t* change_request_encode,
+	decode_t* change_request_decode,
 	long change_request_size
 )
 {
-	s_network_session_parameter_type* parameter = &m_parameters[session_parameter_type];
 	assert(session_parameter_name);
 	assert(update_encode);
 	assert(update_decode);
@@ -26,6 +25,8 @@ void c_network_session_parameter_type_collection::register_session_parameter_typ
 	assert(change_request_decode);
 	assert(change_request_size > 0);
 	assert(change_request_size <= k_network_session_parameters_request_payload_size);
+
+	s_network_session_parameter_type* parameter = &m_parameters[session_parameter_type];
 	assert(!parameter->initialized);
 
 	parameter->session_parameter_name = session_parameter_name;
@@ -37,26 +38,25 @@ void c_network_session_parameter_type_collection::register_session_parameter_typ
 	parameter->change_request_size = change_request_size;
 	parameter->initialized = true;
 
-
-	DECLFUNC(0x004D27A0,
-		void, __thiscall, c_network_session_parameter_type_collection const*,
-		e_network_session_parameter_type,
-		char const*,
-		s_network_session_parameter_type::encode_t*,
-		s_network_session_parameter_type::decode_t*,
-		long,
-		s_network_session_parameter_type::encode_t*,
-		s_network_session_parameter_type::decode_t*,
-		long change_request_size
-	)(
-		this,
-		session_parameter_type,
-		session_parameter_name,
-		update_encode,
-		update_decode,
-		update_size,
-		change_request_encode,
-		change_request_decode,
-		change_request_size
-		);
+	//DECLFUNC(0x004D27A0,
+	//	void, __thiscall, c_network_session_parameter_type_collection const*,
+	//	e_network_session_parameter_type,
+	//	char const*,
+	//	encode_t*,
+	//	decode_t*,
+	//	long,
+	//	encode_t*,
+	//	decode_t*,
+	//	long
+	//)(
+	//	this,
+	//	session_parameter_type,
+	//	session_parameter_name,
+	//	update_encode,
+	//	update_decode,
+	//	update_size,
+	//	change_request_encode,
+	//	change_request_decode,
+	//	change_request_size
+	//);
 }
