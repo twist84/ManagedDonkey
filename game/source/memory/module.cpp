6703f95ea7590b93fb83c9098d14e6aef43c3602
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-10 13:46:44 +0100

    Update module.cpp

--- game/source/memory/module.cpp
+++ game/source/memory/module.cpp
@@ -14,36 +14,36 @@
 
 #include "memory/module.inl"
 
-c_hook_call<0x00505C2B> main_loop_body_begin_call({ .pointer = main_loop_body_begin });
-c_hook_call<0x0050605C> main_loop_body_end_call({ .pointer = main_loop_body_end });
+c_hook_call<0x00505C2B> main_loop_body_begin_hook({ .pointer = main_loop_body_begin });
+c_hook_call<0x0050605C> main_loop_body_end_hook({ .pointer = main_loop_body_end });
 
 // stops hf2p startup functions from running
-//c_data_patch<0x018B59D4> g_hf2p_first_run(false);
+//c_data_patch<0x018B59D4> g_hf2p_first_run_patch(false);
 
 // Bungie OS exception, dirty disc error
-c_hook_call<0x00A9F6C0> damaged_media_exception_call0({ .pointer = damaged_media_exception });
-c_hook_call<0x00A9F6D9> damaged_media_exception_call1({ .pointer = damaged_media_exception });
-c_data_patch_array damaged_media_exception_call_rets(
+c_hook_call<0x00A9F6C0> damaged_media_exception_hook0({ .pointer = damaged_media_exception });
+c_hook_call<0x00A9F6D9> damaged_media_exception_hook1({ .pointer = damaged_media_exception });
+c_data_patch_array damaged_media_exception_patch(
 	{
-		damaged_media_exception_call0.get_address() + 5,
-		damaged_media_exception_call1.get_address() + 5
+		damaged_media_exception_hook0.get_address() + 5,
+		damaged_media_exception_hook1.get_address() + 5
 	}, 
 	{ 0x90, 0x90, 0x90, 0x90, 0x90 }
 );
 
 // test dirty disc error hook
-c_hook_call<0x00567964> main_load_map_call({ .pointer = main_load_map });
+c_hook_call<0x00567964> main_load_map_hook({ .pointer = main_load_map });
 
 // override any mismatched tag checksums
-c_hook_call<0x0050286A> crc_checksum_buffer_alder32_call({ .pointer = crc_checksum_buffer_alder32 });
+c_hook_call<0x0050286A> crc_checksum_buffer_alder32_hook({ .pointer = crc_checksum_buffer_alder32 });
 
 // override header verification
-c_hook_call<0x00501A51> cache_files_verify_header_rsa_signature_call({ .pointer = cache_files_verify_header_rsa_signature });
+c_hook_call<0x00501A51> cache_files_verify_header_rsa_signature_hook({ .pointer = cache_files_verify_header_rsa_signature });
 
 // override `scenario_load::secure_working_memory` verification
-c_hook_call<0x0050309E> security_rsa_compute_and_verify_signature_call({ .pointer = security_rsa_compute_and_verify_signature });
+c_hook_call<0x0050309E> security_rsa_compute_and_verify_signature_hook({ .pointer = security_rsa_compute_and_verify_signature });
 
-c_hook_call<0x005679B5> scenario_load_call({ .pointer = scenario_load });
+c_hook_call<0x005679B5> scenario_load_hook({ .pointer = scenario_load });
 
 // called from `game_engine_running`
-c_hook_call<0x00552410> current_game_engine_call({ .pointer = current_game_engine });
+c_hook_call<0x00552410> current_game_engine_hook({ .pointer = current_game_engine });
