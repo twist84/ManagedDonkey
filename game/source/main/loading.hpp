80e74046d75ee37ef73656ab8d269ac620b0ce09
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-14 12:04:52 +0100

    Added `loading` thunks

--- game/source/main/loading.hpp
+++ game/source/main/loading.hpp
@@ -1,5 +1,35 @@
 #pragma once
 
-extern bool force_load_map_failed;
+#include "cseries/cseries.hpp"
 
-bool __cdecl main_load_map(char* scenario_path, long map_load_type);
+extern void __cdecl loading_basic_progress_complete();
+extern void __cdecl loading_basic_progress_disable();
+extern void __cdecl loading_basic_progress_enable(char const* scenario_path, long insertion_point);
+extern bool __cdecl loading_basic_progress_enabled();
+extern real __cdecl loading_basic_progress_get();
+extern void __cdecl loading_basic_progress_phase_begin(long phase, dword update_size);
+extern void __cdecl loading_basic_progress_phase_end();
+extern void __cdecl loading_basic_progress_update_fraction(real update_fraction);
+extern void __cdecl loading_basic_progress_update_phase_sizes();
+extern void __cdecl loading_basic_progress_update_size(dword update_size);
+extern bool __cdecl main_blocking_load_in_progress(real* out_progress);
+
+extern bool force_load_map_failed;
+extern bool __cdecl main_load_map(char* scenario_path, long map_load_type);
+
+extern real __cdecl main_load_map_loading_progress(long scenario_type, short a2, char const* scenario_path);
+extern long __cdecl main_load_map_status(char const* scenario_path);
+extern long __cdecl main_load_map_status_with_insertion_point(short insertion_point, char const* scenario_path);
+extern bool __cdecl main_load_map_with_insertion_point(short insertion_point, char const* scenario_path, long map_load_type);
+extern bool __cdecl main_load_next_map_loading();
+extern void __cdecl main_loading_dispose();
+extern void __cdecl main_loading_enable_spinner(bool enable_spinner);
+extern bool __cdecl main_loading_get_action(struct s_main_loading_action* out_loading_action);
+extern long __cdecl main_loading_get_gui_game_mode();
+extern long __cdecl main_loading_get_loading_status(wchar_t* status_message);
+extern void __cdecl main_loading_idle_with_blocking_load(bool blocking_load);
+extern void __cdecl main_loading_initialize();
+extern bool __cdecl main_loading_is_idle();
+extern void __cdecl main_loading_progress_done(char const* scenario_path, void* a2);
+extern void __cdecl main_loading_progress_new(char const* scenario_path, void* a2);
+extern void __cdecl main_loading_progress_update(char const* scenario_path, char const* a2, long progress, void* a4);
