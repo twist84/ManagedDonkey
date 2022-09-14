6193585a6a8e199a1c1e0093a6587efff2a8aff9
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-14 12:13:16 +0100

    Update security_functions.cpp

--- game/source/cache/security_functions.cpp
+++ game/source/cache/security_functions.cpp
@@ -1,14 +1,14 @@
 #include "cache/security_functions.hpp"
 
 
-bool __cdecl security_validate_hash(void const* buffer, unsigned long buffer_size, bool calculate, s_network_http_request_hash const* hash, s_network_http_request_hash* out_hash)
+bool __cdecl security_validate_hash(void const* buffer, dword buffer_size, bool calculate, s_network_http_request_hash const* hash, s_network_http_request_hash* out_hash)
 {
 	bool result = DECLTHUNK(0x00509020, security_validate_hash, buffer, buffer_size, calculate, hash, out_hash);
 
 	return result;
 }
 
-bool __cdecl security_calculate_hash(void const* buffer, unsigned long buffer_size, bool calculate, s_network_http_request_hash* hash)
+bool __cdecl security_calculate_hash(void const* buffer, dword buffer_size, bool calculate, s_network_http_request_hash* hash)
 {
 	bool result = DECLTHUNK(0x00508D30, security_calculate_hash, buffer, buffer_size, calculate, hash);
 
