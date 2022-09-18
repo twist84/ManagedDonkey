c60b09f7a902f04527d422d221717d8e3230de34
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-18 23:48:54 +0100

    Updated `launcher`

--- launcher/source/main.cpp
+++ launcher/source/main.cpp
@@ -1,25 +1,45 @@
 #include "main.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 3)
+    {
+        printf(usage);
+
+        return 1;
+    }
+
+    char* ApplicationName = argv[1];
+    char* DllName = argv[2];
+
     GetCurrentDirectoryA(4096, CurrentDirectory);
     if (GetFileAttributesA(CurrentDirectory) == INVALID_FILE_ATTRIBUTES)
-        return 1;
-
-    PathCombineA(ApplicationPath, CurrentDirectory, "hf2p_game_client_cache_release.exe");
-    if (GetFileAttributesA(ApplicationPath) == INVALID_FILE_ATTRIBUTES)
         return 2;
 
-    PathCombineA(DllPath, CurrentDirectory, "game.dll");
-    if (GetFileAttributesA(DllPath) == INVALID_FILE_ATTRIBUTES)
+    printf("Launcher: Checking %s exists\n", ApplicationName);
+
+    PathCombineA(ApplicationPath, CurrentDirectory, ApplicationName);
+    if (GetFileAttributesA(ApplicationPath) == INVALID_FILE_ATTRIBUTES)
         return 3;
 
-    if (DetourCreateProcessWithDllA(ApplicationPath, NULL, NULL, NULL, TRUE, CREATE_DEFAULT_ERROR_MODE, NULL, CurrentDirectory, &StartupInfo, &ProcessInfo, DllPath, NULL) == FALSE)
+    printf("Launcher: Checking %s exists\n", DllName);
+
+    PathCombineA(DllPath, CurrentDirectory, DllName);
+    if (GetFileAttributesA(DllPath) == INVALID_FILE_ATTRIBUTES)
         return 4;
+
+    printf("Launcher: Creating process\n");
+
+    if (DetourCreateProcessWithDllA(ApplicationPath, NULL, NULL, NULL, TRUE, CREATE_DEFAULT_ERROR_MODE, NULL, CurrentDirectory, &StartupInfo, &ProcessInfo, DllPath, NULL) == FALSE)
+        return 5;
+
+    printf("Launcher: Waiting for %s exit\n", ApplicationName);
 
 #ifdef _DEBUG
     WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
 #endif // _DEBUG
+
+    printf("Launcher: Exiting\n");
 
     return 0;
 }
--- launcher/source/main.hpp
+++ launcher/source/main.hpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <detours.h>
 #include <shlwapi.h>
+#include <stdio.h>
 
 char CurrentDirectory[4096]{};
 char ApplicationPath[4096]{};
@@ -10,3 +11,12 @@ char DllPath[4096]{};
 
 STARTUPINFOA StartupInfo = { .cb = sizeof(STARTUPINFOA) };
 PROCESS_INFORMATION ProcessInfo = { };
+
+char usage[] =
+R"(
+Usage:
+    launcher.exe <executable_name> <dll_name>
+
+Example:
+    launcher.exe eldorado.exe game.dll
+)";
