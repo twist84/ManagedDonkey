3b1cdd2f4bb4251d301fab5c2949dda9e47392e9
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-25 14:59:58 +0100

    Added more file functions

--- game/source/tag_files/files_windows.cpp
+++ game/source/tag_files/files_windows.cpp
@@ -6,8 +6,14 @@
 
 #include <windows.h>
 
-HOOK_DECLARE(0x005294F0, file_error);
+HOOK_DECLARE(0x00528B60, file_close);
+HOOK_DECLARE(0x00528C40, file_compare_last_modification_dates);
+HOOK_DECLARE(0x00528C60, file_copy_to);
+HOOK_DECLARE(0x00528DD0, file_copy_to_recursive);
+HOOK_DECLARE(0x00528FB0, file_create);
+HOOK_DECLARE(0x00529130, file_date_format_for_output);
 HOOK_DECLARE(0x00529170, file_delete);
+HOOK_DECLARE(0x005294F0, file_error);
 HOOK_DECLARE(0x005295F0, file_exists);
 HOOK_DECLARE(0x0052A220, file_open);
 
@@ -27,6 +33,56 @@ bool file_errors_suppressed()
         return get_tls()->g_file_errors_suppressed;
 
     return true;
+}
+
+bool __cdecl file_close(s_file_reference* file_reference)
+{
+    bool result = false;
+    HOOK_INVOKE(result =, file_close, file_reference);
+    return result;
+}
+
+int __cdecl file_compare_last_modification_dates(s_file_last_modification_date const* date1, s_file_last_modification_date const* date2)
+{
+    int result = 0;
+    HOOK_INVOKE(result =, file_compare_last_modification_dates, date1, date2);
+    return result;
+}
+
+bool __cdecl file_copy_to(s_file_reference* file_reference, s_file_reference* other, bool fail_if_not_exists)
+{
+    bool result = false;
+    HOOK_INVOKE(result =, file_copy_to, file_reference, other, fail_if_not_exists);
+    return result;
+}
+
+bool __cdecl file_copy_to_recursive(s_file_reference* file_reference, s_file_reference* other)
+{
+    bool result = false;
+    HOOK_INVOKE(result =, file_copy_to_recursive, file_reference, other);
+    return result;
+}
+
+bool __cdecl file_create(s_file_reference* file_reference)
+{
+    bool result = false;
+    HOOK_INVOKE(result =, file_create, file_reference);
+    return result;
+}
+
+void __cdecl file_date_format_for_output(s_file_last_modification_date* date, char* buffer, long buffer_size)
+{
+    HOOK_INVOKE(, file_date_format_for_output, date, buffer, buffer_size);
+    INVOKE(0x00529130, file_date_format_for_output, date, buffer, buffer_size);
+}
+
+bool __cdecl file_delete(s_file_reference* file_reference)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, file_delete, file_reference);
+    return result;
 }
 
 //void file_error(char const*, s_file_reference const*, s_file_reference const*, bool)
@@ -57,16 +113,6 @@ void __cdecl file_error(char const* file_function, s_file_reference* file0, s_fi
 
     //HOOK_INVOKE(, file_error, file_function, file0, file1, suppress_error);
 }
-
-bool __cdecl file_delete(s_file_reference* file_reference)
-{
-    FUNCTION_BEGIN(true);
-
-    bool result = false;
-    HOOK_INVOKE(result =, file_delete, file_reference);
-    return result;
-}
-
 bool __cdecl file_exists(s_file_reference const* file_reference)
 {
     FUNCTION_BEGIN(true);
--- game/source/tag_files/files_windows.hpp
+++ game/source/tag_files/files_windows.hpp
@@ -25,7 +25,14 @@ static_assert(sizeof(s_indirect_file) == 0x4);
 struct s_file_reference;
 extern void suppress_file_errors(bool suppress);
 extern bool file_errors_suppressed();
-extern void __cdecl file_error(char const* file_function, s_file_reference* file0, s_file_reference* file1, bool suppress_error);
+
+extern bool __cdecl file_close(s_file_reference* file_reference);
+extern int __cdecl file_compare_last_modification_dates(s_file_last_modification_date const* date1, s_file_last_modification_date const* date2);
+extern bool __cdecl file_copy_to(s_file_reference* file_reference, s_file_reference* other, bool fail_if_not_exists);
+extern bool __cdecl file_copy_to_recursive(s_file_reference* file_reference, s_file_reference* other);
+extern bool __cdecl file_create(s_file_reference* file_reference);
+extern void __cdecl file_date_format_for_output(s_file_last_modification_date* date, char* buffer, long buffer_size);
 extern bool __cdecl file_delete(s_file_reference* file_reference);
+extern void __cdecl file_error(char const* file_function, s_file_reference* file0, s_file_reference* file1, bool suppress_error);
 extern bool __cdecl file_exists(s_file_reference const* file_reference);
 extern bool __cdecl file_open(s_file_reference*, dword open_flags, dword* error);
