4df06cdf726902687751b8804c7f5299b9526873
theTwister <6237734+twist84@users.noreply.github.com>
2022-09-29 12:59:10 +0100

    Update scenario.cpp

--- game/source/scenario/scenario.cpp
+++ game/source/scenario/scenario.cpp
@@ -39,11 +39,11 @@ bool __cdecl scenario_tags_match(long campaign_id, long map_id, char const* scen
 
 	assert(scenario_path != 0);
 
-	scenario* global_scenario = global_scenario_get();
+	scenario* scenario = global_scenario_get();
 	if (levels_map_id_is_fake(map_id) || (campaign_id == -1 && map_id == -1))
 		return true;
 
-	if (global_scenario->campaign_id == campaign_id && global_scenario->map_id == map_id)
+	if (scenario->campaign_id == campaign_id && scenario->map_id == map_id)
 		return true;
 
 	return false;
@@ -95,31 +95,101 @@ bool __cdecl scenario_load(long campaign_id, long map_id, char const* scenario_p
 }
 
 #define PRINT_ZONE_SETS()\
-c_console::write_line("zone sets");\
-for (long i = 0; i < global_scenario->zone_sets.count; i++)\
+if (scenario->zone_sets.count)\
 {\
-	s_scenario_zone_set* zone_set = global_scenario->zone_sets.elements + i;\
-	assert(zone_set);\
-	char const* name = zone_set->name.get_string();\
-	if (*name)\
-		c_console::write_line("    %s", name);\
-}\
-c_console::write_line("")
+	c_console::write_line("zone sets");\
+	for (long i = 0; i < scenario->zone_sets.count; i++)\
+	{\
+		s_scenario_zone_set* zone_set = scenario->zone_sets.elements + i;\
+		assert(zone_set);\
+		char const* name = zone_set->name.get_string();\
+		if (*name)\
+			c_console::write_line("    %s", name);\
+	}\
+	c_console::write_line("");\
+}
+
+#define PRINT_CAMPAIN_PLAYER()\
+if (scenario->campaign_players.count)\
+{\
+	c_console::write_line("campaign players");\
+	for (long i = 0; i < scenario->campaign_players.count; i++)\
+	{\
+		scenario_campaign_player_representation_names_block* campaign_player = scenario->campaign_players.elements + i;\
+		assert(campaign_player);\
+		char const* name = campaign_player->name.get_string();\
+		if (*name)\
+			c_console::write_line("    %s", name);\
+	}\
+	c_console::write_line("");\
+}
+
+#define PRINT_OBJECT_NAMES()\
+if (scenario->object_names.count)\
+{\
+	c_console::write_line("object names");\
+	for (long i = 0; i < scenario->object_names.count; i++)\
+	{\
+		scenario_object_name* object_name = scenario->object_names.elements + i;\
+		assert(object_name);\
+		char const* name = object_name->name;\
+		if (*name)\
+			c_console::write_line("    %s", name);\
+	}\
+	c_console::write_line("");\
+}
 
 #define PRINT_MAP_VARIANT_PALETTES(NAME)\
-c_console::write_line(#NAME);\
-for (long i = 0; i < global_scenario->map_variant_##NAME##_palette.count; i++)\
+if (scenario->map_variant_##NAME##_palette.count)\
 {\
-	scenario_object_palette_entry_with_string_id* palette = global_scenario->map_variant_##NAME##_palette.elements + i;\
-	assert(palette);\
-	char const* display_name = palette->display_name.get_string();\
-	if (*display_name)\
-		c_console::write_line("    %s", display_name);\
-}\
-c_console::write_line("")
+	c_console::write_line("%s palette", #NAME);\
+	for (long i = 0; i < scenario->map_variant_##NAME##_palette.count; i++)\
+	{\
+		scenario_object_palette_entry_with_string_id* palette = scenario->map_variant_##NAME##_palette.elements + i;\
+		assert(palette);\
+		char const* display_name = palette->display_name.get_string();\
+		if (*display_name)\
+			c_console::write_line("    %s", display_name);\
+	}\
+	c_console::write_line("");\
+}
 
+#define PRINT_SOFT_CEILINGS()\
+if (scenario->soft_ceilings.count)\
+{\
+	c_console::write_line("soft ceilings");\
+	for (long i = 0; i < scenario->soft_ceilings.count; i++)\
+	{\
+		s_scenario_soft_ceiling* soft_ceiling = scenario->soft_ceilings.elements + i;\
+		assert(soft_ceiling);\
+		char const* name = soft_ceiling->name.get_string();\
+		if (name)\
+			c_console::write_line("    %s", name);\
+	}\
+	c_console::write_line("");\
+}
+
+#define PRINT_PLAYER_STARTING_PROFILES()\
+if (scenario->player_starting_profile.count)\
+{\
+	c_console::write_line("player starting profile");\
+	for (long i = 0; i < scenario->player_starting_profile.count; i++)\
+	{\
+		scenario_starting_profile* profile = scenario->player_starting_profile.elements + i;\
+		assert(profile);\
+		char const* name = profile->name;\
+		if (*name)\
+			c_console::write_line("    %s", name);\
+	}\
+	c_console::write_line("");\
+}
 void on_scenario_loaded()
 {
+	scenario* scenario = global_scenario_get();
+
+	PRINT_ZONE_SETS();
+	PRINT_CAMPAIN_PLAYER();
+	PRINT_OBJECT_NAMES();
 	PRINT_MAP_VARIANT_PALETTES(vehicle);
 	PRINT_MAP_VARIANT_PALETTES(weapon);
 	PRINT_MAP_VARIANT_PALETTES(equipment);
@@ -127,8 +197,10 @@ void on_scenario_loaded()
 	PRINT_MAP_VARIANT_PALETTES(teleporters);
 	PRINT_MAP_VARIANT_PALETTES(goals);
 	PRINT_MAP_VARIANT_PALETTES(spawners);
+	PRINT_SOFT_CEILINGS();
+	PRINT_PLAYER_STARTING_PROFILES();
 
-	PRINT_ZONE_SETS();
+	printf("");
 }
 
 #undef PRINT_MAP_VARIANT_PALETTES
