c8e9a6f0b61bb2cb5d43800a753f3fe17f476574
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-04 12:23:35 +0100

    Added `levels` hooks (WIP)

--- game/source/main/levels.cpp
+++ game/source/main/levels.cpp
@@ -2,12 +2,57 @@
 
 #include "cseries/console.hpp"
 #include "cseries/cseries.hpp"
+#include "memory/module.hpp"
+#include "networking/tools/network_blf.hpp"
+
+HOOK_DECLARE(0x0054A2A0, levels_add_campaign);
+HOOK_DECLARE(0x0054A4E0, levels_add_map_from_scripting);
+HOOK_DECLARE(0x0054A6A0, levels_add_fake_map_from_scripting);
+HOOK_DECLARE(0x0054A6C0, levels_add_level);
+
+HOOK_DECLARE(0x0054C320, levels_map_id_is_fake);
+
+void __cdecl levels_add_campaign(s_blf_chunk_campaign const* campaign, bool byte_swap, char const* maps_path, bool is_dlc)
+{
+    FUNCTION_BEGIN(true);
+
+    HOOK_INVOKE(, levels_add_campaign, campaign, byte_swap, maps_path, is_dlc);
+}
+
+void __cdecl levels_add_map_from_scripting(long map_id, char const* scenario_path)
+{
+    FUNCTION_BEGIN(true);
+
+    HOOK_INVOKE(, levels_add_map_from_scripting, map_id, scenario_path);
+}
+
+void __cdecl levels_add_fake_map_from_scripting(char const* scenario_path)
+{
+    FUNCTION_BEGIN(true);
+
+    //HOOK_INVOKE(, levels_add_fake_map_from_scripting, scenario_path);
+
+    levels_add_map_from_scripting(-2, scenario_path);
+}
+
+s_level_datum const* __cdecl levels_add_level(s_blf_chunk_scenario const* scenario, bool byte_swap, char const* maps_path, bool is_dlc)
+{
+    FUNCTION_BEGIN(true);
+
+    s_level_datum const* result = nullptr;
+    HOOK_INVOKE(result =, levels_add_level, scenario, byte_swap, maps_path, is_dlc);
+
+    return result;
+}
+
 
 bool __cdecl levels_map_id_is_fake(long map_id)
 {
     FUNCTION_BEGIN(true);
 
-    //return INVOKE(0x0054C320, levels_map_id_is_fake, map_id);
+    //bool result = false;
+    //HOOK_INVOKE(result =, levels_map_id_is_fake, map_id);
+    //return result;
 
     return map_id == -2;
 }
--- game/source/main/levels.hpp
+++ game/source/main/levels.hpp
@@ -1,4 +1,12 @@
 #pragma once
 
-//bool __cdecl levels_map_id_is_fake(enum e_map_id)
+struct s_blf_chunk_campaign;
+struct s_blf_chunk_scenario;
+
+struct s_level_datum;
+
+extern void __cdecl levels_add_campaign(s_blf_chunk_campaign const* campaign, bool byte_swap, char const* maps_path, bool is_dlc);
+extern void __cdecl levels_add_map_from_scripting(long map_id, char const* scenario_path);
+extern void __cdecl levels_add_fake_map_from_scripting(char const* scenario_path);
+extern s_level_datum const* __cdecl levels_add_level(s_blf_chunk_scenario const* scenario, bool byte_swap, char const* maps_path, bool is_dlc);
 extern bool __cdecl levels_map_id_is_fake(long map_id);
