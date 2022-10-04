9bd6d4e3f2131d6567e61db19fac2a05733b8345
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-04 12:23:07 +0100

    Added `blf` structs (WIP)

--- game/source/networking/tools/network_blf.cpp
+++ game/source/networking/tools/network_blf.cpp
@@ -1,2 +1,185 @@
 #include "networking/tools/network_blf.hpp"
 
+#include "config/version.hpp"
+#include "cseries/console.hpp"
+#include "game/game_globals.hpp"
+#include "memory/byte_swapping.hpp"
+
+#include <string.h>
+
+void s_blf_header::setup(long _signature, long _chunk_size, long _major_version, long _minor_version)
+{
+	FUNCTION_BEGIN(true);
+
+	//signature = _signature;
+	//chunk_size = _chunk_size;
+	//major_version = static_cast<short>(_major_version);
+	//minor_version = static_cast<short>(_minor_version);
+	signature = _byteswap_ulong(_signature);
+	chunk_size = _byteswap_ulong(_chunk_size);
+	major_version = _byteswap_ushort(static_cast<short>(_major_version));
+	minor_version = _byteswap_ushort(static_cast<short>(_minor_version));
+}
+
+s_blf_chunk_start_of_file::s_blf_chunk_start_of_file()
+{
+	FUNCTION_BEGIN(true);
+
+	initialize();
+}
+
+void s_blf_chunk_start_of_file::initialize()
+{
+	FUNCTION_BEGIN(true);
+
+	setup('_blf', sizeof(*this), 1, 2);
+
+	//byte_order_marker = -2;
+	byte_order_marker = _byteswap_ushort(-2);
+
+	memset(name, 0, sizeof(name));
+	memset(pad, 0, sizeof(pad));
+}
+
+s_blf_chunk_end_of_file::s_blf_chunk_end_of_file()
+{
+	FUNCTION_BEGIN(true);
+
+	initialize();
+}
+
+void s_blf_chunk_end_of_file::initialize()
+{
+	FUNCTION_BEGIN(true);
+
+	setup('_eof', sizeof(*this), 1, 1);
+
+	file_size = 0;
+	authentication_type = _blf_file_authentication_type_none;
+}
+
+s_blf_chunk_end_of_file_with_crc::s_blf_chunk_end_of_file_with_crc()
+{
+	FUNCTION_BEGIN(true);
+
+	initialize();
+}
+
+void s_blf_chunk_end_of_file_with_crc::initialize()
+{
+	FUNCTION_BEGIN(true);
+
+	setup('_eof', sizeof(*this), 1, 1);
+
+	file_size = 0;
+	authentication_type = _blf_file_authentication_type_crc;
+}
+
+s_blf_chunk_content_header::s_blf_chunk_content_header()
+{
+	FUNCTION_BEGIN(true);
+
+	initialize();
+}
+
+void s_blf_chunk_content_header::initialize()
+{
+	FUNCTION_BEGIN(true);
+
+	setup('chdr', sizeof(*this), 9, 1);
+
+	build_number = static_cast<short>(version_get_build_number());
+	map_minor_version = static_cast<short>(get_map_minor_version());
+
+	metadata.name[0] = 0;
+	metadata.description[0] = 0;
+	metadata.file_type = -1;
+	metadata.author[0] = 0;
+	metadata.date = 0;
+	metadata.length_seconds = 0;
+	metadata.map_id = -1;
+	metadata.game_engine_type = 0;
+}
+
+s_blf_chunk_author::s_blf_chunk_author()
+{
+	FUNCTION_BEGIN(true);
+
+	initialize();
+}
+
+void s_blf_chunk_author::initialize()
+{
+	FUNCTION_BEGIN(true);
+
+	setup('athr', sizeof(*this), 3, 1);
+
+	memset(build_name, 0, sizeof(build_name));
+	build_identifier = 0;
+	memset(build_string, 0, sizeof(build_string));
+
+	memset(author_name, 0, sizeof(author_name));
+}
+
+s_blf_chunk_game_variant::s_blf_chunk_game_variant() :
+	game_variant()
+{
+	FUNCTION_BEGIN(true);
+
+	s_blf_header::setup('mpvr', sizeof(*this), 3, 1);
+}
+
+s_blf_chunk_map_variant::s_blf_chunk_map_variant() :
+	map_variant()
+{
+	FUNCTION_BEGIN(true);
+
+	s_blf_header::setup('mpvr', sizeof(*this), 12, 1);
+	memset(pad, 0, sizeof(pad));
+}
+
+s_blffile_saved_game_file::s_blffile_saved_game_file() :
+	start_of_file_chunk(),
+	content_header_chunk()
+{
+	FUNCTION_BEGIN(true);
+}
+
+s_blffile_game_variant::s_blffile_game_variant() :
+	s_blffile_saved_game_file(),
+	game_variant_chunk(),
+	end_of_file_chunk()
+{
+	FUNCTION_BEGIN(true);
+
+	memset(pad, 0, sizeof(pad));
+}
+
+s_blffile_map_variant::s_blffile_map_variant() :
+	s_blffile_saved_game_file(),
+	map_variant_chunk(),
+	end_of_file_chunk()
+{
+	FUNCTION_BEGIN(true);
+
+	memset(pad, 0, sizeof(pad));
+}
+
+s_blf_chunk_scenario::s_blf_chunk_scenario()
+{
+	FUNCTION_BEGIN(true);
+
+	s_blf_header::setup('levl', sizeof(*this), 3, 2);
+
+	map_id = -1;
+	type_flags = 0;
+	memset(names, 0, sizeof(names));
+	memset(descriptions, 0, sizeof(descriptions));
+	memset(image_file_base, 0, sizeof(image_file_base));
+	memset(scenario_path, 0, sizeof(scenario_path));
+	multiplayer_minimum_desired_players = 0;
+	multiplayer_maximum_desired_players = 0;
+	memset(engine_maximum_teams, 0, sizeof(engine_maximum_teams));
+	allows_saved_films = false;
+	memset(insertions, 0, sizeof(insertions));
+}
--- game/source/networking/tools/network_blf.hpp
+++ game/source/networking/tools/network_blf.hpp
@@ -1,2 +1,227 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
+#include "game/game_options.hpp"
+#include "saved_games/saved_game_files.hpp"
+
+struct s_blf_header
+{
+public:
+	void setup(long signature, long chunk_size, long major_version, long minor_version);
+
+protected:
+	tag signature;
+	dword chunk_size;
+	word major_version;
+	word minor_version;
+};
+static_assert(sizeof(s_blf_header) == 0xC);
+
+struct s_blf_chunk_start_of_file : s_blf_header
+{
+public:
+	s_blf_chunk_start_of_file();
+
+	void initialize();
+
+protected:
+	word byte_order_marker;
+	string name;
+
+	byte pad[2];
+};
+static_assert(sizeof(s_blf_chunk_start_of_file) == 0x30);
+
+#pragma pack(push, 1)
+
+enum e_blf_file_authentication_type
+{
+	_blf_file_authentication_type_none = 0,
+	_blf_file_authentication_type_crc,
+	_blf_file_authentication_type_sha1,
+	_blf_file_authentication_type_rsa,
+
+	k_blf_file_authentication_type_count
+};
+
+struct s_blf_chunk_end_of_file : s_blf_header
+{
+public:
+	s_blf_chunk_end_of_file();
+
+	void initialize();
+
+protected:
+	long file_size;
+	c_enum<e_blf_file_authentication_type, char, k_blf_file_authentication_type_count> authentication_type;
+};
+static_assert(sizeof(s_blf_chunk_end_of_file) == 0x11);
+
+struct s_blf_chunk_end_of_file_with_crc : s_blf_chunk_end_of_file
+{
+public:
+	s_blf_chunk_end_of_file_with_crc();
+
+	void initialize();
+
+protected:
+	dword checksum;
+};
+static_assert(sizeof(s_blf_chunk_end_of_file_with_crc) == 0x15);
+
+struct s_blf_chunk_author : s_blf_header
+{
+public:
+	s_blf_chunk_author();
+
+	void initialize();
+
+protected:
+	char build_name[16];
+	qword build_identifier;
+	char build_string[28];
+	char author_name[16];
+};
+static_assert(sizeof(s_blf_chunk_author) == 0x50);
+
+#pragma pack(pop)
+
+struct s_blf_chunk_content_header : s_blf_header
+{
+public:
+	s_blf_chunk_content_header();
+
+	void initialize();
+
+protected:
+
+	short build_number;
+	short map_minor_version;
+
+	s_saved_game_item_metadata metadata;
+};
+static_assert(sizeof(s_blf_chunk_content_header) == 0x108);
+
+struct s_blf_chunk_game_variant : s_blf_header
+{
+public:
+	s_blf_chunk_game_variant();
+
+protected:
+	c_game_variant game_variant;
+};
+static_assert(sizeof(s_blf_chunk_game_variant) == 0x270);
+
+struct s_blf_chunk_map_variant : s_blf_header
+{
+public:
+	s_blf_chunk_map_variant();
+
+protected:
+	c_map_variant map_variant;
+	byte pad[4];
+};
+static_assert(sizeof(s_blf_chunk_map_variant) == 0xE0A0);
+
+struct s_blffile_saved_game_file
+{
+public:
+	s_blffile_saved_game_file();
+
+protected:
+	s_blf_chunk_start_of_file start_of_file_chunk;
+	s_blf_chunk_content_header content_header_chunk;
+};
+static_assert(sizeof(s_blffile_saved_game_file) == 0x138);
+
+struct s_blffile_game_variant : s_blffile_saved_game_file
+{
+public:
+	s_blffile_game_variant();
+
+protected:
+	s_blf_chunk_game_variant game_variant_chunk;
+	s_blf_chunk_end_of_file end_of_file_chunk;
+
+	byte pad[3];
+};
+static_assert(sizeof(s_blffile_game_variant) == 0x3BC);
+
+struct s_blffile_map_variant : s_blffile_saved_game_file
+{
+public:
+	s_blffile_map_variant();
+
+protected:
+	s_blf_chunk_map_variant map_variant_chunk;
+	s_blf_chunk_end_of_file end_of_file_chunk;
+
+	byte pad[7];
+};
+static_assert(sizeof(s_blffile_map_variant) == 0xE1F0);
+
+struct s_blf_chunk_campaign : s_blf_header
+{
+	long campaign_id;
+
+	dword_flags type_flags;
+
+	wchar_t names[12][64];
+	wchar_t descriptions[12][128];
+
+	long map_ids[64];
+
+	long : 32;
+};
+static_assert(sizeof(s_blf_chunk_campaign) == 0x1318);
+
+struct s_blf_chunk_scenario_insertion
+{
+	bool visible;
+
+	byte __unknown1;
+
+	short zone_set_index;
+
+	dword __unknown4;
+
+	wchar_t names[12][32];
+	wchar_t descriptions[12][128];
+
+	dword __unknownF08;
+	dword __unknownF0C;
+};
+static_assert(sizeof(s_blf_chunk_scenario_insertion) == 0xF10);
+
+struct s_blf_chunk_scenario : s_blf_header
+{
+public:
+	s_blf_chunk_scenario();
+
+protected:
+	long map_id;
+
+	dword_flags type_flags;
+
+	wchar_t names[12][32];
+	wchar_t descriptions[12][128];
+
+	char image_file_base[256];
+	char scenario_path[256];
+
+	long precense_context_id;
+	long sort_order;
+
+	char multiplayer_minimum_desired_players;
+	char multiplayer_maximum_desired_players;
+
+	char engine_maximum_teams[11];
+
+	bool allows_saved_films;
+
+	short : 16;
+	long : 32;
+
+	s_blf_chunk_scenario_insertion insertions[9];
+};
+static_assert(sizeof(s_blf_chunk_scenario) == 0x98C0);
