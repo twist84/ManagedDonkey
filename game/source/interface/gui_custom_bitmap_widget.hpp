30865586104fb641aafc2769026861adcf3ab657
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-06 06:33:47 +0100

    Mapped `c_gui_custom_bitmap_widget`

--- game/source/interface/gui_custom_bitmap_widget.hpp
+++ game/source/interface/gui_custom_bitmap_widget.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "interface/c_gui_bitmap_widget.hpp"
+#include "multithreading/synchronized_value.hpp"
+#include "tag_files/files.hpp"
 
-struct c_gui_custom_bitmap_widget
+
+struct c_gui_custom_bitmap_widget : c_gui_bitmap_widget
 {
 	enum e_custom_map_image_type
 	{
@@ -16,4 +20,19 @@ struct c_gui_custom_bitmap_widget
 	};
 
 	static bool __cdecl get_map_filename(e_custom_map_image_type type, long map_id, c_static_string<256>* out_filename);
+
+	long m_storage_item_index;
+	char* m_async_load_buffer;
+	long m_async_load_buffer_size;
+	long m_async_task_id;
+	c_synchronized_long m_async_result;
+	c_synchronized_long __unknown14C;
+	c_synchronized_long __unknown150;
+	s_file_reference m_file;
+	bool m_allocate_bitmap;
+	long __unknown268;
+	c_static_string<256> m_path;
+	c_static_string<256> m_name;
+	byte __data46C[0x4];
 };
+static_assert(sizeof(c_gui_custom_bitmap_widget) == 0x470);
