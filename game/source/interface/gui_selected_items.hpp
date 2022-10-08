24fd36bbfe60dd5728cf71b599101b55f0c07810
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-08 15:13:38 +0100

    Added `e_gui_selected_item_type`

---
+++ game/source/interface/gui_selected_items.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+enum e_gui_selected_item_type
+{
+	_gui_selection_type_level = 0,
+	_gui_selection_type_difficulty,
+	_gui_selection_type_map,
+	_gui_selection_type_game_variant,
+	_gui_selection_type_film,
+	_gui_selection_type_unused5,
+	_gui_selection_type_network,
+	_gui_selection_type_unused7,
+
+	k_gui_selected_item_type_count
+};
