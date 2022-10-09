6b8ca402ef78e04f5a3267c5647efedcb17d6b21
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-09 14:34:46 +0100

    Added `c_window_manager` stub

--- game/source/interface/user_interface_window_manager.cpp
+++ game/source/interface/user_interface_window_manager.cpp
@@ -3,6 +3,8 @@
 #include "cseries/console.hpp"
 #include "cseries/cseries.hpp"
 
+void* c_window_manager::this_ptr = reinterpret_cast<void*>(0x05260F34);
+
 void __cdecl window_manager_reset_screens()
 {
 	FUNCTION_BEGIN(true);
--- game/source/interface/user_interface_window_manager.hpp
+++ game/source/interface/user_interface_window_manager.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
+struct c_window_manager
+{
+	// #TODO: map this
+	static void* this_ptr;
+};
+
 extern void __cdecl window_manager_reset_screens();
