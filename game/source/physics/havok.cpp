da4bde401a2b8f29995af0b66b6c6351ae08e5ba
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-10 16:07:55 +0100

    Added `havok` thunks

---
+++ game/source/physics/havok.cpp
@@ -0,0 +1,18 @@
+#include "physics/havok.hpp"
+
+#include "cseries/console.hpp"
+#include "cseries/cseries.hpp"
+
+void __cdecl havok_can_modify_state_allow()
+{
+    FUNCTION_BEGIN(true);
+
+    INVOKE(0x005C45C0, havok_can_modify_state_allow);
+}
+
+void __cdecl havok_can_modify_state_disallow()
+{
+    FUNCTION_BEGIN(true);
+
+    INVOKE(0x005C45D0, havok_can_modify_state_disallow);
+}
---
+++ game/source/physics/havok.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+extern void __cdecl havok_can_modify_state_allow();
+extern void __cdecl havok_can_modify_state_disallow();
