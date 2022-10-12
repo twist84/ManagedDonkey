03c0eaa9a7a8ea2fc5c016af30bbce8e8d7db56e
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-12 15:28:10 +0100

    Updated `c_data_patch`

--- game/source/memory/module.cpp
+++ game/source/memory/module.cpp
@@ -115,10 +115,10 @@ bool c_hook_call::apply(bool revert)
 	return true;
 }
 
-c_data_patch::c_data_patch(dword address, long patch_size, byte const(&bytes)[], bool remove_base) :
+c_data_patch::c_data_patch(dword address, long patch_size, byte const(&patch)[], bool remove_base) :
 	m_addr({ .address = global_address_get(remove_base ? address - 0x00400000 : address) }),
 	m_byte_count(patch_size),
-	m_bytes(bytes),
+	m_bytes(patch),
 	m_bytes_original(new byte[m_byte_count]{})
 {
 	if (g_data_patch_count < k_maximum_individual_modification_count)
