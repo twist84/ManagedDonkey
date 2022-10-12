53efdf0dce520c0ea7a67522f421dcacf1617025
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-12 12:16:49 +0100

    Fixed network selection

---
+++ game/source/interface/gui_selected_items.cpp
@@ -0,0 +1,7 @@
+#include "interface/gui_selected_items.hpp"
+
+#include "memory/module.hpp"
+
+// replace call `c_load_pregame_players_selection_screen_message` with `c_load_pregame_selection_screen_message`
+dword load_pregame_selection_screen_message_ctor = 0x00AC34D0;
+HOOK_DECLARE_CALL_WITH_ADDRESS(0x00B21F33, load_pregame_selection_screen_message_ctor, load_pregame_players_selection_screen_message_ctor);
