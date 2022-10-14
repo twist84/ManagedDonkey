8a33ab80afa59ac223423efac4348f78068f9065
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-14 15:13:44 +0100

    Updated `e_gui_selected_item_type`

--- game/source/interface/gui_selected_items.hpp
+++ game/source/interface/gui_selected_items.hpp
@@ -6,10 +6,10 @@ enum e_gui_selected_item_type
 	_gui_selection_type_difficulty,
 	_gui_selection_type_map,
 	_gui_selection_type_game_variant,
+	_gui_selection_type_hopper,
 	_gui_selection_type_film,
-	_gui_selection_type_unused5,
-	_gui_selection_type_network,
-	_gui_selection_type_unused7,
+	_gui_selection_type_unused,
+	_gui_selection_type_network_mode,
 
 	k_gui_selected_item_type_count
 };
