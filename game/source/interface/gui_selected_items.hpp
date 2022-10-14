c957841b8665552eb7a76fe7f03398e6f9c04f65
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-14 22:28:22 +0100

    Updated `e_gui_selected_item_type`

--- game/source/interface/gui_selected_items.hpp
+++ game/source/interface/gui_selected_items.hpp
@@ -8,8 +8,8 @@ enum e_gui_selected_item_type
 	_gui_selection_type_map,
 	_gui_selection_type_game_variant,
 	_gui_selection_type_hopper,
-	_gui_selection_type_film,
-	_gui_selection_type_screenshot,
+	_gui_selection_type_saved_film,
+	_gui_selection_type_saved_screenshot,
 	_gui_selection_type_network_mode,
 
 	k_gui_selected_item_type_count
