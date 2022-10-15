c39ac4f045fe387028d02c1dae42e34e1e974625
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-15 19:29:07 +0100

    Added `transport_security_globals`

---
+++ game/source/networking/transport/transport_security.cpp
@@ -0,0 +1,3 @@
+#include "networking/transport/transport_security.hpp"
+
+s_transport_security_globals& transport_security_globals = *reinterpret_cast<s_transport_security_globals*>(0x0199FAB0);
--- game/source/networking/transport/transport_security.hpp
+++ game/source/networking/transport/transport_security.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "networking/transport/transport_address.hpp"
+
 struct s_transport_unique_identifier
 {
 	long parts[2];
@@ -19,3 +21,21 @@ struct s_transport_secure_key
 {
 	long parts[4];
 };
+
+#pragma pack(push, 1)
+struct s_transport_security_globals
+{
+	bool initialized;
+	bool address_resolved;
+	s_transport_secure_address secure_address;
+	byte __unknown12;
+	byte __unknown13;
+	transport_address address;
+	s_transport_secure_identifier identifier;
+	s_transport_secure_address local_unique_identifier;
+	byte __data48[0x190];
+};
+static_assert(sizeof(s_transport_security_globals) == 0x1D8);
+#pragma pack(pop)
+
+extern s_transport_security_globals& transport_security_globals;
