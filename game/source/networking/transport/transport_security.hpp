53c861f5b5b62575b8bedc9b4a240340c4f04965
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-18 11:13:28 +0100

    Added `s_transport_session_description`

--- game/source/networking/transport/transport_security.hpp
+++ game/source/networking/transport/transport_security.hpp
@@ -6,21 +6,33 @@ struct s_transport_unique_identifier
 {
 	long parts[2];
 };
+static_assert(sizeof(s_transport_unique_identifier) == 0x8);
 
 struct s_transport_secure_identifier
 {
 	long parts[4];
 };
+static_assert(sizeof(s_transport_secure_identifier) == 0x10);
 
 struct s_transport_secure_address
 {
 	long parts[4];
 };
+static_assert(sizeof(s_transport_secure_address) == 0x10);
 
 struct s_transport_secure_key
 {
 	long parts[4];
 };
+static_assert(sizeof(s_transport_secure_key) == 0x10);
+
+struct s_transport_session_description
+{
+	s_transport_secure_identifier id;
+	s_transport_secure_address address;
+	s_transport_secure_key key;
+};
+static_assert(sizeof(s_transport_session_description) == 0x30);
 
 #pragma pack(push, 1)
 struct s_transport_security_globals
