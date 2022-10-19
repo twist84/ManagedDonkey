b52d2236ff5ac1695e3ae66967b35d7a18e70715
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-19 11:23:49 +0100

    Added `editor_director` functions

---
+++ game/source/camera/editor_director.cpp
@@ -0,0 +1,41 @@
+#include "camera/editor_director.hpp"
+
+void __cdecl c_editor_director::bump_speed()
+{
+	INVOKE(0x00727FC0, bump_speed);
+}
+
+void __cdecl editor_director_save_position()
+{
+	INVOKE(0x00727FE0, editor_director_save_position);
+}
+
+void __cdecl editor_director_set_position(real_point3d const* position, real_euler_angles2d* facing)
+{
+	INVOKE(0x00728030, editor_director_set_position, position, facing);
+}
+
+bool __cdecl editor_director_valid()
+{
+	return INVOKE(0x007280E0, editor_director_valid);
+}
+
+c_editor_director* __cdecl get_editor_director()
+{
+	return INVOKE(0x00728110, get_editor_director);
+}
+
+real __cdecl c_editor_director::get_speed()
+{
+	return INVOKE(0x00728170, get_speed);
+}
+
+void __cdecl c_editor_director::set_position_and_roll(real_point3d const* position, real_euler_angles2d const* facing)
+{
+	DECLFUNC(0x00728320, void, __thiscall, c_editor_director const*, real_point3d const*, real_euler_angles2d const*)(this, position, facing);
+}
+
+void __cdecl c_editor_director::set_scripted(bool scripted)
+{
+	DECLFUNC(0x00728410, void, __thiscall, c_editor_director const*, bool)(this, scripted);
+}
--- game/source/camera/editor_director.hpp
+++ game/source/camera/editor_director.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
 #include "camera/director.hpp"
 
 struct c_editor_director : public c_director
@@ -15,5 +16,15 @@ struct c_editor_director : public c_director
 	{
 		DECLFUNC(0x00727EA0, void, __thiscall, c_director*, long)(this, user_index);
 	}
+
+	static void __cdecl bump_speed();
+	static real __cdecl get_speed();
+	void __cdecl set_position_and_roll(real_point3d const* position, real_euler_angles2d const* facing);
+	void __cdecl set_scripted(bool scripted);
 };
 static_assert(sizeof(c_editor_director) == 0x160);
+
+extern void __cdecl editor_director_save_position();
+extern void __cdecl editor_director_set_position(real_point3d const* position, real_euler_angles2d* facing);
+extern bool __cdecl editor_director_valid();
+extern c_editor_director* __cdecl get_editor_director();
