6ab399d547fb7760dcfeef6962d5d1c4fabd983a
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-20 12:51:19 +0100

    Added `transport_secure_address_to_string`

--- game/source/networking/transport/transport_security.cpp
+++ game/source/networking/transport/transport_security.cpp
@@ -2,10 +2,16 @@
 
 s_transport_security_globals& transport_security_globals = *reinterpret_cast<s_transport_security_globals*>(0x0199FAB0);
 
-bool transport_secure_address_get_insecure(transport_address* address)
+bool __cdecl transport_secure_address_get_insecure(transport_address* address)
 {
 	if (address)
 		*address = transport_security_globals.address;
 
     return transport_security_globals.address_resolved;
 }
+
+char* __cdecl transport_secure_address_to_string(s_transport_secure_address const* secure_address, char* _string, long maximum_string_length, bool include_online, bool include_mac)
+{
+	return INVOKE(0x00430E20, transport_secure_address_to_string, secure_address, _string, maximum_string_length, include_online, include_mac);
+}
+
