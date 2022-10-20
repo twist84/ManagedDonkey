4eae5a0fa7ffef4995ddf984f2712a636cc97c58
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-20 12:52:24 +0100

    Added `e_transport_endpoint_option`

--- game/source/networking/transport/transport_endpoint_winsock.hpp
+++ game/source/networking/transport/transport_endpoint_winsock.hpp
@@ -14,6 +14,30 @@ enum e_transport_type
 	k_transport_type_count
 };
 
+enum e_transport_endpoint_option
+{
+	// SO_REUSEADDR
+	_transport_endpoint_option_reuse_address,
+
+	// SO_LINGER
+	_transport_endpoint_option_linger,
+
+	// SO_BROADCAST
+	_transport_endpoint_option_broardcast,
+
+	// SO_SNDBUF
+	_transport_endpoint_option_send_buffer_size,
+
+	// SO_RCVBUF
+	_transport_endpoint_option_receive_buffer_size,
+
+	// 0x4001
+	_transport_endpoint_option_unknown5,
+
+	k_transport_endpoint_option_count
+};
+
+
 struct transport_endpoint
 {
 	SOCKET socket;
