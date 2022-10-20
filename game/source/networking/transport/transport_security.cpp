94039a1bc116cbbe9bf23556614b8d222badfe33
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-20 12:51:44 +0100

    Added `transport_secure_identifier_get_string`

--- game/source/networking/transport/transport_security.cpp
+++ game/source/networking/transport/transport_security.cpp
@@ -15,3 +15,8 @@ char* __cdecl transport_secure_address_to_string(s_transport_secure_address cons
 	return INVOKE(0x00430E20, transport_secure_address_to_string, secure_address, _string, maximum_string_length, include_online, include_mac);
 }
 
+char* __cdecl transport_secure_identifier_get_string(s_transport_secure_identifier const* secure_identifier)
+{
+	return INVOKE(0x00430ED0, transport_secure_identifier_get_string, secure_identifier);
+}
+
