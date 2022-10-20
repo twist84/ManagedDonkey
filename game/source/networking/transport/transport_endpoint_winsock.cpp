16828f5676986945ddde8c791ed0471b4bfdc593
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-20 15:54:57 +0100

    Added `transport_endpoint` hooks

--- game/source/networking/transport/transport_endpoint_winsock.cpp
+++ game/source/networking/transport/transport_endpoint_winsock.cpp
@@ -1,9 +1,36 @@
 #include "networking/transport/transport_endpoint_winsock.hpp"
 
 #include "cseries/console.hpp"
+#include "memory/module.hpp"
+#include "networking/transport/transport.hpp"
+
+HOOK_DECLARE(0x0043F980, get_platform_socket_option);
+HOOK_DECLARE(0x0043FA30, transport_endpoint_accept);
+HOOK_DECLARE(0x0043FAC0, transport_endpoint_async_connect);
+HOOK_DECLARE(0x0043FB60, transport_endpoint_async_is_connected);
+HOOK_DECLARE(0x0043FCE0, transport_endpoint_bind);
+HOOK_DECLARE(0x0043FDF0, transport_endpoint_create);
+HOOK_DECLARE(0x0043FE50, transport_endpoint_create_socket);
+HOOK_DECLARE(0x0043FED0, transport_endpoint_delete);
+HOOK_DECLARE(0x0043FF30, transport_endpoint_disconnect);
+HOOK_DECLARE(0x0043FF80, transport_endpoint_get_option_value);
+HOOK_DECLARE(0x00440000, transport_endpoint_get_socket_address);
+HOOK_DECLARE(0x00440120, transport_endpoint_get_transport_address);
+HOOK_DECLARE(0x00440290, transport_endpoint_read);
+HOOK_DECLARE(0x004402F0, transport_endpoint_read_from);
+HOOK_DECLARE(0x00440440, transport_endpoint_reject);
+HOOK_DECLARE(0x00440470, transport_endpoint_set_blocking);
+HOOK_DECLARE(0x00440520, transport_endpoint_set_option_value);
+HOOK_DECLARE(0x00440590, transport_endpoint_setup);
+HOOK_DECLARE(0x00440660, transport_endpoint_write);
+HOOK_DECLARE(0x004406C0, transport_endpoint_write_to);
+HOOK_DECLARE(0x00440740, transport_endpoint_writeable);
+HOOK_DECLARE(0x004407D0, transport_get_endpoint_address);
 
 long __cdecl get_platform_socket_option(e_transport_endpoint_option option)
 {
+    FUNCTION_BEGIN(true);
+
     switch (option)
     {
     case _transport_endpoint_option_reuse_address:
@@ -24,4 +51,187 @@ long __cdecl get_platform_socket_option(e_transport_endpoint_option option)
     }
 
     return -1;
+}
+
+transport_endpoint* __cdecl transport_endpoint_accept(transport_endpoint* listening_endpoint)
+{
+    FUNCTION_BEGIN(true);
+
+    transport_endpoint* result = nullptr;
+    HOOK_INVOKE(result =, transport_endpoint_accept, listening_endpoint);
+    return result;
+}
+
+bool __cdecl transport_endpoint_async_connect(transport_endpoint* endpoint, transport_address const* address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_async_connect, endpoint, address);
+    return result;
+}
+
+bool __cdecl transport_endpoint_async_is_connected(transport_endpoint* endpoint, bool* is_connected)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_async_is_connected, endpoint, is_connected);
+    return result;
+}
+
+bool __cdecl transport_endpoint_bind(transport_endpoint* endpoint, transport_address* address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_bind, endpoint, address);
+    return result;
+}
+
+transport_endpoint* __cdecl transport_endpoint_create(e_transport_type type)
+{
+    FUNCTION_BEGIN(true);
+
+    transport_endpoint* result = nullptr;
+    HOOK_INVOKE(result =, transport_endpoint_create, type);
+    return result;
+}
+
+bool __cdecl transport_endpoint_create_socket(transport_endpoint* endpoint, transport_address const* address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_create_socket, endpoint, address);
+    return result;
+}
+
+void __cdecl transport_endpoint_delete(transport_endpoint* endpoint)
+{
+    FUNCTION_BEGIN(true);
+
+    HOOK_INVOKE(, transport_endpoint_delete, endpoint);
+}
+
+void __cdecl transport_endpoint_disconnect(transport_endpoint* endpoint)
+{
+    FUNCTION_BEGIN(true);
+
+    HOOK_INVOKE(, transport_endpoint_disconnect, endpoint);
+}
+
+long __cdecl transport_endpoint_get_option_value(transport_endpoint* endpoint, e_transport_endpoint_option option)
+{
+    FUNCTION_BEGIN(true);
+
+    long result = 0;
+    HOOK_INVOKE(result =, transport_endpoint_get_option_value, endpoint, option);
+    return result;
+}
+
+bool __cdecl transport_endpoint_get_socket_address(transport_address const* address, long* socket_address_size, byte* const socket_address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_get_socket_address, address, socket_address_size, socket_address);
+    return result;
+}
+
+bool __cdecl transport_endpoint_get_transport_address(long socket_address_length, byte const* const socket_address, transport_address* address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_get_transport_address, socket_address_length, socket_address, address);
+    return result;
+}
+
+short __cdecl transport_endpoint_read(transport_endpoint* endpoint, void* buffer, short length)
+{
+    FUNCTION_BEGIN(true);
+
+    short result = 0;
+    HOOK_INVOKE(result =, transport_endpoint_read, endpoint, buffer, length);
+    return result;
+}
+
+short __cdecl transport_endpoint_read_from(transport_endpoint* endpoint, void* buffer, short length, transport_address* source)
+{
+    FUNCTION_BEGIN(false);
+
+    short result = 0;
+    HOOK_INVOKE(result =, transport_endpoint_read_from, endpoint, buffer, length, source);
+    return result;
+}
+
+bool __cdecl transport_endpoint_reject(transport_endpoint* listening_endpoint)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_reject, listening_endpoint);
+    return result;
+}
+
+bool __cdecl transport_endpoint_set_blocking(transport_endpoint* endpoint, bool blocking)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_set_blocking, endpoint, blocking);
+    return result;
+}
+
+bool __cdecl transport_endpoint_set_option_value(transport_endpoint* endpoint, e_transport_endpoint_option option, long value)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_set_option_value, endpoint, option, value);
+    return result;
+}
+
+void __cdecl transport_endpoint_setup(transport_endpoint* endpoint, e_transport_type type)
+{
+    FUNCTION_BEGIN(true);
+
+    HOOK_INVOKE(, transport_endpoint_setup, endpoint, type);
+}
+
+short __cdecl transport_endpoint_write(transport_endpoint* endpoint, void const* buffer, short length)
+{
+    FUNCTION_BEGIN(true);
+
+    short result = 0;
+    HOOK_INVOKE(result =, transport_endpoint_write, endpoint, buffer, length);
+    return result;
+}
+
+short __cdecl transport_endpoint_write_to(transport_endpoint* endpoint, void const* buffer, short length, transport_address const* destination)
+{
+    FUNCTION_BEGIN(true);
+
+    short result = 0;
+    HOOK_INVOKE(result =, transport_endpoint_write_to, endpoint, buffer, length, destination);
+    return result;
+}
+
+bool __cdecl transport_endpoint_writeable(transport_endpoint* endpoint)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_endpoint_writeable, endpoint);
+    return result;
+}
+
+bool __cdecl transport_get_endpoint_address(transport_endpoint* endpoint, transport_address* address)
+{
+    FUNCTION_BEGIN(true);
+
+    bool result = false;
+    HOOK_INVOKE(result =, transport_get_endpoint_address, endpoint, address);
+    return result;
 }
--- game/source/networking/transport/transport_endpoint_winsock.hpp
+++ game/source/networking/transport/transport_endpoint_winsock.hpp
@@ -37,7 +37,6 @@ enum e_transport_endpoint_option
 	k_transport_endpoint_option_count
 };
 
-
 struct transport_endpoint
 {
 	SOCKET socket;
@@ -45,4 +44,28 @@ struct transport_endpoint
 	dword_flags flags;
 };
 
+struct transport_address;
+
 extern long __cdecl get_platform_socket_option(e_transport_endpoint_option option);
+extern transport_endpoint* __cdecl transport_endpoint_accept(transport_endpoint* listening_endpoint);
+extern bool __cdecl transport_endpoint_async_connect(transport_endpoint* endpoint, transport_address const* address);
+extern bool __cdecl transport_endpoint_async_is_connected(transport_endpoint* endpoint, bool* is_connected);
+extern bool __cdecl transport_endpoint_bind(transport_endpoint* endpoint, transport_address* address);
+extern transport_endpoint* __cdecl transport_endpoint_create(e_transport_type type);
+extern bool __cdecl transport_endpoint_create_socket(transport_endpoint* endpoint, transport_address const* address);
+extern void __cdecl transport_endpoint_delete(transport_endpoint* endpoint);
+extern void __cdecl transport_endpoint_disconnect(transport_endpoint* endpoint);
+extern long __cdecl transport_endpoint_get_option_value(transport_endpoint* endpoint, e_transport_endpoint_option option);
+extern bool __cdecl transport_endpoint_get_socket_address(transport_address const* address, long* socket_address_size, byte* const socket_address);
+extern bool __cdecl transport_endpoint_get_transport_address(long socket_address_length, byte const* const socket_address, transport_address* address);
+extern short __cdecl transport_endpoint_read(transport_endpoint* endpoint, void* buffer, short length);
+extern short __cdecl transport_endpoint_read_from(transport_endpoint* endpoint, void* buffer, short length, transport_address* source);
+extern bool __cdecl transport_endpoint_reject(transport_endpoint* listening_endpoint);
+extern bool __cdecl transport_endpoint_set_blocking(transport_endpoint* endpoint, bool blocking);
+extern bool __cdecl transport_endpoint_set_option_value(transport_endpoint* endpoint, e_transport_endpoint_option option, long value);
+extern void __cdecl transport_endpoint_setup(transport_endpoint* endpoint, e_transport_type type);
+extern short __cdecl transport_endpoint_write(transport_endpoint* endpoint, void const* buffer, short length);
+extern short __cdecl transport_endpoint_write_to(transport_endpoint* endpoint, void const* buffer, short length, transport_address const* destination);
+extern bool __cdecl transport_endpoint_writeable(transport_endpoint* endpoint);
+extern bool __cdecl transport_get_endpoint_address(transport_endpoint* endpoint, transport_address* address);
+
