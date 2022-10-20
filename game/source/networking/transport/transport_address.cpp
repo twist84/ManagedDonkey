aa239b787cc6c85e5ae8a4a13d6494f71f56343a
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-20 12:54:26 +0100

    Added `transport_address` hooks

---
+++ game/source/networking/transport/transport_address.cpp
@@ -0,0 +1,175 @@
+#include "networking/transport/transport_address.hpp"
+
+#include "cseries/console.hpp"
+#include "memory/module.hpp"
+#include "networking/transport/transport_security.hpp"
+
+#include <assert.h>
+#include <string.h>
+
+HOOK_DECLARE(0x0043F660, transport_address_equivalent);
+HOOK_DECLARE(0x0043F6F0, transport_address_get_string);
+HOOK_DECLARE(0x0043F720, transport_address_ipv4_extract);
+HOOK_DECLARE(0x0043F730, transport_address_is_loopback);
+HOOK_DECLARE(0x0043F750, transport_address_to_string);
+HOOK_DECLARE(0x0043F860, transport_address_valid);
+HOOK_DECLARE(0x0043F8D0, transport_get_broadcast_address);
+HOOK_DECLARE(0x0043F8F0, transport_get_listen_address);
+
+bool __cdecl transport_address_equivalent(transport_address const* a, transport_address const* b)
+{
+    assert(a != NULL);
+    assert(b != NULL);
+
+    short address_length = a->address_length <= b->address_length ? a->address_length : b->address_length;
+    return a->address_length > 0 && a->address_length == b->address_length && memcmp(a, b, address_length) == 0 && a->port == b->port;
+}
+
+char const* __cdecl transport_address_get_string(transport_address const* address)
+{
+    static char _string[256]{};
+    transport_address_to_string(address, nullptr, _string, 256, false, true);
+
+    return nullptr;
+}
+
+dword __cdecl transport_address_ipv4_extract(transport_address const* address)
+{
+    assert(address);
+
+    return address->ina.addr;
+}
+
+bool __cdecl transport_address_is_loopback(transport_address const* address)
+{
+    assert(address);
+
+    return address->address_length == 4 && address->ina.addr == 0x7F000001;
+}
+
+char* __cdecl transport_address_to_string(transport_address const* address, s_transport_secure_address const* secure_address, char* _string, short maximum_string_length, bool include_port, bool include_extra)
+{
+    assert(address);
+    assert(_string);
+    assert(maximum_string_length > 0);
+
+    char secure_address_string[256]{};
+    s_transport_secure_identifier secure_identifier_from_address;
+    s_transport_secure_address secure_address_from_address;
+
+    csstrnzcpy(_string, "", maximum_string_length);
+    if (address->address_length == 4)
+    {
+        csnzprintf(_string, maximum_string_length, "%hd.%hd.%hd.%hd",
+            address->ina.bytes[3], 
+            address->ina.bytes[2], 
+            address->ina.bytes[1], 
+            address->ina.bytes[0]);
+
+        if (include_port)
+            csnzappendf(_string, maximum_string_length, ":%hd", address->port);
+
+        if (include_extra)
+        {
+            if (secure_address)
+            {
+                transport_secure_address_to_string(secure_address, secure_address_string, 256, false, false);
+                csstrnzcat(_string, secure_address_string, maximum_string_length);
+            }
+            else if (transport_secure_identifier_retrieve(address, 0, &secure_identifier_from_address, &secure_address_from_address))
+            {
+                transport_secure_address_to_string(&secure_address_from_address, secure_address_string, 256, false, false);
+                csstrnzcat(_string, secure_address_string, maximum_string_length);
+
+                char const* secure_identifier_string = transport_secure_identifier_get_string(&secure_identifier_from_address);
+                csnzappendf(_string, maximum_string_length, "[%s]", secure_identifier_string);
+            }
+            else
+            {
+                csnzappendf(_string, maximum_string_length, " (insecure)");
+            }
+        }
+    }
+    else if (address->address_length == 16)
+    {
+        csnzprintf(_string, maximum_string_length, "%04X.%04X.%04X.%04X.%04X.%04X.%04X.%04X",
+            address->ina6.words[0],
+            address->ina6.words[1],
+            address->ina6.words[2],
+            address->ina6.words[3],
+            address->ina6.words[4],
+            address->ina6.words[5],
+            address->ina6.words[6],
+            address->ina6.words[7]);
+
+        if (include_port)
+            csnzappendf(_string, maximum_string_length, ":%hd", address->port);
+    }
+    else if (address->address_length == 0xFFFF)
+    {
+        csstrnzcpy(_string, address->str, maximum_string_length);
+    }
+    else if (address->address_length)
+    {
+        c_console::write_line("invalid transport address");
+    }
+    else
+    {
+        csstrnzcpy(_string, "NULL_ADDRESS", maximum_string_length);
+    }
+
+    return _string;
+}
+
+bool __cdecl transport_address_valid(transport_address const* address)
+{
+    bool result = false;
+    if (address)
+    {
+        switch (address->address_length)
+        {
+        case 0xFFFFFFFF:
+        {
+            result = address->ina.addr != 0;
+        }
+        break;
+        case 4:
+        {
+            result = address->ina.addr != 0;
+            if (!result)
+                c_console::write_line("networking:transport:transport_address_valid: the IPV4 address is NOT valid");
+        }
+        break;
+        case 16:
+        {
+            for (long i = 0; i < 8; ++i)
+            {
+                if (address->ina6.words[i])
+                {
+                    result = true;
+                    break;
+                }
+            }
+            if (!result)
+                c_console::write_line("networking:transport:transport_address_valid: the IPV6 address is NOT valid");
+        }
+        break;
+        }
+    }
+
+    return result;
+}
+
+void __cdecl transport_get_broadcast_address(transport_address* address, word port)
+{
+    address->address_length = 4;
+    address->ina.addr = -1;
+    address->port = port;
+}
+
+void __cdecl transport_get_listen_address(transport_address* address, word port)
+{
+    address->address_length = 4;
+    address->ina.addr = 0;
+    address->port = port;
+}
--- game/source/networking/transport/transport_address.hpp
+++ game/source/networking/transport/transport_address.hpp
@@ -7,22 +7,38 @@ struct transport_address
 	union
 	{
 		// in_addr
+		// address_length == 0x4
 		union
 		{
 			byte bytes[4];
 			word words[2];
 			dword addr;
-		} ip4;
+		} ina;
 
 		// in6_addr
+		// address_length == 0x10
 		union
 		{
 			byte bytes[16];
 			word words[8];
-		} ip6;
+		} ina6;
+
+		// address_length == 0xFFFF
+		char const* str;
 	};
 
-	short port;
+	word port;
 	short address_length;
 };
 static_assert(sizeof(transport_address) == 0x14);
+
+struct s_transport_secure_address;
+
+extern bool __cdecl transport_address_equivalent(transport_address const* a, transport_address const* b);
+extern char const* __cdecl transport_address_get_string(transport_address const* address);
+extern dword __cdecl transport_address_ipv4_extract(transport_address const* address);
+extern bool __cdecl transport_address_is_loopback(transport_address const* address);
+extern char* __cdecl transport_address_to_string(transport_address const* address, s_transport_secure_address const* secure_address, char* _string, short maximum_string_length, bool include_port, bool include_extra);
+extern bool __cdecl transport_address_valid(transport_address const* address);
+extern void __cdecl transport_get_broadcast_address(transport_address* address, word port);
+extern void __cdecl transport_get_listen_address(transport_address* address, word port);
