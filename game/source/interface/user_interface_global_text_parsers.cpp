77fe152bfde94306f6bea8fddc216276df979baf
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-21 14:48:56 +0100

    Added `parse_build_number` hook

---
+++ game/source/interface/user_interface_global_text_parsers.cpp
@@ -0,0 +1,37 @@
+#include "interface/user_interface_global_text_parsers.hpp"
+
+#include "config/version.hpp"
+#include "main/global_preferences.hpp"
+#include "memory/module.hpp"
+#include "text/unicode.hpp"
+
+#include <assert.h>
+#include <string.h>
+
+HOOK_DECLARE(0x00AD86B0, parse_build_number);
+HOOK_DECLARE(0x00AD8E30, parse_version_number);
+
+bool parse_build_number(void* this_ptr, wchar_t* buffer, long buffer_length)
+{
+    assert(buffer != NULL);
+    assert(buffer_length > 0);
+
+    static char const* build_name = version_get_build_name();
+    static char const* build_string = version_get_build_string();
+
+    if (strlen(build_name) > 1)
+        usnzprintf(buffer, buffer_length, L"build number: %hs (%hs)", build_string, build_name);
+    else
+        usnzprintf(buffer, buffer_length, L"build number: %hs", build_string);
+
+    static bool once = true;
+    if (once)
+    {
+        once = false;
+
+        global_preferences_set_watermark_hidden(true);
+    }
+
+    return true;
+}
+
