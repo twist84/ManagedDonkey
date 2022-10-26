4c09aafa680abd0e3e83bc55c9b53faa8dbdf487
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-26 21:23:05 +0100

    Update files_windows.hpp

--- game/source/tag_files/files_windows.hpp
+++ game/source/tag_files/files_windows.hpp
@@ -79,8 +79,6 @@ struct s_file_reference;
 extern void suppress_file_errors(bool suppress);
 extern bool file_errors_suppressed();
 
-extern bool __cdecl file_get_size(s_file_reference* file_reference, dword* out_file_size);
-extern bool __cdecl file_read(s_file_reference* file_reference, dword size, bool print_error, void* buffer);
 extern bool __cdecl file_close(s_file_reference* file_reference);
 extern int __cdecl file_compare_last_modification_dates(s_file_last_modification_date const* date1, s_file_last_modification_date const* date2);
 extern bool __cdecl file_copy_to(s_file_reference* file_reference, s_file_reference* other, bool fail_if_not_exists);
@@ -90,7 +88,11 @@ extern void __cdecl file_date_format_for_output(s_file_last_modification_date* d
 extern bool __cdecl file_delete(s_file_reference* file_reference);
 extern void __cdecl file_error(char const* file_function, s_file_reference* file0, s_file_reference* file1, bool suppress_error);
 extern bool __cdecl file_exists(s_file_reference const* file_reference);
+extern bool __cdecl file_get_size(s_file_reference* file_reference, dword* out_file_size);
 extern bool __cdecl file_open(s_file_reference*, dword open_flags, dword* error);
+extern bool __cdecl file_read(s_file_reference* file_reference, dword size, bool print_error, void* buffer);
+extern bool __cdecl file_read_from_position(s_file_reference* file_reference, dword offset, dword size, bool print_error, void* buffer);
+extern bool __cdecl file_set_position(s_file_reference* file_reference, dword offset, bool print_error);
 
 extern void find_files_end(s_find_file_data* data);
 extern bool find_files_next(s_find_file_data* data, s_file_reference* out_file, struct s_file_last_modification_date* out_date);
