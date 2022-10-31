6ebebdfb0c16db2b622ea3a42977d87d9c51b508
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-31 11:57:15 +0000

    Added `c_http_stream`

--- game/source/networking/tools/http_stream.hpp
+++ game/source/networking/tools/http_stream.hpp
@@ -16,3 +16,23 @@ struct c_http_post_source
 };
 static_assert(sizeof(c_http_post_source) == 0x13C);
 
+struct c_http_stream
+{
+protected:
+	virtual void __cdecl build_headers();
+	virtual bool __cdecl verify_nescessary_state_is_set();
+
+public:
+	virtual bool __cdecl read(char* buffer, long buffer_length, long* bytes_read);
+	virtual long __cdecl get_length();
+	virtual bool __cdecl at_end();
+	virtual bool __cdecl reset();
+
+	c_static_string<256> m_url;
+	c_static_string<1536> m_headers;
+	long __unknown704;
+	c_static_string<1024> m_extra_headers;
+	long m_position;
+};
+static_assert(sizeof(c_http_stream) == 0xB0C);
+
