9012ca59c7cf18f4aec7edb8767220df60753747
theTwister <6237734+twist84@users.noreply.github.com>
2022-10-31 11:56:27 +0000

    Updated `c_http_post_source`

--- game/source/networking/tools/http_stream.hpp
+++ game/source/networking/tools/http_stream.hpp
@@ -10,9 +10,9 @@ struct c_http_post_source
 	c_static_string<260> m_filename;
 	char const* m_contents_buffer;
 	s_file_reference* m_contents_file;
-	dword m_contents_length;
-	dword m_start_position;
-	byte __data138[4];
+	long m_contents_length;
+	long m_start_position;
+	bool __unknown138;
 };
 static_assert(sizeof(c_http_post_source) == 0x13C);
 
