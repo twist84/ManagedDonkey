12fe1f0c74b27dee5e3e2a0b7b4e9e72cf1408c9
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-02 14:52:12 +0000

    Implemented `http_stream` functions

--- game/source/networking/tools/http_stream.cpp
+++ game/source/networking/tools/http_stream.cpp
@@ -3,6 +3,36 @@
 #include <assert.h>
 #include <string>
 
+c_http_stream::c_http_stream() :
+	m_url(),
+	m_headers(),
+	m_headers_length(),
+	m_extra_headers(),
+	m_position()
+{
+}
+
+bool c_http_stream::reset()
+{
+	m_position = 0;
+	return true;
+}
+
+void c_http_stream::add_header(char const* key, char const* value)
+{
+	m_extra_headers.append_print("%s: \"%s\"\r\n", key, value);
+}
+
+void c_http_stream::clear_headers()
+{
+	m_extra_headers.clear();
+}
+
+void c_http_stream::set_headers(char const* headers)
+{
+	m_extra_headers.set(headers);
+}
+
 long c_http_stream::get_position()
 {
 	return m_position;
@@ -19,5 +49,106 @@ void c_http_stream::set_url(char const* url)
 char const* c_http_stream::get_url() const
 {
 	return m_url.get_string();
+}
+
+c_http_get_stream::c_http_get_stream() :
+	c_http_stream()
+{
+}
+
+void c_http_get_stream::build_headers()
+{
+	m_headers.print("GET %s HTTP/1.0\r\n%s\r\n", m_url.get_string(), m_extra_headers.get_string());
+	m_headers_length = m_headers.length();
+}
+
+bool c_http_get_stream::verify_nescessary_state_is_set()
+{
+	return m_url.length() != 0;
+}
+
+bool c_http_get_stream::read(char* buffer, long buffer_length, long* bytes_read)
+{
+	assert(buffer);
+	assert(bytes_read);
+
+	bool result = DECLFUNC(0x00432A10, bool, __thiscall, c_http_get_stream*, char*, long, long*)(this, buffer, buffer_length, bytes_read);
+
+	return result;
+	
+	result = false;
+	
+	char* buf = buffer;
+	long buf_len = buffer_length;
+	
+	if (verify_nescessary_state_is_set())
+	{
+		if (!m_position)
+			build_headers();
+	
+		if (at_end())
+		{
+			*bytes_read = buf - buffer;
+			result = true;
+		}
+	
+		while (true)
+		{
+			if (at_end() || buf_len <= 0)
+				break;
+		}
+	
+		*bytes_read = buf - buffer;
+	}
+	
+	return result;
+}
+
+long c_http_get_stream::get_length()
+{
+	return m_headers_length;
+}
+
+bool c_http_get_stream::at_end()
+{
+	return m_position == m_headers_length;
+}
+
+c_http_post_stream::c_http_post_stream() :
+	c_http_stream(),
+	__stringC48(),
+	__stringD48(),
+	m_extra_headers_length(),
+	__stringC48_length(),
+	__stringD48_length()
+{
+}
+
+void c_http_post_stream::build_headers()
+{
+	DECLFUNC(0x00432720, void, __thiscall, c_http_post_stream*)(this);
+}
+
+bool c_http_post_stream::verify_nescessary_state_is_set()
+{
+	return DECLFUNC(0x00433000, bool, __thiscall, c_http_post_stream*)(this);
+}
+
+bool c_http_post_stream::read(char* buffer, long buffer_length, long* bytes_read)
+{
+	assert(buffer);
+	assert(bytes_read);
+
+	return DECLFUNC(0x00432A10, bool, __thiscall, c_http_post_stream*, char*, long, long*)(this, buffer, buffer_length, bytes_read);
+}
+
+long c_http_post_stream::get_length()
+{
+	return DECLFUNC(0x004328F0, long, __thiscall, c_http_post_stream*)(this);
+}
+
+bool c_http_post_stream::at_end()
+{
+	return DECLFUNC(0x004326B0, bool, __thiscall, c_http_post_stream*)(this);
 }
 
--- game/source/networking/tools/http_stream.hpp
+++ game/source/networking/tools/http_stream.hpp
@@ -23,22 +23,32 @@ static_assert(sizeof(c_http_post_source) == 0x13C);
 struct c_http_stream
 {
 protected:
-	virtual void build_headers();
-	virtual bool verify_nescessary_state_is_set();
+	static long const k_http_header_size = 1024;
 
 public:
-	virtual bool read(char* buffer, long buffer_length, long* bytes_read);
-	virtual long get_length();
-	virtual bool at_end();
+	c_http_stream();
+
+protected:
+	virtual void build_headers() = 0;
+	virtual bool verify_nescessary_state_is_set() = 0;
+
+public:
+	virtual bool read(char* buffer, long buffer_length, long* bytes_read) = 0;
+	virtual long get_length() = 0;
+	virtual bool at_end() = 0;
 	virtual bool reset();
 
+	void add_header(char const* key, char const* value);
+	void clear_headers();
+	void set_headers(char const* headers);
 	long get_position();
 	void set_url(char const* url);
 	char const* get_url() const;
 
+protected:
 	c_static_string<k_url_size> m_url;
 	c_static_string<1536> m_headers;
-	long __unknown704;
+	long m_headers_length;
 	c_static_string<1024> m_extra_headers;
 	long m_position;
 };
@@ -46,15 +56,39 @@ static_assert(sizeof(c_http_stream) == 0xB0C);
 
 struct c_http_get_stream : c_http_stream
 {
+public:
+	c_http_get_stream();
+
+protected:
+	virtual void build_headers() override;
+	virtual bool verify_nescessary_state_is_set() override;
+
+public:
+	virtual bool read(char* buffer, long buffer_length, long* bytes_read) override;
+	virtual long get_length() override;
+	virtual bool at_end() override;
 };
 static_assert(sizeof(c_http_get_stream) == sizeof(c_http_stream));
 
 struct c_http_post_stream : c_http_stream
 {
+public:
+	c_http_post_stream();
+
+protected:
+	virtual void build_headers() override;
+	virtual bool verify_nescessary_state_is_set() override;
+
+public:
+	virtual bool read(char* buffer, long buffer_length, long* bytes_read) override;
+	virtual long get_length() override;
+	virtual bool at_end() override;
+
+protected:
 	c_http_post_source m_post_source;
 	c_static_string<256> __stringC48;
 	c_static_string<128> __stringD48;
-	long m_headers_length;
+	long m_extra_headers_length;
 	long __stringC48_length;
 	long __stringD48_length;
 };
