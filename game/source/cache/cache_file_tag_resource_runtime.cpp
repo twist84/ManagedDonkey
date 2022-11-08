ba7066a54b04390e9187576da60a27c0c6813ed2
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-08 12:30:19 +0000

    Update cache_file_tag_resource_runtime.cpp

--- game/source/cache/cache_file_tag_resource_runtime.cpp
+++ game/source/cache/cache_file_tag_resource_runtime.cpp
@@ -3,13 +3,13 @@
 #include "cseries/console.hpp"
 #include "memory/module.hpp"
 
-HOOK_DECLARE(0x00563E10, tag_resource_get);
-
-void* __cdecl tag_resource_get(s_tag_resource const* resource)
-{
-    FUNCTION_BEGIN(false);
-
-    void* result = nullptr;
-    HOOK_INVOKE(result =, tag_resource_get, resource);
-    return result;
-}
+//HOOK_DECLARE(0x00563E10, tag_resource_get);
+//
+//void* __cdecl tag_resource_get(s_tag_resource const* resource)
+//{
+//    FUNCTION_BEGIN(false);
+//
+//    void* result = nullptr;
+//    HOOK_INVOKE(result =, tag_resource_get, resource);
+//    return result;
+//}
