28b8ab2204f4a9786836ec421d56623f92218e4e
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-13 01:37:30 +0000

    Added `get_equipment_index_by_name`

--- game/source/game/players.cpp
+++ game/source/game/players.cpp
@@ -1,8 +1,12 @@
 #include "players.hpp"
 
+#include "cache/cache_files.hpp"
 #include "cseries/console.hpp"
 #include "cseries/cseries_windows.hpp"
+#include "game/game_globals.hpp"
+#include "game/multiplayer_definitions.hpp"
 #include "memory/module.hpp"
+#include "scenario/scenario.hpp"
 
 HOOK_DECLARE(0x00536680, player_get_weapon_loadout);
 
@@ -16,6 +20,26 @@ long __cdecl players_first_active_user()
 bool __cdecl player_teleport(long player_index, long object_index, real_point3d const* position)
 {
 	return INVOKE(0x0053F550, player_teleport, player_index, object_index, position);
+}
+
+long get_equipment_index_by_name(char const* name)
+{
+	s_game_globals* game_globals = scenario_try_and_get_game_globals();
+	s_multiplayer_globals_definition* multiplayer_globals = static_cast<s_multiplayer_globals_definition*>(tag_get(game_globals->multiplayer_globals.group_tag, game_globals->multiplayer_globals.index));
+
+	s_multiplayer_universal_globals_definition* universal_data = multiplayer_globals->universal.elements;
+	if (!universal_data)
+		return 0;
+
+	for (long equipment_index = 0; equipment_index < universal_data->equipment.count; equipment_index++)
+	{
+		s_multiplayer_equipment& equipment = universal_data->equipment.elements[equipment_index];
+		char const* equipment_name = equipment.name.get_string();
+		if (equipment_name && csstricmp(name, equipment_name) == 0)
+			return equipment_index;
+	}
+
+	return 0;
 }
 
 s_s3d_player_weapon_configuration_loadout* __cdecl player_get_weapon_loadout(player_datum* player)
--- game/source/game/players.hpp
+++ game/source/game/players.hpp
@@ -75,7 +75,7 @@ struct s_s3d_player_weapon_configuration_loadout
 	byte primary_weapon_index;
 	byte secondary_weapon_index;
 	c_enum<e_grenade_type, char, k_grenade_type_count> grenade_index;
-	c_static_array<byte, 4> consumables;
+	c_static_array<char, 4> consumables;
 
 	// padding?
 	byte __unknown7;
