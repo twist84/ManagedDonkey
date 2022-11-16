dc0aca1f61fc3d295ad00354483b2228d7aa4d0f
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-16 04:57:51 +0000

    Added `hs_external_globals`

---
+++ game/source/hs/hs_globals_external.cpp
@@ -0,0 +1,4 @@
+#include "hs/hs_globals_external.hpp"
+
+REFERENCE_DECLARE_ARRAY(0x018EF450, hs_global_external*, hs_external_globals, k_hs_external_global_count);
+
---
+++ game/source/hs/hs_globals_external.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+#include "hs/hs_scenario_definitions.hpp"
+
+struct hs_global_external
+{
+	c_enum<e_hs_type, short, k_hs_type_count> type;
+	short __unknown;
+	void* value;
+};
+static_assert(sizeof(hs_global_external) == 0x8);
+
+short const k_hs_external_global_count = 1315;
+extern hs_global_external* (&hs_external_globals)[k_hs_external_global_count];
+
