7a628550fb0d3f9971f383033d61b74d3b592b4b
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-16 04:57:07 +0000

    Added `hs_function_table`

---
+++ game/source/hs/hs_function.cpp
@@ -0,0 +1,4 @@
+#include "hs/hs_function.hpp"
+
+REFERENCE_DECLARE_ARRAY(0x018ED378, hs_function_definition*, hs_function_table, hs_function_table_count);
+
---
+++ game/source/hs/hs_function.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+#include "hs/hs_scenario_definitions.hpp"
+
+struct hs_function_definition
+{
+	c_enum<e_hs_type, short, k_hs_type_count> return_type;
+	short parse;
+	short(__cdecl* evaluate)(long, long, bool);
+	const char* usage;
+
+#pragma warning(push)
+#pragma warning(disable : 4200)
+	short parameters[];
+#pragma warning(pop)
+};
+static_assert(sizeof(hs_function_definition) == 0xC);
+
+long const hs_function_table_count = 1675;
+extern hs_function_definition* (&hs_function_table)[hs_function_table_count];
+
