c903dd6de11cbcfaaa6d10ba7728183ef381e217
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-19 15:50:38 +0000

    Updated `geometry_data` struct

--- game/source/objects/multiplayer_game_objects.hpp
+++ game/source/objects/multiplayer_game_objects.hpp
@@ -5,13 +5,16 @@
 struct s_multiplayer_object_boundary_geometry_data
 {
 	long boundary_shape;
-	long __unknown4;
-	real __unknown8;
-	real __unknownC;
-	real __unknown10;
-	real __unknown14;
-	real_matrix4x3 world_matrix;
-	real boundary_geometry;
+
+	// shader used for boundary geometry
+	long standard_shader_or_opaque_shader_index;
+
+	real boundary_width;
+	real boundary_box_length;
+	real boundary_positive_height;
+	real boundary_negative_height;
+	real_matrix4x3 boundary_matrix;
+	real boundary_raius;
 };
 static_assert(sizeof(s_multiplayer_object_boundary_geometry_data) == 0x50);
 
