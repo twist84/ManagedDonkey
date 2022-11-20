c833b617855562accde5e71714b3b1985f2275c4
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-20 11:09:05 +0000

    Added `s_ctf_globals`

--- game/source/game/game_engine_ctf.hpp
+++ game/source/game/game_engine_ctf.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "game/game_engine_area_set.hpp"
 #include "game/game_engine_ctf_traits.hpp"
 #include "game/game_engine_default.hpp"
 #include "game/game_engine_variant.hpp"
@@ -52,3 +53,25 @@ public:
 
 	virtual bool verify_state(void) const;
 };
+
+struct s_ctf_shared_globals
+{
+	c_static_array<c_area_set<c_area, 3>, 9> area_set0;
+	c_static_array<c_area_set<c_area, 3>, 9> area_setB1C;
+	byte __data1638[0x12];
+	word_flags flags;
+};
+static_assert(sizeof(s_ctf_shared_globals) == 0x164C);
+
+struct s_ctf_globals
+{
+	s_ctf_shared_globals shared_globals;
+	c_static_array<dword, 9> __unknown164C;
+	c_static_array<word, 9> __unknown1670;
+	c_static_array<dword, 9> __unknown1684;
+	c_static_array<byte, 9> __unknown16A8;
+	c_static_array<word, 9> __unknown16B4;
+	c_static_array<word, 9> __unknown16C4;
+};
+static_assert(sizeof(s_ctf_globals) == 0x16D8);
+
