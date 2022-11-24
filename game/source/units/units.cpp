50dcd1270a54e32f1f82f01fb6e39f573ce7f63c
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-24 12:01:09 +0000

    Added some `unit_add` functions

--- game/source/units/units.cpp
+++ game/source/units/units.cpp
@@ -1,5 +1,25 @@
 #include "units/units.hpp"
 
+void __cdecl unit_add_equipment_to_inventory(long unit_index, long slot_index, long object_index)
+{
+    INVOKE(0x00B38AB0, unit_add_equipment_to_inventory, unit_index, slot_index, object_index);
+}
+
+bool __cdecl unit_add_grenade_to_inventory(long unit_index, long object_index)
+{
+    return INVOKE(0x00B38BA0, unit_add_grenade_to_inventory, unit_index, object_index);
+}
+
+short __cdecl unit_add_grenade_type_to_inventory(long unit_index, short grenade_type, short grenade_count)
+{
+    return INVOKE(0x00B38CD0, unit_add_grenade_type_to_inventory, unit_index, grenade_type, grenade_count);
+}
+
+void __cdecl unit_add_starting_profile_equipment(long unit_index, short profile_index, bool clear_player, bool create_new)
+{
+    INVOKE(0x00B39110, unit_add_starting_profile_equipment, unit_index, profile_index, clear_player, create_new);
+}
+
 bool __cdecl unit_add_weapon_to_inventory(long unit_index, long object_index, long weapon_addition_method)
 {
     return INVOKE(0x00B393D0, unit_add_weapon_to_inventory, unit_index, object_index, weapon_addition_method);
--- game/source/units/units.hpp
+++ game/source/units/units.hpp
@@ -16,6 +16,10 @@ struct s_unit_weapon_set
 	c_static_array<byte, k_weapon_set_count> weapon_indices;
 };
 
+extern void __cdecl unit_add_equipment_to_inventory(long unit_index, long slot_index, long object_index);
+extern bool __cdecl unit_add_grenade_to_inventory(long unit_index, long object_index);
+extern short __cdecl unit_add_grenade_type_to_inventory(long unit_index, short grenade_type, short grenade_count);
+extern void __cdecl unit_add_starting_profile_equipment(long unit_index, short profile_index, bool clear_player, bool create_new);
 extern bool __cdecl unit_add_weapon_to_inventory(long unit_index, long object_index, long weapon_addition_method);
 extern bool __cdecl unit_has_weapon_definition_index(long unit_index, long weapon_definition_index);
 
