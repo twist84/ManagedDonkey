e283f8cf29841155098f26dba50333bfe2bc00d6
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-26 10:18:45 +0000

    Added `data` thunks

--- game/source/memory/data.cpp
+++ game/source/memory/data.cpp
@@ -73,13 +73,152 @@ bool s_data_iterator::operator!=(const s_data_iterator& other) const
 	return !(*this == other);
 }
 
+long __cdecl data_allocation_size(long maximum_count, long size, long alignment_bits)
+{
+	return INVOKE(0x0055AAB0, data_allocation_size, maximum_count, size, alignment_bits);
+}
+
+void __cdecl data_connect(s_data_array* data, long count, void* datums)
+{
+	INVOKE(0x0055AAE0, data_connect, data, count, datums);
+}
+
+void __cdecl data_delete_all(s_data_array* data)
+{
+	INVOKE(0x0055ABF0, data_delete_all, data);
+}
+
+void __cdecl data_disconnect(s_data_array* data)
+{
+	INVOKE(0x0055ACA0, data_disconnect, data);
+}
+
+void __cdecl data_dispose(s_data_array* data)
+{
+	INVOKE(0x0055ACC0, data_dispose, data);
+}
+
+void __cdecl data_initialize(s_data_array* data, char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation)
+{
+	INVOKE(0x0055ACF0, data_initialize, data, name, maximum_count, size, alignment_bits, allocation);
+}
+
+void __cdecl data_initialize_disconnected(s_data_array* data, char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation, dword* in_use_bit_vector)
+{
+	INVOKE(0x0055ADA0, data_initialize_disconnected, data, name, maximum_count, size, alignment_bits, allocation, in_use_bit_vector);
+}
+
 void data_iterator_begin(s_data_iterator* iterator, s_data_array* data)
 {
 	INVOKE(0x0055AE10, data_iterator_begin, iterator, data);
 }
 
-void* data_iterator_next(s_data_iterator* iterator)
+void const* data_iterator_next(s_data_iterator* iterator)
 {
 	return INVOKE(0x0055AE30, data_iterator_next, iterator);
+}
+
+void* __cdecl data_iterator_next_with_word_flags(s_data_iterator* iterator, long flag_offset, word flag_mask, word flag_value)
+{
+	return INVOKE(0x0055AE80, data_iterator_next_with_word_flags, iterator, flag_offset, flag_mask, flag_value);
+}
+
+// sub_55AEE0, called by unreferenced `c_content_catalogue` function
+
+void __cdecl data_make_invalid(s_data_array* data)
+{
+	INVOKE(0x0055AF70, data_make_invalid, data);
+}
+
+void __cdecl data_make_valid(s_data_array* data)
+{
+	INVOKE(0x0055AF80, data_make_valid, data);
+}
+
+s_data_array* __cdecl data_new(char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation)
+{
+	return INVOKE(0x0055AFA0, data_new, name, maximum_count, size, alignment_bits, allocation);
+}
+
+s_data_array* __cdecl data_new_disconnected(char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation)
+{
+	return INVOKE(0x0055B010, data_new_disconnected, name, maximum_count, size, alignment_bits, allocation);
+}
+
+long __cdecl data_next_absolute_index(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B060, data_next_absolute_index, data, index);
+}
+
+// sub_55B0B0, called by `data_iterator_next_with_word_flags`
+
+long __cdecl data_next_index(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B130, data_next_index, data, index);
+}
+
+long __cdecl data_previous_index(s_data_array* data, long index)
+{
+	return INVOKE(0x0055B170, data_previous_index, data, index);
+}
+
+void __cdecl data_set_new_base_address(s_data_array** out_data, s_data_array* data)
+{
+	INVOKE(0x0055B1D0, data_set_new_base_address, out_data, data);
+}
+
+long __cdecl datum_absolute_index_to_index(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B280, datum_absolute_index_to_index, data, index);
+}
+
+bool __cdecl datum_available_at_index(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B2B0, datum_available_at_index, data, index);
+}
+
+void __cdecl datum_delete(s_data_array* data, long index)
+{
+	INVOKE(0x0055B2E0, datum_delete, data, index);
+}
+
+void __cdecl datum_initialize(s_data_array* data, s_datum_header* header)
+{
+	INVOKE(0x0055B370, datum_initialize, data, header);
+}
+
+long __cdecl datum_new(s_data_array* data)
+{
+	return INVOKE(0x0055B410, datum_new, data);
+}
+
+long __cdecl datum_new_at_absolute_index(s_data_array* data, long index)
+{
+	return INVOKE(0x0055B4D0, datum_new_at_absolute_index, data, index);
+}
+
+long __cdecl datum_new_at_index(s_data_array* data, long index)
+{
+	return INVOKE(0x0055B550, datum_new_at_index, data, index);
+}
+
+long __cdecl datum_new_in_range(s_data_array* data, long begin_index, long end_index, bool initialize)
+{
+	return INVOKE(0x0055B5D0, datum_new_in_range, data, begin_index, end_index, initialize);
+}
+
+void* __cdecl datum_try_and_get(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B6D0, datum_try_and_get, data, index);
+}
+
+void* __cdecl datum_try_and_get_absolute(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B710, datum_try_and_get_absolute, data, index);
+}
+
+void* __cdecl datum_try_and_get_unsafe(s_data_array const* data, long index)
+{
+	return INVOKE(0x0055B740, datum_try_and_get_unsafe, data, index);
 }
 
--- game/source/memory/data.hpp
+++ game/source/memory/data.hpp
@@ -177,6 +177,34 @@ struct c_typed_data_array
 };
 static_assert(sizeof(c_typed_data_array<void>) == sizeof(s_data_array));
 
-extern void data_iterator_begin(s_data_iterator* iterator, s_data_array* data);
-extern void* data_iterator_next(s_data_iterator* iterator);
+struct c_allocation_base;
+extern long __cdecl data_allocation_size(long maximum_count, long size, long alignment_bits);
+extern void __cdecl data_connect(s_data_array* data, long count, void* datums);
+extern void __cdecl data_delete_all(s_data_array* data);
+extern void __cdecl data_disconnect(s_data_array* data);
+extern void __cdecl data_dispose(s_data_array* data);
+extern void __cdecl data_initialize(s_data_array* data, char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation);
+extern void __cdecl data_initialize_disconnected(s_data_array* data, char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation, dword* in_use_bit_vector);
+extern void __cdecl data_iterator_begin(s_data_iterator* iterator, s_data_array* data);
+extern void const* __cdecl data_iterator_next(s_data_iterator* iterator);
+extern void* __cdecl data_iterator_next_with_word_flags(s_data_iterator* iterator, long flag_offset, word flag_mask, word flag_value);
+extern void __cdecl data_make_invalid(s_data_array* data);
+extern void __cdecl data_make_valid(s_data_array* data);
+extern s_data_array* __cdecl data_new(char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation);
+extern s_data_array* __cdecl data_new_disconnected(char const* name, long maximum_count, long size, long alignment_bits, c_allocation_base* allocation);
+extern long __cdecl data_next_absolute_index(s_data_array const* data, long index);
+extern long __cdecl data_next_index(s_data_array const* data, long index);
+extern long __cdecl data_previous_index(s_data_array* data, long index);
+extern void __cdecl data_set_new_base_address(s_data_array** out_data, s_data_array* data);
+extern long __cdecl datum_absolute_index_to_index(s_data_array const* data, long index);
+extern bool __cdecl datum_available_at_index(s_data_array const* data, long index);
+extern void __cdecl datum_delete(s_data_array* data, long index);
+extern void __cdecl datum_initialize(s_data_array* data, s_datum_header* header);
+extern long __cdecl datum_new(s_data_array* data);
+extern long __cdecl datum_new_at_absolute_index(s_data_array* data, long index);
+extern long __cdecl datum_new_at_index(s_data_array* data, long index);
+extern long __cdecl datum_new_in_range(s_data_array* data, long begin_index, long end_index, bool initialize);
+extern void* __cdecl datum_try_and_get(s_data_array const* data, long index);
+extern void* __cdecl datum_try_and_get_absolute(s_data_array const* data, long index);
+extern void* __cdecl datum_try_and_get_unsafe(s_data_array const* data, long index);
 
