5e9024e940b9aee3cb1710c70d98d6951fc1f5b6
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-28 11:08:48 +0000

    Added `bink_playback` functions

--- game/source/bink/bink_playback.cpp
+++ game/source/bink/bink_playback.cpp
@@ -2,8 +2,78 @@
 
 #include "cseries/cseries.hpp"
 
+void __cdecl bink_notify_load_masking_complete()
+{
+	INVOKE(0x006168C0, bink_notify_load_masking_complete);
+}
+
 bool __cdecl bink_playback_active()
 {
 	return INVOKE(0x00616910, bink_playback_active);
+}
+
+void __cdecl bink_playback_check_for_terminate()
+{
+	INVOKE(0x00616960, bink_playback_check_for_terminate);
+}
+
+void __cdecl bink_playback_grab_memory()
+{
+	INVOKE(0x00616A30, bink_playback_grab_memory);
+}
+
+bool __cdecl bink_playback_in_progress()
+{
+	return INVOKE(0x00616AA0, bink_playback_in_progress);
+}
+
+void __cdecl bink_playback_memory_dispose()
+{
+	INVOKE(0x00616BA0, bink_playback_memory_dispose);
+}
+
+void __cdecl bink_playback_memory_initialize(e_map_memory_configuration configuration)
+{
+	INVOKE(0x00616C00, bink_playback_memory_initialize, configuration);
+}
+
+void __cdecl bink_playback_render()
+{
+	INVOKE(0x00616CC0, bink_playback_render);
+}
+
+bool __cdecl bink_playback_should_be_playing_deterministic_bink()
+{
+	return INVOKE(0x00616D50, bink_playback_should_be_playing_deterministic_bink);
+}
+
+void __cdecl bink_playback_start_or_queue(char const* bink_full_path, long tag_index, dword_flags flags)
+{
+	INVOKE(0x00616D60, bink_playback_start_or_queue, bink_full_path, tag_index, flags);
+}
+
+void __cdecl bink_playback_stop()
+{
+	INVOKE(0x00617040, bink_playback_stop);
+}
+
+void __cdecl bink_playback_stop_due_to_input_device_change()
+{
+	INVOKE(0x00617080, bink_playback_stop_due_to_input_device_change);
+}
+
+bool __cdecl bink_playback_ui_rendering_inhibited()
+{
+	return INVOKE(0x00617380, bink_playback_ui_rendering_inhibited);
+}
+
+void __cdecl bink_playback_update()
+{
+	INVOKE(0x006173A0, bink_playback_update);
+}
+
+bool __cdecl bink_playback_using_io_during_map_load_masking()
+{
+	return INVOKE(0x00617680, bink_playback_using_io_during_map_load_masking);
 }
 
--- game/source/bink/bink_playback.hpp
+++ game/source/bink/bink_playback.hpp
@@ -1,3 +1,22 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
+
+struct s_game_non_bsp_zone_set;
+enum e_map_memory_configuration;
+
+extern void __cdecl bink_notify_load_masking_complete();
 extern bool __cdecl bink_playback_active();
+extern void __cdecl bink_playback_check_for_terminate();
+extern void __cdecl bink_playback_grab_memory();
+extern bool __cdecl bink_playback_in_progress();
+extern void __cdecl bink_playback_memory_dispose();
+extern void __cdecl bink_playback_memory_initialize(e_map_memory_configuration configuration);
+extern void __cdecl bink_playback_render();
+extern bool __cdecl bink_playback_should_be_playing_deterministic_bink();
+extern void __cdecl bink_playback_start_or_queue(char const* bink_full_path, long tag_index, dword_flags flags);
+extern void __cdecl bink_playback_stop();
+extern void __cdecl bink_playback_stop_due_to_input_device_change();
+extern bool __cdecl bink_playback_ui_rendering_inhibited();
+extern void __cdecl bink_playback_update();
+extern bool __cdecl bink_playback_using_io_during_map_load_masking();
