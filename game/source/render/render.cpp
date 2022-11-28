d88a141aba046bb7b7cbc5f771dbde0bb3076583
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-28 17:53:37 +0000

    Added `render` functions

--- game/source/render/render.cpp
+++ game/source/render/render.cpp
@@ -1,16 +1,68 @@
 #include "render/render.hpp"
 
+#include "config/version.hpp"
 #include "memory/module.hpp"
+#include "text/draw_string.hpp"
 
-// #TODO: reimplement `render_fullscreen_text_internal` to fix this in code
-// 
-// push    offset 0x0160D04C ; "\r\n"
-// push    offset 0x0160C05C ; "blam cache release pc 1.106708 cert_ms23  Mar 20 2015 17:24:42"
-byte const push_full_string_bytes[] = { 0x68, 0x5C, 0xC0, 0x60, 0x01 };
-c_data_patch render_fullscreen_text_internal_patch0(0x00A298CF, NUMBEROF(push_full_string_bytes), push_full_string_bytes);
+void __cdecl render_frame_begin()
+{
+	INVOKE(0x00A29760, render_frame_begin);
+}
 
-void render_fullscreen_text(s_render_fullscreen_text_context const* context, bool simple_font)
+void __cdecl render_fullscreen_text(s_render_fullscreen_text_context const* context, bool simple_font)
 {
 	INVOKE(0x00A297A0, render_fullscreen_text, context, simple_font);
+}
+
+HOOK_DECLARE(0x00A29860, render_fullscreen_text_internal);
+void __cdecl render_fullscreen_text_internal(s_render_fullscreen_text_context const* context, c_draw_string* draw_string, c_font_cache_base* font_cache)
+{
+	static union { char const* address = version_get_full_string(); byte const address_bytes[4]; } string;
+	static c_data_patch render_fullscreen_text_internal_patch0(0x00A298D0, NUMBEROF(string.address_bytes), string.address_bytes);
+	if (string.address)
+		render_fullscreen_text_internal_patch0.apply(false);
+
+	HOOK_INVOKE(, render_fullscreen_text_internal, context, draw_string, font_cache);
+
+	// #TODO: figure out why `draw_string->draw` doesn't render correctly
+	//real_argb_color color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	//
+	//if (context->color)
+	//{
+	//	color.red = context->color->red;
+	//	color.green = context->color->green;
+	//	color.blue = context->color->blue;
+	//}
+	//
+	//if (context->shadow_color)
+	//{
+	//	real_argb_color shadow_color;
+	//	shadow_color.red = context->shadow_color->red;
+	//	shadow_color.green = context->shadow_color->green;
+	//	shadow_color.blue = context->shadow_color->blue;
+	//	shadow_color.alpha = 1.0f;
+	//
+	//	draw_string->set_shadow_color(&shadow_color);
+	//}
+	//
+	//draw_string->set_color(&color);
+	//draw_string->set_scale(context->scale);
+	//draw_string->draw(font_cache, version_get_full_string());
+	//
+	//if (context->text)
+	//{
+	//	draw_string->draw_more(font_cache, "\r\n");
+	//	draw_string->draw_more(font_cache, context->text);
+	//}
+}
+
+void __cdecl render_prepare_for_window(long player_window_index, long output_user_index)
+{
+	INVOKE(0x00A29990, render_prepare_for_window, player_window_index, output_user_index);
+}
+
+void __cdecl render_setup_window(render_camera* camera, render_projection* projection)
+{
+	INVOKE(0x00A2A080, render_setup_window, camera, projection);
 }
 
--- game/source/render/render.hpp
+++ game/source/render/render.hpp
@@ -11,5 +11,15 @@ struct s_render_fullscreen_text_context
 };
 static_assert(sizeof(s_render_fullscreen_text_context) == 0x10);
 
-extern void render_fullscreen_text(s_render_fullscreen_text_context const* context, bool simple_font);
+struct c_draw_string;
+struct c_font_cache_base;
+
+struct render_camera;
+struct render_projection;
+
+extern void __cdecl render_frame_begin();
+extern void __cdecl render_fullscreen_text(s_render_fullscreen_text_context const* context, bool simple_font);
+extern void __cdecl render_fullscreen_text_internal(s_render_fullscreen_text_context const* context, c_draw_string* draw_string, c_font_cache_base* font_cache);
+extern void __cdecl render_prepare_for_window(long player_window_index, long output_user_index);
+extern void __cdecl render_setup_window(render_camera* camera, render_projection* projection);
 
