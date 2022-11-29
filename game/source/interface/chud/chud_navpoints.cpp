8a7044b16fe096d09eef156e4fc96bae5f563fa5
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-29 18:19:28 +0000

    Added `chud_get_string_width`

---
+++ game/source/interface/chud/chud_navpoints.cpp
@@ -0,0 +1,7 @@
+#include "interface/chud/chud_navpoints.hpp"
+
+real __cdecl chud_get_string_width(long font_id, wchar_t* s)
+{
+    return INVOKE(0x00AAF0A0, chud_get_string_width, font_id, s);
+}
+
---
+++ game/source/interface/chud/chud_navpoints.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+extern real __cdecl chud_get_string_width(long font_id, wchar_t* s);
+
