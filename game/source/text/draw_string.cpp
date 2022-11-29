d83f7e123183d0db210c94daa32874b38741213a
theTwister <6237734+twist84@users.noreply.github.com>
2022-11-29 09:27:40 +0000

    Added `draw_string` constructors

--- game/source/text/draw_string.cpp
+++ game/source/text/draw_string.cpp
@@ -1,5 +1,7 @@
 #include "text/draw_string.hpp"
 
+#include "text/font_loading.hpp"
+
 void __cdecl c_draw_string::set_color(real_argb_color const* color)
 {
 	m_color = *color;
@@ -15,8 +17,116 @@ void __cdecl c_draw_string::set_scale(real scale)
 	m_scale.lower = scale;
 }
 
+void __cdecl c_draw_string::set_font(long font_id)
+{
+	if (font_id < 0)
+		font_id = 0;
+	if (font_id > 10)
+		font_id = 10;
+
+	m_font_id = font_id;
+	m_font = font_get_header(font_id);
+}
+
 bool __cdecl c_draw_string::draw_more(c_font_cache_base* font_cache, char const* s)
 {
 	return DECLFUNC(0x00657DD0, bool, __thiscall, c_draw_string*, c_font_cache_base*, char const*)(this, font_cache, s);
+}
+
+c_font_cache_base::c_font_cache_base() :
+	__vftable(reinterpret_cast<decltype(__vftable)>(0x0165FCB0))
+{
+}
+
+c_font_cache_mt_safe::c_font_cache_mt_safe() :
+	c_font_cache_base(),
+	m_locked()
+{
+	__vftable = reinterpret_cast<decltype(__vftable)>(0x0165FCCC);
+	DECLFUNC(0x00659650, void, __thiscall, c_font_cache_mt_safe*)(this);
+}
+
+c_font_cache_mt_safe::~c_font_cache_mt_safe()
+{
+	DECLFUNC(0x00659720, void, __thiscall, c_font_cache_mt_safe*)(this);
+}
+
+c_draw_string::c_draw_string() :
+	__vftable(reinterpret_cast<decltype(__vftable)>(0x0165DB98)),
+	m_flags(),
+	m_font_id(),
+	m_font(),
+	m_style(),
+	m_justification(),
+	m_drop_shadow_style(),
+	m_color(),
+	m_shadow_color(),
+	m_scale(),
+	__unknown44(),
+	m_tab_stop_count(),
+	m_tab_stops(),
+	m_bounds(),
+	m_cursor(),
+	__unknownA0(),
+	__unknownA4(),
+	__unknownA8(),
+	m_parse_string_state(),
+	__unknownF8()
+{
+	DECLFUNC(0x00657010, void, __thiscall, c_draw_string*)(this);
+}
+
+c_draw_string::~c_draw_string()
+{
+	DECLFUNC(0x006571D0, void, __thiscall, c_draw_string*)(this);
+}
+
+c_hardware_draw_string::c_hardware_draw_string() : 
+	c_draw_string(),
+	m_font_cache(),
+	__unknown104(),
+	__unknown108(),
+	__unknown10C(),
+	__unknown110()
+{
+	__vftable = reinterpret_cast<decltype(__vftable)>(0x01692AD0);
+}
+
+c_simple_font_draw_string::c_simple_font_draw_string() :
+	c_draw_string(),
+	__unknown100(),
+	__unknown104(),
+	__unknown108(),
+	string_length(),
+	m_string()
+{
+	__vftable = reinterpret_cast<decltype(__vftable)>(0x01697104);
+	DECLFUNC(0x00A76540, void, __thiscall, c_simple_font_draw_string*)(this);
+}
+
+c_rasterizer_draw_string::c_rasterizer_draw_string() :
+	c_draw_string(),
+	m_rotation_origin(),
+	m_rotation(),
+	m_sine_rotation(),
+	m_cosine_rotation(),
+	__unknown114(),
+	render_data()
+{
+	__vftable = reinterpret_cast<decltype(__vftable)>(0x01692AF0);
+	DECLFUNC(0x00A25F00, void, __thiscall, c_rasterizer_draw_string*)(this);
+}
+
+c_chud_draw_string::c_chud_draw_string() :
+	c_draw_string(),
+	__unknown100(),
+	__unknown104(),
+	__unknown108(),
+	__unknown10C(),
+	__unknown110(),
+	__unknown114()
+{
+	__vftable = reinterpret_cast<decltype(__vftable)>(0x0169A144);
+	DECLFUNC(0x00A95570, void, __thiscall, c_chud_draw_string*)(this);
 }
 
--- game/source/text/draw_string.hpp
+++ game/source/text/draw_string.hpp
@@ -7,9 +7,42 @@ enum e_text_style;
 enum e_text_justification;
 enum e_text_drop_shadow_style;
 enum e_utf32;
-struct c_font_cache_base;
 struct s_font_header;
 struct s_font_character;
+
+// 0165FCB0
+struct c_font_cache_base
+{
+	struct c_font_cache_base_vtbl* __vftable /*VFT*/;
+
+public:
+	c_font_cache_base();
+};
+static_assert(sizeof(c_font_cache_base) == 0x4);
+
+struct c_font_cache_scope_lock
+{
+	volatile bool m_value;
+};
+static_assert(sizeof(c_font_cache_scope_lock) == 0x1);
+
+// 0165FCCC
+struct c_font_cache_mt_safe : // thread safe
+	c_font_cache_base
+{
+public:
+	c_font_cache_mt_safe();
+	~c_font_cache_mt_safe();
+
+protected:
+	c_font_cache_scope_lock m_locked;
+};
+static_assert(sizeof(c_font_cache_mt_safe) == sizeof(c_font_cache_base) + 0x4);
+
+// 0165FCE8
+//c_null_font_cache
+
+// 0165FC44
 struct c_draw_string
 {
 	enum e_text_flags
@@ -31,18 +64,51 @@ struct c_draw_string
 	};
 	static_assert(sizeof(s_parse_string_state) == 0x4C);
 
+	// HACK: so we don't have to manually construct the class
+	struct
+	{
+		void(__thiscall* dtor)(c_draw_string*);
+		bool(__thiscall* draw_utf32)(c_draw_string*, c_font_cache_base* font_cache, e_utf32 const* string);
+		bool(__thiscall* draw_wchar)(c_draw_string*, c_font_cache_base* font_cache, wchar_t const* string);
+		bool(__thiscall* draw)(c_draw_string*, c_font_cache_base* font_cache, char const* string);
+		bool(__thiscall* predict_character)(c_draw_string*, c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character);
+		bool(__thiscall* precache_character)(c_draw_string*, c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, s_font_character const** font_character);
+		void(__thiscall* draw_character)(c_draw_string*, c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, dword a4, dword a5, real a6, real a7, real a8, real a9, real a10, real a11);
+	}*__vftable;
+
 public:
-	virtual ~c_draw_string();
-	virtual bool draw(c_font_cache_base* font_cache, e_utf32 const* string);
-	virtual bool draw(c_font_cache_base* font_cache, wchar_t const* string);
-	virtual bool draw(c_font_cache_base* font_cache, char const* string);
-	virtual bool predict_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character);
-	virtual bool precache_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, s_font_character const** font_character);
-	virtual void draw_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, dword a4, dword a5, real a6, real a7, real a8, real a9, real a10, real a11);
+	c_draw_string();
+	~c_draw_string();
+
+	bool draw(c_font_cache_base* font_cache, e_utf32 const* string)
+	{
+		return __vftable->draw_utf32(this, font_cache, string);
+	}
+	bool draw(c_font_cache_base* font_cache, wchar_t const* string)
+	{
+		return __vftable->draw_wchar(this, font_cache, string);
+	}
+	bool draw(c_font_cache_base* font_cache, char const* string)
+	{
+		return __vftable->draw(this, font_cache, string);
+	}
+	bool predict_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character)
+	{
+		return __vftable->predict_character(this, font_cache, font_id, character);
+	}
+	bool precache_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, s_font_character const** font_character)
+	{
+		return __vftable->precache_character(this, font_cache, font_id, character, font_character);
+	}
+	void draw_character(c_font_cache_base* font_cache, e_font_id font_id, e_utf32 character, dword a4, dword a5, real a6, real a7, real a8, real a9, real a10, real a11)
+	{
+		return __vftable->draw_character(this, font_cache, font_id, character, a4, a5, a6, a7, a8, a9, a10, a11);
+	}
 
 	void __cdecl set_color(real_argb_color const* color);
 	void __cdecl set_shadow_color(real_argb_color const* shadow_color);
 	void __cdecl set_scale(real scale);
+	void __cdecl set_font(long font_id);
 	bool __cdecl draw_more(c_font_cache_base* font_cache, char const* s);
 
 protected:
@@ -51,7 +117,7 @@ protected:
 	// e_font_id
 	long m_font_id;
 
-	s_font_header* m_font;
+	s_font_header const* m_font;
 
 	// e_text_style
 	long m_style;
@@ -82,3 +148,80 @@ protected:
 	word __unknownF8[4];
 };
 static_assert(sizeof(c_draw_string) == 0x100);
+
+// 01692AD0
+struct c_hardware_draw_string :
+	c_draw_string
+{
+public:
+	c_hardware_draw_string();
+
+protected:
+	c_font_cache_mt_safe* m_font_cache;
+	long __unknown104;
+	long __unknown108;
+	long __unknown10C;
+	long __unknown110;
+};
+
+
+// 01697104
+struct c_simple_font_draw_string :
+	c_draw_string
+{
+public:
+	c_simple_font_draw_string();
+
+protected:
+	long __unknown100;
+	long __unknown104;
+	long __unknown108;
+	long string_length;
+	c_static_string<256> m_string;
+};
+static_assert(sizeof(c_simple_font_draw_string) == sizeof(c_draw_string) + 0x110);
+
+// 01692AF0
+struct c_rasterizer_draw_string :
+	c_draw_string
+{
+	struct s_glyph_group_render_data
+	{
+		long __unknown0;
+		long __unknown4;
+		long __unknown8;
+		byte __dataC[0x700];
+		long __unknown70C;
+	};
+	static_assert(sizeof(s_glyph_group_render_data) == 0x710);
+
+public:
+	c_rasterizer_draw_string();
+
+protected:
+	real_point2d m_rotation_origin;
+	real m_rotation;
+	real m_sine_rotation;
+	real m_cosine_rotation;
+	bool __unknown114;
+	s_glyph_group_render_data render_data;
+};
+static_assert(sizeof(c_rasterizer_draw_string) == sizeof(c_draw_string) + 0x728);
+
+// 01692AF0
+struct c_chud_draw_string :
+	c_draw_string
+{
+public:
+	c_chud_draw_string();
+
+protected:
+	real __unknown100;
+	real __unknown104;
+	real __unknown108;
+	real __unknown10C;
+	real __unknown110;
+	real __unknown114;
+};
+static_assert(sizeof(c_chud_draw_string) == sizeof(c_draw_string) + 0x18);
+
