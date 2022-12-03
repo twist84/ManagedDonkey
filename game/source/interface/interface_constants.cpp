dbb33244415073c117a774e9eeae741c348b7b23
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-03 22:00:30 +0000

    Added `interface_get_current_display_settings`

---
+++ game/source/interface/interface_constants.cpp
@@ -0,0 +1,7 @@
+#include "interface/interface_constants.hpp"
+
+long interface_get_current_display_settings( short_rectangle2d* fullscreen_render_pixel_bounds, short_rectangle2d* fullscreen_render_title_safe_pixel_bounds, short_rectangle2d* display_pixel_bounds, short_rectangle2d* display_title_safe_pixel_bounds)
+{
+	return INVOKE(0x00A9FAA0, interface_get_current_display_settings, fullscreen_render_pixel_bounds, fullscreen_render_title_safe_pixel_bounds, display_pixel_bounds, display_title_safe_pixel_bounds);
+}
+
---
+++ game/source/interface/interface_constants.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+extern long interface_get_current_display_settings(short_rectangle2d* fullscreen_render_pixel_bounds, short_rectangle2d* fullscreen_render_title_safe_pixel_bounds, short_rectangle2d* display_pixel_bounds, short_rectangle2d* display_title_safe_pixel_bounds);
+
