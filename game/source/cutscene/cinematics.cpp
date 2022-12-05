837ee5dbeb287437b6df0e3f790ade6c4d479b6a
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-05 07:56:08 +0000

    Added `cinematics` functions

--- game/source/cutscene/cinematics.cpp
+++ game/source/cutscene/cinematics.cpp
@@ -1,8 +1,22 @@
 #include "cutscene/cinematics.hpp"
 
-#include "cseries/cseries.hpp"
-
 bool __cdecl cinematic_in_progress()
 {
 	return INVOKE(0x0067D270, cinematic_in_progress);
 }
+
+void __cdecl cinematic_update()
+{
+	return INVOKE(0x0067EDF0, cinematic_update);
+}
+
+void __cdecl cinematics_game_tick()
+{
+	return INVOKE(0x0067F080, cinematics_game_tick);
+}
+
+void __cdecl draw_quad(short_rectangle2d* rect, dword color)
+{
+	return INVOKE(0x0067F200, draw_quad, rect, color);
+}
+
--- game/source/cutscene/cinematics.hpp
+++ game/source/cutscene/cinematics.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
+
 extern bool __cdecl cinematic_in_progress();
+extern void __cdecl cinematic_update();
+extern void __cdecl cinematics_game_tick();
+extern void __cdecl draw_quad(short_rectangle2d* rect, dword color);
+
