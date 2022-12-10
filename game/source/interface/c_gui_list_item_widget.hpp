62d249d5538bec44270ea1b58320c5f3fc44caf8
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-10 23:45:11 +0000

    Mapped `c_gui_list_item_widget` (WIP)

--- game/source/interface/c_gui_list_item_widget.hpp
+++ game/source/interface/c_gui_list_item_widget.hpp
@@ -2,12 +2,26 @@
 
 #include "interface/c_gui_widget.hpp"
 
+struct s_list_item_widget_definition : s_core_widget_definition
+{
+	string_id item_label;
+};
+static_assert(sizeof(s_list_item_widget_definition) == sizeof(s_core_widget_definition) + 0x4);
+
+struct s_runtime_list_item_widget_definition : s_runtime_core_widget_definition
+{
+	string_id item_label;
+};
+static_assert(sizeof(s_runtime_list_item_widget_definition) == sizeof(s_runtime_core_widget_definition) + 0x4);
+
 struct c_gui_list_item_widget : public c_gui_widget
 {
 protected:
-	byte __dataDC[0x40];
+	long __unknownDC;
+	s_runtime_list_item_widget_definition m_core_definition;
+	long m_list_item_index;
 	bool __unknown11C;
-	wchar_t __unknown11E[200];
+	c_static_wchar_string<200> __string11E;
 };
 static_assert(sizeof(c_gui_list_item_widget) == sizeof(c_gui_widget) + 0x1D4);
 
