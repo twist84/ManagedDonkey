4337db544e12aac3bb65df34f8251596b8c1a1ca
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-10 23:34:54 +0000

    Mapped `c_gui_group_widget` (WIP)

---
+++ game/source/interface/c_gui_group_widget.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+#include "interface/c_gui_widget.hpp"
+#include "tag_files/tag_groups.hpp"
+
+struct s_group_widget_definition : s_core_widget_definition
+{
+	tag_block list;
+	tag_block text_items;
+	tag_block bitmaps;
+	tag_block models;
+};
+static_assert(sizeof(s_group_widget_definition) == sizeof(s_core_widget_definition) + 0x30);
+
+struct s_runtime_group_widget_definition : s_runtime_core_widget_definition
+{
+	tag_block list;
+	tag_block text_items;
+	tag_block bitmaps;
+	tag_block models;
+	long string_list_reference_index;
+};
+static_assert(sizeof(s_runtime_group_widget_definition) == sizeof(s_runtime_core_widget_definition) + 0x34);
+
+struct c_gui_group_widget : public c_gui_widget
+{
+	long __unknownDC;
+	s_runtime_group_widget_definition m_core_definition;
+	bool m_dispose_as_display_group;
+	long __unknown14C;
+};
+static_assert(sizeof(c_gui_group_widget) == sizeof(c_gui_widget) + 0x74);
+
+struct c_gui_button_key_widget : public c_gui_group_widget
+{
+};
+static_assert(sizeof(c_gui_button_key_widget) == sizeof(c_gui_group_widget));
+
