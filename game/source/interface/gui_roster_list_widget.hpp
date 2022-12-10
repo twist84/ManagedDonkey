0259e44ac8f203b693f378ef585a2e49d6331b1e
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-10 15:28:45 +0000

    Added `c_gui_roster_list_widget`

---
+++ game/source/interface/gui_roster_list_widget.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+#include "interface/c_gui_list_widget.hpp"
+
+struct c_gui_roster_list_widget : c_gui_list_widget
+{
+protected:
+	bool m_game_roster_is_local_party;
+	bool m_is_team_game;
+	bool m_team_switching_enabled;
+	bool m_show_party_bar;
+
+	struct
+	{
+		bool temporary_team_change_active;
+		long temporary_team_index;
+		dword lying_end_time;
+		dword lying_begin_time;
+	} m_temporary_team[16];
+
+	byte __data[4];
+};
+static_assert(sizeof(c_gui_roster_list_widget) == sizeof(c_gui_list_widget) + 0x108);
+
