2f3ae23a6f2114095c47c63aec3775a4830ad1fb
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-10 15:24:50 +0000

    Added `c_gui_widget` functions

---
+++ game/source/interface/c_gui_widget.cpp
@@ -0,0 +1,214 @@
+#include "interface/c_gui_widget.hpp"
+
+#include "interface/user_interface_data.hpp"
+
+long __cdecl c_gui_widget::get_ambient_state()
+{
+	return __vftable->get_ambient_state(this);
+}
+
+void* __cdecl c_gui_widget::destructor(dword a1)
+{
+	return __vftable->destructor(this, a1);
+}
+
+void __cdecl c_gui_widget::initialize()
+{
+	__vftable->initialize(this);
+}
+
+void __cdecl c_gui_widget::post_initialize()
+{
+	__vftable->post_initialize(this);
+}
+
+void __cdecl c_gui_widget::dispose()
+{
+	__vftable->dispose(this);
+}
+
+long __cdecl c_gui_widget::get_name()
+{
+	return __vftable->get_name(this);
+}
+
+long __cdecl c_gui_widget::get_element_handle()
+{
+	return __vftable->get_element_handle(this);
+}
+
+long __cdecl c_gui_widget::get_datasource_index()
+{
+	return __vftable->get_datasource_index(this);
+}
+
+bool __cdecl c_gui_widget::get_enabled()
+{
+	return __vftable->get_enabled(this);
+}
+
+void __cdecl c_gui_widget::set_enabled(bool a1)
+{
+	return __vftable->set_enabled(this, a1);
+}
+
+s_runtime_core_widget_definition* __cdecl c_gui_widget::get_core_definition()
+{
+	return __vftable->get_core_definition(this);
+}
+
+real_rectangle2d* __cdecl c_gui_widget::get_current_bounds(real_rectangle2d* a1)
+{
+	return __vftable->get_current_bounds(this, a1);
+}
+
+real_rectangle2d* __cdecl c_gui_widget::get_container_current_bounds(real_rectangle2d* a1)
+{
+	return __vftable->get_container_current_bounds(this, a1);
+}
+
+bool __cdecl c_gui_widget::should_render(bool* a1)
+{
+	return __vftable->should_render(this, a1);
+}
+
+real_argb_color* __cdecl c_gui_widget::get_cumulative_color_tint(real_argb_color* a1)
+{
+	return __vftable->get_cumulative_color_tint(this, a1);
+}
+
+bool __cdecl c_gui_widget::can_receive_focus()
+{
+	return __vftable->can_receive_focus(this);
+}
+
+bool __cdecl c_gui_widget::within_focus_chain()
+{
+	return __vftable->within_focus_chain(this);
+}
+
+c_gui_text_widget* __cdecl c_gui_widget::create_text_widget(s_runtime_text_widget_definition const* a1)
+{
+	return __vftable->create_text_widget(this, a1);
+}
+
+c_gui_bitmap_widget* __cdecl c_gui_widget::create_bitmap_widget(s_runtime_bitmap_widget_definition const* a1)
+{
+	return __vftable->create_bitmap_widget(this, a1);
+}
+
+c_gui_group_widget* __cdecl c_gui_widget::create_group_widget(s_group_widget_definition const* a1)
+{
+	return __vftable->create_group_widget(this, a1);
+}
+
+c_gui_button_key_widget* __cdecl c_gui_widget::create_button_key_widget(s_button_key_definition const* a1)
+{
+	return __vftable->create_button_key_widget(this, a1);
+}
+
+c_gui_list_widget* __cdecl c_gui_widget::create_list_widget(s_list_widget_block const* a1)
+{
+	return __vftable->create_list_widget(this, a1);
+}
+
+c_gui_list_item_widget* __cdecl c_gui_widget::create_list_item_widget(s_list_item_widget_block const* a1)
+{
+	return __vftable->create_list_item_widget(this, a1);
+}
+
+c_gui_model_widget* __cdecl c_gui_widget::create_model_widget(s_model_widget_block const* a1)
+{
+	return __vftable->create_model_widget(this, a1);
+}
+
+void __cdecl c_gui_widget::update(dword a1)
+{
+	__vftable->update(this, a1);
+};
+
+void __cdecl c_gui_widget::update_render_state(dword a1)
+{
+	__vftable->update_render_state(this, a1);
+};
+
+void __cdecl c_gui_widget::set_animated_state_baseline(s_animation_transform* a1)
+{
+	__vftable->set_animated_state_baseline(this, a1);
+};
+
+void __cdecl c_gui_widget::assemble_render_data(s_gui_widget_render_data* a1, rectangle2d const* a2, e_controller_index controller_index, long a4, bool a5, bool a6, bool a7)
+{
+	__vftable->assemble_render_data(this, a1, a2, controller_index, a4, a5, a6, a7);
+}
+
+bool __cdecl c_gui_widget::handle_widget_back_out()
+{
+	return __vftable->handle_widget_back_out(this);
+}
+
+bool __cdecl c_gui_widget::handle_tab(c_controller_input_message const* input_message)
+{
+	return __vftable->handle_tab(this, input_message);
+}
+
+bool __cdecl c_gui_widget::get_string_by_string_id(string_id name, c_static_wchar_string<1024>* out_string)
+{
+	return __vftable->get_string_by_string_id(this, name, out_string);
+}
+
+void __cdecl c_gui_widget::_initialize(s_list_widget_block const* a1)
+{
+	__vftable->_initialize(this, a1);
+}
+
+c_gui_list_item_widget* __cdecl c_gui_widget::get_next_list_item_widget(bool a1)
+{
+	return DECLFUNC(0x00AB9230, c_gui_list_item_widget*, __thiscall, c_gui_widget*, bool)(this, a1);
+}
+
+void __cdecl c_gui_widget::set_visible(bool is_visible)
+{
+	DECLFUNC(0x00ABA7E0, c_gui_widget*, __thiscall, c_gui_widget*, bool)(this, is_visible);
+}
+
+c_gui_bitmap_widget* __cdecl c_gui_widget::get_child_bitmap_widget(string_id name)
+{
+	return (c_gui_bitmap_widget*)get_child_widget(_gui_widget_type_bitmap_widget, name);
+}
+
+c_gui_group_widget* __cdecl c_gui_widget::get_child_group_widget(string_id name)
+{
+	return (c_gui_group_widget*)get_child_widget(_gui_widget_type_group_widget, name);
+}
+
+c_gui_list_item_widget* __cdecl c_gui_widget::get_child_list_item_widget(string_id name)
+{
+	return (c_gui_list_item_widget*)get_child_widget(_gui_widget_type_list_item_widget, name);
+}
+
+c_gui_list_widget* __cdecl c_gui_widget::get_child_list_widget(string_id name)
+{
+	return (c_gui_list_widget*)get_child_widget(_gui_widget_type_list_widget, name);
+}
+
+c_gui_model_widget* __cdecl c_gui_widget::get_child_model_widget(string_id name)
+{
+	return (c_gui_model_widget*)get_child_widget(_gui_widget_type_model_widget, name);
+}
+
+c_gui_text_widget* __cdecl c_gui_widget::get_child_text_widget(string_id name)
+{
+	return (c_gui_text_widget*)get_child_widget(_gui_widget_type_text_widget, name);
+}
+
+c_gui_widget* __cdecl c_gui_widget::get_child_widget(e_gui_widget_type widget_type, string_id name)
+{
+	return DECLFUNC(0x00AB8B00, c_gui_widget*, __thiscall, c_gui_widget*, e_gui_widget_type, string_id)(this, widget_type, name);
+}
+
+c_gui_widget* __cdecl c_gui_widget::get_first_child_widget_by_type(e_gui_widget_type widget_type)
+{
+	return DECLFUNC(0x00AB8F80, c_gui_widget*, __thiscall, c_gui_widget*, e_gui_widget_type)(this, widget_type);
+}
+
--- game/source/interface/c_gui_widget.hpp
+++ game/source/interface/c_gui_widget.hpp
@@ -3,6 +3,7 @@
 #include "cseries/cseries.hpp"
 #include "interface/gui_animation.hpp"
 #include "tag_files/tag_groups.hpp"
+#include "text/unicode.hpp"
 
 enum e_core_widget_definition_flags
 {
@@ -87,10 +88,121 @@ enum e_gui_widget_type
 	k_gui_widget_type_count
 };
 
+enum e_controller_index;
+
+struct s_runtime_core_widget_definition;
+struct c_gui_text_widget;
+struct c_gui_bitmap_widget;
+struct c_gui_group_widget;
+struct c_gui_button_key_widget;
+struct c_gui_list_widget;
+struct c_gui_list_item_widget;
+struct c_gui_model_widget;
+struct s_runtime_text_widget_definition;
+struct s_runtime_bitmap_widget_definition;
+struct s_group_widget_definition;
+struct s_button_key_definition;
+struct s_list_widget_block;
+struct s_list_item_widget_block;
+struct s_model_widget_block;
+struct s_animation_transform;
+struct s_gui_widget_render_data;
+struct c_controller_input_message;
+
 struct c_gui_widget
 {
-	void* __vftable;
+protected:
 
+	// HACK: so we don't have to manually construct the class
+	struct
+	{
+		long(__thiscall* get_ambient_state)(c_gui_widget*);
+		void* (__thiscall* destructor)(c_gui_widget*, dword);
+		void(__thiscall* initialize)(c_gui_widget*);
+		void(__thiscall* post_initialize)(c_gui_widget*);
+		void(__thiscall* dispose)(c_gui_widget*);
+		long(__thiscall* get_name)(c_gui_widget*);
+		long(__thiscall* get_element_handle)(c_gui_widget*);
+		long(__thiscall* get_datasource_index)(c_gui_widget*);
+		bool(__thiscall* get_enabled)(c_gui_widget*);
+		void(__thiscall* set_enabled)(c_gui_widget*, bool);
+		s_runtime_core_widget_definition* (__thiscall* get_core_definition)(c_gui_widget*);
+		real_rectangle2d* (__thiscall* get_current_bounds)(c_gui_widget*, real_rectangle2d*);
+		real_rectangle2d* (__thiscall* get_container_current_bounds)(c_gui_widget*, real_rectangle2d*);
+		bool(__thiscall* should_render)(c_gui_widget*, bool*);
+		real_argb_color* (__thiscall* get_cumulative_color_tint)(c_gui_widget*, real_argb_color*);
+		bool(__thiscall* can_receive_focus)(c_gui_widget*);
+		bool(__thiscall* within_focus_chain)(c_gui_widget*);
+		c_gui_text_widget* (__thiscall* create_text_widget)(c_gui_widget*, s_runtime_text_widget_definition const*);
+		c_gui_bitmap_widget* (__thiscall* create_bitmap_widget)(c_gui_widget*, s_runtime_bitmap_widget_definition const*);
+		c_gui_group_widget* (__thiscall* create_group_widget)(c_gui_widget*, s_group_widget_definition const*);
+		c_gui_button_key_widget* (__thiscall* create_button_key_widget)(c_gui_widget*, s_button_key_definition const*);
+		c_gui_list_widget* (__thiscall* create_list_widget)(c_gui_widget*, s_list_widget_block const*);
+		c_gui_list_item_widget* (__thiscall* create_list_item_widget)(c_gui_widget*, s_list_item_widget_block const*);
+		c_gui_model_widget* (__thiscall* create_model_widget)(c_gui_widget*, s_model_widget_block const*);
+		void(__thiscall* update)(c_gui_widget*, dword);
+		void(__thiscall* update_render_state)(c_gui_widget*, dword);
+		void(__thiscall* set_animated_state_baseline)(c_gui_widget*, s_animation_transform*);
+		void* __func27;
+		void* __func28;
+		void(__thiscall* assemble_render_data)(c_gui_widget*, s_gui_widget_render_data*, rectangle2d const*, e_controller_index, long, bool, bool, bool);
+		bool(__thiscall* handle_widget_back_out)(c_gui_widget*);
+		void* __func31;
+		bool(__thiscall* handle_tab)(c_gui_widget*, c_controller_input_message const*);
+		void* __func33;
+		void* __func34;
+		void* __func35;
+		bool(__thiscall* get_string_by_string_id)(c_gui_widget*, string_id, c_static_wchar_string<1024>*);
+		void(__thiscall* _initialize)(c_gui_widget*, s_list_widget_block const*);
+	}*__vftable;
+
+	long __cdecl get_ambient_state();
+
+public:
+	void* __cdecl destructor(dword a1);
+	void __cdecl initialize();
+	void __cdecl post_initialize();
+	void __cdecl dispose();
+	long __cdecl get_name();
+	long __cdecl get_element_handle();
+	long __cdecl get_datasource_index();
+	bool __cdecl get_enabled();
+	void __cdecl set_enabled(bool a1);
+	s_runtime_core_widget_definition* __cdecl get_core_definition();
+	real_rectangle2d* __cdecl get_current_bounds(real_rectangle2d* a1);
+	real_rectangle2d* __cdecl get_container_current_bounds(real_rectangle2d* a1);
+	bool __cdecl should_render(bool* a1);
+	real_argb_color* __cdecl get_cumulative_color_tint(real_argb_color* a1);
+	bool __cdecl can_receive_focus();
+	bool __cdecl within_focus_chain();
+	c_gui_text_widget* __cdecl create_text_widget(s_runtime_text_widget_definition const* a1);
+	c_gui_bitmap_widget* __cdecl create_bitmap_widget(s_runtime_bitmap_widget_definition const* a1);
+	c_gui_group_widget* __cdecl create_group_widget(s_group_widget_definition const* a1);
+	c_gui_button_key_widget* __cdecl create_button_key_widget(s_button_key_definition const* a1);
+	c_gui_list_widget* __cdecl create_list_widget(s_list_widget_block const* a1);
+	c_gui_list_item_widget* __cdecl create_list_item_widget(s_list_item_widget_block const* a1);
+	c_gui_model_widget* __cdecl create_model_widget(s_model_widget_block const* a1);
+	void __cdecl update(dword a1);
+	void __cdecl update_render_state(dword a1);
+	void __cdecl set_animated_state_baseline(s_animation_transform* a1);
+	void __cdecl assemble_render_data(s_gui_widget_render_data* a1, rectangle2d const* a2, e_controller_index controller_index, long a4, bool a5, bool a6, bool a7);
+	bool __cdecl handle_widget_back_out();
+	bool __cdecl handle_tab(c_controller_input_message const* input_message);
+	bool __cdecl get_string_by_string_id(string_id name, c_static_wchar_string<1024>* out_string);
+	void __cdecl _initialize(s_list_widget_block const* a1);
+	
+	c_gui_list_item_widget* __cdecl get_next_list_item_widget(bool a1);
+	void __cdecl set_visible(bool is_visible);
+	c_gui_widget* __cdecl get_child_widget(e_gui_widget_type widget_type, string_id name);
+	c_gui_bitmap_widget* __cdecl get_child_bitmap_widget(string_id name);
+	c_gui_group_widget* __cdecl get_child_group_widget(string_id name);
+	c_gui_list_item_widget* __cdecl get_child_list_item_widget(string_id name);
+	c_gui_list_widget* __cdecl get_child_list_widget(string_id name);
+	c_gui_model_widget* __cdecl get_child_model_widget(string_id name);
+	c_gui_text_widget* __cdecl get_child_text_widget(string_id name);
+	c_gui_widget* __cdecl get_first_child_widget_by_type(e_gui_widget_type widget_type);
+
+protected:
 	long __unknown4;
 	c_enum<e_gui_widget_type, long, k_gui_widget_type_count> m_type;
 
@@ -124,3 +236,4 @@ struct c_gui_widget
 	long __unknownD8;
 };
 static_assert(sizeof(c_gui_widget) == 0xDC);
+
