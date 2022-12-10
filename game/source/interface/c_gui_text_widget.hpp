bf39530d95f93c9dabce36c7fb24598763292f1e
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-10 23:33:40 +0000

    Mapped `c_gui_text_widget` (WIP)

--- game/source/interface/c_gui_text_widget.hpp
+++ game/source/interface/c_gui_text_widget.hpp
@@ -2,13 +2,27 @@
 
 #include "interface/c_gui_widget.hpp"
 
+struct s_text_widget_definition : s_core_widget_definition
+{
+	string_id value_override_list;
+	string_id value_identifier;
+	string_id text_color_preset;
+	short_enum custom_font;
+	byte : 8;
+	byte : 8;
+};
+static_assert(sizeof(s_text_widget_definition) == sizeof(s_core_widget_definition) + 0x10);
+
 struct s_runtime_text_widget_definition : s_runtime_core_widget_definition
 {
-	byte __data[0x20];
+	string_id value_override_list;
+	string_id value_identifier;
+	real_argb_color text_color_preset;
+	long custom_font;
 };
-static_assert(sizeof(s_runtime_text_widget_definition) == sizeof(s_runtime_core_widget_definition) + 0x20);
+static_assert(sizeof(s_runtime_text_widget_definition) == sizeof(s_runtime_core_widget_definition) + 0x1C);
 
-struct c_gui_text_widget : c_gui_widget
+struct c_gui_text_widget : public c_gui_widget
 {
 	struct s_text_source_data
 	{
@@ -16,11 +30,12 @@ struct c_gui_text_widget : c_gui_widget
 		string_id id;
 		long __unknown8;
 	};
-	static_assert(sizeof(s_text_source_data) + 0xC);
+	static_assert(sizeof(s_text_source_data) == 0xC);
 
-	byte __dataDC[0x4];
+	long __unknownDC;
 	s_text_source_data text_source_data;
 	s_runtime_text_widget_definition m_core_definition;
+	long __unknown13C;
 };
 static_assert(sizeof(c_gui_text_widget) == sizeof(c_gui_widget) + 0x64);
 
