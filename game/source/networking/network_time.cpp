7a2db05e8979d7caf3915b15573b3115ecd0b394
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-12 20:04:02 +0000

    Added `network_time` functions

--- game/source/networking/network_time.cpp
+++ game/source/networking/network_time.cpp
@@ -1,4 +1,84 @@
 #include "networking/network_time.hpp"
 
+#include "cseries/cseries_windows.hpp"
+#include "networking/session/network_session_manager.hpp"
+#include "networking/transport/transport_security.hpp"
+
+#include <assert.h>
+
 REFERENCE_DECLARE(0x019E8D4C, s_network_time_globals, network_time_globals);
+
+long __cdecl network_time_difference_msec(dword time_a, dword time_b)
+{
+    return time_b - time_a;
+}
+
+dword __cdecl network_time_get()
+{
+    //return INVOKE(0x00439E40, network_time_get);
+
+    if (network_time_globals.locked)
+        return network_time_globals.time;
+     
+    return network_time_get_exact();
+}
+
+dword __cdecl network_time_get_exact()
+{
+    //return INVOKE(0x00439E60, network_time_get_exact);
+
+    return system_milliseconds();
+}
+
+long network_time_since(dword time)
+{
+    //return INVOKE(0x00439E70, network_time_since, time);
+
+    return network_time_difference_msec(time, network_time_get());
+}
+
+void __cdecl network_session_time_deregister_session_manager()
+{
+    //INVOKE(0x0045F190, network_session_time_deregister_session_manager);
+
+    assert(network_time_globals.session_manager != NULL);
+
+    network_time_globals.session_manager = NULL;
+}
+
+bool __cdecl network_session_time_exists(s_transport_secure_identifier const* session_id)
+{
+    assert(session_id);
+
+    return INVOKE(0x0045F1A0, network_session_time_get_time, session_id);
+}
+
+bool __cdecl network_session_time_get_id_and_time(long session_index, s_transport_secure_identifier* session_id, dword* time)
+{
+    return INVOKE(0x0045F1D0, network_session_time_get_id_and_time, session_index, session_id, time);
+}
+
+dword __cdecl network_session_time_get_time(s_transport_secure_identifier const* session_id)
+{
+    assert(session_id);
+
+    return INVOKE(0x0045F230, network_session_time_get_time, session_id);
+}
+
+void __cdecl network_session_time_register_session_manager(c_network_session_manager* session_manager)
+{
+    //INVOKE(0x0045F260, network_session_time_register_session_manager, session_manager);
+
+    assert(network_time_globals.session_manager == NULL);
+
+    network_time_globals.session_manager = session_manager;
+}
+
+void __cdecl network_time_lock(bool lock)
+{
+    //INVOKE(0x0045F270, network_time_lock, lock);
+
+    network_time_globals.locked = lock;
+    network_time_globals.time = network_time_get_exact();
+}
 
--- game/source/networking/network_time.hpp
+++ game/source/networking/network_time.hpp
@@ -13,3 +13,16 @@ static_assert(sizeof(s_network_time_globals) == 0xC);
 
 extern s_network_time_globals& network_time_globals;
 
+struct s_transport_secure_identifier;
+
+extern long __cdecl network_time_difference_msec(dword time_a, dword time_b);
+extern dword __cdecl network_time_get();
+extern dword __cdecl network_time_get_exact();
+extern long network_time_since(dword time);
+extern void __cdecl network_session_time_deregister_session_manager();
+extern bool __cdecl network_session_time_exists(s_transport_secure_identifier const* session_id);
+extern bool __cdecl network_session_time_get_id_and_time(long session_index, s_transport_secure_identifier* session_id, dword* time);
+extern dword __cdecl network_session_time_get_time(s_transport_secure_identifier const* session_id);
+extern void __cdecl network_session_time_register_session_manager(c_network_session_manager* session_manager);
+extern void __cdecl network_time_lock(bool lock);
+
