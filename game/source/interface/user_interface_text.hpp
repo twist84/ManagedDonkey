a833b8da9caf9e7f34087b788ace8249ef91a154
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-14 19:50:57 +0000

    Mapped `c_user_interface_text`

--- game/source/interface/user_interface_text.hpp
+++ game/source/interface/user_interface_text.hpp
@@ -3,6 +3,39 @@
 #include "cseries/cseries.hpp"
 #include "text/unicode.hpp"
 
+struct c_font_cache_base;
+struct c_user_interface_text
+{
+public:
+	virtual ~c_user_interface_text();
+	virtual void set_string(wchar_t const* _string, bool parse_xml, long);
+	virtual wchar_t const* get_string();
+	virtual void update(c_font_cache_base* font_cache, long);
+
+protected:
+	// 1:  render_uppercase
+	// 2:  align_vertically
+	// 4:  wrap_horizontally
+	// 8:  has_overflowed
+	// 16: ?
+	// 32: get_string_was_set
+	dword_flags m_flags;
+
+	long __unknown8;
+	long m_font;
+	argb_color m_argb_color;
+	long m_drop_shadow_style;
+	long m_style;
+	long m_justification;
+	word m_tab_stops[16];
+	long m_tab_stop_count;
+	real_point2d m_rotation_origin;
+	real m_rotation;
+	vector2d m_scroll_amount;
+	real m_scale;
+};
+static_assert(sizeof(c_user_interface_text) == 0x5C);
+
 extern void wchar_string_sanitize_for_game(wchar_t* string, long maximum_character_count);
 
 enum e_controller_index;
