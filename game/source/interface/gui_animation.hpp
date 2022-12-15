c1c24507e4465cca4495922d5bdc32bc13ab957f
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-15 23:25:14 +0000

    Updated `s_animation_transform`

--- game/source/interface/gui_animation.hpp
+++ game/source/interface/gui_animation.hpp
@@ -5,7 +5,17 @@
 struct s_animation_transform
 {
 	byte __data0[0x38];
-	real_argb_color tint_color;
-	byte __data48[0x40];
+	real_argb_color tint_color_direct;
+	real_point3d __unknown48;
+	vector2d __unknown54;
+	real_point2d __unknown5C;
+	real __unknown64;
+	real __unknown68;
+	real __unknown6C;
+	real_point2d __unknown70;
+	real_point2d __unknown80;
+	short __unknown82;
+	short __unknown84;
+	long custom_font;
 };
 static_assert(sizeof(s_animation_transform) == 0x88);
