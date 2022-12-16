2bbb4a9ce685e61722d1a126ab53a849f262d5a3
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-16 21:43:27 +0000

    Mapped `life_cycle_handler_leaving` (WIP)

--- game/source/networking/logic/life_cycle/life_cycle_handler_leaving.hpp
+++ game/source/networking/logic/life_cycle/life_cycle_handler_leaving.hpp
@@ -4,6 +4,8 @@
 
 struct c_life_cycle_state_handler_leaving : c_life_cycle_state_handler
 {
-	byte __data[0x8];
+	byte_flags m_flags;
+	bool m_disconnected;
+	byte __data[0x6];
 };
 static_assert(sizeof(c_life_cycle_state_handler_leaving) == 0x30);
