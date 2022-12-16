492fa1e064c2c97c47573cd4b8a1913dc9e1bd4f
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-16 21:43:58 +0000

    Map'd `life_cycle_handler_matchmaking_start` (WIP)

--- game/source/networking/logic/life_cycle/life_cycle_handler_matchmaking_start.hpp
+++ game/source/networking/logic/life_cycle/life_cycle_handler_matchmaking_start.hpp
@@ -4,6 +4,13 @@
 
 struct c_life_cycle_state_handler_matchmaking_start : c_life_cycle_state_handler
 {
-	byte __data[0x20];
+	byte_flags m_flags;
+	long m_find_match_role;
+	long m_start_mode;
+	long m_initial_desperation_sessions_found_count;
+	long m_initial_session_search_count;
+	dword __time3C;
+	dword __time40;
+	byte __data[0x4];
 };
 static_assert(sizeof(c_life_cycle_state_handler_matchmaking_start) == 0x48);
