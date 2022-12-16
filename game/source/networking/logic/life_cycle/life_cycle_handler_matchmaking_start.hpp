4730ca416c65f82a05dadfcee6ac5d4630c31acf
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-16 22:28:59 +0000

    Added `determine_matchmaking_find_match_role` hook

--- game/source/networking/logic/life_cycle/life_cycle_handler_matchmaking_start.hpp
+++ game/source/networking/logic/life_cycle/life_cycle_handler_matchmaking_start.hpp
@@ -4,6 +4,9 @@
 
 struct c_life_cycle_state_handler_matchmaking_start : c_life_cycle_state_handler
 {
+	// e_life_cycle_matchmaking_find_match_role
+	static long __fastcall determine_matchmaking_find_match_role(c_life_cycle_state_handler_matchmaking_start* _this, void* unused, bool a2);
+
 	byte_flags m_flags;
 	long m_find_match_role;
 	long m_start_mode;
