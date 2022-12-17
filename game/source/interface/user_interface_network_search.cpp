9114b4b130e9e43bfbf946ce8fd4568e8a312f88
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-17 04:40:41 +0000

    Upped `maximum_sessions` to 4

--- game/source/interface/user_interface_network_search.cpp
+++ game/source/interface/user_interface_network_search.cpp
@@ -11,7 +11,7 @@ REFERENCE_DECLARE(0x05270A90, bool, g_available_squads_ready);
 bool __cdecl user_interface_available_squads_initialize(long controller_index, dword_flags available_squad_search_flags, char category)
 {
 	c_allocation_base* allocation = user_interface_memory_allocation();
-	g_available_squads_ready = network_search_start(controller_index, category != 0, 1 /* default is `25`, this causes an issue not having enough memory allocate */, available_squad_search_flags, allocation);
+	g_available_squads_ready = network_search_start(controller_index, category != 0, 4 /* default is `25`, this causes an issue not having enough memory allocate */, available_squad_search_flags, allocation);
 
 	return g_available_squads_ready;
 }
