43882d924fa5d9444bcf6898a52fac151d08ccfe
theTwister <6237734+twist84@users.noreply.github.com>
2022-12-20 13:57:50 +0000

    Added `overlapped_task_start_internal` call hooks
    Unable to hook `overlapped_task_start_internal` directly

--- game/source/cseries/async_xoverlapped.cpp
+++ game/source/cseries/async_xoverlapped.cpp
@@ -5,7 +5,27 @@
 #include <assert.h>
 
 //HOOK_DECLARE(0x005A8F40, overlapped_task_start_internal);
+HOOK_DECLARE_CALL(0x0074CA3C, overlapped_task_start_internal); // c_content_enumeration_overlapped_task::success
+HOOK_DECLARE_CALL(0x00A82786, overlapped_task_start_internal); // user_interface_session_string_verify_update
+
+//HOOK_DECLARE_CALL(0x00AA4306, overlapped_task_start_internal); // c_player_profile_interface::update_service_tag
+
+HOOK_DECLARE_CALL(0x00AD3699, overlapped_task_start_internal); // c_player_marketplace::show_purchase_ui
+HOOK_DECLARE_CALL(0x00AD36F6, overlapped_task_start_internal); // c_player_marketplace::start_asset_consume
+HOOK_DECLARE_CALL(0x00AD3810, overlapped_task_start_internal); // c_player_marketplace::start_assets_enumerate
+HOOK_DECLARE_CALL(0x00AE668B, overlapped_task_start_internal); // c_saved_film_take_screenshot::update
+HOOK_DECLARE_CALL(0x00AE6868, overlapped_task_start_internal); // c_saved_film_take_screenshot::update
+HOOK_DECLARE_CALL(0x00AEA33B, overlapped_task_start_internal); // ?
+HOOK_DECLARE_CALL(0x00AF0D67, overlapped_task_start_internal); // c_gui_start_menu_hq_service_record_file_share_choose_item::__vftable50
+HOOK_DECLARE_CALL(0x00AF0FBE, overlapped_task_start_internal); // c_gui_start_menu_hq_service_record_file_share_choose_item::__vftable49
+HOOK_DECLARE_CALL(0x00AF26D6, overlapped_task_start_internal); // c_gui_start_menu_hq_service_record_file_share_item_selected::__vftable50
+HOOK_DECLARE_CALL(0x00AF5088, overlapped_task_start_internal); // c_gui_start_menu_hq_screenshots_options::__vftable24
+HOOK_DECLARE_CALL(0x00AF52D2, overlapped_task_start_internal); // c_gui_start_menu_hq_screenshots_options::__vftable24
 HOOK_DECLARE_CALL(0x00AF8495, overlapped_task_start_internal); // c_start_menu_settings_appearance_emblem::handle_list_item_chosen
+HOOK_DECLARE_CALL(0x00B0373C, overlapped_task_start_internal); // c_gui_screen_pregame_selection::begin_selected_item_text_edit
+HOOK_DECLARE_CALL(0x00B0B8D6, overlapped_task_start_internal); // ?
+HOOK_DECLARE_CALL(0x00B0BAA0, overlapped_task_start_internal); // ?
+HOOK_DECLARE_CALL(0x00B0DA48, overlapped_task_start_internal); // c_gui_screen_game_options::update_save_as_new_operation
 
 c_overlapped_task* c_overlapped_task::constructor(char const* file, long line)
 {
