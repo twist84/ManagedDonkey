32983b5172487e834e6b25f5c4a5c214dd46351c
theTwister <6237734+twist84@users.noreply.github.com>
2023-01-03 22:21:58 +0000

    Mapped `c_simulation_event_handler`

---
+++ game/source/simulation/simulation_event_handler.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+struct c_simulation_world;
+struct c_replication_event_manager;
+struct c_simulation_type_collection;
+struct c_simulation_entity_database;
+struct c_simulation_event_handler
+{
+	bool m_initialized;
+	c_simulation_world* m_world;
+	c_replication_event_manager* m_event_manager;
+	c_simulation_type_collection* m_type_collection;
+	c_simulation_entity_database* m_entity_database;
+	byte __data[4];
+};
+static_assert(sizeof(c_simulation_event_handler) == 0x18);
+
