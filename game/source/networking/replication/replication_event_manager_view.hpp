7fcfa0b9da2aca93ecc7c5560d3abc1b3882fa3c
theTwister <6237734+twist84@users.noreply.github.com>
2023-01-11 20:36:32 +0000

    Updated `c_replication_event_manager_view`

--- game/source/networking/replication/replication_event_manager_view.hpp
+++ game/source/networking/replication/replication_event_manager_view.hpp
@@ -11,10 +11,38 @@ struct s_replication_event_manager_view_statistics
 };
 static_assert(sizeof(s_replication_event_manager_view_statistics) == 0xC);
 
+struct c_replication_outgoing_event;
 struct c_replication_event_manager;
 struct c_replication_event_manager_view : c_replication_scheduler_client
 {
-	byte __data4[0x14];
+	struct c_event_record
+	{
+		c_replication_outgoing_event* m_event;
+		c_event_record* next;
+	};
+	static_assert(sizeof(c_event_record) == 0x8);
+	
+	struct c_packet_record
+	{
+		long m_packet_sequence_number;
+		c_event_record* m_event_record;
+		c_packet_record* m_next;
+	};
+	static_assert(sizeof(c_packet_record) == 0xC);
+
+
+	byte __data4[4];
+
+	bool m_initialized;
+	bool m_fatal_error;
+	byte __unknownA; // pad?
+	byte __unknownB; // pad?
+
+	long m_view_index;
+
+	c_packet_record* m_packet_list;
+	long m_packet_list_length;
+
 	c_replication_event_manager* m_event_manager;
 	s_replication_event_manager_view_statistics m_view_statistics;
 };
