0839e49a9630ecd50216f1be9422a482262eead2
theTwister <6237734+twist84@users.noreply.github.com>
2023-01-17 08:19:09 +0000

    Updated `c_game_statborg`

--- game/source/game/game_statborg.hpp
+++ game/source/game/game_statborg.hpp
@@ -2,41 +2,70 @@
 
 #include "cseries/cseries.hpp"
 
+// there's a mapping in engine between `e_game_engine_type`, `e_statborg_entry` and `e_game_results_statistic`
 enum e_statborg_entry
 {
-	_statborg_entry_score = 0,
-	_statborg_entry_total_score,
+	_statborg_entry_in_round_score = 0,
+	_statborg_entry_in_game_total_score,
 	_statborg_entry_kills,
 	_statborg_entry_deaths,
 	_statborg_entry_suicides,
 	_statborg_entry_betrayals,
 	_statborg_entry_assists,
-	_statborg_entry_unknown7,
-	_statborg_entry_unknown8,
-	_statborg_entry_unknown9,
-	_statborg_entry_unknown10,
-	_statborg_entry_medals,
-	_statborg_entry_consecutive_kills,
-	_statborg_entry_highest_spree,
-	_statborg_entry_unknown14,
+	_statborg_entry_rounds_won,
+
+	_statborg_entry_infection_time_as_human,
+	_statborg_entry_infection_infections,
+	_statborg_entry_infection_zombie_kills,
+
+	_statborg_entry_territories_owned = _statborg_entry_infection_time_as_human,
+	_statborg_entry_territories_captures,
+	_statborg_entry_territories_ousts,
+
+	_statborg_entry_total_wp,
+	_statborg_entry_kills_in_a_row,
+	_statborg_entry_multiple_kills,
+	_statborg_entry_total_kill_damage_dealt,
+
 	_statborg_entry_unknown15,
-	_statborg_entry_unknown16,
+
+	_statborg_entry_team_kills_in_a_row,
+
 	_statborg_entry_unknown17,
-	_statborg_entry_unknown18,
-	_statborg_entry_unknown19,
-	_statborg_entry_unknown20,
-	_statborg_entry_unknown21,
-	_statborg_entry_unknown22,
+
+	_statborg_entry_sniper_kills_in_a_row,
+	_statborg_entry_shotgun_kills_in_a_row,
+	_statborg_entry_sword_kills_in_a_row,
+	_statborg_entry_vehicle_kills_in_a_row,
+	_statborg_entry_team_kills_team,
+
 	_statborg_entry_unknown23,
-	_statborg_entry_unknown24,
-	_statborg_entry_unknown25
+
+	_statborg_entry_kill_damage_type,
+
+	k_statborg_indices_count
 };
+
+struct s_game_statborg_player
+{
+	c_static_array<word, k_statborg_indices_count> statistics;
+	bool __unknown32;
+};
+static_assert(sizeof(s_game_statborg_player) == 0x34);
+
+struct s_game_statborg_team
+{
+	c_static_array<word, 12> statistics;
+	bool __unknown18;
+};
+static_assert(sizeof(s_game_statborg_team) == 0x1A);
 
 struct c_game_statborg
 {
-	short __unknown0[2];
-	c_static_array<c_static_array<word, 26>, 16> players_statistics;
-	c_static_array<c_static_array<word, 13>, 8>  teams_statistics;
+	word __unknown0;
+	word __unknown2;
+	c_static_array<s_game_statborg_player, 16> player;
+	c_static_array<s_game_statborg_team, 8>  team;
 	bool stats_paused;
 	dword __unknown418;
 	dword __unknown41C;
