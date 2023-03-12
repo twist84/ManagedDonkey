f4651daff0a25225b8ea9991fce7b16f66d3f5d6
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-12 18:59:05 +0000

    Added `network_test_set_map_name`

--- game/source/networking/network_globals.cpp
+++ game/source/networking/network_globals.cpp
@@ -1,9 +1,23 @@
 #include "networking/network_globals.hpp"
 
+#include "cseries/console.hpp"
 #include "cseries/cseries.hpp"
+#include "networking/logic/network_session_interface.hpp"
 
 void __cdecl network_update()
 {
 	INVOKE(0x0049E7B0, network_update);
+}
+
+void __cdecl network_test_set_map_name(char const* scenario_path)
+{
+	if (network_squad_session_set_map(-1, -2, scenario_path))
+	{
+		c_console::write_line("set %s successfully", scenario_path);
+	}
+	else
+	{
+		c_console::write_line("unable to set map %s", scenario_path);
+	}
 }
 
--- game/source/networking/network_globals.hpp
+++ game/source/networking/network_globals.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 extern void __cdecl network_update();
+extern void __cdecl network_test_set_map_name(char const* scenario_path);
 
