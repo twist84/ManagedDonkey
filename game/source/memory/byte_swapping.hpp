d10a773e4e0ce21ba5b30db6d005adeb89615b14
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-19 23:00:33 +0000

    Added byteswap inplace macros

--- game/source/memory/byte_swapping.hpp
+++ game/source/memory/byte_swapping.hpp
@@ -5,3 +5,7 @@
 #pragma intrinsic(_byteswap_ushort)
 #pragma intrinsic(_byteswap_ulong)
 #pragma intrinsic(_byteswap_uint64)
+
+#define bswap_word_inplace(value) value = _byteswap_ushort(value)
+#define bswap_dword_inplace(value) value = _byteswap_ulong(value)
+#define bswap_qword_inplace(value) value = _byteswap_uint64(value)
