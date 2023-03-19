b8dff5324ce76e6cfdea49b064771ba3ab9c616d
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-19 23:25:09 +0000

    Update game_engine_sandbox.cpp

--- game/source/game/game_engine_sandbox.cpp
+++ game/source/game/game_engine_sandbox.cpp
@@ -4,6 +4,8 @@
 
 void c_game_engine_sandbox_variant::byteswap()
 {
+	static_cast<c_game_engine_base_variant*>(this)->byteswap();
+
 	bswap_word_inplace(m_respawn_time);
 	m_all_player_traits.byteswap();
 }
