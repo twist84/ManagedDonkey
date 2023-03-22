99ac48f3d4d65f54687125084bced8645dc21c4d
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-22 13:24:42 +0000

    Added `session_parameter_game_variant` funcs

---
+++ game/source/networking/session/network_session_parameters_game_variant.cpp
@@ -0,0 +1,48 @@
+#include "network_session_parameters_game_variant.hpp"
+
+#include "cseries/console.hpp"
+
+c_game_variant const* c_network_session_parameter_game_variant::get() const
+{
+	if (!get_allowed())
+	{
+		c_console::write_line("networking:session_parameters:chunked:game_variant: [%s] can't get variant, unavailable", get_session_description());
+
+		return nullptr;
+	}
+
+	return &m_data.game_variant;
+}
+
+bool c_network_session_parameter_game_variant::request_change(c_game_variant const* game_variant)
+{
+	if (set_allowed())
+		return set(game_variant);
+
+	if (!change_request_allowed())
+	{
+		c_console::write_line("networking:session_parameters:chunked:game_variant: [%s] failed to request change, access denied [%s]", get_session_description(), get_change_request_denied_reason());
+
+		return false;
+	}
+
+	if (game_variant)
+	{
+		m_change_request_data.valid = true;
+		m_change_request_data.game_variant.copy_from_and_validate(game_variant);
+	}
+	else
+	{
+		m_change_request_data.valid = false;
+		csmemset(&m_change_request_data.game_variant, 0, sizeof(c_game_variant));
+	}
+
+	set_desires_change_request();
+	return true;
+}
+
+bool c_network_session_parameter_game_variant::set(c_game_variant const* game_variant)
+{
+	return DECLFUNC(0x00456220, bool, __thiscall, c_network_session_parameter_game_variant*, c_game_variant const*)(this, game_variant);
+}
+
--- game/source/networking/session/network_session_parameters_game_variant.hpp
+++ game/source/networking/session/network_session_parameters_game_variant.hpp
@@ -13,5 +13,9 @@ static_assert(sizeof(s_network_session_parameter_game_variant) == 0x268);
 struct c_network_session_parameter_game_variant :
 	public c_network_session_parameter_chunked<s_network_session_parameter_game_variant, 6144>
 {
+	c_game_variant const* get() const;
+	bool request_change(c_game_variant const* game_variant);
+	bool set(c_game_variant const* game_variant);
 };
 static_assert(sizeof(c_network_session_parameter_game_variant) == 0x3570);
+
