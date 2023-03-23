8a9cc4d50a01b94c0f00da625baf71b3f9f37776
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-23 08:48:35 +0000

    Update game_engine_assault.hpp

--- game/source/game/game_engine_assault.hpp
+++ game/source/game/game_engine_assault.hpp
@@ -8,63 +8,24 @@
 struct c_game_engine_assault_variant : c_game_engine_base_variant
 {
 	c_flags<e_assault_variant_flags, word_flags, k_assault_variant_flags> m_variant_flags;
-
-	// default: `disabled`
 	c_enum<e_assault_respawn_settings, short, _assault_respawn_settings_disabled, k_assault_respawn_settings> m_respawn;
-
-	// default: `multi bomb`
 	c_enum<e_assault_game_type_settings, short, _assault_game_type_settings_multi_bomb, k_assault_game_type_settings> m_game_type;
-
-	// default: `when not carried by enemy`
 	c_enum<e_assault_enemy_bomb_waypoint_settings, short, _assault_enemy_bomb_waypoint_settings_never, k_assault_enemy_bomb_waypoint_settings> m_enemy_bomb_waypoint;
-
-	// default: 3
-	// maximum: 50
-	short m_score_to_win;
-
-	// halo online specific
-	// default: 2
-	// maximum: 50
-	short m_score_unknown0;
-
-	// halo online specific
-	// default: 0
-	// maximum: 50
-	short m_score_unknown1;
-
-	// halo online specific
-	// default: 0
-	// maximum: 50
-	short m_score_unknown2;
-
-	// halo online specific
-	// default: 0
-	// maximum: 50
-	short m_score_unknown3;
-
-	// default: `off`
+	c_enum<long, short, 0, 50> m_score_to_win;         // default: 3
+	c_enum<long, short, 0, 50> m_score_unknown0;       // default: 2, halo online specific
+	c_enum<long, short, 0, 50> m_score_unknown1;       // default: 0, halo online specific
+	c_enum<long, short, 0, 50> m_score_unknown2;       // default: 0, halo online specific
+	c_enum<long, short, 0, 50> m_score_unknown3;       // default: 0, halo online specific
 	c_enum<e_assault_variant_sudden_death_time, short, _assault_variant_sudden_death_time_infinite, k_assault_variant_sudden_death_time> m_sudden_death_time;
-
-	// default: 30
-	// maximum: 90
-	short m_bomb_reset_time; // seconds
-
-	// default: 35
-	// maximum: 90
-	short m_bomb_arming_time; // seconds
-
-	// default: 35
-	// maximum: 90
-	short m_bomb_disarming_time; // seconds
-
-	// default: 35
-	// maximum: 90
-	short m_bomb_fuse_time; // seconds
-
+	c_enum<long, short, 30, 90> m_bomb_reset_time;     // default: 30, 0-60 in halo 3
+	c_enum<long, short, 30, 90> m_bomb_arming_time;    // default: 35, 0-60 in halo 3
+	c_enum<long, short, 30, 90> m_bomb_disarming_time; // default: 35, 0-60 in halo 3
+	c_enum<long, short, 30, 90> m_bomb_fuse_time;      // default: 35, 0-60 in halo 3
 	c_player_traits m_carrier_traits;
 	c_player_traits m_arming_traits;
 
 	byte m_pad1[4];
+
 	byte unused[0x38];
 
 	void byteswap();
