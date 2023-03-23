85582af2f8d56dfb8eef73ac71eb4101de4aa6f6
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-23 08:49:59 +0000

    Update game_engine_vip.hpp

--- game/source/game/game_engine_vip.hpp
+++ game/source/game/game_engine_vip.hpp
@@ -8,59 +8,24 @@
 
 struct c_game_engine_vip_variant : c_game_engine_base_variant
 {
-	// default: 15
-	// maximum: 500
-	short m_score_to_win_round;
-
-	// halo online specific
-	// default: 10
-	// maximum: 500
-	short m_score_unknown;
-
+	c_enum<long, short, 0, 500> m_score_to_win_round;         // default: 15
+	c_enum<long, short, 0, 500> m_score_unknown;              // default: 10, halo online specific
 	c_flags<e_vip_variant_flags, word_flags, k_vip_variant_flags> m_variant_flags;
-
-	// default: 0
-	// maximum: 20
-	char m_kill_points;
-
-	// default: 0
-	// maximum: 20
-	char m_takedown_points;
-
-	// default: 0
-	// maximum: 20
-	char m_kill_as_vip_points;
-
-	// default: 0
-	// maximum: 20
-	char m_vip_death_points;
-
-	// default: 0
-	// maximum: 20
-	char m_destination_arrival_points;
-
-	// default: 0
-	// maximum: 20
-	char m_suicide_points;
-
-	// default: 0
-	// maximum: 20
-	char m_betrayal_points;
-
-	// default: 0
-	// maximum: 20
-	char m_vip_suicide_points;
-
+	c_enum<long, char, -10, 10> m_kill_points;                // default: 0
+	c_enum<long, char, -10, 10> m_takedown_points;            // default: 0
+	c_enum<long, char, -10, 10> m_kill_as_vip_points;         // default: 0
+	c_enum<long, char, -10, 10> m_vip_death_points;           // default: 0
+	c_enum<long, char, -10, 10> m_destination_arrival_points; // default: 0
+	c_enum<long, char, -10, 10> m_suicide_points;             // default: 0
+	c_enum<long, char, -10, 10> m_betrayal_points;            // default: 0
+	c_enum<long, char, -10, 10> m_vip_suicide_points;         // default: 0
 	c_enum<e_vip_selection_settings, char, _vip_selection_settings_random, k_vip_selection_settings> m_vip_selection;
 	c_enum<e_vip_zone_movement_settings, char, _vip_zone_movement_settings_off, k_vip_zone_movement_settings> m_zone_movement;
 	c_enum<e_vip_zone_order_settings, char, _vip_zone_order_settings_random, k_vip_zone_order_settings> m_zone_order;
 
 	byte m_pad1[1];
 
-	// default: 0
-	// maximum: 50
-	short m_influence_radius;
-
+	c_enum<long, short, 0, 50> m_influence_radius;
 	c_player_traits m_vip_team_traits;
 	c_player_traits m_vip_influence_traits;
 	c_player_traits m_vip_traits;
