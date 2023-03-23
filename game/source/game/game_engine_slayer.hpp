6b22c37ea6bedcfb9868cc1c8603ed1de98512a9
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-23 08:59:55 +0000

    Update game_engine_slayer.hpp

--- game/source/game/game_engine_slayer.hpp
+++ game/source/game/game_engine_slayer.hpp
@@ -9,21 +9,21 @@ struct c_game_engine_slayer_variant : c_game_engine_base_variant
 	// `c_game_engine_base_variant::m_team_scoring_method` override
 	// c_enum<e_slayer_team_scoring_settings, short, k_slayer_team_scoring_settings> m_team_scoring
 
-	c_enum<long, short, 25, 251> m_score_to_win;
-	c_enum<long, short, 20, 250> m_score_unknown; // halo online specific
-	c_enum<long, short, 1, 20> m_kill_points;
-	c_enum<long, char, 0, 20> m_assist_points;
-	c_enum<long, char, 0, 20> m_death_points;
-	c_enum<long, char, -1, 20> m_suicide_points;
-	c_enum<long, char, -1, 20> m_betrayal_points;
-	c_enum<long, char, 0, 20> m_leader_killed_points;
-	c_enum<long, char, 0, 20> m_elimination_points;
-	c_enum<long, char, 0, 20> m_assassination_points;
-	c_enum<long, char, 0, 20> m_headshot_points;
-	c_enum<long, char, 0, 20> m_melee_points;
-	c_enum<long, char, 0, 20> m_sticky_points;
-	c_enum<long, char, 0, 20> m_splatter_points;
-	c_enum<long, char, 0, 20> m_killing_spree_points;
+	c_enum<long, short, -1, 250> m_score_to_win;        // default: 25
+	c_enum<long, short, 0, 250> m_score_unknown;        // default: 20, halo online specific
+	c_enum<long, short, -10, 10> m_kill_points;         // default: 1
+	c_enum<long, char, -10, 10> m_assist_points;        // default: 0
+	c_enum<long, char, -10, 10> m_death_points;         // default: 0
+	c_enum<long, char, -10, 10> m_suicide_points;       // default: -1
+	c_enum<long, char, -10, 10> m_betrayal_points;      // default: -1
+	c_enum<long, char, -10, 10> m_leader_killed_points; // default: 0
+	c_enum<long, char, -10, 10> m_elimination_points;   // default: 0
+	c_enum<long, char, -10, 10> m_assassination_points; // default: 0
+	c_enum<long, char, -10, 10> m_headshot_points;      // default: 0
+	c_enum<long, char, -10, 10> m_melee_points;         // default: 0
+	c_enum<long, char, -10, 10> m_sticky_points;        // default: 0
+	c_enum<long, char, -10, 10> m_splatter_points;      // default: 0
+	c_enum<long, char, -10, 10> m_killing_spree_points; // default: 0
 	c_player_traits m_leader_traits;
 
 	byte m_pad1[2];
