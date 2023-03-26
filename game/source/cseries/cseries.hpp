a0117a9918cd4c511a77a9dc6f3e56d3002fe236
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-26 18:19:16 +0100

    Added `bit_count`

--- game/source/cseries/cseries.hpp
+++ game/source/cseries/cseries.hpp
@@ -113,6 +113,17 @@ const long LONG_BITS = SIZEOF_BITS(long);
 
 #define NONE -1
 
+constexpr long bit_count(long val)
+{
+	long result = 0;
+	while (val != 0)
+	{
+		val >>= 1;
+		result++;
+	}
+	return result;
+}
+
 constexpr bool pointer_is_aligned(void* pointer, long alignment_bits)
 {
 	return ((unsigned long)pointer & ((1 << alignment_bits) - 1)) == 0;
