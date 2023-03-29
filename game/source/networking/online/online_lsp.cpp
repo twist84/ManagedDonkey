1c15b24afcd5e1327cda6afc2a7c026e93d0c1f0
theTwister <6237734+twist84@users.noreply.github.com>
2023-03-29 03:29:50 +0100

    Updated `c_online_lsp_manager::acquire_server`

--- game/source/networking/online/online_lsp.cpp
+++ game/source/networking/online/online_lsp.cpp
@@ -9,7 +9,7 @@ REFERENCE_DECLARE(0x0199FE88, c_online_lsp_manager, g_online_lsp_manager);
 HOOK_DECLARE_CLASS(0x00431440, c_online_lsp_manager, acquire_server);
 
 //enum e_online_lsp_server_acquire_result __cdecl c_online_lsp_manager::acquire_server(enum e_online_lsp_service_type service_type, long, long* connection_token_out, long* ip_address, unsigned short* port, char const* service_description)
-long __fastcall c_online_lsp_manager::acquire_server(void* _this, void* unused, long service_type, long* connection_token_out, long* ip_address, unsigned short* port, char const* service_description)
+long __fastcall c_online_lsp_manager::acquire_server(c_online_lsp_manager* _this, void* unused, long service_type, long* connection_token_out, long* ip_address, unsigned short* port, char const* service_description)
 {
     return 0;
 }
