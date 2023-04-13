875d08fe855342f220b7355aba751e5d81dc85f3
theTwister <6237734+twist84@users.noreply.github.com>
2023-04-13 22:13:44 +0100

    Updated `http_stream`

--- game/source/networking/tools/http_stream.cpp
+++ game/source/networking/tools/http_stream.cpp
@@ -73,15 +73,14 @@ bool c_http_get_stream::verify_necessary_state_is_set()
 
 bool c_http_get_stream::read(char* buffer, long buffer_length, long* bytes_read)
 {
-	bool success = false;
+	//bool success = DECLFUNC(0x00432A10, bool, __thiscall, c_http_get_stream*, char*, long, long*)(this, buffer, buffer_length, bytes_read);
 
 	ASSERT(buffer);
 	ASSERT(bytes_read);
 
-	//bool success = DECLFUNC(0x00432A10, bool, __thiscall, c_http_get_stream*, char*, long, long*)(this, buffer, buffer_length, bytes_read);
-
-	char* dest_buffer = buffer;
-	long dest_buffer_length = buffer_length;
+	char* buf = buffer;
+	long len = buffer_length;
+	bool success = false;
 
 	if (verify_necessary_state_is_set())
 	{
@@ -94,7 +93,7 @@ bool c_http_get_stream::read(char* buffer, long buffer_length, long* bytes_read)
 		{
 			while (true)
 			{
-				if (at_end() || dest_buffer_length <= 0)
+				if (at_end() || len <= 0)
 					break;
 
 				long position = m_position;
@@ -105,19 +104,19 @@ bool c_http_get_stream::read(char* buffer, long buffer_length, long* bytes_read)
 					break;
 				}
 
-				if (dest_buffer_length > m_headers_length - position)
-					dest_buffer_length = m_headers_length - position;
+				if (len > m_headers_length - position)
+					len = m_headers_length - position;
 
-				memmove(dest_buffer, &m_headers.get_string()[position], dest_buffer_length);
+				memmove(buf, &m_headers.get_string()[position], len);
 
-				dest_buffer += dest_buffer_length;
-				m_position += dest_buffer_length;
-				dest_buffer_length = buffer_length - (dest_buffer - buffer);
+				buf += len;
+				m_position += len;
+				len = buffer_length - (buf - buffer);
 
 			}
 		}
 
-		*bytes_read = dest_buffer - buffer;
+		*bytes_read = buf - buffer;
 	}
 
 	return success;
@@ -145,7 +144,38 @@ c_http_post_stream::c_http_post_stream() :
 
 void c_http_post_stream::build_headers()
 {
-	DECLFUNC(0x00432720, void, __thiscall, c_http_post_stream*)(this);
+	//DECLFUNC(0x00432720, void, __thiscall, c_http_post_stream*)(this);
+
+	if (m_post_source.__unknown138)
+	{
+		__stringD48.set("\r\n");
+		__stringD48_length = 2;
+
+		__stringC48.set("\r\n");
+		__stringC48_length = 2;
+
+		m_headers.print("POST %s HTTP/1.0\r\nContent-Type: application/json\r\nContent-Length: %d\r\n%s\r\n",
+			m_url.get_string(),
+			m_post_source.m_contents_length - m_post_source.m_start_position + __stringD48_length + 2,
+			m_extra_headers.get_string());
+
+		m_extra_headers_length = m_headers.length();
+	}
+	else
+	{
+		__stringD48.set("\r\n--BUNGIEr0x0rz--\r\n");
+		__stringD48_length = __stringD48.length();
+
+		__stringC48.print("--BUNGIEr0x0rz\r\nContent-Disposition: form-data; name=\"upload\"; filename=\"%s\"\r\nContent-Type: %s\r\n\r\n", m_post_source.m_filename.get_string(), m_post_source.m_content_type.get_string());
+		__stringC48_length = __stringC48.length();
+
+		m_headers.print("POST %s HTTP/1.0\r\nContent-type: multipart/form-data; boundary=BUNGIEr0x0rz\r\nContent-Length: %d\r\n%s\r\n",
+			m_url.get_string(),
+			__stringC48_length + __stringD48_length + m_post_source.m_contents_length - m_post_source.m_start_position,
+			m_extra_headers.get_string());
+
+		m_extra_headers_length = m_headers.length();
+	}
 }
 
 bool c_http_post_stream::verify_necessary_state_is_set()
@@ -163,11 +193,18 @@ bool c_http_post_stream::read(char* buffer, long buffer_length, long* bytes_read
 
 long c_http_post_stream::get_length()
 {
-	return DECLFUNC(0x004328F0, long, __thiscall, c_http_post_stream*)(this);
+	//return DECLFUNC(0x004328F0, long, __thiscall, c_http_post_stream*)(this);
+
+	return m_extra_headers_length
+		+ __stringC48_length
+		+ __stringD48_length
+		+ m_post_source.m_contents_length - m_post_source.m_start_position;
 }
 
 bool c_http_post_stream::at_end()
 {
-	return DECLFUNC(0x004326B0, bool, __thiscall, c_http_post_stream*)(this);
+	//return DECLFUNC(0x004326B0, bool, __thiscall, c_http_post_stream*)(this);
+
+	return m_position == get_length();
 }
 
