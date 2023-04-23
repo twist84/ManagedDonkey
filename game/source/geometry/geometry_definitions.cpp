a6410ca3c02ccee9f07cd04aa2041bfe5df63641
theTwister <6237734+twist84@users.noreply.github.com>
2023-04-23 16:23:43 +0100

    Unhooked `mesh_has_subpart_mopp`

--- game/source/geometry/geometry_definitions.cpp
+++ game/source/geometry/geometry_definitions.cpp
@@ -1,17 +1,24 @@
 #include "geometry/geometry_definitions.hpp"
 
 #include "memory/module.hpp"
+#include "visibility/visibility_collection.hpp"
 
-HOOK_DECLARE_CALL(0x009745F6, mesh_has_subpart_mopp);
+//HOOK_DECLARE_CALL(0x009745F6, mesh_has_subpart_mopp);
 
 // emulate having no mopps
-bool const clear_mopps = false;
+bool const clear_mopps = true;
 
 bool __cdecl mesh_has_subpart_mopp(s_render_geometry* render_geometry, long mesh_index)
 {
-	if (clear_mopps && render_geometry->per_mesh_mopp.count())
-			render_geometry->per_mesh_mopp.clear();
+	//if (clear_mopps && render_geometry->per_mesh_mopp.count())
+	//		render_geometry->per_mesh_mopp.clear();
+	//return INVOKE(0x0078C780, mesh_has_subpart_mopp, render_geometry, mesh_index);
 
-	return INVOKE(0x0078C780, mesh_has_subpart_mopp, render_geometry, mesh_index);
+	for (word cluster_index = 0; cluster_index < c_visible_items::m_items.clusters.m_count; cluster_index++)
+	{
+		c_visible_items::m_items.clusters.m_elements[cluster_index].flags |= FLAG(3);
+	}
+
+	return false;
 }
 
