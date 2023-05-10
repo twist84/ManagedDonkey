3b06b82855c90b8a18fa462857b59bb805446a60
theTwister <6237734+twist84@users.noreply.github.com>
2023-05-10 13:05:12 +0100

    Add `simulation_action_game_engine_globals_update`

---
+++ game/source/simulation/game_interface/simulation_game_action.cpp
@@ -0,0 +1,8 @@
+#include "simulation/game_interface/simulation_game_action.hpp"
+
+//void __cdecl simulation_action_game_engine_globals_update(c_flags<long, qword, 64> flags)
+void __cdecl simulation_action_game_engine_globals_update(qword flags)
+{
+	INVOKE(0x004B2570, simulation_action_game_engine_globals_update, flags);
+}
+
---
+++ game/source/simulation/game_interface/simulation_game_action.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+extern void __cdecl simulation_action_game_engine_globals_update(qword flags);
+
