2058970fda806cb320412af2edb7d5a2bbbfd37b
theTwister <6237734+twist84@users.noreply.github.com>
2023-05-15 11:32:20 +0100

    Added `c_rasterizer_globals`

--- game/source/rasterizer/rasterizer.hpp
+++ game/source/rasterizer/rasterizer.hpp
@@ -2,6 +2,7 @@
 
 #include "cseries/cseries.hpp"
 #include "rasterizer/rasterizer_text.hpp"
+#include "tag_files/tag_groups.hpp"
 
 #include <d3d9.h>
 
@@ -88,3 +89,215 @@ struct c_rasterizer
 	static dword& g_max_vs_gprs;
 	static dword& g_max_ps_gprs;
 };
+
+struct s_global_bitmaps;
+struct s_texture_references_block;
+struct c_rasterizer_globals
+{
+public:
+	dword __cdecl get_max_vs_gprs() const
+	{
+		return m_max_vs_gprs;
+	}
+
+	dword __cdecl get_max_ps_gprs() const
+	{
+		return m_max_ps_gprs;
+	}
+
+protected:
+	struct s_explicit_shader
+	{
+		s_tag_reference explicit_vertex_shader;
+		s_tag_reference explicit_pixel_shader;
+	};
+	static_assert(sizeof(s_explicit_shader) == 0x20);
+
+	// default bitmaps
+	//	0:  ............... default 2D texture (white)
+	//	1:  ............... default normal map (no bump)
+	//	2:  ............... default cube map (error pattern)
+	//	3:  ............... color bars
+	//	4:  ............... black 2D texture
+	//	5:  ............... transparent black 2D texture
+	//	6:  ............... gray 2D texture
+	//	7:  ............... auto exposure weight
+	//	8:  ............... auto exposure weight (4x3)
+	//	9:  ............... stencil dither pattern
+	//	10: ............... smooth noise warp
+	//	11: ............... ripple pattern
+	c_typed_tag_block<s_global_bitmaps> default_bitmaps;
+
+	// material textures
+	//	0: ............... SH Glossy CT CC0236
+	//	1: ............... SH Glossy CT C78D78
+	//	2: ............... SH Glossy CT DD0236
+	//	3: ............... TBD
+	//	4: ............... TBD
+	//	5: ............... TBD
+	c_typed_tag_block<s_texture_references_block> material_textures;
+
+	// default shader vertex shader
+	//	Renderer uses this shader when a vertex shader is invalid
+	c_typed_tag_reference<'vtsh'> default_vertex_shader;
+
+	// default pixel shader
+	//	Renderer uses this shader when a pixel shader is invalid
+	c_typed_tag_reference<'pixl'> default_pixel_shader;
+
+	// debug override shader
+	//	When this shader is set, it overrides all shaders coming from geometry.
+	//	However, if you leave a field as default in this shader, that field will not override the corresponding field in user shaders.
+	//	For example, to render everything with a white albedo texture, select a white albedo texture in this shader
+	//	and leave everything as defaults.
+
+	// explicit shaders
+	//	0: ............... debug
+	//	1: ............... debug2d
+	//	2: ............... copy w/scale
+	//	3: ............... bloom: spike blur (vertical)
+	//	4: ............... bloom: spike blur (horizontal)
+	//	5: ............... bloom: downsample 4x4 (with bloom curve)
+	//	6: ............... bloom: downsample 4x4
+	//	7: ............... bloom: copy RGBE -> RGB
+	//	8: ............... bloom: update persistence
+	//	9: ............... bloom: add downsampled
+	//	10: .............. bloom: add
+	//	11: .............. bloom: kernel blur (11 horizontal)
+	//	12: .............. bloom: kernel blur (11 vertical)
+	//	13: .............. cubemap: blur (phi)
+	//	14: .............. cubemap: blur (theta)
+	//	15: .............. cubemap: clamp
+	//	16: .............. cubemap: divide
+	//	17: .............. write depth
+	//	18: .............. final composite
+	//	19: .............. sky dome (simple)
+	//	20: .............. transparent
+	//	21: .............. (legacy) shield meter
+	//	22: .............. (legacy) general meter
+	//	23: .............. overhead map geometry
+	//	24: .............. (legacy) hud bitmap
+	//	25: .............. bloom: blend 3 textures
+	//	26: .............. particle update
+	//	27: .............. particle spawn
+	//	28: .............. screenshot combine
+	//	29: .............. downsample 2x2
+	//	30: .............. rotate 2d
+	//	31: .............. bspline resample
+	//	32: .............. bloom; downsample 4x4 (with bloom curve + DOF)
+	//	33: .............. final composite (with DOF)
+	//	34: .............. convolve 5-tap kernel
+	//	35: .............. exposure downsample
+	//	36: .............. yuv to rgb
+	//	37: .............. displacement
+	//	38: .............. screenshot display
+	//	39: .............. downsample 4x4 block
+	//	40: .............. crop
+	//	41: .............. screenshot combine (with DOF)
+	//	42: .............. gamma correction
+	//	43: .............. contrail spawn
+	//	44: .............. contrail update
+	//	45: .............. stencil 8x8 dither pattern
+	//	46: .............. lens flare render
+	//	47: .............. decorator render
+	//	48: .............. bloom: NEW downsample 4x4 block (with bloom curve)
+	//	49: .............. bloom: NEW downsample 4x4 gaussian
+	//	50: .............. apply color matrix
+	//	51: .............. copy
+	//	52: .............. shadow geometry
+	//	53: .............. shadow apply
+	//	54: .............. gradient
+	//	55: .............. alpha test
+	//	56: .............. patchy fog
+	//	57: .............. light volume
+	//	58: .............. water ripple
+	//	59: .............. double gradient
+	//	60: .............. sniper scope
+	//	61: .............. shield impact
+	//	62: .............. player emblem-world
+	//	63: .............. player emblem-screen
+	//	64: .............. implicit hill
+	//	65: .............. chud overlay blend
+	//	66: .............. bloom add alpha1
+	//	67: .............. bloom: NEW downsample 4x4 block (with bloom curve) LDR
+	//	68: .............. restore LDR, HDR, and depth to EDRAM
+	//	69: .............. beam
+	//	70: .............. decorators no wind
+	//	71: .............. decorators static
+	//	72: .............. decorators sun only
+	//	73: .............. decorators wavy
+	//	74: .............. final composite zoom
+	//	75: .............. final composite debug
+	//	81: .............. displacement with motion blur
+	//	83: .............. screenshot memexport
+	//	84: .............. bloom: NEW downsample 4x4 gauss with curve LDR
+	//	85: ...............bloom_downsample_4x4_gaussian_bloom
+	//	86: ...............bloom_downsample_4x4_gaussian_bloom new
+	//	87: ...............bloom_curve
+	//	88: ...............custom gamma correct
+	//	89: ...............pixel copy
+	//	90: ...............hud camera
+	//	91: ...............hud night vision
+	//	92: ...............hud camera mask
+	//	93: ...............shield impact procedural uv
+	//	94: ...............screenshot night vision
+	//	95: .............. bloom: NEW downsample 4x4 gauss with curve LDR
+	//	96: .............. decorators edit
+	//	97: .............. smirnov test
+	//	98: .............. fxaa
+	//	99: .............. sniper scope stencil pc
+	//	100: ............. ssao
+	//	101: ............. ssao_blur
+	//	102: ............. lightshafts
+	//	103: ............. radial_blur
+	//	104: ............. clear_letterbox
+	c_typed_tag_block<s_explicit_shader> explicit_shaders;
+
+	// atmosphere lookup tables
+	//	0: ............... 1D Neta Table
+	//	1: ............... TBD
+	//	2: ............... TBD
+	//	3: ............... TBD
+	//	4: ............... TBD
+	//	5: ............... TBD
+	c_typed_tag_block<s_texture_references_block> atmosphere_lookup_tables;
+
+	// runtime
+	dword m_max_vs_gprs;
+	dword m_max_ps_gprs;
+
+	c_typed_tag_reference<'bitm'> active_camo_distortion_texture;
+	c_typed_tag_reference<'perf'> default_performance_throttles;
+	c_typed_tag_reference<'shit'> shield_impact_settings;
+	c_typed_tag_reference<'vmdx'> vision_mode_settings;
+
+	// Motion blur parameters
+	//	max blur:............  max amount to blur, as a percentage of the screen
+	//	blur scale:..........  scales blur for a given velocity
+	//	center falloff:....... reduces motion blur around the screen center (larger values give smaller reduction regions...)
+	//	expected dt:.......... expected time per tick, in seconds (used to correct motion blur in fast/slow frames)
+
+	long number_of_taps;
+	real max_blur_x;
+	real max_blur_y;
+	real blur_scale_x;
+	real blur_scale_y;
+	real center_falloff;
+	real expected_dt;
+
+	byte __padB4[4];
+};
+
+struct s_global_bitmaps
+{
+	dword_flags options;
+	c_typed_tag_reference<'bitm'> default_bitmaps;
+};
+static_assert(sizeof(s_global_bitmaps) == 0x14);
+
+struct s_texture_references_block
+{
+	c_typed_tag_reference<'bitm'> reference;
+};
+static_assert(sizeof(s_texture_references_block) == sizeof(s_tag_reference));
+
