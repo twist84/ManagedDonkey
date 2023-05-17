3df4bf8f8fe610a0206651e3afc30d5dec92c347
theTwister <6237734+twist84@users.noreply.github.com>
2023-05-17 15:25:00 +0100

    Updated `remote_command`

--- game/source/networking/tools/remote_command.hpp
+++ game/source/networking/tools/remote_command.hpp
@@ -51,6 +51,128 @@ struct s_remote_command_globals
 };
 static_assert(sizeof(s_remote_command_globals) == 0x104B4);
 
+//-----------------------------------------------------------------------------
+
+#define k_maximum_number_of_tokens 100
+#define k_token_length 256
+
+#define COMMAND_CALLBACK_DECLARE(_name) callback_result_t _name##_callback(void const* userdata, long token_count, tokens_t const tokens)
+#define COMMAND_CALLBACK_REGISTER(_name, _parameter_count, _parameters, ...) { #_name, _parameter_count, _name##_callback, _parameters, __VA_ARGS__ }
+
+#define COMMAND_CALLBACK_PARAMETER_CHECK                                      \
+ASSERT(userdata != nullptr);                                                  \
+ASSERT((token_count - 1) >= 0);                                               \
+                                                                              \
+s_command const& command = *static_cast<s_command const*>(userdata);          \
+callback_result_t result = command.name;                                      \
+result.append_line(": succeeded");                                            \
+if ((token_count - 1) != command.parameter_count)                             \
+{                                                                             \
+    result = "Invalid usage. ";                                               \
+    result.append_print_line("%s %s", command.name, command.parameter_types); \
+    result.append(command.extra_info);                                        \
+    return result;                                                            \
+}
+
+//-----------------------------------------------------------------------------
+
+using _token_t = c_static_string<k_token_length>;
+using token_t = _token_t*;
+using tokens_t = c_static_array<token_t, k_maximum_number_of_tokens>;
+
+using callback_result_t = c_static_string<4096>;
+using callback_t = callback_result_t(void const* userdata, long, tokens_t const);
+
+struct s_command
+{
+	char const* name;
+	long parameter_count;
+	callback_t* callback;
+	char const* parameter_types;
+	char const* extra_info;
+};
+
+//-----------------------------------------------------------------------------
+
+COMMAND_CALLBACK_DECLARE(help);
+COMMAND_CALLBACK_DECLARE(breakpoint);
+COMMAND_CALLBACK_DECLARE(close_game);
+COMMAND_CALLBACK_DECLARE(script_start);
+COMMAND_CALLBACK_DECLARE(game_splitscreen);
+COMMAND_CALLBACK_DECLARE(game_coop_players);
+COMMAND_CALLBACK_DECLARE(game_start);
+COMMAND_CALLBACK_DECLARE(net_session_create);
+COMMAND_CALLBACK_DECLARE(net_test_map_name);
+COMMAND_CALLBACK_DECLARE(net_test_variant);
+COMMAND_CALLBACK_DECLARE(net_test_session_mode);
+COMMAND_CALLBACK_DECLARE(net_test_ui_game_mode);
+COMMAND_CALLBACK_DECLARE(net_test_advertisement_mode);
+COMMAND_CALLBACK_DECLARE(net_test_game_variant_parameter);
+COMMAND_CALLBACK_DECLARE(net_build_game_variant);
+COMMAND_CALLBACK_DECLARE(net_verify_game_variant);
+COMMAND_CALLBACK_DECLARE(net_load_and_use_game_variant);
+COMMAND_CALLBACK_DECLARE(net_build_map_variant);
+COMMAND_CALLBACK_DECLARE(net_verify_map_variant);
+COMMAND_CALLBACK_DECLARE(game_export_variant_settings);
+
+//-----------------------------------------------------------------------------
+
+// #TODO: move this somewhere else?
+
+#define k_maximum_number_of_tokens 100
+#define k_token_length 256
+
+#define COMMAND_CALLBACK_DECLARE(_name) callback_result_t _name##_callback(void const* userdata, long token_count, tokens_t const tokens)
+#define COMMAND_CALLBACK_REGISTER(_name, _parameter_count, _parameters, ...) { #_name, _parameter_count, _name##_callback, _parameters, __VA_ARGS__ }
+
+#define COMMAND_CALLBACK_PARAMETER_CHECK                                      \
+ASSERT(userdata != nullptr);                                                  \
+ASSERT((token_count - 1) >= 0);                                               \
+                                                                              \
+s_command const& command = *static_cast<s_command const*>(userdata);          \
+callback_result_t result = command.name;                                      \
+result.append_line(": succeeded");                                            \
+if ((token_count - 1) != command.parameter_count)                             \
+{                                                                             \
+    result = "Invalid usage. ";                                               \
+    result.append_print_line("%s %s", command.name, command.parameter_types); \
+    result.append(command.extra_info);                                        \
+    return result;                                                            \
+}
+
+//-----------------------------------------------------------------------------
+
+s_command const k_registered_commands[] =
+{
+	COMMAND_CALLBACK_REGISTER(help, 0, "", "prints a description of the named function.\r\nNETWORK SAFE: Unknown, assumed unsafe"),
+	COMMAND_CALLBACK_REGISTER(breakpoint, 1, "<string>", "If breakpoints are enabled, pause execution when this statement is hit (displaying the given message).\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(close_game, 0, "", "closes the game.\r\nNETWORK SAFE: Unknown, assumed unsafe"),
+
+	COMMAND_CALLBACK_REGISTER(script_start, 1, "<string>", "debug script launching: starts a scenario script by name.\r\nNETWORK SAFE: No, for mainmenu only"),
+	COMMAND_CALLBACK_REGISTER(game_splitscreen, 1, "<long>", "debug map launching: sets the number of multiplayer splitscreen players for the next map.\r\nNETWORK SAFE: No, for mainmenu only"),
+	COMMAND_CALLBACK_REGISTER(game_coop_players, 1, "<long>", "debug map launching: sets the number of coop players for the next map.\r\nNETWORK SAFE: No, for mainmenu only"),
+	COMMAND_CALLBACK_REGISTER(game_start, 1, "<string>", "debug map launching: starts a game on the specified map.\r\nNETWORK SAFE: No, for mainmenu only"),
+
+	COMMAND_CALLBACK_REGISTER(net_session_create, 2, "<string> <string>", "<ui_game_mode> <advertisement_mode> creates a session to play\r\nNETWORK SAFE: No, for mainmenu only"),
+
+	COMMAND_CALLBACK_REGISTER(net_test_map_name, 1, "<string>", "network test: sets the name of the scenario to play\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(net_test_variant, 1, "<string>", "network test: sets the game variant to play\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(net_test_session_mode, 1, "<string>", "network test: sets the session mode to play\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(net_test_ui_game_mode, 1, "<string>", "network test: sets the ui game mode to play\r\nNETWORK SAFE: No, for mainmenu only"),
+	COMMAND_CALLBACK_REGISTER(net_test_advertisement_mode, 1, "<string>", "network test: sets the advertisement mode to play\r\nNETWORK SAFE: No, for mainmenu only"),
+	COMMAND_CALLBACK_REGISTER(net_test_game_variant_parameter, 2, "<string> <long>", "network test: sets a parameter of the current game variant\r\nNETWORK SAFE: No, for mainmenu only"),
+
+	COMMAND_CALLBACK_REGISTER(net_build_game_variant, 1, "<string>", "writes the current game variant to a file\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(net_verify_game_variant, 1, "<string>", "verifies the contents of a packed game variant file\r\nNETWORK SAFE: Unknown, assumed unsafe"),
+	COMMAND_CALLBACK_REGISTER(net_load_and_use_game_variant, 1, "<string>", "loads the contents of a packed game variant file and submits to networking for use in the current game\r\nNETWORK SAFE: Unknown, assumed unsafe"),
+
+	COMMAND_CALLBACK_REGISTER(net_build_map_variant, 1, "<string>", "writes the current map variant to a file\r\nNETWORK SAFE: Yes"),
+	COMMAND_CALLBACK_REGISTER(net_verify_map_variant, 1, "<string>", "verifies the contents of a packed map variant file\r\nNETWORK SAFE: Unknown, assumed unsafe"),
+	COMMAND_CALLBACK_REGISTER(game_export_variant_settings, 1, "<string>", "export the current game engine variant settings to the specified text file\r\nNETWORK SAFE: No"),
+};
+
+extern void command_tokenize(char const* input, tokens_t& tokens, long* token_count);
+
 extern s_remote_command_globals remote_command_globals;
 
 extern void patch_remote_command();
