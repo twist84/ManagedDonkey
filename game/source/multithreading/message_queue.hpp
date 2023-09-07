4e94142f97652359af5ec71e49d2fa371eaaa440
theTwister <6237734+twist84@users.noreply.github.com>
2023-09-07 12:53:29 +0100

    Updated `t_message_queue`

--- game/source/multithreading/message_queue.hpp
+++ game/source/multithreading/message_queue.hpp
@@ -5,12 +5,14 @@
 template<typename t_type, long k_maximum_count, long k_storage_count = ((k_maximum_count - 1) >> 5) + 1>
 struct t_message_queue
 {
-	c_synchronized_long m_count;
+	static long const k_max_per_frame = k_maximum_count;
 
-	//c_synchronized_array<k_maximum_count >> 5> m_added_entries;
-	c_synchronized_array<k_storage_count> m_added_entries;
+	c_synchronized_long m_message_count;
 
-	t_type m_entries[k_maximum_count];
+	//c_synchronized_array<k_max_per_frame >> 5> m_messages_added;
+	c_synchronized_array<k_storage_count> m_messages_added;
+
+	t_type m_messages[k_max_per_frame];
 };
 static_assert(sizeof(t_message_queue<long, 1024>) == 0x1084);
 
