b35213e64e52ffae7e44bc97098c5d225c6dc0d1
theTwister <6237734+twist84@users.noreply.github.com>
2023-09-09 14:31:30 +0100

    Mapped `s_simulation_queue_decoded_update_data`

--- game/source/simulation/simulation_queue_entities.hpp
+++ game/source/simulation/simulation_queue_entities.hpp
@@ -18,3 +18,16 @@ struct s_simulation_queue_decoded_creation_data
 };
 static_assert(sizeof(s_simulation_queue_decoded_creation_data) == 0x8018);
 
+struct s_simulation_queue_decoded_update_data
+{
+    // header
+    long entity_type;
+    long gamestate_index;
+
+    qword update_mask;
+
+    byte update_data_payload[0x4000];
+    long update_data_payload_size;
+};
+static_assert(sizeof(s_simulation_queue_decoded_update_data) == 0x4018);
+
