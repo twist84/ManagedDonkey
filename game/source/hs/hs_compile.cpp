fdae237302ab5b77de2c7e66185d81c4e473458f
theTwister <6237734+twist84@users.noreply.github.com>
2023-09-26 11:00:30 +0100

    Added `hs_type_primitive_parser_real`

--- game/source/hs/hs_compile.cpp
+++ game/source/hs/hs_compile.cpp
@@ -33,6 +33,42 @@ bool hs_type_primitive_parser_bool(long expression_index)
 	return false;
 }
 
+bool hs_type_primitive_parser_real(long expression_index)
+{
+	hs_syntax_node* expression = hs_syntax_get(expression_index);
+	char const* source_offset = &hs_compile_globals.compiled_source[expression->source_offset];
+
+	ASSERT(expression->type == _hs_type_real);
+	ASSERT(expression->constant_type == expression->type);
+
+	real& value = *reinterpret_cast<real*>(expression->data);
+
+	if (*source_offset == '-')
+		++source_offset;
+
+	bool result = true;
+	bool error_occurred = false;
+	while (*source_offset)
+	{
+		if (!isdigit(*source_offset))
+		{
+			if (error_occurred || *source_offset != '.')
+			{
+				hs_compile_globals.error_message = "this is not a valid real number.";
+				hs_compile_globals.error_offset = expression->source_offset;
+
+				result = false;
+				break;
+			}
+			error_occurred = true;
+		}
+		source_offset++;
+	}
+
+	value = static_cast<real>(atof(&hs_compile_globals.compiled_source[expression->source_offset]));
+	return result;
+}
+
 hs_type_primitive_parser_t hs_type_primitive_parsers[k_hs_type_count]
 {
 	// non-types
@@ -44,5 +80,6 @@ hs_type_primitive_parser_t hs_type_primitive_parsers[k_hs_type_count]
 	nullptr,
 
 	hs_type_primitive_parser_bool,
+	hs_type_primitive_parser_real,
 };
 
