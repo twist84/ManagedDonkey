27813971c3f0cf7c608bef006369a2c627444adf
theTwister <6237734+twist84@users.noreply.github.com>
2023-09-26 15:06:06 +0100

    Added `hs_type_primitive_parser_string_id`

--- game/source/hs/hs_compile.cpp
+++ game/source/hs/hs_compile.cpp
@@ -156,6 +156,29 @@ bool hs_type_primitive_parser_script(long expression_index)
 	return false;
 }
 
+bool hs_type_primitive_parser_string_id(long expression_index)
+{
+	hs_syntax_node* expression = hs_syntax_get(expression_index);
+	char const* source_offset = &hs_compile_globals.compiled_source[expression->source_offset];
+
+	ASSERT(expression->type == _hs_type_string_id);
+	ASSERT(expression->constant_type == expression->type);
+
+	string_id& value = *reinterpret_cast<string_id*>(expression->data);
+
+	string_id id = string_id_retrieve(source_offset);
+	if (id != NONE)
+	{
+		value = id;
+		return true;
+	}
+
+	hs_compile_globals.error_message = "this is not a valid string parameter.";
+	hs_compile_globals.error_offset = expression->source_offset;
+
+	return false;
+}
+
 hs_type_primitive_parser_t hs_type_primitive_parsers[k_hs_type_count]
 {
 	nullptr,                                  // unparsed
@@ -169,5 +192,6 @@ hs_type_primitive_parser_t hs_type_primitive_parsers[k_hs_type_count]
 	hs_type_primitive_parser_integer,         // long_integer
 	hs_type_primitive_parser_string,          // string
 	hs_type_primitive_parser_script,          // script
+	hs_type_primitive_parser_string_id,       // string_id
 };
 
