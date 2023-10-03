b2d77119ed76b9aacda934154b66054d465d0cc6
theTwister <6237734+twist84@users.noreply.github.com>
2023-10-03 16:33:45 +0100

    Updated hs print hooks

--- game/source/hs/hs_library_external.cpp
+++ game/source/hs/hs_library_external.cpp
@@ -7,15 +7,13 @@
 HOOK_DECLARE(0x0096D8B0, hs_log_print);
 HOOK_DECLARE(0x0096EF60, hs_print);
 
-void __cdecl hs_log_print(char const* text)
+void __cdecl hs_log_print(char const* s)
 {
-	generate_event(_event_level_warning, "hs: %s", text);
+	generate_event(_event_level_warning, "hs: %s", s);
 }
-//HOOK_DECLARE_CALL(0x007302EA, hs_log_print);
 
-void __cdecl hs_print(char const* text)
+void __cdecl hs_print(char const* s)
 {
-	terminal_printf(global_real_argb_green, text);
+	terminal_printf(global_real_argb_green, s);
 }
-//HOOK_DECLARE_CALL(0x0072FE9A, hs_print);
 
--- game/source/hs/hs_library_external.hpp
+++ game/source/hs/hs_library_external.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-extern void __cdecl hs_log_print(char const* text);
-extern void __cdecl hs_print(char const* text);
+extern void __cdecl hs_log_print(char const* s);
+extern void __cdecl hs_print(char const* s);
 
