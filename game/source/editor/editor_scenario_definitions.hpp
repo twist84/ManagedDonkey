4f70216654c6ecd10c4a45e4127f92dd3b4698e2
theTwister <6237734+twist84@users.noreply.github.com>
2023-10-05 17:26:51 +0100

    Added `s_scenario_editor_folder`

--- game/source/editor/editor_scenario_definitions.hpp
+++ game/source/editor/editor_scenario_definitions.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
-struct s_scenario_editor_folder;
+struct s_scenario_editor_folder
+{
+	long parent_folder; // long_block_index
+
+	c_static_string<k_tag_long_string_length> name;
+};
+static_assert(sizeof(s_scenario_editor_folder) == 0x104);
+
