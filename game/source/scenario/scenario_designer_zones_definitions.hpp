cb70a4914a4f802484dc85f16f2e072c05bcdc70
theTwister <6237734+twist84@users.noreply.github.com>
2023-10-05 17:08:56 +0100

    Added `s_scenario_designer_zone`

--- game/source/scenario/scenario_designer_zones_definitions.hpp
+++ game/source/scenario/scenario_designer_zones_definitions.hpp
@@ -1,3 +1,29 @@
 #pragma once
 
-struct s_scenario_designer_zone;
+#include "cseries/cseries.hpp"
+#include "tag_files/tag_groups.hpp"
+
+struct s_scenario_designer_zone
+{
+	c_string_id name;
+	dword_flags flags;
+
+	s_tag_block biped;
+	s_tag_block vehicle;
+	s_tag_block weapon;
+	s_tag_block equipment;
+	s_tag_block scenery;
+	s_tag_block machine;
+	s_tag_block terminal;
+	s_tag_block arg_device;
+	s_tag_block control;
+	s_tag_block sound_scenery;
+	s_tag_block crate;
+	s_tag_block creature;
+	s_tag_block giant;
+	s_tag_block effect_scenery;
+	s_tag_block character;
+	s_tag_block budget_reference;
+};
+static_assert(sizeof(s_scenario_designer_zone) == 0xC8);
+
