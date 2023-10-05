a9e8d5a09ae86f1192f8c4ff5ec0dbeeae62d624
theTwister <6237734+twist84@users.noreply.github.com>
2023-10-05 16:33:57 +0100

    Moved `orders_definition`

--- game/source/ai/ai_orders.hpp
+++ game/source/ai/ai_orders.hpp
@@ -1,4 +1,35 @@
 #pragma once
 
-struct orders_definition;
+#include "cseries/cseries.hpp"
+#include "tag_files/tag_groups.hpp"
+
+struct orders_definition
+{
+	c_static_string<k_tag_string_length> name;
+	short style;
+
+	// pad
+	byte YATIWNRNR[0x2];
+
+	dword_flags flags;
+	short_enum force_combat_status;
+
+	// pad
+	byte PWY[0x2];
+
+	c_static_string<k_tag_string_length> entry_script;
+	short script_index;
+	short follow_squad;
+	real follow_radius;
+
+	s_tag_block primary_area_set;
+	s_tag_block secondary_area_set;
+	s_tag_block secondary_set_trigger;
+	s_tag_block special_movement;
+	s_tag_block order_endings;
+	s_tag_block pureform_distribution;
+};
+static_assert(sizeof(orders_definition) == 0x9C);
+
 struct trigger_definition;
+
--- game/source/scenario/scenario_definitions.hpp
+++ game/source/scenario/scenario_definitions.hpp
@@ -770,34 +770,6 @@ struct s_scenario_cutscene_title
 };
 static_assert(sizeof(s_scenario_cutscene_title) == 0x28);
 
-struct orders_definition
-{
-	c_static_string<k_tag_string_length> name;
-	short style;
-
-	// pad
-	byte YATIWNRNR[0x2];
-
-	dword_flags flags;
-	short_enum force_combat_status;
-
-	// pad
-	byte PWY[0x2];
-
-	c_static_string<k_tag_string_length> entry_script;
-	short script_index;
-	short follow_squad;
-	real follow_radius;
-
-	s_tag_block primary_area_set;
-	s_tag_block secondary_area_set;
-	s_tag_block secondary_set_trigger;
-	s_tag_block special_movement;
-	s_tag_block order_endings;
-	s_tag_block pureform_distribution;
-};
-static_assert(sizeof(orders_definition) == 0x9C);
-
 struct s_background_bitmap_reference_definition
 {
 	c_typed_tag_reference<BITMAP_TAG> background_bitmap_group_reference;
