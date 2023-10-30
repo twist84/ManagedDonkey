f088713375eedb006973f5ecf13ceafdb208c238
theTwister <6237734+twist84@users.noreply.github.com>
2023-10-30 08:33:34 +0000

    Added `simulation_gamestate_entities_*`

--- game/source/simulation/simulation_gamestate_entities.cpp
+++ game/source/simulation/simulation_gamestate_entities.cpp
@@ -1,7 +1,45 @@
 #include "simulation/simulation_gamestate_entities.hpp"
 
+#include "main/main_game.hpp"
+#include "memory/thread_local.hpp"
+
+void __cdecl simulation_gamestate_entities_dispose()
+{
+	//INVOKE(0x00471CE0, simulation_gamestate_entities_dispose);
+
+	TLS_DATA_GET_VALUE_REFERENCE(simulation_gamestate_entity_data);
+
+	simulation_gamestate_entity_data = NULL;
+}
+
+void __cdecl simulation_gamestate_entities_dispose_from_old_map()
+{
+	//INVOKE(0x00471D10, simulation_gamestate_entities_dispose_from_old_map);
+
+	TLS_DATA_GET_VALUE_REFERENCE(simulation_gamestate_entity_data);
+
+	if (!main_game_reset_in_progress())
+		data_make_invalid(*simulation_gamestate_entity_data);
+}
+
 void __cdecl simulation_gamestate_entities_initialize()
 {
-	INVOKE(0x00471D40, simulation_gamestate_entities_initialize);
+	//INVOKE(0x00471D40, simulation_gamestate_entities_initialize);
+
+	TLS_DATA_GET_VALUE_REFERENCE(simulation_gamestate_entity_data);
+
+	simulation_gamestate_entity_data = data_new("sim. gamestate entities", 2048, sizeof(s_simulation_gamestate_entity), 0, &g_simulation_gamestate_entity_data_allocator);
+
+	ASSERT(*simulation_gamestate_entity_data != NULL);
+}
+
+void __cdecl simulation_gamestate_entities_initialize_for_new_map()
+{
+	//INVOKE(0x00471D80, simulation_gamestate_entities_initialize_for_new_map);
+
+	TLS_DATA_GET_VALUE_REFERENCE(simulation_gamestate_entity_data);
+
+	data_make_valid(*simulation_gamestate_entity_data);
+	data_delete_all(*simulation_gamestate_entity_data);
 }
 
--- game/source/simulation/simulation_gamestate_entities.hpp
+++ game/source/simulation/simulation_gamestate_entities.hpp
@@ -12,5 +12,8 @@ struct s_simulation_gamestate_entity : s_datum_header
 };
 static_assert(sizeof(s_simulation_gamestate_entity) == 0x10);
 
+extern void __cdecl simulation_gamestate_entities_dispose();
+extern void __cdecl simulation_gamestate_entities_dispose_from_old_map();
 extern void __cdecl simulation_gamestate_entities_initialize();
+extern void __cdecl simulation_gamestate_entities_initialize_for_new_map();
 
