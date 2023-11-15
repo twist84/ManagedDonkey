7fa38316b630767d980f82c94af1ab82dd71c6e0
theTwister <6237734+twist84@users.noreply.github.com>
2023-11-15 15:32:28 +0000

    Mapped `s_object_early_mover` (WIP)

--- game/source/objects/object_early_movers.hpp
+++ game/source/objects/object_early_movers.hpp
@@ -2,16 +2,53 @@
 
 #include "cseries/cseries.hpp"
 
+#define MAXIMUM_OBJECT_EARLY_MOVERS_PER_MAP 32
+
 struct s_object_early_mover
 {
-	byte __data[0x130];
+	// object_early_mover_update
+	vector3d update_linear_velocity;
+	vector3d update_angular_velocity;
+	real_point3d update_center_of_mass;
+
+	// object_early_mover_move
+	real_point3d center_of_mass_copy;
+	real_point3d center_of_mass;
+	vector3d linear_velocity_copy;
+	vector3d linear_velocity;
+	vector3d angular_velocity_copy;
+	vector3d angular_velocity;
+	real_matrix4x3 transform_copy;
+	real_matrix4x3 transform;
+	real_matrix4x3 inverse_transform;
+	bool __unknown107;
+	bool __unknown108;
+
+	// object_early_mover_update
+	bool __unknown109;
+
+	// object_early_mover_update
+	long collision_model_bsp_info_count;
+	union packed_index_t
+	{
+		struct
+		{
+			byte node_index;
+			byte bsp_index;
+			byte permutation_index;
+			byte region_index;
+		};
+
+		dword value;
+	};
+	c_static_array<packed_index_t, 8> collision_model_bsp_info;
 };
 static_assert(sizeof(s_object_early_mover) == 0x130);
 
 struct s_object_early_movers_globals
 {
-	s_object_early_mover early_movers[32];
-	long object_indices[32];
+	s_object_early_mover meta[MAXIMUM_OBJECT_EARLY_MOVERS_PER_MAP];
+	long object_indices[MAXIMUM_OBJECT_EARLY_MOVERS_PER_MAP];
 	long object_index_count;
 	bool map_initialized;
 };
