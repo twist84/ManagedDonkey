96fb18e4173c6a62cc2217dd6e08af27f0306eba
theTwister <6237734+twist84@users.noreply.github.com>
2023-11-15 16:12:37 +0000

    Mapped `s_object_scripting_state` (WIP)

--- game/source/objects/object_scripting.hpp
+++ game/source/objects/object_scripting.hpp
@@ -4,7 +4,19 @@
 
 struct s_object_scripting_state
 {
-	byte __data[0x304];
+	struct s_variable // #TODO: find the actual name
+	{
+		c_string_id name;
+		real __unknown4;
+		real __unknown8;
+		long __unknownC;
+		long __unknown10;
+	};
+	static_assert(sizeof(s_variable) == 0x14);
+
+	s_variable variables[32];
+	long object_indices[32];
+	bool map_initialized;
 };
 static_assert(sizeof(s_object_scripting_state) == 0x304);
 
