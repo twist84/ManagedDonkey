d821430cc68008cd002ee42c966a46f00838c26a
theTwister <6237734+twist84@users.noreply.github.com>
2023-11-30 14:42:44 +0000

    Updated `c_tag_resource_cache_controller`

--- game/source/tag_files/tag_resource_cache_control.hpp
+++ game/source/tag_files/tag_resource_cache_control.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
-#include "memory/data.hpp"
 
+struct s_data_array;
 struct c_tag_resource_cache_controller
 {
 	byte __data0[0x8];
-	s_data_array* __data_array;
-	byte __dataC[0x54];
+	s_data_array* m_resource_control_data;
+	byte __dataC[0x10];
+	c_wrapped_flags __wrapped_flags1C;
+	byte __data[0x3C];
 };
 static_assert(sizeof(c_tag_resource_cache_controller) == 0x60);
 
