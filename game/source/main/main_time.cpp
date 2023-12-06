4b308603d1d144b6c0fc269cee832852acffb363
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-06 20:00:00 +0000

    Added `main_time` functions

--- game/source/main/main_time.cpp
+++ game/source/main/main_time.cpp
@@ -1,11 +1,15 @@
 #include "main/main_time.hpp"
 
 #include "interface/interface_constants.hpp"
+#include "game/game.hpp"
+#include "memory/module.hpp"
+#include "memory/thread_local.hpp"
 #include "text/draw_string.hpp"
 #include "rasterizer/rasterizer_globals.hpp"
 
 REFERENCE_DECLARE(0x022B47FC, bool, display_framerate);
 
+bool debug_disable_frame_rate_throttle = false;
 bool display_frame_deltas = false;
 
 void rasterizer_render_debug_frame_deltas()
@@ -46,9 +50,109 @@ void __cdecl main_time_frame_rate_display()
 		rasterizer_render_debug_frame_deltas();
 }
 
+//.text:00507D70 ; __int64 sub_507D70() // return g_main_time_globals->last_milliseconds;
+
+__int64 __cdecl main_time_get_absolute_milliseconds()
+{
+	return INVOKE(0x00507D90, main_time_get_absolute_milliseconds);
+}
+
+//.text:00507DD0
+//.text:00507DE0
+
+__int64 __cdecl main_time_get_input_collection_time()
+{
+	return INVOKE(0x00507DF0, main_time_get_input_collection_time);
+}
+
+long __cdecl main_time_get_native_tick_rate()
+{
+	return INVOKE(0x00507E10, main_time_get_native_tick_rate);
+}
+
+__int64 __cdecl main_time_get_publishing_end_time()
+{
+	return INVOKE(0x00507E40, main_time_get_publishing_end_time);
+}
+
+__int64 __cdecl main_time_get_publishing_start_time()
+{
+	return INVOKE(0x00507E60, main_time_get_publishing_start_time);
+}
+
+__int64 __cdecl main_time_get_target_display_vblank_index()
+{
+	return INVOKE(0x00507E80, main_time_get_target_display_vblank_index);
+}
+
+long __cdecl main_time_get_time_since_input_collection(__int64 a1)
+{
+	return INVOKE(0x00507EA0, main_time_get_time_since_input_collection, a1);
+}
+
+long __cdecl main_time_get_vblank_rate()
+{
+	return INVOKE(0x00507EC0, main_time_get_vblank_rate);
+}
+
+void __cdecl main_time_globals_post_copy_update(void* userdata)
+{
+	INVOKE(0x00507EF0, main_time_globals_post_copy_update, userdata);
+}
+
+void __cdecl main_time_initialize()
+{
+	INVOKE(0x00507F10, main_time_initialize);
+}
+
+bool __cdecl main_time_is_throttled()
+{
+	return INVOKE(0x00507FF0, main_time_is_throttled);
+
+	//if (!game_in_progress())
+	//	return false;
+	//
+	//if (debug_disable_frame_rate_throttle)
+	//	return false;
+	//
+	//TLS_DATA_GET_VALUE_REFERENCE(g_main_time_globals);
+	//
+	//if (g_main_time_globals->__unknown0)
+	//	return g_main_time_globals->__unknown1;
+	//
+	//return true;
+}
+
+void __cdecl main_time_mark_input_collection_time()
+{
+	INVOKE(0x00508030, main_time_mark_input_collection_time);
+}
+
+void __cdecl main_time_mark_publishing_end_time()
+{
+	INVOKE(0x00508070, main_time_mark_publishing_end_time);
+}
+
+void __cdecl main_time_mark_publishing_start_time()
+{
+	INVOKE(0x005080B0, main_time_mark_publishing_start_time);
+}
+
+//.text:005080F0 ; void __cdecl sub_5080F0()
+
 void __cdecl main_time_reset()
 {
 	INVOKE(0x00508110, main_time_reset);
+}
+
+void __cdecl main_time_restore(long a1)
+{
+	INVOKE(0x00508120, main_time_restore, a1);
+}
+
+void __cdecl main_time_set_temporary_throttle(bool temporary_throttle)
+{
+	INVOKE(0x00508130, main_time_set_temporary_throttle, temporary_throttle);
 }
 
 void __cdecl main_time_throttle(__int64 a1)
--- game/source/main/main_time.hpp
+++ game/source/main/main_time.hpp
@@ -4,30 +4,45 @@
 
 struct s_main_time_globals
 {
-	dword __unknown0;
-	dword __unknown4;
-	dword last_milliseconds;
-	dword __unknownC;
-	dword __unknown10;
-	dword __unknown14;
-	dword __unknown18;
-	dword __unknown1C;
-	dword target_display_vblank_index;
-	dword __unknown24;
-	dword input_collection_time;
-	dword __unknown2C;
-	dword publishing_start_time;
-	dword __unknown34;
-	dword publishing_end_time;
-	dword __unknown3C;
+	// main_time_set_temporary_throttle(bool temporary_throttle)
+	// __unknown0 = true;
+	// __unknown1 = temporary_throttle;
+	bool __unknown0;
+	bool __unknown1;
+
+	dword elapsed_ticks;
+	__int64 last_milliseconds;
+	__int64 absolute;
+	__int64 __unknown18;
+	__int64 target_display_vblank_index;
+	__int64 input_collection_time;
+	__int64 publishing_start_time;
+	__int64 publishing_end_time;
 };
 static_assert(sizeof(s_main_time_globals) == 0x40);
 
 extern bool& display_framerate;
+extern bool debug_disable_frame_rate_throttle;
 extern bool display_frame_deltas;
 
 extern void __cdecl main_time_frame_rate_display();
+extern __int64 __cdecl main_time_get_absolute_milliseconds();
+extern __int64 __cdecl main_time_get_input_collection_time();
+extern long __cdecl main_time_get_native_tick_rate();
+extern __int64 __cdecl main_time_get_publishing_end_time();
+extern __int64 __cdecl main_time_get_publishing_start_time();
+extern __int64 __cdecl main_time_get_target_display_vblank_index();
+extern long __cdecl main_time_get_time_since_input_collection(__int64 a1);
+extern long __cdecl main_time_get_vblank_rate();
+extern void __cdecl main_time_globals_post_copy_update(void* userdata);
+extern void __cdecl main_time_initialize();
+extern bool __cdecl main_time_is_throttled();
+extern void __cdecl main_time_mark_input_collection_time();
+extern void __cdecl main_time_mark_publishing_end_time();
+extern void __cdecl main_time_mark_publishing_start_time();
 extern void __cdecl main_time_reset();
+extern void __cdecl main_time_restore(long a1);
+extern void __cdecl main_time_set_temporary_throttle(bool temporary_throttle);
 extern void __cdecl main_time_throttle(__int64 a1);
 extern real __cdecl main_time_update();
 
