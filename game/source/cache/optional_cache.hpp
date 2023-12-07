e54fd22a012811eeb584e2c44a771f0535ea1828
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-07 10:45:21 +0000

    Updated `c_optional_cache_backend::deallocate`

--- game/source/cache/optional_cache.hpp
+++ game/source/cache/optional_cache.hpp
@@ -13,7 +13,7 @@ private:
 	virtual void initialize(e_map_memory_configuration memory_configuration, c_static_array<s_optional_cache_user_memory_configuration, 6> const*);
 	virtual void dispose();
 	virtual void* try_to_allocate(e_optional_cache_user, e_optional_cache_user_priority, long, c_optional_cache_user_callback*);
-	virtual void deallocate(e_optional_cache_user, long, void*);
+	virtual void deallocate(e_optional_cache_user, void*);
 
 	bool m_active;
 	dword __unknown8;
