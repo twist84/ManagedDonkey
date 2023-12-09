e6e54ed8db810c35543ca7ad37d45fb1b8d052f9
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-09 18:13:12 +0000

    Added WinMain

---
+++ game/source/shell/shell_windows.cpp
@@ -0,0 +1,45 @@
+//#include "cseries/cseries.hpp"
+//
+//#include "memory/module.hpp"
+//
+//#include <Windows.h>
+//
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+//{
+//	SetLastError(NO_ERROR);
+//	SetProcessDPIAware();
+//
+//	g_lpCmdLine = lpCmdLine;
+//	g_hInstance = hInstance;
+//	g_nShowCmd = nShowCmd;
+//	g_lpfnWndProc = WndProc;
+//	g_hWnd = NULL;
+//
+//	c_static_string<64> name;
+//	name.print("HaloOnline %s", sub_5013A0());
+//	csstrnzcpy(g_lpszClassName, name.get_string(), sizeof(g_lpszClassName));
+//	csstrnzcpy(g_lpszWindowName, name.get_string(), sizeof(g_lpszClassName));
+//
+//	physical_memory_initialize();
+//	physical_memory_stage_push(_memory_stage_game_initialize);
+//
+//	SetBinDllDirectory(); // PC
+//
+//	if (shell_initialize(false))
+//	{
+//		RegisterHotKey_Snapshot();
+//
+//		main_loop();
+//		shell_dispose();
+//
+//		UnregisterHotKey_Snapshot();
+//	}
+//
+//	physical_memory_stage_pop(_memory_stage_game_initialize);
+//	physical_memory_dispose();
+//
+//	return 0;
+//}
+//HOOK_DECLARE(0x0042EB10, WinMain);
+//
+//
