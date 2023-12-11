6a1a0f234865f5a8811120f0c0f905e3968420a0
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-11 19:50:24 +0000

    Added `game_state_procs`

---
+++ game/source/saved_games/game_state_procs.cpp
@@ -0,0 +1,46 @@
+#include "saved_games/game_state_procs.hpp"
+
+#include "cseries/cseries.hpp"
+
+void __cdecl game_state_call_after_load_procs(long game_state_proc_flags)
+{
+	INVOKE(0x0058A4B0, game_state_call_after_load_procs, game_state_proc_flags);
+}
+
+void __cdecl game_state_call_after_save_procs(long game_state_proc_flags)
+{
+	INVOKE(0x0058A550, game_state_call_after_save_procs, game_state_proc_flags);
+}
+
+void __cdecl game_state_call_before_load_procs(long game_state_proc_flags)
+{
+	INVOKE(0x0058A5F0, game_state_call_before_load_procs, game_state_proc_flags);
+}
+
+void __cdecl game_state_call_before_save_procs(long game_state_proc_flags)
+{
+	INVOKE(0x0058A6A0, game_state_call_before_save_procs, game_state_proc_flags);
+}
+
+void __cdecl game_state_proc_globals_dispose()
+{
+	INVOKE(0x0058A760, game_state_proc_globals_dispose);
+}
+
+e_game_state_proc_state __cdecl game_state_proc_globals_get_state()
+{
+	return INVOKE(0x0058A770, game_state_proc_globals_get_state);
+
+	//return static_cast<e_game_state_proc_state>(g_game_state_proc_globals.state.peek());
+}
+
+void __cdecl game_state_proc_globals_initialize()
+{
+	INVOKE(0x0058A780, game_state_proc_globals_initialize);
+
+	//g_game_state_proc_globals.state = _game_state_proc_state_none;
+}
+
+//.text:015967E0 ; `dynamic initializer for 'g_game_state_proc_globals''
+//.text:015F9B70 ; `dynamic atexit destructor for 'g_game_state_proc_globals''
+
---
+++ game/source/saved_games/game_state_procs.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+#include "multithreading/synchronized_value.hpp"
+
+enum e_game_state_proc_state
+{
+	_game_state_proc_state_none = 0,
+	_game_state_proc_state_loading,
+	_game_state_proc_state_saving,
+
+	k_number_of_game_state_proc_states
+};
+
+struct s_game_state_proc_globals
+{
+	c_synchronized_long state; // e_game_state_proc_state
+	dword wait_for_render_thread;
+};
+static_assert(sizeof(s_game_state_proc_globals) == 0x8);
+
+extern s_game_state_proc_globals& g_game_state_proc_globals;
+
+extern void __cdecl game_state_call_after_load_procs(long game_state_proc_flags);
+extern void __cdecl game_state_call_after_save_procs(long game_state_proc_flags);
+extern void __cdecl game_state_call_before_load_procs(long game_state_proc_flags);
+extern void __cdecl game_state_call_before_save_procs(long game_state_proc_flags);
+extern e_game_state_proc_state __cdecl game_state_proc_globals_get_state();
+extern void __cdecl game_state_proc_globals_initialize();
+
