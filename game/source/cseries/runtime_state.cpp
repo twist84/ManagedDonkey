1af1d3e7941f9b04153b858550f0cc591b677019
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-11 19:51:21 +0000

    Added `g_runtime_state_allocation`

---
+++ game/source/cseries/runtime_state.cpp
@@ -0,0 +1,4 @@
+#include "cseries/runtime_state.hpp"
+
+REFERENCE_DECLARE(0x0189D394, c_allocation_base*, g_runtime_state_allocation);
+
---
+++ game/source/cseries/runtime_state.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+extern c_allocation_base*& g_runtime_state_allocation;
+
