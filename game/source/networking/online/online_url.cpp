a3e5e64c512e2b8affb0789ab957c52323edf36c
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-28 20:46:52 +0000

    Hooked `c_motd_popup_data_downloader ::get_data`

--- game/source/networking/online/online_url.cpp
+++ game/source/networking/online/online_url.cpp
@@ -17,6 +17,7 @@ void create_title_url_base(c_url_string* url)
 		url->m_string.set(k_title_url_base_override);
 
 	g_title_url_base_length = url->m_string.length();
+}
 
 struct c_motd_data_downloader :
 	public c_http_blf_simple_downloader<s_motd_data>
@@ -46,3 +47,45 @@ protected:
 HOOK_DECLARE_CLASS_MEMBER(0x00AEC340, c_motd_data_downloader, get_data);
 
 struct c_motd_popup_data_downloader :
+	public c_http_blf_simple_downloader<s_motd_popup_data>
+{
+public:
+	e_download_status __thiscall get_data(s_motd_popup_data** data, long* data_size)
+	{
+		static s_motd_popup_data message_of_the_day_popup{};
+
+		if (message_of_the_day_popup.title_index_identifier != 85839908)
+		{
+			message_of_the_day_popup.title_index_identifier = 85839908;
+			message_of_the_day_popup.button_key_wait_time = 5000;
+
+			ustrnzcpy(message_of_the_day_popup.title, L"Halo: Reach E3 Trailer!", NUMBEROF(message_of_the_day_popup.title));
+			message_of_the_day_popup.title_size = ustrnlen(message_of_the_day_popup.title, NUMBEROF(message_of_the_day_popup.title)) * 2;
+
+			ustrnzcpy(message_of_the_day_popup.header, L"Prepare for Launch.", NUMBEROF(message_of_the_day_popup.header));
+			message_of_the_day_popup.header_size = ustrnlen(message_of_the_day_popup.header, NUMBEROF(message_of_the_day_popup.header)) * 2;
+
+			ustrnzcpy(message_of_the_day_popup.button_key, L"Lift-off!", NUMBEROF(message_of_the_day_popup.button_key));
+			message_of_the_day_popup.button_key_size = ustrnlen(message_of_the_day_popup.button_key, NUMBEROF(message_of_the_day_popup.button_key)) * 2;
+
+			ustrnzcpy(message_of_the_day_popup.button_key_wait, L"T-minus 5...", NUMBEROF(message_of_the_day_popup.button_key_wait));
+			message_of_the_day_popup.button_key_wait_size = ustrnlen(message_of_the_day_popup.button_key_wait, NUMBEROF(message_of_the_day_popup.button_key_wait)) * 2;
+
+			ustrnzcpy(message_of_the_day_popup.message, L"Going where no man has gone before is standard operating procedure for Noble Team, but even they weren't prepared for Operation Uppercut. Backs against the wall and facing the full might of the Covenant invasion, Six must rely on his unique skill set and previous involvement in ONI's secret Sabre program to launch a surprise attack against the Covenant fleet in the last place they expect to be met by resistance. And since you weren't on hand to witness this trailer's maiden voyage at E3, Bungie brought back some footage so you can relive the launch sequence on Bungie.net and Marketplace.", NUMBEROF(message_of_the_day_popup.message));
+			message_of_the_day_popup.message_size = ustrnlen(message_of_the_day_popup.message, NUMBEROF(message_of_the_day_popup.message)) * 2;
+		}
+
+		if (data)
+			*data = &message_of_the_day_popup;
+
+		return _http_download_status_succeeded;
+	}
+
+protected:
+	long __unknown_index_or_count;
+	char const* m_chunk_buffer;
+	long m_chunk_buffer_size;
+};
+HOOK_DECLARE_CLASS_MEMBER(0x00AE74E0, c_motd_popup_data_downloader, get_data);
+DATA_PATCH_DECLARE2(0x00AE82C4, is_signed_in_to_live_test_jz, 2, { 0x74, 0x00 });
+
