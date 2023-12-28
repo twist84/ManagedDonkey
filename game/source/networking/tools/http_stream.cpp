ef82af1c63b1d45bd014e72b1fbb6c29968613e0
theTwister <6237734+twist84@users.noreply.github.com>
2023-12-28 12:48:16 +0000

    Updated `http_stream`

--- game/source/networking/tools/http_stream.cpp
+++ game/source/networking/tools/http_stream.cpp
@@ -1,6 +1,122 @@
 #include "networking/tools/http_stream.hpp"
 
+#include "tag_files/files_windows.hpp"
+
 #include <string>
+
+c_http_post_source::c_http_post_source() :
+	m_source_type(_contents_source_none),
+	m_content_type("text/plain"),
+	m_filename("blob"),
+	m_contents_buffer(NULL),
+	m_contents_file(NULL),
+	m_contents_length(0),
+	m_start_position(0),
+	m_is_json(false)
+{
+}
+
+void c_http_post_source::set_source_as_buffer(char const* contents_buffer, long contents_buffer_length)
+{
+	ASSERT(contents_buffer);
+
+	m_source_type = _contents_source_as_buffer;
+	m_contents_buffer = contents_buffer;
+	m_contents_length = static_cast<dword>(contents_buffer_length);
+}
+
+bool c_http_post_source::set_source_as_file(s_file_reference* contents_file)
+{
+	ASSERT(contents_file);
+
+	m_source_type = _contents_source_as_file;
+	m_contents_file = contents_file;
+	if (file_get_size(contents_file, &m_contents_length))
+	{
+		if (m_contents_length < 0)
+			ASSERT2("We shouldn't be uploading files larger than 2 GB anyway.");
+
+		return true;
+	}
+	return false;
+}
+
+void c_http_post_source::set_content_type(char const* content_type)
+{
+	ASSERT(content_type);
+	ASSERT(strlen(content_type) < k_content_type_length);
+
+	m_content_type.set(content_type);
+}
+
+void c_http_post_source::set_filename(char const* filename)
+{
+	ASSERT(filename);
+	ASSERT(strlen(filename) < FILENAME_MAX);
+
+	m_filename.set(filename);
+}
+
+void c_http_post_source::set_start_position(long start_position)
+{
+	ASSERT(start_position >= 0);
+
+	m_start_position = start_position;
+}
+
+c_http_post_source& c_http_post_source::operator=(c_http_post_source const& other)
+{
+	m_source_type = other.m_source_type;
+	m_content_type = other.m_content_type;
+	m_filename = other.m_filename;
+	m_contents_buffer = other.m_contents_buffer;
+	m_contents_file = other.m_contents_file;
+	m_contents_length = other.m_contents_length;
+	m_start_position = other.m_start_position;
+	m_is_json = other.m_is_json;
+
+	return *this;
+}
+
+char const* c_http_post_source::get_content_type() const
+{
+	return m_content_type.get_string();
+}
+
+char const* c_http_post_source::get_filename() const
+{
+	return m_filename.get_string();
+}
+
+long c_http_post_source::get_contents_length() const
+{
+	return m_contents_length;
+}
+
+c_http_post_source::e_contents_source c_http_post_source::get_source_type() const
+{
+	return m_source_type;
+}
+
+char const* c_http_post_source::get_contents_buffer() const
+{
+	return m_contents_buffer;
+}
+
+s_file_reference* c_http_post_source::get_contents_file() const
+{
+	return m_contents_file;
+}
+
+long c_http_post_source::get_start_position() const
+{
+	return m_start_position;
+}
+
+bool c_http_post_source::is_json() const
+{
+	return m_is_json;
+}
 
 c_http_stream::c_http_stream() :
 	m_url(),
@@ -66,7 +182,7 @@ void c_http_get_stream::build_headers()
 	m_headers_length = m_headers.length();
 }
 
-bool c_http_get_stream::verify_necessary_state_is_set()
+bool c_http_get_stream::verify_nescessary_state_is_set()
 {
 	return m_url.length() != 0;
 }
@@ -82,7 +198,7 @@ bool c_http_get_stream::read(char* buffer, long buffer_length, long* bytes_read)
 	long dest_buffer_length = buffer_length;
 	bool success = false;
 
-	if (verify_necessary_state_is_set())
+	if (verify_nescessary_state_is_set())
 	{
 		if (!m_position)
 			build_headers();
@@ -134,61 +250,160 @@ bool c_http_get_stream::at_end()
 
 c_http_post_stream::c_http_post_stream() :
 	c_http_stream(),
-	__stringC48(),
-	__stringD48(),
+	m_mime_header(),
+	m_mime_footer(),
 	m_extra_headers_length(),
-	__stringC48_length(),
-	__stringD48_length()
+	m_mime_header_length(),
+	m_mime_footer_length()
 {
+}
+
+void c_http_post_stream::set_source(c_http_post_source const* source)
+{
+	//DECLFUNC(0x00432D90, void, __thiscall, c_http_post_stream*, c_http_post_source const*)(this, source);
+
+	m_post_source = *source;
+	reset();
 }
 
 void c_http_post_stream::build_headers()
 {
 	//DECLFUNC(0x00432720, void, __thiscall, c_http_post_stream*)(this);
 
-	if (m_post_source.__unknown138)
+	// added for HO
+	if (m_post_source.is_json())
 	{
-		__stringD48.set("\r\n");
-		__stringD48_length = 2;
+		m_mime_footer.set("\r\n");
+		m_mime_footer_length = m_mime_footer.length();
 
-		__stringC48.set("\r\n");
-		__stringC48_length = 2;
+		m_mime_header.set("\r\n");
+		m_mime_header_length = m_mime_header.length();
 
 		m_headers.print("POST %s HTTP/1.0\r\nContent-Type: application/json\r\nContent-Length: %d\r\n%s\r\n",
 			m_url.get_string(),
-			m_post_source.m_contents_length - m_post_source.m_start_position + __stringD48_length + 2,
+			m_post_source.get_contents_length() - m_post_source.get_start_position() + m_mime_footer_length + 2,
 			m_extra_headers.get_string());
-
 		m_extra_headers_length = m_headers.length();
 	}
 	else
 	{
-		__stringD48.set("\r\n--BUNGIEr0x0rz--\r\n");
-		__stringD48_length = __stringD48.length();
+		m_mime_footer.set("\r\n--BUNGIEr0x0rz--\r\n");
+		m_mime_footer_length = m_mime_footer.length();
 
-		__stringC48.print("--BUNGIEr0x0rz\r\nContent-Disposition: form-data; name=\"upload\"; filename=\"%s\"\r\nContent-Type: %s\r\n\r\n", m_post_source.m_filename.get_string(), m_post_source.m_content_type.get_string());
-		__stringC48_length = __stringC48.length();
+		m_mime_header.print("--BUNGIEr0x0rz\r\nContent-Disposition: form-data; name=\"upload\"; filename=\"%s\"\r\nContent-Type: %s\r\n\r\n",
+			m_post_source.get_filename(),
+			m_post_source.get_content_type());
+		m_mime_header_length = m_mime_header.length();
 
 		m_headers.print("POST %s HTTP/1.0\r\nContent-type: multipart/form-data; boundary=BUNGIEr0x0rz\r\nContent-Length: %d\r\n%s\r\n",
 			m_url.get_string(),
-			__stringC48_length + __stringD48_length + m_post_source.m_contents_length - m_post_source.m_start_position,
+			m_mime_header_length + m_mime_footer_length + m_post_source.get_contents_length() - m_post_source.get_start_position(),
 			m_extra_headers.get_string());
-
 		m_extra_headers_length = m_headers.length();
 	}
 }
 
-bool c_http_post_stream::verify_necessary_state_is_set()
+bool c_http_post_stream::verify_nescessary_state_is_set()
 {
 	return DECLFUNC(0x00433000, bool, __thiscall, c_http_post_stream*)(this);
+
+	//byte necessary_state_is_set = m_post_source.get_source_type() == c_http_post_source::_contents_source_as_file
+	//	|| m_post_source.get_source_type() == c_http_post_source::_contents_source_as_buffer;
+	//return necessary_state_is_set & (m_url.length() != 0);
 }
 
 bool c_http_post_stream::read(char* buffer, long buffer_length, long* bytes_read)
 {
-	ASSERT(buffer);
-	ASSERT(bytes_read);
-
 	return DECLFUNC(0x00432AD0, bool, __thiscall, c_http_post_stream*, char*, long, long*)(this, buffer, buffer_length, bytes_read);
+
+	//ASSERT(buffer);
+	//ASSERT(bytes_read);
+	//
+	//char* buf = buffer;
+	//long len = buffer_length;
+	//
+	//bool success = false;
+	//
+	//if (verify_nescessary_state_is_set())
+	//{
+	//	if (!get_position())
+	//		build_headers();
+	//
+	//	success = true;
+	//	while (!at_end() && len > 0)
+	//	{
+	//		long position = get_position();
+	//		if (position >= m_extra_headers_length)
+	//		{
+	//			position -= m_extra_headers_length;
+	//
+	//			if (position >= m_mime_header_length)
+	//			{
+	//				position -= m_mime_header_length;
+	//
+	//				if (position >= m_post_source.get_contents_length())
+	//				{
+	//					position -= m_post_source.get_contents_length();
+	//
+	//					if (position >= m_mime_footer_length)
+	//					{
+	//						if (!at_end())
+	//							ASSERT2("c_http_stream::read: Read past the maximum length of the stream.");
+	//
+	//						success = false;
+	//						break;
+	//					}
+	//
+	//					if (len > m_mime_footer_length - position)
+	//						len = m_mime_footer_length - position;
+	//
+	//					csmemcpy(buf, &m_mime_footer.get_string()[position], len);
+	//				}
+	//				else
+	//				{
+	//					if (len > m_post_source.get_contents_length() - position)
+	//						len = m_post_source.get_contents_length() - position;
+	//
+	//					switch (m_post_source.get_source_type())
+	//					{
+	//					case c_http_post_source::_contents_source_as_buffer:
+	//					{
+	//						csmemcpy(buf, &m_post_source.get_contents_buffer()[m_post_source.get_start_position()], len);
+	//					}
+	//					break;
+	//					case c_http_post_source::_contents_source_as_file:
+	//					{
+	//						position += m_post_source.get_start_position();
+	//						if (!file_read_from_position(m_post_source.get_contents_file(), position, len, false, buf))
+	//							success = false;
+	//					}
+	//					break;
+	//					}
+	//				}
+	//			}
+	//			else
+	//			{
+	//				if (len > m_mime_header_length - position)
+	//					len = m_mime_header_length - position;
+	//				csmemcpy(buf, &m_mime_header.get_string()[position], len);
+	//			}
+	//		}
+	//		else
+	//		{
+	//			if (len > m_extra_headers_length - position)
+	//				len = m_extra_headers_length - position;
+	//			csmemcpy(buf, &m_headers.get_string()[position], len);
+	//		}
+	//
+	//		buf += len;
+	//		m_position += len;
+	//		len = buffer_length - (buf - buffer);
+	//	}
+	//
+	//	*bytes_read = buf - buffer;
+	//}
+	//
+	//return success;
 }
 
 long c_http_post_stream::get_length()
@@ -196,9 +411,9 @@ long c_http_post_stream::get_length()
 	//return DECLFUNC(0x004328F0, long, __thiscall, c_http_post_stream*)(this);
 
 	return m_extra_headers_length
-		+ __stringC48_length
-		+ __stringD48_length
-		+ m_post_source.m_contents_length - m_post_source.m_start_position;
+		+ m_mime_header_length
+		+ m_mime_footer_length
+		+ m_post_source.get_contents_length() - m_post_source.get_start_position();
 }
 
 bool c_http_post_stream::at_end()
--- game/source/networking/tools/http_stream.hpp
+++ game/source/networking/tools/http_stream.hpp
@@ -2,35 +2,70 @@
 
 #include "cseries/cseries.hpp"
 
-const long k_url_size = 256;
+long const k_url_size = 256;
 
 struct s_file_reference;
 
 struct c_http_post_source
 {
-	long m_source_type;
-	c_static_string<32> m_content_type;
+private:
+	static long const k_content_type_length = 32;
+
+public: 
+	enum e_contents_source
+	{
+		_contents_source_none,
+		_contents_source_as_buffer,
+		_contents_source_as_file,
+
+		k_contents_source_count
+	};
+
+	c_http_post_source();
+
+	void set_source_as_buffer(char const* contents_buffer, long contents_buffer_length);
+	bool set_source_as_file(s_file_reference* contents_file);
+	void set_content_type(char const* content_type);
+	void set_filename(char const* filename);
+	void set_start_position(long start_position);
+
+	c_http_post_source& operator=(c_http_post_source const& other);
+
+	char const* get_content_type() const;
+	char const* get_filename() const;
+	long get_contents_length() const;
+	e_contents_source get_source_type() const;
+	char const* get_contents_buffer() const;
+	s_file_reference* get_contents_file() const;
+	long get_start_position() const;
+	bool is_json() const;
+
+protected:
+	c_enum<e_contents_source, long, _contents_source_none, k_contents_source_count> m_source_type;
+	c_static_string<k_content_type_length> m_content_type;
 	c_static_string<260> m_filename;
 	char const* m_contents_buffer;
 	s_file_reference* m_contents_file;
-	long m_contents_length;
+	dword m_contents_length;
 	long m_start_position;
-	bool __unknown138;
-	byte __pad139[3];
+
+	// Halo Online
+	bool m_is_json;
 };
 static_assert(sizeof(c_http_post_source) == 0x13C);
 
 struct c_http_stream
 {
 protected:
-	static long const k_http_header_size = 1024;
+	static long const k_http_header_size = 1536;
+	static long const k_extra_headers_size = 1024;
 
 public:
 	c_http_stream();
 
 protected:
 	virtual void build_headers() = 0;
-	virtual bool verify_necessary_state_is_set() = 0;
+	virtual bool verify_nescessary_state_is_set() = 0;
 
 public:
 	virtual bool read(char* buffer, long buffer_length, long* bytes_read) = 0;
@@ -48,21 +83,22 @@ public:
 
 protected:
 	c_static_string<k_url_size> m_url;
-	c_static_string<1536> m_headers;
+	c_static_string<k_http_header_size> m_headers;
 	long m_headers_length;
-	c_static_string<1024> m_extra_headers;
+	c_static_string<k_extra_headers_size> m_extra_headers;
 	long m_position;
 };
 static_assert(sizeof(c_http_stream) == 0xB0C);
 
-struct c_http_get_stream : c_http_stream
+struct c_http_get_stream :
+	public c_http_stream
 {
 public:
 	c_http_get_stream();
 
 protected:
 	virtual void build_headers() override;
-	virtual bool verify_necessary_state_is_set() override;
+	virtual bool verify_nescessary_state_is_set() override;
 
 public:
 	virtual bool read(char* buffer, long buffer_length, long* bytes_read) override;
@@ -71,14 +107,21 @@ public:
 };
 static_assert(sizeof(c_http_get_stream) == sizeof(c_http_stream));
 
-struct c_http_post_stream : c_http_stream
+struct c_http_post_stream :
+	public c_http_stream
 {
+private:
+	static long const k_mime_header_size = 256;
+	static long const k_mime_footer_size = 128;
+
 public:
 	c_http_post_stream();
 
+	void set_source(c_http_post_source const* source);
+
 protected:
 	virtual void build_headers() override;
-	virtual bool verify_necessary_state_is_set() override;
+	virtual bool verify_nescessary_state_is_set() override;
 
 public:
 	virtual bool read(char* buffer, long buffer_length, long* bytes_read) override;
@@ -87,11 +130,11 @@ public:
 
 protected:
 	c_http_post_source m_post_source;
-	c_static_string<256> __stringC48;
-	c_static_string<128> __stringD48;
+	c_static_string<k_mime_header_size> m_mime_header;
+	c_static_string<k_mime_footer_size> m_mime_footer;
 	long m_extra_headers_length;
-	long __stringC48_length;
-	long __stringD48_length;
+	long m_mime_header_length;
+	long m_mime_footer_length;
 };
 static_assert(sizeof(c_http_post_stream) == 0xDD4);
 
