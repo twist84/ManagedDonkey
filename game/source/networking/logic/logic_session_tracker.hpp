4a4e73b8b4f3aab1d064134c3804f109954afb55
theTwister <6237734+twist84@users.noreply.github.com>
2024-01-05 17:28:59 +0000

    Added `s_network_session_tracker_session_data`

--- game/source/networking/logic/logic_session_tracker.hpp
+++ game/source/networking/logic/logic_session_tracker.hpp
@@ -1,2 +1,13 @@
 #pragma once
 
+#include "networking/messages/network_messages_out_of_band.hpp"
+
+struct s_network_session_tracker_session_data
+{
+	char name[16];
+	s_transport_session_description description;
+	byte __data40[0x20];
+	s_network_session_status_data status_data;
+};
+static_assert(sizeof(s_network_session_tracker_session_data) == 0x16520);
+
