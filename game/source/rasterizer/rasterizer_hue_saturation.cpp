5d272ee3986ed0f198aa428c69a3d3d5ac3e2480
theTwister <6237734+twist84@users.noreply.github.com>
2024-01-15 16:39:38 +0000

    Mapped `c_hue_saturation_control`

---
+++ game/source/rasterizer/rasterizer_hue_saturation.cpp
@@ -0,0 +1,64 @@
+#include "rasterizer/rasterizer_hue_saturation.hpp"
+
+#include "memory/module.hpp"
+#include "memory/thread_local.hpp"
+
+//REFERENCE_DECLARE(0x01914A7C, c_hue_saturation_control, x_default);
+//REFERENCE_DECLARE(0x050FB650, c_hue_saturation_control, x_exposed_to_hs);
+
+REFERENCE_DECLARE(0x01914A7C, c_hue_saturation_control, g_hue_saturation_control_default);
+REFERENCE_DECLARE(0x050FB650, c_hue_saturation_control, g_hue_saturation_control_exposed_to_hs);
+
+HOOK_DECLARE(0x00A41130, render_postprocess_reset_color_tweaking);
+
+//c_hue_saturation_control c_hue_saturation_control::x_default
+//{
+//	.m_hue = 0.0f,
+//	.m_saturation = 1.0f,
+//	.m_filter_red = 1.0f,
+//	.m_filter_green = 1.0f,
+//	.m_filter_blue = 1.0f
+//};
+
+//c_hue_saturation_control c_hue_saturation_control::x_exposed_to_hs = c_hue_saturation_control::x_default;
+
+void __cdecl c_hue_saturation_control::copy_from_gamestate()
+{
+	//INVOKE(0x00530470, copy_from_gamestate);
+
+	TLS_DATA_GET_VALUE_REFERENCE(g_hue_saturation_control_in_gamestate);
+
+	//c_hue_saturation_control::x_exposed_to_hs = *c_hue_saturation_control::x_in_gamestate;
+	g_hue_saturation_control_exposed_to_hs = *g_hue_saturation_control_in_gamestate;
+}
+
+void __cdecl c_hue_saturation_control::copy_to_gamestate()
+{
+	//INVOKE(0x005304B0, copy_from_gamestate);
+
+	TLS_DATA_GET_VALUE_REFERENCE(g_hue_saturation_control_in_gamestate);
+
+	//*c_hue_saturation_control::x_in_gamestate = c_hue_saturation_control::x_exposed_to_hs;
+	*g_hue_saturation_control_in_gamestate = g_hue_saturation_control_exposed_to_hs;
+}
+
+void __cdecl render_postprocess_reset_color_tweaking()
+{
+	//INVOKE(0x00A41130, render_postprocess_reset_color_tweaking);
+
+	g_hue_saturation_control_exposed_to_hs.reset();
+}
+
+void c_hue_saturation_control::reset()
+{
+	//DECLFUNC(0x00A41170, void, __thiscall, c_hue_saturation_control*)(this);
+
+	m_hue = 0.0f;
+	m_saturation = 1.0f;
+	m_filter_red = 1.0f;
+	m_filter_green = 1.0f;
+	m_filter_blue = 1.0f;
+}
+
+//.text:00A411A0 ; c_hue_saturation_control::set_hue_saturaton_and_color_filters, #TODO: fix __usercall calling convention
+
--- game/source/rasterizer/rasterizer_hue_saturation.hpp
+++ game/source/rasterizer/rasterizer_hue_saturation.hpp
@@ -2,13 +2,29 @@
 
 #include "cseries/cseries.hpp"
 
+struct real_linear_rgb_color;
 struct c_hue_saturation_control
 {
-	dword graphics_override;
-	real saturation;
-	real color;
-	real __unknownC;
-	real __unknown10;
+public:
+	static void __cdecl copy_from_gamestate();
+	static void __cdecl copy_to_gamestate();
+
+	void reset();
+
+	//static c_hue_saturation_control& x_default;
+	//static c_hue_saturation_control& x_exposed_to_hs;
+	//thread_local c_hue_saturation_control* x_in_gamestate;
+
+	real m_hue;
+	real m_saturation;
+	real m_filter_red;
+	real m_filter_green;
+	real m_filter_blue;
 };
 static_assert(sizeof(c_hue_saturation_control) == 0x14);
+
+extern c_hue_saturation_control& g_hue_saturation_control_default;
+extern c_hue_saturation_control& g_hue_saturation_control_exposed_to_hs;
+
+extern void __cdecl render_postprocess_reset_color_tweaking();
 
