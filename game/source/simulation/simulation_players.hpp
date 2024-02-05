b5c325b0b4a64ec806f7a07114b511c501d33b71
theTwister <6237734+twist84@users.noreply.github.com>
2024-02-05 16:01:59 +0000

    Mapped `c_simulation_watcher ` WIP

--- game/source/simulation/simulation_players.hpp
+++ game/source/simulation/simulation_players.hpp
@@ -4,6 +4,36 @@
 #include "game/player_control.hpp"
 #include "game/players.hpp"
 
+enum e_simulation_player_update_type
+{
+	_simulation_player_update_type_left_game = 0,
+	_simulation_player_update_type_swap,
+	_simulation_player_update_type_remove,
+	_simulation_player_update_type_added,
+	_simulation_player_update_type_configuration,
+
+	k_simulation_player_update_type_count
+};
+
+// generated based on `e_simulation_player_update_type`
+struct s_player_collection_player
+{
+	s_player_identifier identifier;
+	bool left_game;
+	dword left_game_time;
+	s_machine_identifier machine_identifier;
+	s_player_configuration configuration;
+};
+static_assert(sizeof(s_player_collection_player) == 0x1640);
+
+struct s_player_collection
+{
+	dword player_valid_mask;
+	dword __unknown4;
+	s_player_collection_player collection_players[16];
+};
+static_assert(sizeof(s_player_collection) == 0x16408);
+
 struct c_simulation_world;
 struct c_simulation_player
 {
--- game/source/simulation/simulation_watcher.hpp
+++ game/source/simulation/simulation_watcher.hpp
@@ -1,10 +1,33 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "game/players.hpp"
+#include "networking/session/network_session.hpp"
+#include "simulation/simulation_players.hpp"
 
-struct c_simulation_watcher
+struct c_simulation_world;
+struct c_network_observer;
+struct c_network_session;
+struct c_simulation_watcher :
+	public c_network_channel_owner
 {
-	byte __data[0x16658];
+	c_simulation_world* m_world;
+	c_network_observer* m_observer;
+	c_network_session* m_session;
+	dword m_session_local_update_number;
+	dword m_machine_last_membership_update_number;
+	dword __unknown18;
+	dword m_local_machine_valid_mask;
+	dword m_local_machine_index;
+	s_machine_identifier m_local_machine_identifiers[17];
+	bool __unknown134;
+	s_player_collection player_collection;
+	dword m_machine_valid_mask;
+	s_machine_identifier m_machine_identifiers[17];
+	bool __unknown16654;
+	bool m_game_reverted;
+	bool __unknown16656;
+	bool __unknown16657;
 };
 static_assert(sizeof(c_simulation_watcher) == 0x16658);
 
