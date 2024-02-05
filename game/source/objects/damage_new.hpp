06cdc35a11cf8e6eac5db07e8d451bcfa9f748c3
theTwister <6237734+twist84@users.noreply.github.com>
2024-02-05 15:55:35 +0000

    Mapped `s_damage_globals`

--- game/source/objects/damage_new.hpp
+++ game/source/objects/damage_new.hpp
@@ -4,7 +4,20 @@
 
 struct s_damage_globals
 {
-	byte __data[0x810];
+	struct s_damage_acceleration
+	{
+		real_point3d point;
+		vector3d acceleration;
+		long object_index;
+		short node_index;
+		word_flags flags;
+	};
+	static_assert(sizeof(s_damage_acceleration) == 0x20);
+
+	long damage_acceleration_count;
+	c_static_array<s_damage_acceleration, 64> damage_accelerations;
+	c_static_flags<64> damage_accelerations_evictable;
+	bool damage_acceleration_queue_active;
 };
 static_assert(sizeof(s_damage_globals) == 0x810);
 
