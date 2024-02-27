49811900b16c4693e9bde8f14653708b51cb6e85
theTwister <6237734+twist84@users.noreply.github.com>
2024-02-27 12:43:36 +0000

    Implemented `cheat_chevy`

--- game/source/objects/damage.cpp
+++ game/source/objects/damage.cpp
@@ -1,5 +1,11 @@
 #include "objects/damage.hpp"
 
+#include "game/cheats.hpp"
+#include "memory/module.hpp"
+
+HOOK_DECLARE_CALL(0x00B4F3FC, compute_total_damage);
+HOOK_DECLARE_CALL(0x00B53410, compute_total_damage);
+
 bool debug_damage_radius = false;
 bool debug_damage_this_event = false;
 bool debug_damage_verbose = false;
@@ -18,4 +24,14 @@ void render_debug_object_damage()
 		// #TODO: implement
 	}
 }
+
+real __cdecl compute_total_damage(void* damage_data, void* damage_effect_definition, void const* damage_definition, long a4, bool* a5)
+{
+	real result = INVOKE(0x00B4FB10, compute_total_damage, damage_data, damage_effect_definition, damage_definition, a4, a5);
+
+	if (cheat.chevy)
+		result = 0.0f;
+
+	return result;
+};
 
--- game/source/objects/damage.hpp
+++ game/source/objects/damage.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
+
 extern bool debug_damage_radius;
 extern bool debug_damage_this_event;
 extern bool debug_damage_verbose;
@@ -7,4 +9,5 @@ extern bool debug_player_damage;
 extern bool debug_damage;
 
 extern void render_debug_object_damage();
+extern real __cdecl compute_total_damage(void* damage_data, void* damage_effect_definition, void const* damage_definition, long a4, bool* a5);
 
