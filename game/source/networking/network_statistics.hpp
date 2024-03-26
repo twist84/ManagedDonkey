89e41b47c7c7660cd8aa7e86f5ae118f8468f66e
theTwister <6237734+twist84@users.noreply.github.com>
2024-03-26 07:27:11 +0000

    Updated `network_statistics`

--- game/source/networking/network_statistics.hpp
+++ game/source/networking/network_statistics.hpp
@@ -2,38 +2,65 @@
 
 #include "cseries/cseries.hpp"
 
-#pragma pack(push, 1)
 struct c_network_time_statistics
 {
-	dword __unknown0;
-	dword __unknown4;
-	dword __unknown8;
-	dword __unknownC;
-	dword __unknown10;
-	qword __unknown14;
-	dword m_period_duration_msec;
-	dword m_interval_duration_msec;
-	real __unknown24;
-	dword m_next_interval_index;
-	byte __data2C[160];
-	qword __unknownCC;
-	byte __dataD4[4];
+private:
+	static long const k_network_statistics_interval_count = 20;
+
+public:
+
+	struct s_interval // #TODO: find a better name?
+	{
+		// names based on `m_stored_total` asserts
+		// the struct for `m_intervals` could be different?
+
+		long events;
+		long total_values;
+	};
+	static_assert(sizeof(s_interval) == 0x8);
+
+	qword m_total_events;
+	qword m_total_values;
+
+	long __time10;
+	s_interval __unknown14;
+
+	long m_period_duration_msec;
+	long m_interval_duration_msec;
+	real m_period_duration;
+	long m_next_interval_index;
+	s_interval m_intervals[k_network_statistics_interval_count]; // #TODO: find a better name?
+	s_interval m_stored_total;
 };
 static_assert(sizeof(c_network_time_statistics) == 0xD8);
-#pragma pack(pop)
 
 struct c_network_window_statistics
 {
+private:
+	static long const k_network_statistics_maximum_window_size = 32;
+
+public:
+
+	struct s_window_entry // #TODO: find a better name?
+	{
+		// m_window_entries[this->m_window_next_entry].add_time = network_time_get();
+		long add_time;
+
+		// m_window_entries[m_window_next_entry].value = value;
+		long value;
+	};
+	static_assert(sizeof(s_window_entry) == 0x8);
+
 	long m_window_size;
 	long m_window_next_entry;
+	s_window_entry m_window_entries[k_network_statistics_maximum_window_size];
 
-	struct
-	{
-		byte __data[8];
-	} m_window_entries[32];
-
+	// m_window_total_values -= m_window_entries[m_window_next_entry].value;
+	// m_window_total_values += value;
 	long m_window_total_values;
+
+	// m_window_aperture_msec = network_time_difference_msec(m_window_entries[m_window_next_entry].add_time, network_time_get());
 	long m_window_aperture_msec;
 };
-static_assert(sizeof(c_network_time_statistics) == 0xD8);
+static_assert(sizeof(c_network_window_statistics) == 0x110);
 
