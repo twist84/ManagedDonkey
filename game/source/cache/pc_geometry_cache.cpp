228ba30c299f0c9f0bbaa495838b0d575041e4e3
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-13 14:58:05 +0100

    Added `geometry_cache` functions

--- game/source/cache/pc_geometry_cache.cpp
+++ game/source/cache/pc_geometry_cache.cpp
@@ -2,16 +2,29 @@
 
 #include "cseries/cseries.hpp"
 
+void __cdecl geometry_cache_close()
+{
+	INVOKE(0x00618CC0, geometry_cache_close);
+}
+
+long __cdecl geometry_cache_get_structure_block_pending_count()
+{
+	return INVOKE(0x00618D20, geometry_cache_get_structure_block_pending_count);
+}
+
 bool __cdecl geometry_cache_is_blocking()
 {
 	return INVOKE(0x00618D50, geometry_cache_is_blocking);
-
-	return false;
 }
 
 void __cdecl geometry_cache_open()
 {
 	INVOKE(0x00618D90, geometry_cache_open);
+}
+
+void __cdecl geometry_cache_update_for_render()
+{
+	INVOKE(0x00618DC0, geometry_cache_update_for_render);
 }
 
 void __cdecl geometry_cache_debug_render()
--- game/source/cache/pc_geometry_cache.hpp
+++ game/source/cache/pc_geometry_cache.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+extern void __cdecl geometry_cache_close();
+extern long __cdecl geometry_cache_get_structure_block_pending_count();
 extern bool __cdecl geometry_cache_is_blocking();
 extern void __cdecl geometry_cache_open();
+extern void __cdecl geometry_cache_update_for_render();
 extern void __cdecl geometry_cache_debug_render();
 
