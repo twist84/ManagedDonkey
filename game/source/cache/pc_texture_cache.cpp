fb0f8f5669744e0e0f0c4a4839990829cc46bd76
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-13 14:59:42 +0100

    Added `texture_cache` functions

--- game/source/cache/pc_texture_cache.cpp
+++ game/source/cache/pc_texture_cache.cpp
@@ -2,16 +2,37 @@
 
 #include "cseries/cseries.hpp"
 
+//.text:00618B30 ; bool __cdecl texture_cache_bitmap_new(long, bitmap_data*)
+//.text:00618B70 ; void __cdecl texture_cache_block_for_one_frame(e_texture_cache_block_type)
+
+void __cdecl texture_cache_close()
+{
+	INVOKE(0x00618B80, texture_cache_close);
+}
+
+void __cdecl texture_cache_close_pregame()
+{
+	INVOKE(0x00618B90, texture_cache_close_pregame);
+}
+
 bool __cdecl texture_cache_is_blocking()
 {
-	//return INVOKE(0x00618BF0, texture_cache_is_blocking);
-
-	return false;
+	return INVOKE(0x00618BF0, texture_cache_is_blocking);
 }
 
 void __cdecl texture_cache_open()
 {
 	INVOKE(0x00618C20, texture_cache_open);
+}
+
+void __cdecl texture_cache_open_pregame()
+{
+	INVOKE(0x00618C30, texture_cache_open_pregame);
+}
+
+void __cdecl texture_cache_update_for_render()
+{
+	INVOKE(0x00618C60, texture_cache_update_for_render);
 }
 
 void __cdecl texture_cache_debug_render()
--- game/source/cache/pc_texture_cache.hpp
+++ game/source/cache/pc_texture_cache.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+extern void __cdecl texture_cache_close();
+extern void __cdecl texture_cache_close_pregame();
 extern bool __cdecl texture_cache_is_blocking();
 extern void __cdecl texture_cache_open();
+extern void __cdecl texture_cache_open_pregame();
+extern void __cdecl texture_cache_update_for_render();
 extern void __cdecl texture_cache_debug_render();
 
