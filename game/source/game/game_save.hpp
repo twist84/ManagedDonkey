903cf49891b0e104de8367930578a3a0a6c3f7c9
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-20 22:37:10 +0100

    Updated `s_game_save_globals`

--- game/source/game/game_save.hpp
+++ game/source/game/game_save.hpp
@@ -2,9 +2,34 @@
 
 #include "cseries/cseries.hpp"
 
+enum e_game_save_type
+{
+	_game_save_type_cancel = 0,
+	_game_save_type_safe,
+	_game_save_type_no_timeout,
+	_game_save_type_immediate,
+	_game_save_type_cinematic_skip,
+
+	k_game_save_type_count
+};
+
 struct s_game_save_globals
 {
-	byte __data[0x18];
+	// e_game_save_type
+	long game_save_type;
+
+	// timeout?
+	long __unknown4;
+
+	// timeout?
+	long __unknown8;
+
+	// attempt count?
+	long __unknownC;
+
+	long time_of_last_game_save;
+
+	long unsafe_object_index;
 };
 static_assert(sizeof(s_game_save_globals) == 0x18);
 
