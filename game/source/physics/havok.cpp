c7b5b5586fdd6f57888bca811bb91ef62706df72
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-22 13:32:55 +0100

    Updated `havok` code

--- game/source/physics/havok.cpp
+++ game/source/physics/havok.cpp
@@ -28,99 +28,103 @@ void __cdecl havok_can_modify_state_disallow()
 
 void __cdecl havok_display_stats(bool display_as_event)
 {
-    INVOKE(0x005C4FC0, havok_display_stats, display_as_event);
+	INVOKE(0x005C4FC0, havok_display_stats, display_as_event);
 }
 
 void __cdecl havok_display_stats_printf(bool display_as_event, real_argb_color const* color, char const* format, ...)
 {
-    va_list list;
-    va_start(list, format);
+	va_list list;
+	va_start(list, format);
 
-    char buffer[260]{};
-    cvsnzprintf(buffer, 255, format, list);
+	char buffer[260]{};
+	cvsnzprintf(buffer, 255, format, list);
 
-    // missing in Halo Online
-    if (display_as_event)
-    {
-        generate_event(_event_level_warning, "%s", buffer);
-    }
-    else
-    {
-        terminal_printf(NULL, "%s", buffer);
-    }
+	// missing in Halo Online
+	if (display_as_event)
+	{
+		generate_event(_event_level_warning, "%s", buffer);
+	}
+	else
+	{
+		terminal_printf(NULL, "%s", buffer);
+	}
 }
 
 void __cdecl havok_prepare_fpu_for_update()
 {
-    INVOKE(0x005C7890, havok_prepare_fpu_for_update);
+	INVOKE(0x005C7890, havok_prepare_fpu_for_update);
 
-    //TLS_DATA_GET_VALUE_REFERENCE(havok_style_fpu_exceptions_count);
-    //ASSERT(havok_style_fpu_exceptions_count >= 0);
-    //
-    //if (!havok_style_fpu_exceptions_count)
-    //    //_controlfp(_MCW_EM | _PC_53, _MCW_EM);
-    //    _controlfp(0x9001F, 0x8001F);
-    //
-    //havok_style_fpu_exceptions_count++;
+	//TLS_DATA_GET_VALUE_REFERENCE(havok_style_fpu_exceptions_count);
+	//ASSERT(havok_style_fpu_exceptions_count >= 0);
+	//
+	//if (!havok_style_fpu_exceptions_count)
+	//    //_controlfp(_MCW_EM | _PC_53, _MCW_EM);
+	//    _controlfp(0x9001F, 0x8001F);
+	//
+	//havok_style_fpu_exceptions_count++;
 }
 
 void __cdecl havok_restore_fpu_from_update()
 {
-    INVOKE(0x005C7A30, havok_restore_fpu_from_update);
+	INVOKE(0x005C7A30, havok_restore_fpu_from_update);
 
-    //TLS_DATA_GET_VALUE_REFERENCE(havok_style_fpu_exceptions_count);
-    //ASSERT(havok_style_fpu_exceptions_count > 0);
-    //
-    //if (!--havok_style_fpu_exceptions_count)
-    //{
-    //    _clearfp();
-    //    real_math_reset_precision();
-    //}
+	//TLS_DATA_GET_VALUE_REFERENCE(havok_style_fpu_exceptions_count);
+	//ASSERT(havok_style_fpu_exceptions_count > 0);
+	//
+	//if (!--havok_style_fpu_exceptions_count)
+	//{
+	//    _clearfp();
+	//    real_math_reset_precision();
+	//}
 }
 
 s_havok_constants const* get_havok_constants()
 {
-    return &g_havok_constants;
+	return &g_havok_constants;
 }
 
 void havok_debug_render()
 {
-    bool set_single_thread_request_flag = debug_objects
-        && (debug_objects_physics_models
-            || debug_objects_water_physics
-            || debug_objects_contact_points
-            || debug_objects_constraints
-            || debug_objects_vehicle_physics
-            || debug_objects_mass)
-        || get_havok_constants()->display_stats
-        || get_havok_constants()->impacts_debug
-        || get_havok_constants()->proxies_debug
-        || get_havok_constants()->collision_damage_debug
-        || get_havok_constants()->physics_debug;
-    
-    main_set_single_thread_request_flag(1, set_single_thread_request_flag);
-    if (restricted_region_locked_for_current_thread(k_game_state_update_region))
-    {
-        havok_prepare_fpu_for_update();
-        
-        if (get_havok_constants()->display_stats)
-            havok_display_stats(false);
-        
-        //if (get_havok_constants()->impacts_debug)
-        //    impacts_render_debug(); // #TODO: implement
-        //
-        //if (get_havok_constants()->proxies_debug)
-        //    havok_proxies_render_debug(); // #TODO: implement
-        //
-        //if (get_havok_constants()->collision_damage_debug)
-        //    havok_collision_damage_render_debug(); // #TODO: implement
-        //
-        //if (get_havok_constants()->physics_debug)
-        //{
-        //    // #TODO: implement
-        //}
-        
-        havok_restore_fpu_from_update();
-    }
+	return;
+
+	bool set_single_thread_request_flag = debug_objects
+		&& (debug_objects_physics_models
+			|| debug_objects_water_physics
+			|| debug_objects_contact_points
+			|| debug_objects_constraints
+			|| debug_objects_vehicle_physics
+			|| debug_objects_mass)
+		|| get_havok_constants()->display_stats
+		|| get_havok_constants()->impacts_debug
+		|| get_havok_constants()->proxies_debug
+		|| get_havok_constants()->collision_damage_debug
+		|| get_havok_constants()->physics_debug;
+
+	// this will freeze comment for now
+	//main_set_single_thread_request_flag(1, set_single_thread_request_flag);
+
+	if (restricted_region_locked_for_current_thread(k_game_state_update_region))
+	{
+		havok_prepare_fpu_for_update();
+
+		if (get_havok_constants()->display_stats)
+			havok_display_stats(false);
+
+		//if (get_havok_constants()->impacts_debug)
+		//    impacts_render_debug(); // #TODO: implement
+		//
+		//if (get_havok_constants()->proxies_debug)
+		//    havok_proxies_render_debug(); // #TODO: implement
+		//
+		//if (get_havok_constants()->collision_damage_debug)
+		//    havok_collision_damage_render_debug(); // #TODO: implement
+		//
+		//if (get_havok_constants()->physics_debug)
+		//{
+		//    // #TODO: implement
+		//}
+
+		havok_restore_fpu_from_update();
+	}
 }
 
