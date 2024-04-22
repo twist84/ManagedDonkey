dc284b8ccdaec21cb7ee36c033a95bc2911150ae
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-22 18:16:44 +0100

    Updated `sub_6948C0` hook

--- game/source/effects/decals.cpp
+++ game/source/effects/decals.cpp
@@ -1,12 +1,17 @@
 #include "effects/decals.hpp"
 
+#include "game/game.hpp"
 #include "memory/module.hpp"
 
-HOOK_DECLARE(0x006948C0, sub_6948C0);
+HOOK_DECLARE_CALL(0x006947FE, sub_6948C0);
+
+bool disable_sub_6948C0 = true;
 
 void __cdecl sub_6948C0(long a1)
 {
-	// skipping this decal render function, added by Saber
-	return;
+	if (disable_sub_6948C0)
+		return;
+
+	INVOKE(0x006948C0, sub_6948C0, a1);
 }
 
