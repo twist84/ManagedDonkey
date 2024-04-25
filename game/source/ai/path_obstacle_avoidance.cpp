814a6efd6148b98bff74eae5c34843e1ba69f085
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-25 14:29:16 +0100

    Initial work on `render_debug_obstacle_path`

--- game/source/ai/path_obstacle_avoidance.cpp
+++ game/source/ai/path_obstacle_avoidance.cpp
@@ -1,12 +1,107 @@
 #include "ai/path_obstacle_avoidance.hpp"
 
+#include "main/console.hpp"
+
 bool debug_obstacle_path = false;
+bool debug_obstacle_path_on_failure = false;
+real_point3d debug_obstacle_path_start_point = {};
+c_sector_ref debug_obstacle_path_start_sector_ref = {};
+real_point3d debug_obstacle_path_goal_point = {};
+c_sector_ref debug_obstacle_path_goal_sector_ref = {};
+short debug_obstacle_path_projection_axis = 0;
+real debug_obstacle_path_radius = 0.0f;
+struct obstacles debug_obstacle_path_obstacles = {};
+bool debug_obstacle_path_projection_sign = false;
+bool debug_obstacle_path_finishing = false;
+bool debug_ignore_broken_surfaces = false;
+bool debug_obstacle_final_step = false;
+obstacle_path debug_obstacle_path_path = {};
+
+bool __cdecl path_iterate(obstacle_path* path, bool final_step)
+{
+	return INVOKE(0x014E19B0, path_iterate, path, final_step);
+}
+
+bool __cdecl path_new(
+	obstacle_path* path,
+	struct obstacles const* obstacles,
+	bool ignore_broken_surfaces,
+	real radius,
+	real_point3d const* start,
+	c_sector_ref start_sector_ref,
+	real_point3d const* goal,
+	c_sector_ref goal_sector_ref,
+	short projection_axis,
+	bool projection_sign,
+	bool final_step,
+	bool a12)
+{
+	return INVOKE(0x014E1EB0, path_new,
+		path,
+		obstacles,
+		ignore_broken_surfaces,
+		radius,
+		start,
+		start_sector_ref,
+		goal,
+		goal_sector_ref,
+		projection_axis,
+		projection_sign,
+		final_step,
+		a12);
+}
+
+void render_debug_obstacles(struct obstacles const* obstacles, real radius)
+{
+	// #TODO: implement this
+}
+
+void render_debug_path(obstacle_path const* path)
+{
+	// #TODO: implement this
+}
 
 void render_debug_obstacle_path()
 {
 	if (debug_obstacle_path)
 	{
-		// #TODO: implement
+		if (!debugging_system_has_focus())
+		{
+			if (input_key_frames_down(_key_code_r, _input_type_game))
+			{
+				debug_obstacle_path_on_failure = true;
+				debug_obstacle_path = false;
+			}
+
+			if (input_key_frames_down(_key_code_g, _input_type_game))
+			{
+				debug_obstacle_path_on_failure = true;
+				debug_obstacle_path = false;
+			}
+
+			if (input_key_frames_down(_key_code_n, _input_type_game))
+			{
+				path_new(
+					&debug_obstacle_path_path,
+					&debug_obstacle_path_obstacles,
+					debug_ignore_broken_surfaces,
+					debug_obstacle_path_radius,
+					&debug_obstacle_path_start_point,
+					debug_obstacle_path_start_sector_ref,
+					&debug_obstacle_path_goal_point,
+					debug_obstacle_path_goal_sector_ref,
+					debug_obstacle_path_projection_axis,
+					debug_obstacle_path_projection_sign,
+					debug_obstacle_final_step,
+					false);
+			}
+
+			if (input_key_frames_down(_key_code_i, _input_type_game))
+				path_iterate(&debug_obstacle_path_path, debug_obstacle_final_step);
+		}
+
+		render_debug_obstacles(&debug_obstacle_path_obstacles, debug_obstacle_path_radius);
+		render_debug_path(&debug_obstacle_path_path);
 	}
 }
 
--- game/source/ai/path_obstacle_avoidance.hpp
+++ game/source/ai/path_obstacle_avoidance.hpp
@@ -1,6 +1,124 @@
 #pragma once
 
-extern bool debug_obstacle_path;
+#include "cseries/cseries.hpp"
 
+#define MAXIMUM_DISC_COUNT 256
+#define MAXIMUM_OBSTACLE_AVOIDANCE_STEPS 64
+
+struct c_sector_ref
+{
+	short __unkown0;
+	short __unkown2;
+};
+static_assert(sizeof(c_sector_ref) == 0x4);
+
+struct disc
+{
+	word_flags flags;
+	short obstacle_index;
+	long __unknown4;
+	real_point2d point;
+	real __unknown10;
+};
+static_assert(sizeof(struct disc) == 0x14);
+
+struct obstacles
+{
+	short obstacle_count;
+	short disc_count;
+	short __unknown4;
+	short __unknown6;
+	short __unknown8;
+	byte __dataA[2];
+	disc discs[MAXIMUM_DISC_COUNT];
+};
+static_assert(sizeof(struct obstacles) == 0x140C);
+
+struct step
+{
+	real_point3d __unknown0;
+	long __unknownC;
+	vector2d __unknown10;
+	real __unknown18;
+	word obstacle_index;
+	word __unknown1E;
+	bool turning_point;
+	word __unknown22[2];
+	byte __data26[2];
+	real heap_cost;
+	short __unknown2C;
+	short __unknown2E;
+	short __unknown30;
+	short __unknown32;
+	bool __unknown34;
+	byte __data35[3];
+};
+static_assert(sizeof(struct step) == 0x38);
+
+struct obstacle_path
+{
+	real radius;
+
+	bool ignore_broken_surfaces;
+	byte __pad5[0x3];
+
+	struct obstacles* obstacles;
+	real_point3d goal_point;
+	c_sector_ref goal_sector_ref;
+	word obstacle_index;
+	short __unknown1E;
+
+	short __unknown20;
+	byte __pad22[0x2];
+
+	real __unknown24;
+	short projection_axis;
+	bool projection_sign;
+	bool __unknown2B;
+
+	bool __unknown2C;
+	byte __pad2D[0x1];
+
+	short step_count;
+	step steps[MAXIMUM_OBSTACLE_AVOIDANCE_STEPS];
+
+	short heap_count;
+	short heap[MAXIMUM_OBSTACLE_AVOIDANCE_STEPS];
+
+	short __unknownEB2;
+};
+static_assert(sizeof(struct obstacle_path) == 0xEB4);
+
+extern bool debug_obstacle_path;
+extern bool debug_obstacle_path_on_failure;
+extern real_point3d debug_obstacle_path_start_point;
+extern c_sector_ref debug_obstacle_path_start_sector_ref;
+extern real_point3d debug_obstacle_path_goal_point;
+extern c_sector_ref debug_obstacle_path_goal_sector_ref;
+extern short debug_obstacle_path_projection_axis;
+extern real debug_obstacle_path_radius;
+extern obstacles debug_obstacle_path_obstacles;
+extern bool debug_obstacle_path_projection_sign;
+extern bool debug_obstacle_path_finishing;
+extern bool debug_ignore_broken_surfaces;
+extern bool debug_obstacle_final_step;
+extern obstacle_path debug_obstacle_path_path;
+
+extern bool __cdecl path_iterate(obstacle_path* path, bool final_step);
+extern bool __cdecl path_new(
+	obstacle_path* path,
+	struct obstacles const* obstacles,
+	bool ignore_broken_surfaces,
+	real radius,
+	real_point3d const* start,
+	c_sector_ref start_sector_ref,
+	real_point3d const* goal,
+	c_sector_ref goal_sector_ref,
+	short projection_axis,
+	bool projection_sign,
+	bool final_step,
+	bool a12);
+
+extern void render_debug_path(obstacle_path const* path);
 extern void render_debug_obstacle_path();
 
