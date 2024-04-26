151d16d43c6b6c8c01e67683ff634f75fa5ea821
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-26 21:14:09 +0100

    Added `collision_constants`

--- game/source/physics/collisions.hpp
+++ game/source/physics/collisions.hpp
@@ -2,19 +2,7 @@
 
 #include "cseries/cseries.hpp"
 #include "game/materials.hpp"
-
-// #TODO: confirm this
-enum e_collision_test_flag
-{
-	_collision_test_structure_bit = 0,
-	_collision_test_water_bit = 1,
-	_collision_test_instanced_geometry_bit = 3,
-	_collision_test_render_only_bsps_bit = 4,
-	_collision_test_try_to_keep_location_valid_bit = 17,
-	_collision_test_objects_bit = 32,
-};
-
-using s_collision_test_flags = c_static_flags<64>;
+#include "physics/collision_constants.hpp"
 
 struct collision_result
 {
