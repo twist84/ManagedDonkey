71563c7dc00dccf83dc87819d836324a23c2e9cc
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-27 21:01:08 +0100

    Added `c_character_physics_component`

---
+++ game/source/physics/character_physics.cpp
@@ -0,0 +1,7 @@
+#include "physics/character_physics.hpp"
+
+c_character_physics_component::e_mode c_character_physics_component::get_mode() const
+{
+	return m_mode;
+}
+
---
+++ game/source/physics/character_physics.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+struct c_character_physics_component
+{
+public:
+	enum e_mode
+	{
+		_mode_none = 0,
+		_mode_ground,
+		_mode_flying,
+		_mode_dead,
+		_mode_posture,  // sentinel
+		_mode_climbing, // sentinel
+		_mode_melee,
+
+		k_mode_count,
+
+		_mode_first = _mode_none,
+		_mode_last = _mode_melee,
+	};
+
+	e_mode get_mode() const;
+
+protected:
+	short __unknown0;
+	c_enum<e_mode, byte, _mode_first, k_mode_count> m_mode;
+	byte m_collision_damage_immunity_duration;
+	long m_object_index;
+	byte __data8[0x78];
+};
+static_assert(sizeof(c_character_physics_component) == 0x80); // #TODO: find the exact size
+
