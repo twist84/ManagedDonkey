0490e8142060a3048b9de2d58849f9d60eefe3e8
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-30 20:19:58 +0100

    Added `profiler_stopwatch`

---
+++ game/source/profiler/profiler_stopwatch.cpp
@@ -0,0 +1,73 @@
+//#include "profiler/profiler_stopwatch.hpp"
+//
+//// 00618DD0
+//c_stop_watch::c_stop_watch()
+//{
+//}
+//
+//// 00618E30
+//c_stop_watch::c_stop_watch(bool paused)
+//{
+//}
+//
+//// 00618E80
+//__int64 c_stop_watch::cycles_per_second()
+//{
+//	return 0;
+//}
+//
+//// 00618EB0
+//real c_stop_watch::cycles_to_seconds(__int64 cycles)
+//{
+//	return real();
+//}
+//
+//// 00618F50
+//__int64 c_stop_watch::get_absolute_time()
+//{
+//	return 0;
+//}
+//
+//// 00618F80
+//__int64 c_stop_watch::get_current_delta() const
+//{
+//	return 0;
+//}
+//
+//// 00618FD0
+//__int64 c_stop_watch::hold()
+//{
+//	return 0;
+//}
+//
+//// 00619030
+//__int64 c_stop_watch::reset()
+//{
+//	return 0;
+//}
+//
+//// 006190A0
+//__int64 c_stop_watch::start()
+//{
+//	return 0;
+//}
+//
+//// 00619100
+//__int64 c_stop_watch::stop()
+//{
+//	return 0;
+//}
+//
+//// 00619170
+//__int64 c_stop_watch::total_elapsed_time_in_cycles() const
+//{
+//	return 0;
+//}
+//
+//// 006191E0
+//real c_stop_watch::total_elapsed_time_in_seconds() const
+//{
+//	return real();
+//}
+//
+//
