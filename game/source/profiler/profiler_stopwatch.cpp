ea756a4cbcd84fbc7802f811862b751d910cbd3e
theTwister <6237734+twist84@users.noreply.github.com>
2024-04-30 20:36:36 +0100

    Added `c_stop_watch`

--- game/source/profiler/profiler_stopwatch.cpp
+++ game/source/profiler/profiler_stopwatch.cpp
@@ -1,73 +1,72 @@
-//#include "profiler/profiler_stopwatch.hpp"
-//
-//// 00618DD0
-//c_stop_watch::c_stop_watch()
-//{
-//}
-//
-//// 00618E30
-//c_stop_watch::c_stop_watch(bool paused)
-//{
-//}
-//
-//// 00618E80
-//__int64 c_stop_watch::cycles_per_second()
-//{
-//	return 0;
-//}
-//
-//// 00618EB0
-//real c_stop_watch::cycles_to_seconds(__int64 cycles)
-//{
-//	return real();
-//}
-//
-//// 00618F50
-//__int64 c_stop_watch::get_absolute_time()
-//{
-//	return 0;
-//}
-//
-//// 00618F80
-//__int64 c_stop_watch::get_current_delta() const
-//{
-//	return 0;
-//}
-//
-//// 00618FD0
-//__int64 c_stop_watch::hold()
-//{
-//	return 0;
-//}
-//
-//// 00619030
-//__int64 c_stop_watch::reset()
-//{
-//	return 0;
-//}
-//
-//// 006190A0
-//__int64 c_stop_watch::start()
-//{
-//	return 0;
-//}
-//
-//// 00619100
-//__int64 c_stop_watch::stop()
-//{
-//	return 0;
-//}
-//
-//// 00619170
-//__int64 c_stop_watch::total_elapsed_time_in_cycles() const
-//{
-//	return 0;
-//}
-//
-//// 006191E0
-//real c_stop_watch::total_elapsed_time_in_seconds() const
-//{
-//	return real();
-//}
-//
-//
+#include "profiler/profiler_stopwatch.hpp"
+
+c_stop_watch::c_stop_watch() :
+	__data0(),
+	m_paused(),
+	__data11()
+{
+	DECLFUNC(0x00618DD0, c_stop_watch*, __thiscall, c_stop_watch*)(this);
+}
+
+c_stop_watch::c_stop_watch(bool paused) :
+	__data0(),
+	m_paused(),
+	__data11()
+{
+	DECLFUNC(0x00618E30, c_stop_watch*, __thiscall, c_stop_watch*, bool)(this, paused);
+}
+
+c_stop_watch::~c_stop_watch()
+{
+}
+
+__int64 __cdecl c_stop_watch::cycles_per_second()
+{
+	return INVOKE(0x00618E80, cycles_per_second);
+}
+
+real __cdecl c_stop_watch::cycles_to_seconds(__int64 cycles)
+{
+	return INVOKE(0x00618EB0, cycles_to_seconds, cycles);
+}
+
+__int64 __cdecl c_stop_watch::get_absolute_time()
+{
+	return INVOKE(0x00618F50, get_absolute_time);
+}
+
+__int64 c_stop_watch::get_current_delta() const
+{
+	return DECLFUNC(0x00618F80, __int64, __thiscall, c_stop_watch const*)(this);
+}
+
+__int64 c_stop_watch::hold()
+{
+	return DECLFUNC(0x00618FD0, __int64, __thiscall, c_stop_watch*)(this);
+}
+
+__int64 c_stop_watch::reset()
+{
+	return DECLFUNC(0x00619030, __int64, __thiscall, c_stop_watch*)(this);
+}
+
+__int64 c_stop_watch::start()
+{
+	return DECLFUNC(0x006190A0, __int64, __thiscall, c_stop_watch*)(this);
+}
+
+__int64 c_stop_watch::stop()
+{
+	return DECLFUNC(0x00619100, __int64, __thiscall, c_stop_watch*)(this);
+}
+
+__int64 c_stop_watch::total_elapsed_time_in_cycles() const
+{
+	return DECLFUNC(0x00619170, __int64, __thiscall, c_stop_watch const*)(this);
+}
+
+real c_stop_watch::total_elapsed_time_in_seconds() const
+{
+	return DECLFUNC(0x006191E0, real, __thiscall, c_stop_watch const*)(this);
+}
+
--- game/source/profiler/profiler_stopwatch.hpp
+++ game/source/profiler/profiler_stopwatch.hpp
@@ -1,32 +1,33 @@
-//#pragma once
-//
-//#include "cseries/cseries.hpp"
-//
-//struct c_stop_watch
-//{
-//public:
-//	c_stop_watch();
-//	c_stop_watch(bool paused);
-//	static __int64 cycles_per_second();
-//	static real cycles_to_seconds(__int64 cycles);
-//
-//private:
-//	static __int64 get_absolute_time();
-//	__int64 get_current_delta() const;
-//
-//public:
-//	__int64 hold();
-//	__int64 reset();
-//	__int64 start();
-//	__int64 stop();
-//	__int64 total_elapsed_time_in_cycles() const;
-//	real total_elapsed_time_in_seconds() const;
-//
-//protected:
-//	byte __data0[0x10];
-//	bool m_paused;
-//	byte __data11[0x7];
-//};
-//static_assert(sizeof(c_stop_watch) == 0x18);
-//
-//
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+struct c_stop_watch
+{
+public:
+	c_stop_watch();
+	c_stop_watch(bool paused);
+	~c_stop_watch();
+
+	static __int64 __cdecl cycles_per_second();
+	static real __cdecl cycles_to_seconds(__int64 cycles);
+
+private:
+	static __int64 __cdecl get_absolute_time();
+	__int64 get_current_delta() const;
+
+public:
+	__int64 hold();
+	__int64 reset();
+	__int64 start();
+	__int64 stop();
+	__int64 total_elapsed_time_in_cycles() const;
+	real total_elapsed_time_in_seconds() const;
+
+protected:
+	byte __data0[0x10];
+	bool m_paused;
+	byte __data11[0x7];
+};
+static_assert(sizeof(c_stop_watch) == 0x18);
+
