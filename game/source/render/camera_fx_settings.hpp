7b984bfb4d5c61e877a1fe8b232c11cb79b302c9
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-01 11:27:52 +0100

    Update camera_fx_settings.hpp

--- game/source/render/camera_fx_settings.hpp
+++ game/source/render/camera_fx_settings.hpp
@@ -11,6 +11,25 @@ static_assert(sizeof(s_scripted_exposure) == 0x34);
 struct c_camera_fx_settings
 {
 	byte __data[0x170];
+	
+	//struct exposure;
+	//struct auto_exposure_sensitivity;
+	//struct auto_exposure_anti_bloom;
+	//struct bloom_point;
+	//struct bloom_inherent;
+	//struct bloom_intensity;
+	//struct bloom_large_color;
+	//struct bloom_medium_color;
+	//struct bloom_small_color;
+	//struct bling_intensity;
+	//struct bling_size;
+	//struct bling_angle;
+	//struct bling_count;
+	//struct self_illum_preferred;
+	//struct self_illum_scale;
+	//struct ssao;
+	//struct color_grading;
+	//struct lightshafts;
 };
 static_assert(sizeof(c_camera_fx_settings) == 0x170);
 
