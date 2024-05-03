7a87940839b039f7bd0779d5d021c49970585fda
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-03 18:26:45 +0100

    Mapped `lights_game_globals_definition` (WIP)

--- game/source/objects/lights.hpp
+++ game/source/objects/lights.hpp
@@ -12,7 +12,13 @@ static_assert(sizeof(light_datum) == 0xE4);
 
 struct lights_game_globals_definition
 {
-	byte __data[0x40];
+	bool can_use_flashlights;
+	long can_use_flashlights_time;
+	bool __unknown8;
+	byte __data9[0x3];
+
+	// bitvector/static flags where each active bit corresponds to a light datum
+	byte __dataC[0x34];
 };
 static_assert(sizeof(lights_game_globals_definition) == 0x40);
 
