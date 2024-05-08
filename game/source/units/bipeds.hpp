a50155c0adc9adba96ec0abf6e0b3e115f500542
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-08 11:39:34 +0100

    Mapped `biped_datum` (WIP) functional

--- game/source/units/bipeds.hpp
+++ game/source/units/bipeds.hpp
@@ -1,6 +1,59 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "memory/ring_buffer.hpp"
+#include "physics/character_physics.hpp"
+
+struct biped_datum
+{
+	unit_datum unit;
+	word_flags biped_flags;
+
+	byte __unknown592[0x42];
+
+	long bump_object_index;
+	char bump_ticks;
+	char jump_control_ticks;
+	byte stun_ticks; // char?
+	char last_known_speed_scale;
+	real lean;
+	real camera_offset_z;
+	real camera_offset_y;
+	real camera_height;
+	long jump_time;
+	long land_time;
+	short current_gate_type;
+	short current_gate_index;
+	long current_gate_start_time;
+
+	byte __data5FC[0x28];
+
+	c_character_physics_component physics;
+
+	byte __data6A4[0x4];
+
+	object_header_block_reference last_node_matrices_storage;
+	vector3d root_offset;
+	vector3d ground_fit_normal;
+	real pivot_on_foot_scale;
+	real pivot_on_foot_scale_boost;
+	real_point3d pivot_point;
+	vector2d pivot_fixup;
+
+	real_matrix4x3 __matrix6E0;
+	real_matrix4x3 __matrix714;
+
+	byte __data748[0xC];
+
+	t_static_ring_buffer<real_matrix4x3, 3> root_matrix_history;
+	real landing_recovery_offset;
+	real pendulum_scale;
+	vector3d pendulum_vector;
+	real gravity_scale;
+
+	byte __data44[0x44];
+};
+static_assert(sizeof(biped_datum) == sizeof(unit_datum) + 0x2C8);
 
 extern bool debug_objects_physics_control_node;
 extern bool debug_objects_biped_autoaim_pills;
