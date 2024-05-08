a41fa79bd8164d45289268e476d85c443700b176
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-08 11:37:40 +0100

    Added `t_static_ring_buffer `

--- game/source/memory/ring_buffer.hpp
+++ game/source/memory/ring_buffer.hpp
@@ -9,3 +9,12 @@ struct c_ring_buffer
 	long element_count;
 };
 static_assert(sizeof(c_ring_buffer) == 0xC);
+
+template<typename t_type, long k_maximum_count>
+struct t_static_ring_buffer :
+	public c_ring_buffer
+{
+	t_type elements[k_maximum_count];
+};
+static_assert(sizeof(t_static_ring_buffer<long, 16>) == 0x4C);
+
