62dd88a548e534cea306167d637684aa90472d68
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-12 19:04:56 +0100

    Mapped `dynamic_firing_set_datum` (WIP) functional

--- game/source/ai/actor_dynamic_firing_position.hpp
+++ game/source/ai/actor_dynamic_firing_position.hpp
@@ -1,12 +1,35 @@
 #pragma once
 
+#include "ai/actors.hpp"
+#include "ai/sector.hpp"
 #include "cseries/cseries.hpp"
 #include "memory/data.hpp"
+
+struct s_firing_position_definition
+{
+	byte __data0[0x28];
+};
+static_assert(sizeof(s_firing_position_definition) == 0x28);
 
 struct dynamic_firing_set_datum :
 	s_datum_header
 {
-	byte __data[0x582];
+	short type;
+	long support_object_index;
+	short position_count;
+	short num_members;
+	c_ai_point3d position;
+	vector3d up;
+	vector3d forward;
+	c_sector_ref __unknown34;
+	bool state_valid;
+
+	byte __pad39[0x3];
+
+	long last_update_time;
+	c_static_flags<32> initialized;
+	s_firing_position_definition const firing_position[32];
+	short const firing_position_types[32];
 };
 static_assert(sizeof(dynamic_firing_set_datum) == 0x584);
 
