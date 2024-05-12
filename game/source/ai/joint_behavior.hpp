95a64ace5dbcbe5bbb4c63fe42580e155e83d7a5
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-12 17:13:25 +0100

    Update joint_behavior.hpp

--- game/source/ai/joint_behavior.hpp
+++ game/source/ai/joint_behavior.hpp
@@ -3,6 +3,12 @@
 #include "cseries/cseries.hpp"
 #include "memory/data.hpp"
 
+struct invitation_data
+{
+	byte __data[0x10];
+};
+static_assert(sizeof(invitation_data) == 0x10);
+
 struct joint_state_datum :
 	s_datum_header
 {
