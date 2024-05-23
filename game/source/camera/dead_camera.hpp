1a487c0cf3088537c4df07fae26436871bf1dbc9
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-23 21:51:24 +0100

    Updated `c_dead_camera`

--- game/source/camera/dead_camera.hpp
+++ game/source/camera/dead_camera.hpp
@@ -6,7 +6,7 @@ struct c_dead_camera :
 	public c_camera
 {
 	real_point3d m_position;
-	euler_angles2d m_facing;
+	euler_angles2d m_orientation;
 	real m_focus_distance;
 	real m_field_of_view;
 	real __unknown2C;
