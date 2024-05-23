c01df81ada914f292596a53ed9b8935132efb73d
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-23 21:51:54 +0100

    Updated `c_orbiting_camera`

--- game/source/camera/orbiting_camera.hpp
+++ game/source/camera/orbiting_camera.hpp
@@ -9,9 +9,9 @@ struct c_orbiting_camera :
 	real m_distance;
 	real m_z_offset;
 
-	bool __unknown20;
-	bool __unknown21;
-	bool __unknown22;
+	bool m_orientation_enabled;
+	bool m_movement_enabled;
+	bool m_distance_updated;
 
 	byte pad[0x1];
 	byte unused[0x28];
