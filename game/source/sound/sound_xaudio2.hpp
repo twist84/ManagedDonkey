7d822c7a06bf857c27efd78b73832f168bb8df7e
theTwister <6237734+twist84@users.noreply.github.com>
2024-05-26 02:45:55 +0100

    Removed xaudio2 test implemented

--- game/source/sound/sound_xaudio2.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern void __cdecl platform_sound_source_queue_sound(long source_index, void const* sound);
-extern void __cdecl platform_sound_dispose();
-extern bool __cdecl platform_sound_initialize(void const* preferences);
-
