23923c3c08a63165d239c969a634e0a2f8ee1aa3
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-01 02:41:49 +0100

    Update main_predict.cpp

--- game/source/main/main_predict.cpp
+++ game/source/main/main_predict.cpp
@@ -9,11 +9,11 @@ void __cdecl main_predict_reset_for_map_reload()
 
 void __cdecl main_render_predict_initialize()
 {
-	INVOKE(0x00605E10, main_render_predict_initialize);
+	//INVOKE(0x00605E10, main_render_predict_initialize);
 }
 
 void __cdecl main_render_predict_tag_resources()
 {
-	INVOKE(0x00605E20, main_render_predict_tag_resources);
+	//INVOKE(0x00605E20, main_render_predict_tag_resources);
 }
 
