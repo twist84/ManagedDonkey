bb4400a064370bc30c8fd6d107b37b83fddce52d
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-02 03:48:48 +0100

    Mapped `c_gui_player_select_screen_widget`

---
+++ game/source/interface/gui_screens/player_select/gui_player_select_screen_widget.cpp
@@ -0,0 +1,95 @@
+#include "interface/gui_screens/player_select/gui_player_select_screen_widget.hpp"
+
+// #TODO: reimplement `c_gui_player_select_screen_widget::initialize`
+// #TODO: reimplement `c_gui_player_select_screen_widget::update`
+// #TODO: reimplement `c_gui_player_select_screen_widget::update_render_state`
+// #TODO: reimplement `c_gui_player_select_screen_widget::handle_controller_input_message`
+// #TODO: reimplement `c_gui_player_select_screen_widget::initialize_datasource`
+// #TODO: reimplement `c_gui_player_select_screen_widget::handle_list_item_chosen`
+// #TODO: reimplement `c_gui_player_select_screen_widget::handle_dialog_result`
+// #TODO: reimplement `c_gui_player_select_screen_widget::__vftable54`
+
+//c_gui_player_select_screen_widget::initialize
+//
+//PLAYER DETAILS
+//
+//hf2p_game_client_cache_release (references and ordering)
+//
+//<target-player-name
+//<cc-level
+//<sc-level
+//<cs-level
+//<ss-level
+//<cc-players
+//<sc-players
+//<cs-players
+//<ss-players
+//<cc-score
+//<sc-score
+//<cs-score
+//<ss-score
+//<kills
+//<deaths
+//<points-gained
+//<points-lost
+//<implement-name
+//<implement-count
+//<biggest-kill-points
+//<biggest-kill-name
+//<time-completed
+//<survival-wave-index
+//<survival-round-index
+//<survival-set-index
+//
+//halo3_tag_test (references and ordering)
+//
+//<target-player-name
+//<player-rank
+//<player-grade
+//<xp-value
+//<skill-value
+//<ranked-value
+//<social-value
+//<custom-value
+//<kills
+//<deaths
+//<kd-ratio
+//<headshots
+//<best-spree
+//<implement-name
+//<implement-count
+//<most-killed-name
+//<most-killed-count
+//<most-killed-by-name
+//<most-killed-by-count
+//<rematch-timer
+//
+//atlas_tag_test (references and ordering)
+//basically a reordered odst
+//
+//<target-player-name
+//<kills
+//<deaths
+//<points-gained
+//<points-lost
+//<implement-name
+//<implement-count
+//<biggest-kill-points
+//<biggest-kill-name
+//<time-completed
+//<survival-wave-index
+//<survival-round-index
+//<survival-set-index
+//<cc-level
+//<sc-level
+//<cs-level
+//<ss-level
+//<cc-players
+//<sc-players
+//<cs-players
+//<ss-players
+//<cc-score
+//<sc-score
+//<cs-score
+//<ss-score
+
---
+++ game/source/interface/gui_screens/player_select/gui_player_select_screen_widget.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "game/players.hpp"
+#include "interface/c_gui_screen_widget.hpp"
+
+struct c_gui_player_select_screen_widget :
+	public c_gui_screen_widget
+{
+	s_player_identifier m_current_player_id;
+	s_player_identifier m_target_player_id;
+	s_player_configuration m_player_data;
+
+	bool __unknown30D0;
+	bool __unknown30D1;
+	byte __data30D2[0x2];
+	
+	c_string_id m_biggest_kill_name;
+
+	byte __data30D8[0x4];
+
+	long m_damage_reporting_type;
+
+	long __unknown30E0;
+	byte __data30E4[0x4];
+	long __unknown30E8;
+	byte __data30EC[0x4];
+};
+static_assert(sizeof(c_gui_player_select_screen_widget) == sizeof(c_gui_screen_widget) + 0x1650);
+
