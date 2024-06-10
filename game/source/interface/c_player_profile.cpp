3af427fa14f7610048ff91df1c945279550064a5
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-10 06:54:55 +0100

    Changed campaign level unlocking to patch

--- game/source/interface/c_player_profile.cpp
+++ game/source/interface/c_player_profile.cpp
@@ -1,11 +1,9 @@
 #include "interface/c_player_profile.hpp"
 
+#include "memory/module.hpp"
+
 REFERENCE_DECLARE(0x0191D4FC, bool, g_controller_all_solo_levels_unlocked);
 
-bool g_controller_all_solo_levels_unlocked_unset = []() -> bool
-{
-	g_controller_all_solo_levels_unlocked = false;
-
-	return g_controller_all_solo_levels_unlocked == false;
-}();
+t_value_type<bool> const controller_all_solo_levels_unlocked = { .value = false };
+DATA_PATCH_DECLARE(0x0191D4FC, controller_all_solo_levels_unlocked, controller_all_solo_levels_unlocked.bytes);
 
