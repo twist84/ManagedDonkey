c09670604e2b85447f854a2eaa7bb92339ae408d
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-16 17:34:32 +0100

    Added `observer` functions
    Implemented and hooked `observer_update`

--- game/source/camera/observer.cpp
+++ game/source/camera/observer.cpp
@@ -5,36 +5,147 @@
 #include "main/main_render.hpp"
 #include "memory/module.hpp"
 #include "memory/thread_local.hpp"
+#include "physics/collision_constants.hpp"
 #include "render/render_cameras.hpp"
 #include "render/render_debug.hpp"
 
+REFERENCE_DECLARE(0x018BB17C, real, g_observer_wave_height);
+REFERENCE_DECLARE(0x018BB180, real, g_camera_speed);
+
 HOOK_DECLARE(0x00612710, observer_game_tick);
+HOOK_DECLARE(0x00613A60, observer_update);
 
 bool g_debug_observer_render = false;
 
-//.text:00610940 ; void __cdecl observer_adopt_global_update_list()
-//.text:00610A20 ; void __cdecl observer_apply_camera_effect(e_output_user_index)
-//.text:00610CF0 ; 
-//.text:00610E50 ; void __cdecl observer_block_for_one_frame()
-//.text:00610E70 ; void __cdecl observer_build_result_from_point_and_vectors(s_observer_result*, real_point3d const*, vector3d const*, vector3d const*)
+void __cdecl observer_adopt_global_update_list()
+{
+	INVOKE(0x00610940, observer_adopt_global_update_list);
+}
+
+void __cdecl observer_apply_camera_effect(e_output_user_index output_user_index)
+{
+	INVOKE(0x00610A20, observer_apply_camera_effect, output_user_index);
+}
+
+void __cdecl observer_apply_rotational_displacement(vector3d const* in_vector, vector3d* out_vector1, vector3d* out_vector2)
+{
+	INVOKE(0x00610CF0, observer_apply_rotational_displacement, in_vector, out_vector1, out_vector2);
+}
+
+void __cdecl observer_block_for_one_frame()
+{
+	INVOKE(0x00610E50, observer_adopt_global_update_list);
+}
+
+void __cdecl observer_build_result_from_point_and_vectors(s_observer_result* out_result, real_point3d const* position, vector3d const* forward, vector3d const* up)
+{
+	INVOKE(0x00610E70, observer_build_result_from_point_and_vectors, out_result, position, forward, up);
+}
+
 //.text:00610F00 ; void __cdecl observer_check_penetration(e_output_user_index, real_point3d*, vector3d*, vector3d const*, real_point3d const*, real, long, long, real*, real, real)
-//.text:00611420 ; void __cdecl observer_clear(s_observer*)
-//.text:00611570 ; void __cdecl observer_clear_all()
-//.text:006115F0 ; void __cdecl observer_clear_global_update_list()
-//.text:00611620 ; struct s_collision_test_flags __cdecl observer_collision_flags_get(e_output_user_index)
-//.text:00611690 ; void __cdecl observer_collision_get_initial_configuration(e_output_user_index, real_point3d const*, vector3d const*, real, real*, vector3d*, real_point3d*, real)
+
+void __cdecl observer_clear(s_observer* observer)
+{
+	INVOKE(0x00611420, observer_clear, observer);
+}
+
+void __cdecl observer_clear_all()
+{
+	INVOKE(0x00611570, observer_clear_all);
+}
+
+void __cdecl observer_clear_global_update_list()
+{
+	INVOKE(0x006115F0, observer_clear_global_update_list);
+}
+
+s_collision_test_flags __cdecl observer_collision_flags_get(e_output_user_index output_user_index)
+{
+	return INVOKE(0x00611620, observer_collision_flags_get, output_user_index);
+}
+
+//.text:00611690 ; void __cdecl observer_collision_get_initial_configuration(e_output_user_index output_user_index, real_point3d const*, vector3d const*, real, real*, vector3d*, real_point3d*, real)
 //.text:00611AA0 ; bool __cdecl observer_collision_in_safe_pill(e_output_user_index, real_point3d*, vector3d*, real)
-//.text:00611B90 ; 
-//.text:00611C30 ; 
-//.text:00611C90 ; void __cdecl observer_command_clear(s_observer_command*)
-//.text:00611D00 ; void __cdecl observer_command_get_collision_ignore_objects(e_output_user_index, s_observer_command const*, long*, long*)
-//.text:00611DA0 ; 
-//.text:00611E10 ; void __cdecl observer_compute_result(e_output_user_index, s_focus_and_distance*)
-//.text:00612460 ; void __cdecl observer_compute_view_offset_matrix(e_output_user_index, real_matrix4x3*)
-//.text:00612520 ; void __cdecl observer_dispose()
-//.text:00612530 ; void __cdecl observer_dispose_from_old_map()
-//.text:00612540 ; void __cdecl observer_dispose_from_old_structure_bsp(dword)
-//.text:00612550 ; void __cdecl observer_find_displacement(real const* const, real const* const, s_observer_derivative*)
+
+bool __cdecl sub_611B90(e_output_user_index output_user_index, real_point3d* point_a, real_point3d* point_b, vector3d* vector, real scale, long first_ignore_object_index, long second_ignore_object_index, real collision_scale)
+{
+	return INVOKE(0x00611B90, sub_611B90, output_user_index, point_a, point_b, vector, scale, first_ignore_object_index, second_ignore_object_index, collision_scale);
+}
+
+bool __cdecl sub_611C30(e_output_user_index output_user_index, real_point3d* point_a, real_point3d* point_b, long first_ignore_object_index, long second_ignore_object_index, real collision_scale)
+{
+	return INVOKE(0x00611C30, sub_611C30, output_user_index, point_a, point_b, first_ignore_object_index, second_ignore_object_index, collision_scale);
+}
+
+void __cdecl observer_command_clear(s_observer_command* command)
+{
+	INVOKE(0x00611C90, observer_command_clear, command);
+
+	//csmemset(command, 0, sizeof(s_observer_command));
+	//command->forward = *global_forward3d;
+	//command->up = *global_up3d;
+	//command->field_of_view = observer_suggested_field_of_view();
+}
+
+void __cdecl observer_command_get_collision_ignore_objects(e_output_user_index output_user_index, s_observer_command const* command, long* out_first_ignore_object_index, long* out_second_ignore_object_index)
+{
+	INVOKE(0x00611D00, observer_command_get_collision_ignore_objects, output_user_index, command, out_first_ignore_object_index, out_second_ignore_object_index);
+
+	//s_observer* observer = observer_get(output_user_index);
+	//
+	//*out_first_ignore_object_index = NONE;
+	//*out_second_ignore_object_index = NONE;
+	//
+	//if (TEST_BIT(command->flags, 6))
+	//{
+	//	*out_first_ignore_object_index = observer->__unknown100;
+	//	*out_second_ignore_object_index = observer->__unknown104;
+	//}
+	//
+	//if (TEST_BIT(command->flags, 5))
+	//{
+	//	observer->__unknown100 = NONE;
+	//	observer->__unknown104 = NONE;
+	//
+	//	if (command->number_of_parents_objects > 0)
+	//		*out_first_ignore_object_index = observer->__unknown100 = command->parent_objects[0];
+	//
+	//	if (command->number_of_parents_objects > 1)
+	//		*out_first_ignore_object_index = observer->__unknown104 = command->parent_objects[1];
+	//}
+}
+
+bool __cdecl observer_command_has_finished(e_output_user_index output_user_index)
+{
+	return INVOKE(0x00611DA0, observer_command_has_finished, output_user_index);
+}
+
+void __cdecl observer_compute_result(e_output_user_index output_user_index, s_focus_and_distance* focus_and_distance)
+{
+	INVOKE(0x00611E10, observer_compute_result, output_user_index, focus_and_distance);
+}
+
+void __cdecl observer_compute_view_offset_matrix(e_output_user_index output_user_index, real_matrix4x3* view_offset_matrix)
+{
+	INVOKE(0x00612460, observer_compute_view_offset_matrix, output_user_index, view_offset_matrix);
+}
+
+void __cdecl observer_dispose()
+{
+	INVOKE(0x00612520, observer_dispose);
+}
+
+void __cdecl observer_dispose_from_old_map()
+{
+	INVOKE(0x00612530, observer_dispose_from_old_map);
+}
+
+void __cdecl observer_dispose_from_old_structure_bsp(dword a1)
+{
+	INVOKE(0x00612540, observer_dispose_from_old_structure_bsp, a1);
+}
+
+//.text:00612550 ; void __cdecl observer_find_displacement(real const* const, real const* const, s_observer_derivative* derivative)
 //.text:006125E0 ; void __cdecl observer_fix_focus_position_to_safe_position(e_output_user_index, real_point3d*, long, long, real_point3d const*)
 
 void __cdecl observer_game_tick()
@@ -89,27 +200,108 @@ s_observer_result const* observer_get_camera(e_output_user_index output_user_ind
 	return result;
 }
 
-//.text:006127F0 ; real __cdecl observer_get_max_wave_height()
-//.text:00612800 ; real __cdecl observer_get_near_plane_farthest_distance(real, real)
-//.text:006128B0 ; void __cdecl observer_initialize()
-//.text:00612970 ; void __cdecl observer_initialize_after_load_saved_game(long)
-//.text:006129A0 ; void __cdecl observer_initialize_before_saved_game(long)
-//.text:00612A50 ; void __cdecl observer_initialize_for_new_map()
-//.text:00612AA0 ; void __cdecl observer_initialize_for_new_structure_bsp(dword)
-//.text:00612B30 ; void __cdecl observer_initialize_for_saved_game(long)
-//.text:00612B40 ; void __cdecl observer_obsolete_position(e_output_user_index)
-//.text:00612B70 ; void __cdecl observer_pass_time()
+real __cdecl observer_get_max_wave_height()
+{
+	return INVOKE(0x006127F0, observer_get_max_wave_height);
+}
+
+real __cdecl observer_get_near_plane_farthest_distance(real horizontal_fov, real vertical_fov)
+{
+	return INVOKE(0x00612800, observer_get_near_plane_farthest_distance, horizontal_fov, vertical_fov);
+}
+
+void __cdecl observer_initialize()
+{
+	INVOKE(0x006128B0, observer_initialize);
+}
+
+void __cdecl observer_initialize_after_load_saved_game(long flags)
+{
+	INVOKE(0x00612970, observer_initialize_after_load_saved_game, flags);
+}
+
+void __cdecl observer_initialize_before_saved_game(long flags)
+{
+	INVOKE(0x006129A0, observer_initialize_before_saved_game, flags);
+}
+
+void __cdecl observer_initialize_for_new_map()
+{
+	INVOKE(0x00612A50, observer_initialize_for_new_map);
+}
+
+void __cdecl observer_initialize_for_new_structure_bsp(dword a1)
+{
+	INVOKE(0x00612AA0, observer_initialize_for_new_structure_bsp, a1);
+}
+
+void __cdecl observer_initialize_for_saved_game(long flags)
+{
+	INVOKE(0x00612B30, observer_initialize_for_saved_game, flags);
+}
+
+void __cdecl observer_obsolete_position(e_output_user_index output_user_index)
+{
+	INVOKE(0x00612B40, observer_obsolete_position, output_user_index);
+}
+
+void __cdecl observer_pass_time(e_output_user_index output_user_index)
+{
+	INVOKE(0x00612B70, observer_pass_time, output_user_index);
+}
+
 //.text:00612C10 ; void __cdecl observer_penetration_check_volume(real_point3d*, vector3d const*, real_point3d const*, real, real)
-//.text:00612D60 ; void __cdecl observer_perform_collision(e_output_user_index, s_focus_and_distance const*, real)
-//.text:00613040 ; void __cdecl observer_post_global_update_list()
-//.text:00613130 ; void __cdecl observer_result_clear(s_observer_result*)
-//.text:006131E0 ; void __cdecl observer_result_compute_parameters(s_observer_result*)
-//.text:00613460 ; void __cdecl observer_result_set_position(e_output_user_index, real_point3d const*, real, vector3d const*)
-//.text:00613630 ; bool __cdecl observer_result_valid(e_output_user_index)
-//.text:00613660 ; void __cdecl observer_rotational_displacement(vector3d const*, vector3d const*, vector3d const*, vector3d const*, vector3d*)
-//.text:006136A0 ; void __cdecl observer_set_camera(e_output_user_index, s_observer_command*)
-//.text:00613730 ; real __cdecl observer_suggested_field_of_view()
-//.text:00613780 ; real __cdecl observer_suggested_field_of_view_change_time()
+
+void __cdecl observer_perform_collision(e_output_user_index output_user_index, s_focus_and_distance const* focus_and_distance, real world_seconds_elapsed)
+{
+	INVOKE(0x00612D60, observer_perform_collision, output_user_index, focus_and_distance, world_seconds_elapsed);
+}
+
+void __cdecl observer_post_global_update_list()
+{
+	INVOKE(0x00613040, observer_post_global_update_list);
+}
+
+void __cdecl observer_result_clear(s_observer_result* result)
+{
+	INVOKE(0x00613130, observer_result_clear, result);
+}
+
+void __cdecl observer_result_compute_parameters(s_observer_result* result)
+{
+	INVOKE(0x006131E0, observer_result_compute_parameters, result);
+}
+
+void __cdecl observer_result_set_position(e_output_user_index output_user_index, real_point3d const* position, real distance, vector3d const* forward)
+{
+	INVOKE(0x00613460, observer_result_set_position, output_user_index, position, distance, forward);
+}
+
+bool __cdecl observer_result_valid(e_output_user_index output_user_index)
+{
+	return INVOKE(0x00613630, observer_result_valid, output_user_index);
+}
+
+void __cdecl observer_rotational_displacement(vector3d const* forward0, vector3d const* up0, vector3d const* forward1, vector3d const* up1, vector3d* displacement)
+{
+	INVOKE(0x00613660, observer_rotational_displacement, forward0, up0, forward1, up1, displacement);
+}
+
+void __cdecl observer_set_camera(e_output_user_index output_user_index, s_observer_command* command)
+{
+	INVOKE(0x006136A0, observer_set_camera, output_user_index, command);
+}
+
+real __cdecl observer_suggested_field_of_view()
+{
+	return INVOKE(0x00613730, observer_suggested_field_of_view);
+}
+
+real __cdecl observer_suggested_field_of_view_change_time()
+{
+	return INVOKE(0x00613780, observer_suggested_field_of_view_change_time);
+}
+
 //.text:00613790 ; void __cdecl observer_test_water(real_point3d const*, vector3d*, real_point3d*, real, real)
 
 s_observer_result const* __cdecl observer_try_and_get_camera(e_output_user_index output_user_index)
@@ -117,33 +309,113 @@ s_observer_result const* __cdecl observer_try_and_get_camera(e_output_user_index
 	return INVOKE(0x00613960, observer_try_and_get_camera, output_user_index);
 }
 
-//.text:006139C0 ; s_observer_result* __cdecl observer_try_and_get_writable_camera(e_output_user_index)
-//.text:00613A10 ; s_observer_result const* __cdecl observer_try_to_get_valid_camera_invalid_location_okay(e_output_user_index)
+s_observer_result* __cdecl observer_try_and_get_writable_camera(e_output_user_index output_user_index)
+{
+	return INVOKE(0x006139C0, observer_try_and_get_writable_camera, output_user_index);
+}
+
+s_observer_result const* __cdecl observer_try_to_get_valid_camera_invalid_location_okay(e_output_user_index output_user_index)
+{
+	return INVOKE(0x00613A10, observer_try_to_get_valid_camera_invalid_location_okay, output_user_index);
+}
 
 void __cdecl observer_update(real world_seconds_elapsed)
 {
-	INVOKE(0x00613A60, observer_update, world_seconds_elapsed);
+	//INVOKE(0x00613A60, observer_update, world_seconds_elapsed);
+
+	TLS_DATA_GET_VALUE_REFERENCE(g_observer_globals);
+	
+	g_observer_globals->timestep = g_camera_speed * world_seconds_elapsed;
+	
+	for (e_output_user_index user_index = first_output_user(); user_index != k_output_user_none; user_index = next_output_user(user_index))
+	{
+		if (player_mapping_output_user_is_active(user_index))
+		{
+			g_observer_globals->observers[user_index].updated_for_frame = true;
+	
+			observer_update_command(user_index);
+	
+			if (g_observer_globals->timestep != 0.0f)
+				observer_pass_time(user_index);
+	
+			s_focus_and_distance focus_and_distance{};
+			observer_compute_result(user_index, &focus_and_distance);
+			observer_apply_camera_effect(user_index);
+			observer_perform_collision(user_index, &focus_and_distance, world_seconds_elapsed);
+		}
+	}
+	
+	if (g_observer_globals->__unknownF24)
+		return;
+	
+	observer_post_global_update_list();
+	g_observer_globals->__unknownF24 = false;
 }
 
-//.text:00613B30 ; void __cdecl observer_update_accelerations(e_output_user_index)
-//.text:00613D40 ; void __cdecl observer_update_command(e_output_user_index)
-//.text:00613FA0 ; void __cdecl observer_update_displacements(e_output_user_index)
-//.text:00613FE0 ; void __cdecl observer_update_polynomial(e_output_user_index)
-//.text:00614300 ; void __cdecl observer_update_positions(e_output_user_index)
-//.text:00614A70 ; void __cdecl observer_update_velocities(e_output_user_index)
-//.text:00614CB0 ; bool __cdecl observer_valid_camera_command(s_observer_command*)
-
-void __cdecl observer_validate_camera_command(s_observer_command* result)
+void __cdecl observer_update_accelerations(e_output_user_index output_user_index)
 {
-	INVOKE(0x00614D60, observer_validate_camera_command, result);
+	INVOKE(0x00613B30, observer_update_accelerations, output_user_index);
 }
 
-//.text:00615050 ; bool __cdecl valid_field_of_view(real)
-//.text:00615090 ; bool __cdecl valid_focus_distance(real)
-//.text:006150D0 ; bool __cdecl valid_timer(real)
-//.text:00615110 ; bool __cdecl valid_world_real(real)
-//.text:00615150 ; bool __cdecl valid_world_real_point3d(real_point3d*)
+void __cdecl observer_update_command(e_output_user_index output_user_index)
+{
+	INVOKE(0x00613D40, observer_update_command, output_user_index);
+}
 
+void __cdecl observer_update_displacements(e_output_user_index output_user_index)
+{
+	INVOKE(0x00613FA0, observer_update_displacements, output_user_index);
+}
+
+void __cdecl observer_update_polynomial(e_output_user_index output_user_index)
+{
+	INVOKE(0x00613FE0, observer_update_polynomial, output_user_index);
+}
+
+void __cdecl observer_update_positions(e_output_user_index output_user_index)
+{
+	INVOKE(0x00614300, observer_update_positions, output_user_index);
+}
+
+void __cdecl observer_update_velocities(e_output_user_index output_user_index)
+{
+	INVOKE(0x00614A70, observer_update_velocities, output_user_index);
+}
+
+bool __cdecl observer_valid_camera_command(s_observer_command* command)
+{
+	return INVOKE(0x00614CB0, observer_valid_camera_command, command);
+}
+
+void __cdecl observer_validate_camera_command(s_observer_command* command)
+{
+	INVOKE(0x00614D60, observer_validate_camera_command, command);
+}
+
+bool __cdecl valid_field_of_view(real field_of_view)
+{
+	return INVOKE(0x00615050, valid_field_of_view, field_of_view);
+}
+
+bool __cdecl valid_focus_distance(real focus_distance)
+{
+	return INVOKE(0x00615090, valid_focus_distance, focus_distance);
+}
+
+bool __cdecl valid_timer(real timer)
+{
+	return INVOKE(0x006150D0, valid_timer, timer);
+}
+
+bool __cdecl valid_world_real(real world_real)
+{
+	return INVOKE(0x00615110, valid_world_real, world_real);
+}
+
+bool __cdecl valid_world_real_point3d(real_point3d* world_real_point3d)
+{
+	return INVOKE(0x00615150, valid_world_real_point3d, world_real_point3d);
+}
 
 void __cdecl debug_render_observer()
 {
--- game/source/camera/observer.hpp
+++ game/source/camera/observer.hpp
@@ -3,6 +3,13 @@
 #include "cseries/cseries.hpp"
 #include "memory/data.hpp"
 
+struct s_focus_and_distance
+{
+	real_point3d focus;
+	real distance;
+};
+static_assert(sizeof(s_focus_and_distance) == 0x10);
+
 struct s_observer_command
 {
 	dword_flags flags;
@@ -71,25 +78,43 @@ struct s_observer
 	bool __unknownF6;
 	bool __unknownF7;
 	real __unknownF8;
-	byte __dataFC[0x10];
+	dword __unknownFC;
+	long __unknown100;
+	long __unknown104;
+	byte __unknown108;
+	byte __unknown109;
+	byte __unknown10A;
+	byte __unknown10B;
 	s_observer_result result;
-	real_point3d focus_position;
-	vector3d focus_offset;
-	real_point2d look_shift;
-	real focus_point;
-	real horizontal_field_of_view;
-	vector3d forward;
-	vector3d up;
+
+	union
+	{
+		struct
+		{
+			real_point3d focus_position;
+			vector3d focus_offset;
+			real_point2d look_shift;
+			real focus_point;
+			real horizontal_field_of_view;
+			vector3d forward;
+			vector3d up;
+		};
+
+		real n[16];
+	} positions;
+
 	real_matrix4x3 focus_space;
-	real velocities[13];
-	real accelerations[13];
-	real __unknown260[13];
-	real __unknown294[13];
-	real __unknown2C8[13];
-	real __unknown2FC[13];
-	real __unknown330[13];
-	real __unknown364[13];
-	real displacements[13];
+
+	union { real n[13]; } velocities;
+	union { real n[13]; } accelerations;
+	union { real n[13]; } __unknown260;
+	union { real n[13]; } __unknown294;
+	union { real n[13]; } __unknown2C8;
+	union { real n[13]; } __unknown2FC;
+	union { real n[13]; } __unknown330;
+	union { real n[13]; } __unknown364;
+	union { real n[13]; } displacements;
+
 	tag trailer_signature;
 };
 static_assert(sizeof(s_observer) == 0x3C8);
@@ -98,22 +123,77 @@ struct s_observer_globals
 {
 	real timestep;
 	s_observer observers[4];
-	byte __dataF24[0x1];
+	bool __unknownF24;
 	bool block_for_one_frame_block_type1;
 	bool block_for_one_frame_block_type0;
 	byte __dataF27[0x1];
 };
 static_assert(sizeof(s_observer_globals) == 0xF28);
 
+extern real& g_observer_wave_height;
+extern real& g_camera_speed;
+
 extern bool g_debug_observer_render;
 
 enum e_output_user_index;
+struct s_collision_test_flags;
 
+extern void __cdecl observer_adopt_global_update_list();
+extern void __cdecl observer_apply_camera_effect(e_output_user_index user_index);
+extern void __cdecl observer_apply_rotational_displacement(vector3d const* in_vector, vector3d* out_vector1, vector3d* out_vector2);
+extern void __cdecl observer_block_for_one_frame();
+extern void __cdecl observer_build_result_from_point_and_vectors(s_observer_result* out_result, real_point3d const* position, vector3d const* forward, vector3d const* up);
+extern void __cdecl observer_clear(s_observer* observer);
+extern void __cdecl observer_clear_all();
+extern void __cdecl observer_clear_global_update_list();
+extern s_collision_test_flags __cdecl observer_collision_flags_get(e_output_user_index output_user_index);
+extern bool __cdecl sub_611B90(e_output_user_index output_user_index, real_point3d* point_a, real_point3d* point_b, vector3d* vector, real scale, long first_ignore_object_index, long second_ignore_object_index, real collision_scale);
+extern bool __cdecl sub_611C30(e_output_user_index output_user_index, real_point3d* point_a, real_point3d* point_b, long first_ignore_object_index, long second_ignore_object_index, real collision_scale);
+extern void __cdecl observer_command_clear(s_observer_command* command);
+extern void __cdecl observer_command_get_collision_ignore_objects(e_output_user_index output_user_index, s_observer_command const* command, long* out_first_ignore_object_index, long* out_second_ignore_object_index);
+extern bool __cdecl observer_command_has_finished(e_output_user_index output_user_index);
+extern void __cdecl observer_compute_result(e_output_user_index user_index, s_focus_and_distance* focus_and_distance);
+extern void __cdecl observer_compute_view_offset_matrix(e_output_user_index output_user_index, real_matrix4x3* view_offset_matrix);
+extern void __cdecl observer_dispose();
+extern void __cdecl observer_dispose_from_old_map();
+extern void __cdecl observer_dispose_from_old_structure_bsp(dword a1);
 extern void __cdecl observer_game_tick();
 extern s_observer* __cdecl observer_get(e_output_user_index output_user_index);
 extern s_observer_result const* observer_get_camera(e_output_user_index output_user_index);
+extern real __cdecl observer_get_max_wave_height();
+extern real __cdecl observer_get_near_plane_farthest_distance(real horizontal_fov, real vertical_fov);
+extern void __cdecl observer_initialize();
+extern void __cdecl observer_initialize_after_load_saved_game(long flags);
+extern void __cdecl observer_initialize_before_saved_game(long flags);
+extern void __cdecl observer_initialize_for_new_map();
+extern void __cdecl observer_initialize_for_new_structure_bsp(dword a1);
+extern void __cdecl observer_initialize_for_saved_game(long flags);
+extern void __cdecl observer_obsolete_position(e_output_user_index output_user_index);
+extern void __cdecl observer_pass_time(e_output_user_index user_index);
+extern void __cdecl observer_perform_collision(e_output_user_index user_index, s_focus_and_distance const* focus_and_distance, real world_seconds_elapsed);
+extern void __cdecl observer_post_global_update_list();
+extern void __cdecl observer_result_clear(s_observer_result* result);
+extern void __cdecl observer_result_compute_parameters(s_observer_result* result);
+extern void __cdecl observer_result_set_position(e_output_user_index output_user_index, real_point3d const* position, real distance, vector3d const* forward);
+extern bool __cdecl observer_result_valid(e_output_user_index output_user_index);
+extern void __cdecl observer_rotational_displacement(vector3d const* forward0, vector3d const* up0, vector3d const* forward1, vector3d const* up1, vector3d* displacement);
+extern void __cdecl observer_set_camera(e_output_user_index output_user_index, s_observer_command* command);
+extern real __cdecl observer_suggested_field_of_view();
+extern real __cdecl observer_suggested_field_of_view_change_time();
 extern s_observer_result const* __cdecl observer_try_and_get_camera(e_output_user_index output_user_index);
 extern void __cdecl observer_update(real world_seconds_elapsed);
-extern void __cdecl observer_validate_camera_command(s_observer_command* result);
+extern void __cdecl observer_update_accelerations(e_output_user_index user_index);
+extern void __cdecl observer_update_command(e_output_user_index user_index);
+extern void __cdecl observer_update_displacements(e_output_user_index user_index);
+extern void __cdecl observer_update_polynomial(e_output_user_index user_index);
+extern void __cdecl observer_update_positions(e_output_user_index user_index);
+extern void __cdecl observer_update_velocities(e_output_user_index user_index);
+extern bool __cdecl observer_valid_camera_command(s_observer_command* command);
+extern void __cdecl observer_validate_camera_command(s_observer_command* command);
+extern bool __cdecl valid_field_of_view(real field_of_view);
+extern bool __cdecl valid_focus_distance(real focus_distance);
+extern bool __cdecl valid_timer(real timer);
+extern bool __cdecl valid_world_real(real world_real);
+extern bool __cdecl valid_world_real_point3d(real_point3d* world_real_point3d);
 extern void __cdecl debug_render_observer();
 
