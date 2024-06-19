fea801449f982546687e3ca2bd949bde913491c3
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-19 17:27:26 +0100

    Added fallback font stubb

---
+++ game/source/text/font_fallback.cpp
@@ -0,0 +1,6 @@
+#include "text/font_fallback.hpp"
+
+// #TODO: add fallback font
+
+s_font_header const* const g_fallback_font_header = nullptr;
+
---
+++ game/source/text/font_fallback.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+struct s_font_header;
+
+extern s_font_header const* const g_fallback_font_header;
+
