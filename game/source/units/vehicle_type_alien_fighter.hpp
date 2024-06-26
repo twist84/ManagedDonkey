58b6ec3491f7f293759173ad4088fd84b5a92070
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:47:03 +0100

    Updated `c_vehicle_type_alien_fighter`

--- game/source/units/vehicle_type_alien_fighter.hpp
+++ game/source/units/vehicle_type_alien_fighter.hpp
@@ -7,9 +7,26 @@ struct c_vehicle_type_alien_fighter
 public:
 
 protected:
-	// #TODO: map this
+	c_animation_id m_steering_animation_id;
 
-	byte __data[0x60];
+	// STRING_ID(global, thrust)
+	// STRING_ID(global, wingtip_contrail_new)
+	real __unknown4;
+
+	// STRING_ID(global, engine_rpm)
+	real __unknown8;
+
+	real __unknownC;
+
+	// STRING_ID(global, engine_rpm)
+	real __unknown10;
+
+	real __unknown14;
+
+	byte m_trick_type; // e_vehicle_trick_type
+	byte m_trick_duration_ticks;
+
+	byte __data[0x60 - 0x1A];
 };
 static_assert(sizeof(c_vehicle_type_alien_fighter) == 0x60);
 
