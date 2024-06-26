b764596d956672671f7532780c6d484d78eade48
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:47:45 +0100

    Updated `c_vehicle_type_human_plane`

--- game/source/units/vehicle_type_human_plane.hpp
+++ game/source/units/vehicle_type_human_plane.hpp
@@ -7,9 +7,14 @@ struct c_vehicle_type_human_plane
 public:
 
 protected:
-	// #TODO: map this
+	real __unknown0;
+	real __unknown4;
+	real __unknown8;
+	real __unknownC;
+	real __unknown10;
+	c_animation_id m_steering_animation_id;
 
-	byte __data[0x60];
+	byte __data[0x60 - 0x18];
 };
 static_assert(sizeof(c_vehicle_type_human_plane) == 0x60);
 
