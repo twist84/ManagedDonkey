2b31138e08c368571517964d4bed4886456a955e
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 09:18:37 +0100

    Update object_activation_regions.cpp

--- game/source/objects/object_activation_regions.cpp
+++ game/source/objects/object_activation_regions.cpp
@@ -1,7 +1,26 @@
 #include "objects/object_activation_regions.hpp"
 
+//.text:00B9A330 ; 
+//.text:00B9A350 ; 
+//.text:00B9A360 ; void __cdecl __tls_set_g_object_activation_region_data_allocator(void*)
+//.text:00B9A390 ; 
+//.text:00B9A3D0 ; 
+//.text:00B9A3F0 ; 
+//.text:00B9A420 ; 
+//.text:00B9A450 ; 
+//.text:00B9A460 ; 
+//.text:00B9A470 ; 
+//.text:00B9A490 ; void __cdecl object_activation_regions_add_region(long, bool)
+//.text:00B9A590 ; void __cdecl object_activation_regions_dispose()
+//.text:00B9A5B0 ; void __cdecl object_activation_regions_dispose_from_old_map()
+//.text:00B9A5D0 ; void __cdecl object_activation_regions_initialize()
+//.text:00B9A610 ; void __cdecl object_activation_regions_initialize_for_new_map()
+
 void __cdecl object_activation_regions_update()
 {
 	INVOKE(0x00B9A630, object_activation_regions_update);
 }
+
+//.text:00B9A720 ; 
+//.text:00B9A740 ; 
 
