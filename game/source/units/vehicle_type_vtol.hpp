fa63f801521a3f24f62dcd7c5495e437f7fa1044
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:48:53 +0100

    Updated `c_vehicle_type_vtol`
    This is the largest of all the vehicle types

--- game/source/units/vehicle_type_vtol.hpp
+++ game/source/units/vehicle_type_vtol.hpp
@@ -7,9 +7,38 @@ struct c_vehicle_type_vtol
 public:
 
 protected:
-	// #TODO: map this
 
-	byte __data[0x60];
+	real __unknown0;
+	real __unknown4;
+	real __unknown8;
+
+	// STRING_ID(global, engine_power_left)
+	byte m_lift_effect_scale_left_history[4];
+
+	// STRING_ID(global, engine_power_right)
+	byte m_lift_effect_scale_right_history[4];
+
+	// left values
+	euler_angles2d __unknown14;
+	euler_angles2d __unknown1C;
+
+	// right values
+	euler_angles2d __unknown24;
+	euler_angles2d __unknown2C;
+
+	// left values
+	vector2d __unknown34;
+	vector2d __unknown3C;
+
+	// right values
+	vector2d __unknown44;
+	vector2d __unknown4C;
+
+	c_animation_id m_left_lift_aim_animation_id;
+	c_animation_id m_right_lift_aim_animation_id;
+
+	// STRING_ID(global, gear_position)
+	real __unknown5C;
 };
 static_assert(sizeof(c_vehicle_type_vtol) == 0x60);
 
