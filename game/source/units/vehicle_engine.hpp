5d29592ec69f3ba9fcfc64bde3556a5af31dd33b
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:46:53 +0100

    Updated `s_vehicle_engine`

--- game/source/units/vehicle_engine.hpp
+++ game/source/units/vehicle_engine.hpp
@@ -6,19 +6,24 @@ struct s_vehicle_engine
 {
 	real cruising_to_loaded_transition;
 
-	real __unknown4;
+	// wheel_ground_speed = __unknown4 * engine_definition->gears[gear].gear_ratio
+	real __unknown4; // velocity?
+
 	real __unknown8;
 
 	char gear;
 	char desired_gear;
 
+	// some calculation with `__unknownE` gives us `rpm_function_scale`
 	byte __unknownE;
 
 	byte clutch_in_ticks;
 
 	byte __unknown10;
-	byte __unknown11;
-	byte __unknown12;
+
+	byte time_to_upshift_ticks;
+	byte time_to_downshift_ticks;
+
 	byte __unknown13;
 };
 static_assert(sizeof(s_vehicle_engine) == 0x14);
