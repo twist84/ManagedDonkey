452e3a8a3a3f0200d8b0d202185d610bf4a1f13b
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:48:02 +0100

    Updated `c_vehicle_type_human_tank`

--- game/source/units/vehicle_type_human_tank.hpp
+++ game/source/units/vehicle_type_human_tank.hpp
@@ -15,8 +15,14 @@ public:
 protected:
 	s_vehicle_engine m_engine;
 
-	c_animation_id __animation_id14;
-	byte __data18[0xC];
+	c_animation_id __animation_id14; // m_steering_animation_id?
+	real __unknown18;
+
+	// STRING_ID(global, left_tread_position)
+	real __unknown1C; // vehicle_function_safe_divide(this->__unknown1C, human_tank_definition->wheel_circumferance);
+
+	// STRING_ID(global, right_tread_position)
+	real __unknown20; // vehicle_function_safe_divide(this->__unknown20, human_tank_definition->wheel_circumferance);
 
 	c_vehicle_stop_counter m_stop_counter;
 
