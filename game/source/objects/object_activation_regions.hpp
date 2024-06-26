cf766a4bb85dde79f0a720bc2503fc5f3ccb4cfb
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 09:18:34 +0100

    Mapped `s_object_activation_region`

--- game/source/objects/object_activation_regions.hpp
+++ game/source/objects/object_activation_regions.hpp
@@ -6,7 +6,18 @@
 struct s_object_activation_region :
 	s_datum_header
 {
-	byte __data[0x26];
+	// all names subject to change
+
+	// alignment?
+	byte __data2[0x2];
+
+	long activation_time;
+	real_rectangle3d region_bounds;
+	long havok_group; // e_havok_group
+	s_location region_location;
+
+	// pad?
+	byte __data26[0x2];
 };
 static_assert(sizeof(s_object_activation_region) == 0x28);
 
