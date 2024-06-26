b2fae135a8017a9ba35c57148ef62aae054cfa06
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:47:31 +0100

    Updated `c_vehicle_type_guardian`

--- game/source/units/vehicle_type_guardian.hpp
+++ game/source/units/vehicle_type_guardian.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "units/vehicle_auto_turret.hpp"
 
 struct c_vehicle_type_guardian
 {
 public:
 
 protected:
-	// #TODO: map this
+	real __unknown0;
+	real __unknown4;
 
-	byte __data[0x60];
+	c_vehicle_auto_turret m_auto_turret;
+
+	byte __data[0x60 - 0x38];
 };
 static_assert(sizeof(c_vehicle_type_guardian) == 0x60);
 
