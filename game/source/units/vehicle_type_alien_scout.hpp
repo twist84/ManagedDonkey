3a8bc94b2a858e65d01cdf02933d4c13b2249766
theTwister <6237734+twist84@users.noreply.github.com>
2024-06-26 22:47:10 +0100

    Updated `c_vehicle_type_alien_scout`

--- game/source/units/vehicle_type_alien_scout.hpp
+++ game/source/units/vehicle_type_alien_scout.hpp
@@ -7,9 +7,27 @@ struct c_vehicle_type_alien_scout
 public:
 
 protected:
-	// #TODO: map this
+	real __unknown0;
 
-	byte __data[0x60];
+	// STRING_ID(global, engine_power)
+	real __unknown4;
+
+	// STRING_ID(global, wingtip_contrail)
+	real __unknown8;
+
+	real __unknownC;
+
+	// STRING_ID(global, hover)
+	real __unknown10;
+
+	// STRING_ID(global, gear_position)
+	real __unknown14;
+
+	real __unknown18;
+
+	c_animation_id m_steering_animation_id;
+
+	byte __data[0x60 - 0x20];
 };
 static_assert(sizeof(c_vehicle_type_alien_scout) == 0x60);
 
