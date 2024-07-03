af7314b4143d542a068fc17cd58e9223ab9bd586
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-03 14:19:53 +0100

    Added `disable_expensive_physics_rebuilding`

---
+++ game/source/physics/havok_collision_damage.cpp
@@ -0,0 +1,6 @@
+#include "physics/havok_collision_damage.hpp"
+
+#include "cseries/cseries.hpp"
+
+REFERENCE_DECLARE(0x04FDE15C, bool, disable_expensive_physics_rebuilding);
+
---
+++ game/source/physics/havok_collision_damage.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+extern bool& disable_expensive_physics_rebuilding;
+
