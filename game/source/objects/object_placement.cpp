1411fc3aa97ab9b726a738e264ed4d9cb5371903
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-07 02:01:32 +0100

    Update object_placement.cpp

--- game/source/objects/object_placement.cpp
+++ game/source/objects/object_placement.cpp
@@ -1,6 +1,7 @@
 #include "objects/object_placement.hpp"
 
 //.text:00B59150 ; bool __cdecl object_placement_attached_to_valid_parent(s_scenario_object const*)
+//.text:00B59190
 
 void __cdecl object_placement_create_active_zone_set_objects(e_object_placement_zone_set_create_mode create_mode)
 {
@@ -23,4 +24,40 @@ void __cdecl object_placement_create_global_objects(e_game_mode game_mode, bool 
 //.text:00B59E10 ; void __cdecl object_placement_initialize()
 //.text:00B59EB0 ; void __cdecl object_placement_initialize_for_new_map()
 //.text:00B59FC0 ; void __cdecl object_placement_initialize_for_new_structure_bsp(dword)
+//.text:00B5A020 ; bool __cdecl object_placement_inside_structure_bsp(e_object_type, s_scenario_object const*)
+//.text:00B5A040 ; bool __cdecl object_placement_inside_structure_bsp_mask(dword, e_object_type, s_scenario_object const*)
+//.text:00B5A100 ; 
+//.text:00B5A110 ; void __cdecl object_placement_mark_name_on_create(short)
+//.text:00B5A190 ; void __cdecl object_placement_mark_name_on_delete(short)
+//.text:00B5A200 ; void __cdecl object_placement_mark_object_on_delete(long)
+//.text:00B5A270 ; 
+//.text:00B5A2C0 ; 
+//.text:00B5A310 ; void __cdecl object_placement_prepare_for_new_zone_set(dword, dword)
+//.text:00B5A470 ; void __cdecl object_placement_prepare_for_designer_zone_set_switch(dword, dword, c_scenario_resource_registry*)
+//.text:00B5A4E0 ; 
+//.text:00B5A4F0 ; void __cdecl object_placement_reset_from_teleport(long)
+//.text:00B5A550 ; void __cdecl object_placement_restore(long)
+//.text:00B5A560 ; void __cdecl object_placement_set_in_limbo(long, bool)
+//.text:00B5A5A0 ; 
+//.text:00B5A620 ; void __cdecl object_placement_unload(long)
+//.text:00B5A640 ; long __cdecl object_type_get_scenario_placement_offset(e_object_type)
+//.text:00B5A650 ; 
+//.text:00B5A660 ; 
+//.text:00B5A6A0 ; 
+//.text:00B5A840 ; 
+//.text:00B5A880 ; s_tag_block* __cdecl scenario_get_object_type_scenario_datums(scenario*, e_object_type, long*)
+//.text:00B5A8B0 ; s_tag_block* __cdecl scenario_get_object_type_scenario_palette(scenario*, e_object_type)
+//.text:00B5A8D0 ; 
+//.text:00B5A920 ; 
+//.text:00B5A9A0 ; 
+//.text:00B5A9E0 ; s_scenario_object const* __cdecl scenario_object_try_and_get(scenario*, e_object_type, long)
+//.text:00B5AA30 ; 
+//.text:00B5AA60 ; 
+//.text:00B5AAD0 ; scenario_object_palette_entry* __cdecl scenario_object_get_palette_entry(scenario*, s_scenario_object const*)
+//.text:00B5AB30 ; 
+//.text:00B5AB60 ; bool __cdecl scenario_object_has_persistent_state(scenario*, s_scenario_object const*)
+//.text:00B5ABB0 ; bool __cdecl scenario_object_is_placed_globally(scenario*, s_scenario_object const*)
+//.text:00B5AC40 ; 
+//.text:00B5AC60 ; s_scenario_object const* __cdecl scenario_object_try_and_get(scenario*, e_object_type, long)
+//.text:00B5ACB0 ; void __cdecl scenario_objects_synchronize_parent_placement(dword const*, scenario*)
 
