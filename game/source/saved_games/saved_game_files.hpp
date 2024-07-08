e3750f03d06fb508328f731f22dc802cfa8f7fe7
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-08 12:40:22 +0100

    Mapped `c_saved_game_files_globals`

--- game/source/saved_games/saved_game_files.hpp
+++ game/source/saved_games/saved_game_files.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "interface/user_interface_text_parser.hpp"
 
 enum e_saved_game_file_type
 {
@@ -52,4 +53,26 @@ struct s_content_item_metadata
 	void decode_from_mcc(c_bitstream* packet);
 };
 static_assert(sizeof(s_content_item_metadata) == 0xF8);
+
+struct s_saved_game_file_text_parser_input
+{
+	long __unknown0;
+	long __unknown4;
+	long __unknown8;
+	long __unknownC;
+	long __unknown10;
+};
+static_assert(sizeof(s_saved_game_file_text_parser_input) == 0x14);
+
+struct c_saved_game_files_globals
+{
+	byte_flags flags;
+	byte __data1[3];
+	s_saved_game_file_text_parser_input text_parser_input;
+	c_magic_string_game_tag_parser map_variant_name_parser;
+	c_magic_string_game_tag_parser game_variant_name_parser;
+	c_magic_string_game_tag_parser campaign_map_name_parser;
+	c_magic_string_game_tag_parser difficulty_parser;
+};
+static_assert(sizeof(c_saved_game_files_globals) == 0x68);
 
