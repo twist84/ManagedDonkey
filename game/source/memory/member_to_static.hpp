14c67162cb03d5d35c88cc36f24c07faffe5534b
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-11 06:31:39 +0100

    Updated `member_to_static`
    We may need a const version of these at some point

--- game/source/memory/member_to_static.hpp
+++ game/source/memory/member_to_static.hpp
@@ -30,6 +30,33 @@ struct t_member_to_static_function<t_result(t_class::*)(t_args...)>
     }
 };
 
+//template<typename t_result, typename t_class, typename...t_args>
+//struct t_member_to_static_function<t_result(t_class::*)(t_args...) const>
+//{
+//    using t_member_function = t_result(t_class::*)(t_args...) const;
+//    using t_static_function = t_result(*)(t_class* _this, t_args...);
+//
+//    t_member_to_static_function() = delete;
+//    t_member_to_static_function(t_member_to_static_function const&) = delete;
+//    t_member_to_static_function(t_member_function _member_function) :
+//        member_function(_member_function)
+//    {
+//
+//    }
+//
+//    t_member_function member_function;
+//
+//    operator t_member_function()
+//    {
+//        return member_function;
+//    }
+//
+//    operator t_static_function()
+//    {
+//        return *reinterpret_cast<t_static_function*>(&member_function);
+//    }
+//};
+
 template<typename t_userdata, typename t_member_function>
 struct t_member_to_static_function_userdata;
 
@@ -59,6 +86,33 @@ struct t_member_to_static_function_userdata<t_userdata, t_result(t_class::*)(t_a
         return *reinterpret_cast<t_static_function*>(&member_function);
     }
 };
+
+//template<typename t_userdata, typename t_result, typename t_class, typename...t_args>
+//struct t_member_to_static_function_userdata<t_userdata, t_result(t_class::*)(t_args...) const>
+//{
+//    using t_member_function = t_result(t_class::*)(t_args...) const;
+//    using t_static_function = t_result(*)(t_userdata* _this, t_args...);
+//
+//    t_member_to_static_function_userdata() = delete;
+//    t_member_to_static_function_userdata(t_member_to_static_function_userdata const&) = delete;
+//    t_member_to_static_function_userdata(t_member_function _member_function) :
+//        member_function(_member_function)
+//    {
+//
+//    }
+//
+//    t_member_function member_function;
+//
+//    operator t_member_function()
+//    {
+//        return member_function;
+//    }
+//
+//    operator t_static_function()
+//    {
+//        return *reinterpret_cast<t_static_function*>(&member_function);
+//    }
+//};
 
 template<typename t_member_function>
 auto member_to_static_function(t_member_function member_function)
