606777fdeac2b06d9d61e6716127e33ba3f50404
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-11 07:18:10 +0100

    Added `IN_RANGE` macro

--- game/source/cseries/cseries.hpp
+++ game/source/cseries/cseries.hpp
@@ -26,6 +26,7 @@
 
 #define OFFSETOF(s,m) __builtin_offsetof(s,m)
 #define NUMBEROF(_array) (sizeof(_array) / sizeof(_array[0]))
+#define IN_RANGE(value, begin, end) ((value) > (begin) && (value) < (end))
 #define IN_RANGE_INCLUSIVE(value, begin, end) ((value) >= (begin) && (value) <= (end))
 #define VALID_INDEX(index, count) ((index) >= 0 && (index) < (count))
 #define VALID_COUNT(index, count) ((index) >= 0 && (index) <= (count))
