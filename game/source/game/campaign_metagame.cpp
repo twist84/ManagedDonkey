3f1d6d1402fd91754ce7867cc87bd6b81c6785a8
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-12 03:00:12 +0100

    `campaign_metagame_runtime_globals_secondary`
    Mapped `s_campaign_metagame_runtime_globals_secondary`
    Added `g_campaign_metagame_runtime_globals_secondary`

--- game/source/game/campaign_metagame.cpp
+++ game/source/game/campaign_metagame.cpp
@@ -1,5 +1,9 @@
 #include "game/campaign_metagame.hpp"
 
+REFERENCE_DECLARE(0x0244A898, bool, chud_debug_metagame);
+REFERENCE_DECLARE(0x0244A8A0, s_campaign_metagame_runtime_globals_secondary, g_campaign_metagame_runtime_globals_secondary);
+REFERENCE_DECLARE(0x0244C9B8, dword, dword_244C9B8);
+
 void __cdecl campaign_metagame_update()
 {
 	INVOKE(0x0060A4F0, campaign_metagame_update);
--- game/source/game/campaign_metagame.hpp
+++ game/source/game/campaign_metagame.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "game/campaign_metagame_definitions.hpp"
+#include "game/players.hpp"
 
 struct s_campaign_metagame_runtime_globals
 {
@@ -16,6 +18,38 @@ struct s_campaign_metagame_runtime_globals
 	byte __data[0x8];
 };
 static_assert(sizeof(s_campaign_metagame_runtime_globals) == 0x1A158);
+
+struct s_campaign_metagame_runtime_sizeof_10C
+{
+	s_player_identifier identifier;
+	long campaign_deaths;
+	long campaign_betrayals;
+
+	byte __data10[4];
+
+	long points_lost;
+	byte buckets[k_campaign_metagame_bucket_type_count][k_campaign_metagame_bucket_class_count];
+
+	byte __unknown108[4];
+};
+static_assert(sizeof(s_campaign_metagame_runtime_sizeof_10C) == 0x10C);
+
+struct s_campaign_metagame_runtime_globals_secondary
+{
+	long game_ticks;
+	bool initialized;
+	byte __pad5[0x3];
+
+	s_campaign_metagame_runtime_sizeof_10C __unknown8[4];
+	real_rectangle3d __unknown438[16];
+
+	long __unknown5B8; // __unknown5BC_index
+	byte __unknown5BC[500][0xE];
+};
+static_assert(sizeof(s_campaign_metagame_runtime_globals_secondary) == 0x2114);
+
+extern bool& chud_debug_metagame;
+extern s_campaign_metagame_runtime_globals_secondary& g_campaign_metagame_runtime_globals_secondary;
 
 extern void __cdecl campaign_metagame_update();
 extern bool __cdecl sub_60B080();
