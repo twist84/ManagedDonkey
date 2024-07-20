1f2c97e6a13e223e5df1f7f7629c8b0204715160
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-20 13:00:14 +0100

    Update online_marketplace.hpp

--- game/source/networking/online/online_marketplace.hpp
+++ game/source/networking/online/online_marketplace.hpp
@@ -3,23 +3,34 @@
 #include "cseries/async_xoverlapped.hpp"
 
 struct c_online_marketplace_purchase_task :
-	c_overlapped_task
+	public c_overlapped_task
 {
-	byte __data10[0x30];
+	static unsigned int const k_max_offer_count = 4;
+
+	long m_controller_index;
+	long m_entry_point;
+	qword m_offers[k_max_offer_count]; // is this the correct name
+	long m_offer_count;
+	long m_purchase_result;
 };
 static_assert(sizeof(c_online_marketplace_purchase_task) == 0x40);
 
 struct c_online_marketplace_enumerate_assets_task :
-	c_overlapped_task
+	public c_overlapped_task
 {
-	byte __data10[0x90];
+	long m_controller_index;
+	void* m_enumeration_handle;
+
+	byte m_results[0x84]; // is this the correct name
+	long m_result_count;
 };
 static_assert(sizeof(c_online_marketplace_enumerate_assets_task) == 0xA0);
 
 struct c_online_marketplace_consume_asset_task :
-	c_overlapped_task
+	public c_overlapped_task
 {
-	byte __data10[0x8];
+	long m_controller_index;
+	void* m_marketplace_asset; // XMARKETPLACE_ASSET
 };
 static_assert(sizeof(c_online_marketplace_consume_asset_task) == 0x18);
 
