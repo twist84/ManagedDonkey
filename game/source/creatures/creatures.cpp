984038cd75c009bfa6f36976e2ec12eb85c60494
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-31 21:53:18 +0100

    Added `creatures` functions comments

---
+++ game/source/creatures/creatures.cpp
@@ -0,0 +1,27 @@
+#include "creatures/creatures.hpp"
+
+//.text:00BB3D30 ; void __cdecl creature_accelerate(long, vector3d const*)
+//.text:00BB3DD0 ; void __cdecl creature_cause_attached_damage(long)
+//.text:00BB3F20 ; bool __cdecl creature_compute_function_value(long, long, long, bool, real*, bool*, bool*)
+//.text:00BB3F90 ; void __cdecl creature_control(long, s_creature_control_data const*)
+//.text:00BB4000 ; void __cdecl creature_control_clear(s_creature_control_data*)
+//.text:00BB4050 ; 
+//.text:00BB4060 ; void __cdecl creature_damage_aftermath_apply(long, s_damage_aftermath_result_data const*)
+//.text:00BB40B0 ; void __cdecl creature_damage_aftermath_compute(long, long, s_damage_data const*, s_damage_aftermath_result_data const*)
+//.text:00BB41B0 ; 
+//.text:00BB41F0 ; void __cdecl creature_affected_by_shape_phantom_this_tick(long)
+//.text:00BB4220 ; void __cdecl creature_get_center_of_mass(long, real_point3d*)
+//.text:00BB4260 ; void __cdecl creature_get_facing_vector(long, vector3d*)
+//.text:00BB4280 ; void __cdecl creature_get_head_position(long, real_point3d*)
+//.text:00BB4300 ; bool __cdecl creature_is_busy(long)
+//.text:00BB4340 ; void __cdecl creature_kill(long)
+//.text:00BB4380 ; void __cdecl creature_move(long)
+//.text:00BB4390 ; bool __cdecl creature_new(long, object_placement_data*, bool*)
+//.text:00BB4550 ; void __cdecl creature_render_debug(long)
+//.text:00BB4560 ; void __cdecl creature_set_actively_controlled(long, bool)
+//.text:00BB45C0 ; bool __cdecl creature_try_to_impact(long)
+//.text:00BB4B40 ; bool __cdecl creature_update(long)
+//.text:00BB4D50 ; 
+//.text:00BB4D60 ; 
+//.text:00BB4D90 ; 
+
