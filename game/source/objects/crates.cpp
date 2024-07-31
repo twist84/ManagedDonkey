07072061c60bbedd8d8c0827210c6142eae59704
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-31 09:40:56 +0100

    Added `crates` functions comments

---
+++ game/source/objects/crates.cpp
@@ -0,0 +1,10 @@
+#include "objects/crates.hpp"
+
+//.text:00BAD000 ; bool __cdecl crate_compute_function_value(long, long, long, real*, bool*, bool*)
+//.text:00BAD010 ; void __cdecl crate_move(long)
+//.text:00BAD030 ; bool __cdecl crate_new(long, object_placement_data*, bool*)
+//.text:00BAD190 ; short __cdecl crate_pathfinding_policy(long, s_scenario_crate*)
+//.text:00BAD1F0 ; void __cdecl crate_place(long, s_scenario_crate*)
+//.text:00BAD2B0 ; bool __cdecl crate_update(long)
+//.text:00BAD370 ; bool __cdecl crate_will_self_destruct(long)
+
