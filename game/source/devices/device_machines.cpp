6edbcd57e41e85a19a64756e9b42d9c55482a154
theTwister <6237734+twist84@users.noreply.github.com>
2024-07-31 22:27:45 +0100

    Added `device_machines` functions comments

---
+++ game/source/devices/device_machines.cpp
@@ -0,0 +1,18 @@
+#include "devices/device_machines.hpp"
+
+//.text:00BB4DB0 ; void __cdecl machine_align_rigid_bodies_to_node_positions(long)
+//.text:00BB4DF0 ; bool __cdecl machine_compute_activation(long, s_game_cluster_bit_vectors const*, bool*)
+//.text:00BB4F10 ; bool __cdecl machine_compute_function_value(long, long, long, real*, bool*, bool*)
+//.text:00BB4F20 ; bool __cdecl machine_definition_is_elevator(machine_definition const*)
+//.text:00BB4F50 ; void __cdecl machine_delete(long)
+//.text:00BB4FF0 ; bool __cdecl machine_door_test_object(long, long*, short)
+//.text:00BB5250 ; bool __cdecl machine_new(long, object_placement_data*, bool*)
+//.text:00BB52E0 ; short __cdecl machine_pathfinding_policy(long, s_scenario_machine const*)
+//.text:00BB5360 ; void __cdecl machine_place(long, s_scenario_machine*)
+//.text:00BB5460 ; void __cdecl machine_render_debug(long)
+//.text:00BB5470 ; void __cdecl machine_try_to_open_with_damage(long)
+//.text:00BB54C0 ; void __cdecl machine_unplace(long)
+//.text:00BB5500 ; bool __cdecl machine_update(long)
+//.text:00BB59B0 ; void __cdecl machine_update_occlusion(long)
+//.text:00BB5AF0 ; bool __cdecl object_is_elevator(long)
+
