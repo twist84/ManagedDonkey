da797fc6af738af6b8459d2175986588acf22163
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-03 18:28:53 +0100

    Create ai_hint_definitions.cpp

---
+++ game/source/ai/ai_hint_definitions.cpp
@@ -0,0 +1,31 @@
+#include "ai/ai_hint_definitions.hpp"
+
+//.text:0148A2C0 ; bool __cdecl ai_hint_door_traversable(pathfinding_data const*, sector_link*, pathfinding_hint_data const*)
+//.text:0148A370 ; bool __cdecl ai_hint_find_closest_point(object_marker*, real_point3d const*, real, real_point3d*)
+//.text:0148A490 ; bool __cdecl ai_hint_find_hoist_marker(long, long, real, object_marker*, real_point3d*)
+//.text:0148A660 ; bool __cdecl ai_hint_find_marker_point(long, long, short, real, object_marker*, real_point3d*)
+//.text:0148A6E0 ; bool __cdecl ai_hint_find_mount_marker(long, long, real, object_marker*, real_point3d*)
+//.text:0148A8A0 ; bool __cdecl ai_hint_find_pill_entry_point(long, long, long, long, long, real_point3d const*, vector3d const*, real_point3d*, c_ai_point3d*, vector3d*, c_sector_ref*)
+//.text:0148A9C0 ; 
+//.text:0148AA20 ; bool __cdecl ai_hint_find_root_entry_point(long, long, long, real_point3d const*, vector3d const*, real_point3d*, c_ai_point3d*, vector3d*, c_sector_ref*)
+//.text:0148AB50 ; bool __cdecl ai_hint_find_vault_marker(long, long, real, object_marker*, real_point3d*)
+//.text:0148AE40 ; e_action_hoist __cdecl ai_hint_get_best_hoist_height(long, real)
+//.text:0148AEF0 ; 
+//.text:0148AFC0 ; 
+//.text:0148B030 ; short __cdecl ai_hint_get_hoist_markers(long, object_marker*, short, short)
+//.text:0148B0B0 ; bool __cdecl ai_hint_get_jump_destination_normal(pathfinding_data*, hint_jump_data*, vector3d*)
+//.text:0148B1C0 ; bool __cdecl ai_hint_get_jump_destination_rail(short, hint_jump_data*, real_point3d const*, real_point3d*, real_point3d*, c_sector_ref*, short*)
+//.text:0148B350 ; bool __cdecl ai_hint_get_jump_maneuver_point(pathfinding_data*, pathfinding_hint_data*, c_ai_point3d const*, c_sector_ref, real, c_ai_point3d*, c_sector_ref*, c_ai_point3d*, c_sector_ref*)
+//.text:0148B610 ; double __cdecl ai_hint_get_jump_velocity(short)
+//.text:0148B670 ; 
+//.text:0148B9E0 ; double __cdecl ai_hint_get_max_jump_down_height(short)
+//.text:0148BA40 ; short __cdecl ai_hint_get_mount_markers(long, object_marker*, short, short)
+//.text:0148BAF0 ; 
+//.text:0148BB60 ; short __cdecl ai_hint_get_vault_markers(long, object_marker*, short, short)
+//.text:0148BBD0 ; 
+//.text:0148BC90 ; 
+//.text:0148BCD0 ; bool __cdecl ai_hint_marker_valid(object_marker*)
+//.text:0148BD20 ; bool __cdecl ai_hint_test_jump(real_point3d const*, real_point3d const*, real_point3d const*, real_point3d const*, short)
+//.text:0148BE80 ; bool __cdecl ai_hint_vault_range_valid(real_point3d const*, real_point3d const*, special_movement const*)
+//.text:0148BF30 ; bool __cdecl ai_link_hint_traversable(struct pathfinding_data const*, long)
+
