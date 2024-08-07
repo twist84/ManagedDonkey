8dcb6a1b8303a5dcd40b73b3e6d40f9cc284e2c2
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-07 07:41:38 +0100

    Updated `giants` functions comments

---
+++ game/source/units/giants.cpp
@@ -0,0 +1,47 @@
+#include "units/giants.hpp"
+
+//.text:00BBB480 ; double __cdecl compute_score_of_buckle_angles(real, real, real_matrix4x3 const*, real_matrix4x3*)
+//.text:00BBB640 ; 
+//.text:00BBB650 ; 
+//.text:00BBB660 ; 
+//.text:00BBB680 ; 
+//.text:00BBB6A0 ; void __cdecl giant_adjust_node_matrices(long, long, real_matrix4x3*)
+//.text:00BBBAE0 ; void __cdecl giant_animation_setup_mixing_board(c_animation_manager*)
+//.text:00BBBB20 ; real __cdecl giant_buckling_magnitude_get(long)
+//.text:00BBBBD0 ; 
+//.text:00BBBC20 ; void __cdecl giant_clear_foot_tracking_data(long)
+//.text:00BBBED0 ; bool __cdecl giant_compute_function_value(long, long, long, real*, bool*, bool*)
+//.text:00BBBF70 ; 
+//.text:00BBBF80 ; bool __cdecl giant_find_ground_point(long, real_point3d const*, real_point3d*, vector3d*, real_point3d*)
+//.text:00BBC100 ; void __cdecl giant_find_pathfinding_location(long, short*, long*, c_sector_ref*, real_point3d*, long*, dword*)
+//.text:00BBC350 ; giant_buckle_parameters const* __cdecl giant_get_current_buckle_parameters(long)
+//.text:00BBC3A0 ; long __cdecl giant_get_current_buckle_state_name(long)
+//.text:00BBC400 ; bool __cdecl giant_handle_motor_update(long, bool)
+//.text:00BBC6A0 ; bool __cdecl giant_ik_foot_chain(long, real_point3d*, real, long, real, real, long, real_matrix4x3*)
+//.text:00BBCDF0 ; 
+//.text:00BBCE80 ; bool __cdecl giant_new(long, object_placement_data*, bool*)
+//.text:00BBCF40 ; 
+//.text:00BBCF80 ; void __cdecl giant_place(long, s_scenario_giant*)
+//.text:00BBD020 ; bool __cdecl giant_predict_future_foot_position(long, long, long, real_point3d*, real_point3d*, vector3d*, real_point3d*)
+//.text:00BBD080 ; void __cdecl giant_preprocess_node_orientations(long, c_static_flags<255> const*, long, real_orientation*)
+//.text:00BBD170 ; void __cdecl giant_render_debug(long)
+//.text:00BBD180 ; void __cdecl giant_set_buckle_desire(long, bool)
+//.text:00BBD340 ; void __cdecl giant_set_crouch_desire(long, bool)
+//.text:00BBD390 ; void __cdecl giant_set_death_desire(long, bool)
+//.text:00BBD400 ; void __cdecl giant_setup_buckling_parameters(long)
+//.text:00BBD990 ; void __cdecl giant_setup_foot_tracking_data(long)
+//.text:00BBDA20 ; bool __cdecl giant_solve_future_marker_position(long, long, long, real_matrix4x3*)
+//.text:00BBDD90 ; bool __cdecl giant_update(long)
+//.text:00BBDEE0 ; void __cdecl giant_update_crouch(long)
+//.text:00BBDF60 ; void __cdecl giant_update_elevation(long, real)
+//.text:00BBE070 ; void __cdecl giant_update_foot_tracking(long)
+//.text:00BBE1A0 ; bool __cdecl giant_waiting_to_buckle(long)
+//.text:00BBE1D0 ; bool __cdecl giant_waiting_to_buckle_or_buckling(long)
+//.text:00BBE250 ; 
+//.text:00BBE2B0 ; 
+//.text:00BBE2E0 ; 
+//.text:00BBE300 ; 
+//.text:00BBE320 ; void __cdecl setup_buckling_globals(long, long, long, real_matrix4x3 const*, real, real, real, real)
+//.text:00BBE590 ; 
+//.text:00BBE5B0 ; 
+
