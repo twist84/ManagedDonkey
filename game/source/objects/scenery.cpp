e2c56f5bd50f88ae77f52ba602899ed9a45d37b0
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-07 06:51:48 +0100

    Added `scenery` functions comments

---
+++ game/source/objects/scenery.cpp
@@ -0,0 +1,33 @@
+#include "objects/scenery.hpp"
+
+//.text:00BB84C0 ; void __cdecl scenery_animation_finish(long)
+//.text:00BB85B0 ; short __cdecl scenery_animation_get_ticks_remaining_for_scripting(long)
+//.text:00BB8680 ; void __cdecl scenery_animation_start(long, long, long)
+//.text:00BB86A0 ; void __cdecl scenery_animation_start_at_frame(long, long, long, short)
+//.text:00BB86C0 ; void __cdecl scenery_animation_start_at_frame_loop(long, long, long, short)
+//.text:00BB86E0 ; void __cdecl scenery_animation_start_idle(long)
+//.text:00BB8760 ; void __cdecl scenery_animation_start_loop(long, long, long)
+//.text:00BB8780 ; bool __cdecl scenery_animation_start_private(long, long, long, short, long, bool, bool)
+//.text:00BB88B0 ; void __cdecl scenery_animation_start_relative(long, long, long, long)
+//.text:00BB88D0 ; void __cdecl scenery_animation_start_relative(long, c_animation_id, real_matrix4x3*)
+//.text:00BB8950 ; void __cdecl scenery_animation_start_relative_at_frame(long, long, long, long, short)
+//.text:00BB8970 ; void __cdecl scenery_animation_start_relative_loop(long, long, long, long)
+//.text:00BB8990 ; bool __cdecl scenery_compute_activation(long, s_game_cluster_bit_vectors const*, bool*)
+//.text:00BB8A70 ; void __cdecl scenery_delete(long)
+//.text:00BB8A80 ; 
+//.text:00BB8A90 ; 
+//.text:00BB8AA0 ; long __cdecl scenery_get_mopp_index(long)
+//.text:00BB8B00 ; bool __cdecl scenery_has_physics_model(long)
+//.text:00BB8B20 ; void __cdecl scenery_initialize_for_new_structure_bsp(long)
+//.text:00BB8B30 ; void __cdecl scenery_initialize_lightmap_object_index(long)
+//.text:00BB8BE0 ; bool __cdecl scenery_is_fixed_rigid_body(long)
+//.text:00BB8D40 ; short __cdecl scenery_lightmapping_policy(long)
+//.text:00BB8D80 ; void __cdecl scenery_move(long)
+//.text:00BB8DD0 ; bool __cdecl scenery_new(long, object_placement_data*, bool*)
+//.text:00BB8E90 ; short __cdecl scenery_pathfinding_policy(long, s_scenario_scenery*)
+//.text:00BB8F00 ; void __cdecl scenery_place(long, s_scenario_scenery*)
+//.text:00BB8F90 ; bool __cdecl scenery_play_animation_id(long, c_animation_id, dword, long)
+//.text:00BB90F0 ; void __cdecl scenery_preprocess_root_node_matrix(long, real_matrix4x3*)
+//.text:00BB9170 ; void __cdecl scenery_render_debug(long)
+//.text:00BB9180 ; bool __cdecl scenery_update(long)
+
