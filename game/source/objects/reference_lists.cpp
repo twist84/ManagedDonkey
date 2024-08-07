44d6c695f82661609459d091dd1158ab07b66b5b
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-07 06:51:34 +0100

    Added `reference_lists` functions comments

---
+++ game/source/objects/reference_lists.cpp
@@ -0,0 +1,12 @@
+#include "objects/reference_lists.hpp"
+
+//.text:00BB6030 ; void __cdecl reference_list_add(s_data_array*, long*, long, long, void const*)
+//.text:00BB6080 ; bool __cdecl reference_list_contains(s_data_array*, long*, long)
+//.text:00BB60D0 ; 
+//.text:00BB60F0 ; void __cdecl reference_list_delete(s_data_array*, long)
+//.text:00BB6130 ; long __cdecl reference_list_get_next_datum_and_payload(s_data_array*, long*, long, void const**)
+//.text:00BB6170 ; long __cdecl reference_list_get_next_datum_index(s_data_array*, long*)
+//.text:00BB61A0 ; s_data_array* __cdecl reference_list_new(char const*, long, long, c_allocation_base*)
+//.text:00BB6210 ; void __cdecl reference_list_remove(s_data_array*, long*, long)
+//.text:00BB6270 ; 
+
