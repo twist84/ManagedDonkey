39df76ba4f5f4c761d171cbf097b0634ac460c32
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-07 06:50:50 +0100

    Added `device_terminals` functions comments

---
+++ game/source/devices/device_terminals.cpp
@@ -0,0 +1,36 @@
+#include "devices/device_terminals.hpp"
+
+//.text:00BB62E0 ; public: __cdecl c_load_terminal_screen_message::c_load_terminal_screen_message(e_controller_index, e_window_index, long, long)
+//.text:00BB6310 ; 
+//.text:00BB6320 ; 
+//.text:00BB6340 ; 
+//.text:00BB6350 ; 
+//.text:00BB6360 ; public: virtual void* __cdecl c_load_terminal_screen_message::`vector deleting destructor'(unsigned int)
+//.text:00BB6390 ; public: virtual void __cdecl c_load_terminal_screen_message::apply_initial_state(c_gui_screen_widget*) const
+//.text:00BB63B0 ; 
+//.text:00BB63C0 ; 
+//.text:00BB6560 ; bool __cdecl terminal_compute_function_value(long, long, long, real*, bool*, bool*)
+//.text:00BB66C0 ; void __cdecl terminal_delete(long)
+//.text:00BB66D0 ; 
+//.text:00BB6770 ; long __cdecl terminal_get_action_string(long)
+//.text:00BB67B0 ; bool __cdecl terminal_new(long, object_placement_data*, bool*)
+//.text:00BB68E0 ; void __cdecl terminal_place(long, s_scenario_terminal*)
+//.text:00BB68F0 ; 
+//.text:00BB6950 ; bool __cdecl terminal_update(long)
+//.text:00BB75F0 ; 
+//.text:00BB7600 ; 
+//.text:00BB7630 ; 
+//.text:00BB76A0 ; 
+//.text:00BB77D0 ; 
+//.text:00BB7870 ; 
+//.text:00BB78C0 ; 
+//.text:00BB78D0 ; 
+//.text:00BB79D0 ; void __cdecl terminal_seek(long, real, real, real)
+//.text:00BB7AF0 ; void __cdecl terminal_seek_to_zero(long, real)
+//.text:00BB7D20 ; 
+//.text:00BB7D80 ; 
+//.text:00BB7DC0 ; 
+//.text:00BB8090 ; 
+//.text:00BB8120 ; 
+//.text:00BB81B0 ; 
+
