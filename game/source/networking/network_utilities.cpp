ed26662b0bcea87bb4cb45d01fdd61869f9c1168
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-15 20:56:38 +0100

    Added `network_utilities` function comments

--- game/source/networking/network_utilities.cpp
+++ game/source/networking/network_utilities.cpp
@@ -8,10 +8,19 @@
 
 HOOK_DECLARE(0x0042ED50, network_get_machine_name);
 
+//.text:0042EC70 ; 
+//.text:0042EC80 ; 
+
 short __cdecl _random_range(dword* seed_ptr, char const* name, char const* filename, dword line, short range_begin, short range_end)
 {
 	return INVOKE(0x0042EC90, _random_range, seed_ptr, name, filename, line, range_begin, range_end);
 }
+
+//.text:0042ECC0 ; 
+//.text:0042ECD0 ; 
+//.text:0042ECE0 ; bool __cdecl network_build_is_compatible(long, long, long)
+//.text:0042ED10 ; 
+//.text:0042ED20 ; void __cdecl network_get_build_identifiers(long*, long*, long*)
 
 bool __cdecl network_get_machine_name(wchar_t* machine_name, long machine_name_len)
 {
@@ -24,6 +33,13 @@ bool __cdecl network_get_machine_name(wchar_t* machine_name, long machine_name_l
 	ascii_string_to_wchar_string(system_identifier, machine_name, machine_name_len, nullptr);
 	return true;
 }
+
+//.text:0042ED60 ; void __cdecl network_get_player_colors(e_game_team, bool, c_enum<e_player_color_index, char, -1, 32> const* const, real_rgb_color* const)
+//.text:0042EE60 ; bool __cdecl network_get_random_player_name(wchar_t*, long)
+//.text:0042EF10 ; real_rgb_color __cdecl network_get_team_color(e_game_team)
+//.text:0042EF60 ; bool __cdecl network_get_unique_player_name(wchar_t*, long, long, wchar_t const**)
+//.text:0042F0A0 ; bool __cdecl network_is_random_player_name(wchar_t const*)
+//.text:0042F160 ; bool __cdecl network_joining_should_be_disabled()
 
 void __cdecl network_remote_reporting_dispose()
 {
@@ -42,6 +58,9 @@ void __cdecl network_remote_reporting_initialize()
 	//
 	//netdebug_initialize(shell_get_target(), version_get_build_identifier(), system_identifier);
 }
+
+//.text:0042F1F0 ; void __cdecl network_set_machine_name(wchar_t const*)
+//.text:0042F200 ; 
 
 void bandwidth_profiler_render()
 {
