66b72d5ab6fe62af603fc16e7a8a920e938ab609
theTwister <6237734+twist84@users.noreply.github.com>
2024-08-15 20:58:15 +0100

    Mapped `first_person_weapon` (WIP)

--- game/source/interface/first_person_weapons.hpp
+++ game/source/interface/first_person_weapons.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "animations/animation_interpolation.hpp"
 #include "cseries/cseries.hpp"
+
+enum
+{
+	k_first_person_max_weapons = 2
+};
 
 struct s_first_person_orientations
 {
@@ -8,11 +14,28 @@ struct s_first_person_orientations
 };
 static_assert(sizeof(s_first_person_orientations) == 0x12C00);
 
+struct first_person_weapon_data
+{
+	dword_flags flags;
+	byte __data4[0x279C];
+};
+static_assert(sizeof(first_person_weapon_data) == 0x27A0);
+
 struct first_person_weapon
 {
-	byte __data[0x14000];
+	dword_flags flags;
+	long unit_index;
+
+	byte __data8[0x30];
+
+	first_person_weapon_data weapon_data[k_first_person_max_weapons];
+
+	c_interpolator_control __unknown4F78;
+	byte __data4F84[0x3C];
+	real_matrix4x3 __identity4FC0;
+	byte __data4FF4[0xC];
 };
-static_assert(sizeof(first_person_weapon) == 0x14000);
+static_assert(sizeof(first_person_weapon) == 0x5000);
 
 enum e_output_user_index;
 
