a745e801a45724a670ea5209ef55f1dcc91e03ef
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-04 04:27:56 +0100

    Added `overlapped_memory_dispose/initialize`

--- game/source/cseries/async_xoverlapped_memory.cpp
+++ game/source/cseries/async_xoverlapped_memory.cpp
@@ -4,3 +4,18 @@ void* __cdecl overlapped_malloc_tracked(dword size, char const* file, long line)
 {
 	return INVOKE(0x0066D570, overlapped_malloc_tracked, size, file, line);
 }
+
+//.text:0066D620 ; c_allocation_base* __cdecl get_overlapped_allocation()
+
+void __cdecl overlapped_memory_dispose()
+{
+	INVOKE(0x0066D630, overlapped_memory_dispose);
+}
+
+//.text:0066D650 ; void __cdecl overlapped_memory_dump(char const*)
+
+void __cdecl overlapped_memory_initialize()
+{
+	INVOKE(0x0066D670, overlapped_memory_initialize);
+}
+
--- game/source/cseries/async_xoverlapped_memory.hpp
+++ game/source/cseries/async_xoverlapped_memory.hpp
@@ -3,3 +3,6 @@
 #include "cseries/cseries.hpp"
 
 extern void* __cdecl overlapped_malloc_tracked(dword size, char const* file, long line);
+extern void __cdecl overlapped_memory_dispose();
+extern void __cdecl overlapped_memory_initialize();
+
