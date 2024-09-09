22e6608dcb80c46683b4fb442e5e3128cab113b4
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-09 10:39:32 +0100

    Updated `c_start_menu_settings_appearance_model`

--- game/source/interface/gui_screens/start_menu/panes/settings_appearance_model/start_menu_settings_appearance_model.hpp
+++ game/source/interface/gui_screens/start_menu/panes/settings_appearance_model/start_menu_settings_appearance_model.hpp
@@ -2,11 +2,12 @@
 
 #include "cseries/cseries.hpp"
 #include "interface/gui_screens/start_menu/start_menu_pane_screen_widget.hpp"
+#include "shell/shell.hpp"
 
 struct c_start_menu_settings_appearance_model :
 	public c_start_menu_pane_screen_widget
 {
-	byte __data2168[0x8];
+	c_string_id m_model_customizations[k_number_of_player_model_choices];
 };
 static_assert(sizeof(c_start_menu_settings_appearance_model) == sizeof(c_start_menu_pane_screen_widget) + 0x8);
 
