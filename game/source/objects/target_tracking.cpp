3b1b6b4b72370ccd5eb5c031cda64bf6b98a74af
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-17 12:56:05 +0100

    Create target_tracking.cpp

---
+++ game/source/objects/target_tracking.cpp
@@ -0,0 +1,24 @@
+#include "objects/target_tracking.hpp"
+
+//.text:00B74BE0 ; private: long __cdecl c_target_tracking_system::get_best_target_index(real*) const
+//.text:00B74C90 ; public: real __cdecl c_target_tracking_system::get_locking_theta() const
+//.text:00B74CB0 ; public: real __cdecl c_tracked_target::get_locking_theta() const
+//.text:00B74D00 ; public: c_aim_target_object const* __cdecl c_target_tracking_system::get_target() const
+//.text:00B74D20 ; 
+//.text:00B74D30 ; public: void __cdecl c_target_tracking_system::initialize()
+//.text:00B74D70 ; 
+//.text:00B74D90 ; public: bool __cdecl c_target_tracking_system::is_locked() const
+//.text:00B74DD0 ; 
+//.text:00B74DD0 ; 
+//.text:00B74DD0 ; 
+//.text:00B74DE0 ; public: bool __cdecl c_target_tracking_system::is_tracking() const
+//.text:00B74E40 ; 
+//.text:00B74E50 ; 
+//.text:00B74E60 ; public: bool __cdecl c_target_tracking_system::set_from_weapon_fire_event(c_aim_target_simulation_entity const*, s_target_tracking_parameters const*)
+//.text:00B74EB0 ; public: bool __cdecl c_tracked_target::set_from_weapon_fire_event(c_aim_target_simulation_entity const*, s_target_tracking_parameters const*)
+//.text:00B74F30 ; 
+//.text:00B74F70 ; bool __cdecl target_tracking_system_has_tracking_type(s_target_tracking_parameters const*, long)
+//.text:00B74FA0 ; public: void __cdecl c_target_tracking_system::update(c_aim_target_object const*, s_target_tracking_parameters const*)
+//.text:00B75100 ; 
+//.text:00B751C0 ; 
+
