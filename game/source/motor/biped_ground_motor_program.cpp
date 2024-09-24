ccf118eb06543f64686b8f2778b0b78fc12e00a0
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-24 08:20:27 +0100

    Implemented `cheat_super_jump`
    I still dislike inline assembly

---
+++ game/source/motor/biped_ground_motor_program.cpp
@@ -0,0 +1,51 @@
+#include "motor/biped_ground_motor_program.hpp"
+
+#include "game/cheats.hpp"
+#include "memory/module.hpp"
+#include "objects/objects.hpp"
+
+//.text:00BE1B30 ; bool __cdecl biped_jump(long)
+
+__declspec(naked) void super_jump_inline()
+{
+	// original instructions
+	//     push    0
+	//     lea     eax, [ebp-0x40]
+	//     push    eax
+	//     push    esi
+	//     call    object_get_velocities
+
+	// all this for the folling
+	//if (cheat.super_jump && biped->unit.player_index != NONE)
+	//    jump_velocity = jump_velocity * 4.0f;
+
+	static real const velocity_multiplier = 4.0f;
+	ASM_ADDR(0x00BE1C39, addr_BE1C39);
+
+	__asm
+	{
+		// execute our instructions
+		cmp     cheat.super_jump, 0
+		jz      loc_BE1C2D
+		cmp     dword ptr[edi + 0x198], NONE
+		jz      loc_BE1C2D
+
+		movss   xmm0, [ebp-0x8]
+		movss   xmm1, dword ptr[velocity_multiplier]
+		mulss   xmm0, xmm1
+		movss   [ebp-0x8], xmm0
+
+		// execute the original instructions
+	loc_BE1C2D:
+		push    0
+		lea     eax, [ebp-0x40]
+		push    eax
+		push    esi
+		call    object_get_velocities
+
+		// jump out to after our hook
+		jmp     addr_BE1C39
+	}
+}
+HOOK_DECLARE(0x00BE1C2D, super_jump_inline);
+
