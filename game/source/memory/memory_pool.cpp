55e986cd0ba74dea4e82b90536f5b611d62c70a4
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-30 12:48:14 +0100

    Implemented `memory_pool_block_free_handle`

---
+++ game/source/memory/memory_pool.cpp
@@ -0,0 +1,131 @@
+#include "memory/memory_pool.hpp"
+
+struct s_memory_pool_block_header
+{
+	long __unknown0;
+	long __unknown4;
+	const char* __unknown8;
+	long __unknownC;
+};
+static_assert(sizeof(s_memory_pool_block_header) == 0x10);
+
+struct s_memory_pool_block
+{
+	dword size;
+	byte __data4[0x4];
+	long next_block_handle;
+	long previous_block_handle;
+};
+static_assert(sizeof(s_memory_pool_block) == 0x10);
+
+dword memory_pool_handle_from_address(s_memory_pool const* pool, void const* pointer)
+{
+	ASSERT(memory_pool);
+	ASSERT(pointer);
+
+	return pointer_distance(pool, pointer);
+}
+
+long __cdecl sub_969DB0(long a1)
+{
+	return INVOKE(0x00969DB0, sub_969DB0, a1);
+}
+
+void* __cdecl memory_pool_get_address(s_memory_pool const* memory_pool, long payload_handle)
+{
+	ASSERT(memory_pool);
+
+	return payload_handle ? (void*)offset_pointer(memory_pool, payload_handle) : NULL;
+}
+
+s_memory_pool_block* __cdecl memory_pool_get_block(s_memory_pool const* pool, long payload_handle)
+{
+	ASSERT(pool);
+
+	return (s_memory_pool_block*)memory_pool_get_address(pool, payload_handle);
+}
+
+s_memory_pool_block* __cdecl memory_pool_block_get(s_memory_pool const* pool, long payload_handle)
+{
+	ASSERT(payload_handle);
+
+	//memory_pool_verify(pool);
+	s_memory_pool_block* block = memory_pool_get_block(pool, sub_969DB0(payload_handle));
+	//ASSERT(memory_pool_block_valid(pool, block));
+
+	s_memory_pool_block* other_block = NULL;
+	for (other_block = memory_pool_get_block(pool, pool->first_block_handle);
+		other_block&& block != other_block;
+		other_block = memory_pool_get_block(pool, other_block->next_block_handle))
+	{
+		;
+	}
+
+	ASSERT(other_block);
+
+	return block;
+}
+
+void __cdecl memory_pool_block_free_handle(s_memory_pool* pool, dword payload_handle)
+{
+	//INVOKE(0x00969C80, memory_pool_block_free_handle, pool, payload_handle);
+
+	s_memory_pool_block* block = memory_pool_block_get(pool, payload_handle);
+	//memory_pool_verify(pool);
+	//ASSERT(memory_pool_block_valid(pool, block));
+
+	pool->free_size += block->size;
+	ASSERT(pool->free_size <= pool->size);
+
+	if (block->previous_block_handle)
+		memory_pool_get_block(pool, block->previous_block_handle)->next_block_handle = block->next_block_handle;
+	else
+		pool->first_block_handle = block->next_block_handle;
+
+	if (block->next_block_handle)
+		memory_pool_get_block(pool, block->next_block_handle)->previous_block_handle = block->previous_block_handle;
+	else
+		pool->last_block_handle = block->previous_block_handle;
+
+	//csmemset(memory_pool_block_get_base(pool, block), 0xDD, block->size);
+	//memory_pool_verify(pool);
+
+	//int v2; // ecx
+	//int next_block_handle; // eax
+	//int previous_block_handle; // esi
+	//int v5; // esi
+	//int v6; // eax
+	//
+	//if (payload_handle == 0x10)
+	//	v2 = 0;
+	//else
+	//	v2 = (int)pool + payload_handle - 0x10;
+	//pool->free_size += *(dword*)v2;
+	//next_block_handle = *(dword*)(v2 + 8);
+	//previous_block_handle = *(dword*)(v2 + 12);
+	//if (previous_block_handle)
+	//	*(dword*)&pool->name[previous_block_handle + 4] = next_block_handle;
+	//else
+	//	pool->first_block_handle = next_block_handle;
+	//v5 = *(dword*)(v2 + 8);
+	//v6 = *(dword*)(v2 + 12);
+	//if (v5)
+	//	*(dword*)&pool->name[v5 + 8] = v6;
+	//else
+	//	pool->last_block_handle = v6;
+}
+
+void memory_pool_block_free(s_memory_pool* pool, void const** payload_data)
+{
+	//INVOKE(0x00969CD0, memory_pool_block_free, pool, payload_data);
+
+	ASSERT(payload_data);
+
+	memory_pool_block_free_handle(pool, memory_pool_handle_from_address(pool, *payload_data));
+}
+
+long __cdecl memory_pool_get_block_size(s_memory_pool* memory_pool, void const** payload_handle)
+{
+	return INVOKE(0x0096A4E0, memory_pool_get_block_size, memory_pool, payload_handle);
+}
+
--- game/source/memory/memory_pool.hpp
+++ game/source/memory/memory_pool.hpp
@@ -17,9 +17,15 @@ struct s_memory_pool
 	bool allocation_from_anywhere_in_pool;
 	bool verification_enabled;
 	bool __unknown3F_bool;
-	long callback_list;
+	long reference_tracking_index;
 
 	// block?
 	byte __data[0x10];
 };
 static_assert(sizeof(s_memory_pool) == 0x44 + 0x10);
+
+extern dword memory_pool_handle_from_address(s_memory_pool const* memory_pool, void const* pointer);
+extern void __cdecl memory_pool_block_free_handle(s_memory_pool* pool, dword payload_handle);
+extern void memory_pool_block_free(s_memory_pool* pool, void const** payload_data);
+extern long __cdecl memory_pool_get_block_size(s_memory_pool* memory_pool, void const** payload_handle);
+
