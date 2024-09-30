9f443e6b2c9487cd750bdfc14291195cf62bec77
theTwister <6237734+twist84@users.noreply.github.com>
2024-09-30 12:50:45 +0100

    Hooked `user_interface_free`

--- game/source/interface/user_interface_memory.cpp
+++ game/source/interface/user_interface_memory.cpp
@@ -1,8 +1,39 @@
 #include "interface/user_interface_memory.hpp"
 
-void __cdecl user_interface_free(void const* pointer)
+#include "game/game.hpp"
+#include "memory/memory_pool.hpp"
+#include "memory/module.hpp"
+#include "multithreading/synchronization.hpp"
+
+REFERENCE_DECLARE(0x0526979C, s_memory_pool*, g_ui_memory_pool);
+
+HOOK_DECLARE(0x00AB4E50, user_interface_free);
+
+c_ui_memory_scope_lock::c_ui_memory_scope_lock()
 {
-	INVOKE(0x00AB4E50, user_interface_free, pointer);
+	if (game_is_multithreaded())
+		internal_critical_section_enter(_critical_section_ui_memory);
+}
+
+c_ui_memory_scope_lock::~c_ui_memory_scope_lock()
+{
+	if (game_is_multithreaded())
+		internal_critical_section_leave(_critical_section_ui_memory);
+}
+
+void __cdecl user_interface_free(void const* ptr)
+{
+	//INVOKE(0x00AB4E50, user_interface_free, ptr);
+
+	ASSERT(ptr != NULL);
+	ASSERT(g_ui_memory_pool != NULL);
+	//ASSERT(user_interface_is_tracked_memory(ptr));
+	//ASSERT(!user_interface_messaging_is_memory_in_message_queue(ptr));
+
+	c_ui_memory_scope_lock scope_lock;
+
+	memory_pool_get_block_size(g_ui_memory_pool, &ptr);
+	memory_pool_block_free(g_ui_memory_pool, &ptr);
 }
 
 void* __cdecl user_interface_malloc_tracked(dword size, char const* file, long line)
--- game/source/interface/user_interface_memory.hpp
+++ game/source/interface/user_interface_memory.hpp
@@ -2,7 +2,14 @@
 
 #include "cseries/cseries.hpp"
 
-extern void __cdecl user_interface_free(void const* pointer);
+struct c_ui_memory_scope_lock
+{
+public:
+	c_ui_memory_scope_lock();
+	~c_ui_memory_scope_lock();
+};
+
+extern void __cdecl user_interface_free(void const* ptr);
 extern void* __cdecl user_interface_malloc_tracked(dword size, char const* file, long line);
 extern c_allocation_base* __cdecl user_interface_memory_allocation();
 
