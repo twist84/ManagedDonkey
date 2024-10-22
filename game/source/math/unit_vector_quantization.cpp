8b10e172f8b5e99513e0c6bfb201173fa79bb3e5
theTwister <6237734+twist84@users.noreply.github.com>
2024-10-22 16:07:28 +0100

    Added `dequantize_unit_vector3d`

---
+++ game/source/math/unit_vector_quantization.cpp
@@ -0,0 +1,7 @@
+#include "math/unit_vector_quantization.hpp"
+
+void __cdecl dequantize_unit_vector3d(long quantized_value, vector3d* up, long size_in_bits)
+{
+	INVOKE(0x005AF780, dequantize_unit_vector3d, quantized_value, up, size_in_bits);
+}
+
---
+++ game/source/math/unit_vector_quantization.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+extern void __cdecl dequantize_unit_vector3d(long quantized_value, vector3d* up, long size_in_bits);
+
