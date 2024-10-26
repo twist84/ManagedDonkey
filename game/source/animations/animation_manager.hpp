1aea4445a00e0aeedae53c440efb8800b48aa78c
theTwister <6237734+twist84@users.noreply.github.com>
2024-10-26 21:53:31 +0100

    Mapped more of `c_animation_manager`

--- game/source/animations/animation_manager.hpp
+++ game/source/animations/animation_manager.hpp
@@ -6,10 +6,26 @@
 
 struct c_animation_manager
 {
+	long get_state_name() const
+	{
+		return m_state_name.get_value();
+	}
+
 	c_animation_channel __unknown0;
 	c_animation_channel __unknown3C;
 	c_interpolator_control __unknown78;
-	byte __data84[0x3C];
+
+	long m_mixing_board_definition_index;
+
+	byte __data88[0x4];
+
+	int m_graph_tag_index;
+
+	byte __data90[0x10];
+
+	c_string_id m_state_name;
+
+	byte __data[0x1C];
 };
 static_assert(sizeof(c_animation_manager) == 0xC0);
 
