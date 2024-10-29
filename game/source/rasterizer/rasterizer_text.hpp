d5d0059912c8ba83ab9a18f6bb281281a1daee2f
theTwister <6237734+twist84@users.noreply.github.com>
2024-10-29 23:56:06 +0000

    Mapped `hardware_character_cache_globals`

--- game/source/rasterizer/rasterizer_text.hpp
+++ game/source/rasterizer/rasterizer_text.hpp
@@ -19,5 +19,37 @@ public:
 };
 static_assert(sizeof(c_rasterizer_texture_ref) == 0x4);
 
+struct s_hardware_character
+{
+	long character_data_index;
+	long __unknown4;
+	long __unknown8;
+	short __unknownC;
+	short __unknownE;
+	short __unknown10;
+	short __unknown12;
+};
+static_assert(sizeof(s_hardware_character) == 0x14);
+
+struct s_hardware_character_cache_globals
+{
+	bool initialized;
+	byte __pad1[0x3];
+	long __unknown4;
+	long __unknown8;
+	long __unknownC;
+	long __unknown10;
+	long __unknown14;
+	bitmap_data* bitmap;
+	c_rasterizer_texture_ref hardware_format;
+	real __unknown20;
+	real __unknown24;
+	c_static_array<short, 512> __unknown28;
+	c_static_array<s_hardware_character, 512> hardware_characters;
+};
+static_assert(sizeof(s_hardware_character_cache_globals) == 0x2C28);
+
+extern s_hardware_character_cache_globals& hardware_character_cache_globals;
+
 extern void __cdecl rasterizer_bitmap_2d_changed_rect(bitmap_data const* bitmap, c_rasterizer_texture_ref texture_ref, short_rectangle2d const* dirty_rect, bool a4);
 
