d001fb6fec5f67c761d0fa4b4711fdd0cf54e7aa
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-03 04:48:48 +0000

    Updated `c_rasterizer_loading_screen` functions
    All of these are just nullsubs

--- game/source/rasterizer/rasterizer_loading_screen.cpp
+++ game/source/rasterizer/rasterizer_loading_screen.cpp
@@ -2,50 +2,61 @@
 
 bool __cdecl c_rasterizer_loading_screen::active()
 {
-	return INVOKE(0x00A204E0, c_rasterizer_loading_screen::active);
+	//return INVOKE(0x00A204E0, c_rasterizer_loading_screen::active);
+
+	return false;
 }
 
 void __cdecl c_rasterizer_loading_screen::dispose()
 {
-	INVOKE(0x00A204F0, c_rasterizer_loading_screen::dispose);
+	//INVOKE(0x00A204F0, c_rasterizer_loading_screen::dispose);
 }
 
 bool __cdecl c_rasterizer_loading_screen::fading_out()
 {
-	return INVOKE(0x00A20500, c_rasterizer_loading_screen::fading_out);
+	//return INVOKE(0x00A20500, c_rasterizer_loading_screen::fading_out);
+
+	return false;
 }
 
-// .text:00A20510  ; 
+real __cdecl c_rasterizer_loading_screen::get_completion_remainder()
+{
+	//return INVOKE(0x00A20510, c_rasterizer_loading_screen::get_completion_remainder);
+
+	return 0.0f;
+}
 
 void __cdecl c_rasterizer_loading_screen::initialize()
 {
-	INVOKE(0x00A20520, c_rasterizer_loading_screen::initialize);
+	//INVOKE(0x00A20520, c_rasterizer_loading_screen::initialize);
 }
 
 void __cdecl c_rasterizer_loading_screen::render()
 {
-	INVOKE(0x00A20530, c_rasterizer_loading_screen::render);
+	//INVOKE(0x00A20530, c_rasterizer_loading_screen::render);
 }
 
 void __cdecl c_rasterizer_loading_screen::set_completion_remainder(real completion_remainder)
 {
-	INVOKE(0x00A20540, c_rasterizer_loading_screen::set_completion_remainder, completion_remainder);
+	//INVOKE(0x00A20540, c_rasterizer_loading_screen::set_completion_remainder, completion_remainder);
 }
 
 void __cdecl c_rasterizer_loading_screen::start()
 {
-	INVOKE(0x00A20550, c_rasterizer_loading_screen::start);
+	//INVOKE(0x00A20550, c_rasterizer_loading_screen::start);
 }
 
-// .text:00A20560  ; bool __cdecl loading_basic_progress_keep_pumping()
+//.text:00A20560 ; bool __cdecl loading_basic_progress_keep_pumping()
 
 void __cdecl c_rasterizer_loading_screen::stop()
 {
-	INVOKE(0x00A20570, c_rasterizer_loading_screen::stop);
+	//INVOKE(0x00A20570, c_rasterizer_loading_screen::stop);
 }
 
 bool __cdecl c_rasterizer_loading_screen::suppress_render_scene()
 {
-	return INVOKE(0x00A20580, c_rasterizer_loading_screen::suppress_render_scene);
+	//return INVOKE(0x00A20580, c_rasterizer_loading_screen::suppress_render_scene);
+
+	return false;
 }
 
--- game/source/rasterizer/rasterizer_loading_screen.hpp
+++ game/source/rasterizer/rasterizer_loading_screen.hpp
@@ -7,6 +7,7 @@ struct c_rasterizer_loading_screen
 	static bool __cdecl active();
 	static void __cdecl dispose();
 	static bool __cdecl fading_out();
+	static real __cdecl get_completion_remainder();
 	static void __cdecl initialize();
 	static void __cdecl render();
 	static void __cdecl set_completion_remainder(real completion_remainder);
