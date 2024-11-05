29ae38f79a55538c10e85150e8cc258a6876acd6
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-05 08:59:32 +0000

    Added `render_sky_prepare_for_window`
    nullsub

---
+++ game/source/render/render_sky.cpp
@@ -0,0 +1,9 @@
+#include "render/render_sky.hpp"
+
+#include "cseries/cseries.hpp"
+
+void __cdecl render_sky_prepare_for_window()
+{
+	//INVOKE(0x00A7A8F0, render_sky_prepare_for_window);
+}
+
---
+++ game/source/render/render_sky.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+extern void __cdecl render_sky_prepare_for_window();
+
