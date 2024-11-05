fab7ce218b94fd8fc72756b1a186e70e40b107c2
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-05 09:02:14 +0000

    Added `c_render_flags`

---
+++ game/source/render/render_flags.cpp
@@ -0,0 +1,60 @@
+#include "render/render_flags.hpp"
+
+#define MAXIMUM_PLAYER_WINDOWS 4
+
+REFERENCE_DECLARE(0x0524B6D4, dword, c_render_flags::g_this_frame);
+REFERENCE_DECLARE(0x0524B6D8, dword, c_render_flags::g_last_frame);
+REFERENCE_DECLARE(0x0524B6DC, dword, c_render_flags::g_this_window);
+REFERENCE_DECLARE(0x0524B6E0, dword, c_render_flags::g_last_window);
+REFERENCE_DECLARE_ARRAY(0x0524B6E4, dword, c_render_flags::g_last_windows, 4);
+REFERENCE_DECLARE(0x0524B6F4, dword, c_render_flags::g_active_player_window_index);
+
+void __cdecl c_render_flags::prepare_for_frame()
+{
+	INVOKE(0x00A29680, c_render_flags::prepare_for_frame);
+
+	//c_render_flags::g_last_frame = c_render_flags::g_this_frame;
+	//c_render_flags::g_this_frame = 0;
+}
+
+void __cdecl c_render_flags::prepare_for_player_window(long player_window_index)
+{
+	INVOKE(0x00A296A0, c_render_flags::prepare_for_player_window, player_window_index);
+
+	//ASSERT(VALID_INDEX(player_window_index, MAXIMUM_PLAYER_WINDOWS));
+	//
+	//c_render_flags::g_last_windows[c_render_flags::g_active_player_window_index] = c_render_flags::g_this_window;
+	//c_render_flags::g_active_player_window_index = player_window_index;
+	//
+	//c_render_flags::g_last_window = c_render_flags::g_last_windows[c_render_flags::g_active_player_window_index];
+	//c_render_flags::g_this_window = 0;
+}
+
+void __cdecl c_render_flags::set_frame_bit(e_frame_bit frame_bit, dword flags)
+{
+	INVOKE(0x00A3B760, c_render_flags::set_frame_bit, frame_bit, flags);
+
+	//SET_BIT(c_render_flags::g_this_frame, frame_bit, flags);
+}
+
+bool __cdecl c_render_flags::test_frame_bit(e_frame_bit frame_bit)
+{
+	return INVOKE(0x00A3BF90, c_render_flags::test_frame_bit, frame_bit);
+
+	//return TEST_BIT(c_render_flags::g_this_frame, frame_bit);
+}
+
+bool __cdecl c_render_flags::test_last_window_bit(e_window_bit window_bit)
+{
+	return INVOKE(0x00A3BFB0, c_render_flags::test_last_window_bit, window_bit);
+
+	//return TEST_BIT(c_render_flags::g_last_window, window_bit);
+}
+
+void __cdecl c_render_flags::or_window_bit(e_window_bit window_bit, dword flags)
+{
+	INVOKE(0x00A781A0, c_render_flags::or_window_bit, window_bit, flags);
+
+	//c_render_flags::g_this_window |= (flags & 1) << window_bit;
+}
+
---
+++ game/source/render/render_flags.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "cseries/cseries.hpp"
+
+struct c_render_flags
+{
+public:
+	enum e_frame_bit
+	{
+		// #TODO: find all bits
+	};
+	
+	enum e_window_bit
+	{
+		// #TODO: find all bits
+	};
+
+	static void __cdecl prepare_for_frame();
+	static void __cdecl prepare_for_player_window(long player_window_index);
+	static void __cdecl set_frame_bit(e_frame_bit frame_bit, dword flags);
+	static bool __cdecl test_frame_bit(e_frame_bit frame_bit);
+	static bool __cdecl test_last_window_bit(e_window_bit window_bit);
+	static void __cdecl or_window_bit(e_window_bit window_bit, dword flags);
+
+	static dword& g_this_frame;
+	static dword& g_last_frame;
+	static dword& g_this_window;
+	static dword& g_last_window;
+	static dword(&g_last_windows)[4];
+	static dword& g_active_player_window_index;
+};
+
