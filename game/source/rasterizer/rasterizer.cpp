5e4c55162b18de32ae8856d881484757c0c21230
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-05 09:00:24 +0000

    Added `c_rasterizer::window_reset`

--- game/source/rasterizer/rasterizer.cpp
+++ game/source/rasterizer/rasterizer.cpp
@@ -363,6 +363,15 @@ void __cdecl c_rasterizer::shell_initialize(bool window_exists, bool windowed)
 	}
 }
 
+void __cdecl c_rasterizer::window_reset()
+{
+	INVOKE(0x00A204D0, c_rasterizer::window_reset);
+
+	//c_rasterizer::invalidate_all_surfaces();
+	//rasterizer_memory_reset();
+	//rasterizer_occlusion_window_reset();
+}
+
 void __cdecl c_rasterizer::set_render_resolution(long width, long height, bool fullscreen)
 {
 	//INVOKE(0x00A22130, c_rasterizer::set_render_resolution, width, height, fullscreen);
