8d5ddd536b828eb0031635ce4502cbe19cf5d162
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-18 13:08:05 +0000

    Updated `c_gui_roster_data`

--- game/source/interface/gui_roster_data.cpp
+++ game/source/interface/gui_roster_data.cpp
@@ -11,19 +11,19 @@ HOOK_DECLARE_CLASS_MEMBER(0x00B24FE0, c_gui_roster_data, _get_text_value);
 
 // #TODO: reimplement `c_gui_active_roster_data::update`
 
-bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, long name, long* integer_value)
+bool __thiscall c_gui_roster_data::_get_integer_value(long element_handle, long value_name, long* value)
 {
 	bool result = false;
-	HOOK_INVOKE_CLASS_MEMBER(result =, c_gui_roster_data, _get_integer_value, player_row_index, name, integer_value);
+	HOOK_INVOKE_CLASS_MEMBER(result =, c_gui_roster_data, _get_integer_value, element_handle, value_name, value);
 
-	s_player_configuration* player_data = user_interface_session_get_player_data(m_players[player_row_index].player_index);
-	switch (name)
+	s_player_configuration* player_data = user_interface_session_get_player_data(m_players[element_handle].session_player_index);
+	switch (value_name)
 	{
 	case STRING_ID(gui, base_color):
 	case STRING_ID(gui, base_color_hilite):
 	{
 		if (player_data)
-			*integer_value = player_data->host.armor.loadouts[player_data->host.armor.loadout_index].colors[_color_type_primary].value;
+			*value = player_data->host.armor.loadouts[player_data->host.armor.loadout_index].colors[_color_type_primary].value;
 
 		if (!result)
 			return true;
@@ -38,12 +38,12 @@ bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, lon
 	case STRING_ID(gui, player_row_type):
 	{
 		// simulate added controllers
-		if (player_row_index >= 1 && player_row_index < 4)
+		if (element_handle >= 1 && element_handle < 4)
 		{
-			*integer_value = c_gui_roster_data::_player_row_type_player;
-			//*integer_value = c_gui_roster_data::_player_row_type_player_found;
-			//*integer_value = c_gui_roster_data::_player_row_type_looking_for_player;
-			//*integer_value = c_gui_roster_data::_player_row_type_press_a_to_join;
+			*value = c_gui_roster_data::_player_row_type_player;
+			//*value = c_gui_roster_data::_player_row_type_player_found;
+			//*value = c_gui_roster_data::_player_row_type_looking_for_player;
+			//*value = c_gui_roster_data::_player_row_type_press_a_to_join;
 		}
 
 		if (!result)
@@ -70,11 +70,11 @@ bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, lon
 	break;
 	case STRING_ID(gui, experience):
 	{
-		*integer_value = (player_row_index % 4) + 1;
+		*value = (element_handle % 4) + 1;
 
 		// #TODO: pull this from an api?
 		if (player_data && player_data->host.weapon.loadouts[0].bungienet_user.test(_bungienet_user_bungie))
-			*integer_value = 42;
+			*value = 42;
 
 		if (!result)
 			return true;
@@ -82,11 +82,11 @@ bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, lon
 	break;
 	case STRING_ID(gui, skill_level):
 	{
-		*integer_value = (player_row_index % 4) + 1;
+		*value = (element_handle % 4) + 1;
 
 		// #TODO: pull this from an api?
 		if (player_data && player_data->host.weapon.loadouts[0].bungienet_user.test(_bungienet_user_bungie))
-			*integer_value = 50;
+			*value = 50;
 
 		if (!result)
 			return true;
@@ -95,10 +95,10 @@ bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, lon
 	case STRING_ID(gui, bungienet_user):
 	{
 		// Look At Me. I'm The Bungie Now.
-		*integer_value |= FLAG(_bungienet_user_default);
+		*value |= FLAG(_bungienet_user_default);
 
 		if (player_data)
-			*integer_value = player_data->host.weapon.loadouts[0].bungienet_user;
+			*value = player_data->host.weapon.loadouts[0].bungienet_user;
 
 		if (!result)
 			return true;
@@ -109,12 +109,12 @@ bool __thiscall c_gui_roster_data::_get_integer_value(long player_row_index, lon
 	return result;
 }
 
-bool __thiscall c_gui_roster_data::_get_text_value(long player_row_index, long name, c_static_wchar_string<1024>* text_value)
+bool __thiscall c_gui_roster_data::_get_text_value(long element_handle, long value_name, c_static_wchar_string<1024>* value)
 {
 	bool result = false;
-	HOOK_INVOKE_CLASS_MEMBER(result =, c_gui_roster_data, _get_text_value, player_row_index, name, text_value);
+	HOOK_INVOKE_CLASS_MEMBER(result =, c_gui_roster_data, _get_text_value, element_handle, value_name, value);
 
-	switch (name)
+	switch (value_name)
 	{
 	case STRING_ID(global, player_name):
 	{
@@ -130,41 +130,41 @@ bool __thiscall c_gui_roster_data::_get_text_value(long player_row_index, long n
 	break;
 	case STRING_ID(global, press_a_to_join):
 	{
-		text_value->set(L"PRESS A TO JOIN");
+		value->set(L"PRESS A TO JOIN");
 		if (!result)
 			return true;
 	}
 	break;
 	case STRING_ID(gui, looking_for_player):
 	{
-		text_value->set(L"LOOKING FOR PLAYER");
+		value->set(L"LOOKING FOR PLAYER");
 		if (!result)
 			return true;
 	}
 	break;
 	case STRING_ID(gui, player_found):
 	{
-		text_value->set(L"PLAYER FOUND");
+		value->set(L"PLAYER FOUND");
 		if (!result)
 			return true;
 	}
 	break;
 	case STRING_ID(gui, service_tag):
 	{
-		if (!m_players[player_row_index].player_data.host.appearance.service_tag.length())
+		if (!m_players[element_handle].player_configuration.host.appearance.service_tag.length())
 		{
-			long player_index = m_players[player_row_index].player_index;
+			long player_index = m_players[element_handle].session_player_index;
 			if (user_interface_session_is_local_player(player_index))
 			{
-				c_controller_interface* controller = controller_get(m_players[player_row_index].controller_index);
+				c_controller_interface* controller = controller_get(m_players[element_handle].local_controller_index);
 
-				if (!text_value->length())
+				if (!value->length())
 				{
-					text_value->print(L"%s - %s", L"SPARTAN", controller->m_player_profile.m_desired_service_tag);
+					value->print(L"%s - %s", L"SPARTAN", controller->m_player_profile.m_desired_service_tag);
 				}
 				else
 				{
-					text_value->append(controller->m_player_profile.m_desired_service_tag);
+					value->append(controller->m_player_profile.m_desired_service_tag);
 				}
 			}
 			else
@@ -172,13 +172,13 @@ bool __thiscall c_gui_roster_data::_get_text_value(long player_row_index, long n
 				s_player_configuration* player_data = user_interface_session_get_player_data(player_index);
 				if (player_data)
 				{
-					if (!text_value->length())
+					if (!value->length())
 					{
-						text_value->print(L"%s - %s", L"SPARTAN", player_data->host.appearance.service_tag.get_string());
+						value->print(L"%s - %s", L"SPARTAN", player_data->host.appearance.service_tag.get_string());
 					}
 					else
 					{
-						text_value->append(player_data->host.appearance.service_tag.get_string());
+						value->append(player_data->host.appearance.service_tag.get_string());
 					}
 				}
 			}
--- game/source/interface/gui_roster_data.hpp
+++ game/source/interface/gui_roster_data.hpp
@@ -24,42 +24,57 @@ struct c_gui_roster_data :
 	struct s_player_row
 	{
 		c_enum<e_player_row_type, long, _player_row_type_player, k_player_row_type_count> player_row_type;
-		long player_index;
+		long session_player_index;
+
 		s_player_identifier player_identifier;
-		bool __unknown10;
+		bool player_identifier_valid;
+
+		// pad
 		long __unknown14;
-		s_player_configuration player_data;
-		bool __unknown1638;
-		long skill_level;
-		long __unknown1640;
-		long peer_index;
-		long peer_user_index;
+
+		s_player_configuration player_configuration;
+		bool player_configuration_valid;
+
+		struct
+		{
+			long skill_level;
+			long experience;
+		} configuration;
+
+		long machine_index;
+		long squad_join_sequence_number;
 		long __unknown164C;
-		qword party_nonce;
-		long leader_team_index;
-		c_enum<e_controller_index, long, _controller_index0, k_number_of_controllers> controller_index;
-		long voice_talking_state;
+
+		qword squad_nonce;
+
+		long leader_team;
+		c_enum<e_controller_index, long, _controller_index0, k_number_of_controllers> local_controller_index;
+		long voice_state;
 		long party_bar_length;
 		bool is_leader;
 		bool show_teams;
 		long special_status;
-		byte __data[0x8];
+		
+		//bool in_matchmaking;
+		struct
+		{
+			long highest_party_skill;
+			long highest_party_experience;
+		} calculated_for_group;
 	};
 	static_assert(sizeof(s_player_row) == 0x1678);
 
 public:
-	bool __thiscall _get_integer_value(long player_row_index, long name, long* integer_value);
-	bool __thiscall _get_text_value(long player_row_index, long name, c_static_wchar_string<1024>* text_value);
+	bool __thiscall _get_integer_value(long element_handle, long value_name, long* value);
+	bool __thiscall _get_text_value(long element_handle, long value_name, c_static_wchar_string<1024>* value);
 
 protected:
-	long __unknown10C;
-	long __unknown110;
-	byte __data114[4];
-	c_gui_roster_data::s_player_row m_players[16];
+	long m_matchmaking_last_known_good_extra_slots_searching;
+	long m_matchmaking_last_known_good_extra_slots_found;
+	c_static_array<c_gui_roster_data::s_player_row, 16> m_players;
 	long m_player_count;
-	c_enum<e_controller_index, long, _controller_index0, k_number_of_controllers> m_controller_index;
-	bool __unknown168A0;
-	byte __data168A1[0x7];
+	c_enum<e_controller_index, long, _controller_index0, k_number_of_controllers> m_driving_controller;
+	bool m_pause_updating;
 };
 static_assert(sizeof(c_gui_roster_data) == 0x168A8);
 
