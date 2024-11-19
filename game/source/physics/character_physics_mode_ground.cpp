24cf3fc5b3c22bc80a8439a33bc674e2784b46a6
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-19 14:58:00 +0000

    Updated `c_character_physics_mode_ground_datum`

--- game/source/physics/character_physics_mode_ground.cpp
+++ game/source/physics/character_physics_mode_ground.cpp
@@ -16,39 +16,41 @@ HOOK_DECLARE_CLASS_MEMBER(0x00724890, c_character_physics_mode_ground_datum, upd
 
 vector3d const* c_character_physics_mode_ground_datum::get_ground_plane() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_ground_plane);
+	return INVOKE_CLASS_MEMBER(0x00723570, c_character_physics_mode_ground_datum, get_ground_plane);
 }
 
 real c_character_physics_mode_ground_datum::get_ground_plane_contact_offset() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_ground_plane_contact_offset);
+	return INVOKE_CLASS_MEMBER(0x00723580, c_character_physics_mode_ground_datum, get_ground_plane_contact_offset);
 }
 
 real c_character_physics_mode_ground_datum::get_landing_velocity() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_landing_velocity);
+	return INVOKE_CLASS_MEMBER(0x007235C0, c_character_physics_mode_ground_datum, get_landing_velocity);
 }
 
 //.text:007235D0
 
 long c_character_physics_mode_ground_datum::get_support_havok_component_index() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_support_havok_component_index);
+	return INVOKE_CLASS_MEMBER(0x007235E0, c_character_physics_mode_ground_datum, get_support_havok_component_index);
+
+	//return m_support_havok_component_index;
 }
 
 real_matrix4x3 const* c_character_physics_mode_ground_datum::get_support_havok_component_matrix() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_support_havok_component_matrix);
+	return INVOKE_CLASS_MEMBER(0x007235F0, c_character_physics_mode_ground_datum, get_support_havok_component_matrix);
 }
 
 long c_character_physics_mode_ground_datum::get_support_object_index() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_support_object_index);
+	return INVOKE_CLASS_MEMBER(0x00723600, c_character_physics_mode_ground_datum, get_support_object_index);
 }
 
 long c_character_physics_mode_ground_datum::get_support_rigid_body_index() const
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, get_support_rigid_body_index);
+	return INVOKE_CLASS_MEMBER(0x00723610, c_character_physics_mode_ground_datum, get_support_rigid_body_index);
 }
 
 bool havok_contact_point_reference_valid(s_havok_contact_point_reference const* reference)
@@ -61,14 +63,14 @@ void c_character_physics_mode_ground_datum::initialize()
 	INVOKE_CLASS_MEMBER(0x00723640, c_character_physics_mode_ground_datum, initialize);
 }
 
-void c_character_physics_mode_ground_datum::initialize_internal(bool a1)
+void c_character_physics_mode_ground_datum::initialize_internal(bool initialize_support_data)
 {
-	INVOKE_CLASS_MEMBER(0x007236C0, c_character_physics_mode_ground_datum, initialize_internal, a1);
+	INVOKE_CLASS_MEMBER(0x007236C0, c_character_physics_mode_ground_datum, initialize_internal, initialize_support_data);
 }
 
-void c_character_physics_mode_ground_datum::move(s_character_physics_move_output_datum* output, s_character_physics_move_input_datum const* input)
+void c_character_physics_mode_ground_datum::move(s_character_physics_move_output_datum* physics_output, s_character_physics_move_input_datum const* physics_input)
 {
-	INVOKE_CLASS_MEMBER(0x00723720, c_character_physics_mode_ground_datum, move, output, input);
+	INVOKE_CLASS_MEMBER(0x00723720, c_character_physics_mode_ground_datum, move, physics_output, physics_input);
 }
 
 //.text:00723DC0
@@ -79,21 +81,21 @@ void c_character_physics_mode_ground_datum::notify_in_slip_surface_volume()
 	INVOKE_CLASS_MEMBER(0x00723E60, c_character_physics_mode_ground_datum, notify_in_slip_surface_volume);
 }
 
-void c_character_physics_mode_ground_datum::set_contact(long a1, long a2, vector3d const* a3)
+void c_character_physics_mode_ground_datum::set_contact(long havok_component_index, s_havok_contact_point_reference const* primary_contact_point_reference, vector3d const* ground_plane)
 {
-	INVOKE_CLASS_MEMBER(0x00723E70, c_character_physics_mode_ground_datum, set_contact, a1, a2, a3);
+	INVOKE_CLASS_MEMBER(0x00723E70, c_character_physics_mode_ground_datum, set_contact, havok_component_index, primary_contact_point_reference, ground_plane);
 }
 
-void c_character_physics_mode_ground_datum::set_ground_plane(vector3d const* a1)
+void c_character_physics_mode_ground_datum::set_ground_plane(vector3d const* ground_plane)
 {
-	INVOKE_CLASS_MEMBER(0x00723F00, c_character_physics_mode_ground_datum, set_ground_plane, a1);
+	INVOKE_CLASS_MEMBER(0x00723F00, c_character_physics_mode_ground_datum, set_ground_plane, ground_plane);
 }
 
 //.text:00723F20 ; c_character_physics_mode_ground_datum::snap_facing
 
-void c_character_physics_mode_ground_datum::update(s_character_physics_update_output_datum* output, struct s_character_physics_update_input_datum const* input)
+void c_character_physics_mode_ground_datum::update(s_character_physics_update_output_datum* physics_output, struct s_character_physics_update_input_datum const* physics_input)
 {
-	INVOKE_CLASS_MEMBER(0x00724550, c_character_physics_mode_ground_datum, update, output, input);
+	INVOKE_CLASS_MEMBER(0x00724550, c_character_physics_mode_ground_datum, update, physics_output, physics_input);
 }
 
 //.text:00724800
@@ -101,12 +103,12 @@ void c_character_physics_mode_ground_datum::update(s_character_physics_update_ou
 // fix jump canceling on forged objects
 bool __thiscall c_character_physics_mode_ground_datum::update_ground_velocity(s_character_physics_update_input_datum const* physics_input, vector3d* linear_velocity, matrix3x3* rotation)
 {
+	//INVOKE_CLASS_MEMBER(0x00724890, c_character_physics_mode_ground_datum, update_ground_velocity, physics_input, linear_velocity, rotation);
+
 	bool result = false;
 	HOOK_INVOKE_CLASS_MEMBER(result =, c_character_physics_mode_ground_datum, update_ground_velocity, physics_input, linear_velocity, rotation);
 
-	//long support_havok_component_index = get_support_havok_component_index();
-	REFERENCE_DECLARE(((byte*)this) + 0x1C, long, support_havok_component_index);
-
+	long support_havok_component_index = get_support_havok_component_index();
 	if (support_havok_component_index == NONE)
 		return result;
 
@@ -123,14 +125,14 @@ bool __thiscall c_character_physics_mode_ground_datum::update_ground_velocity(s_
 	return result;
 }
 
-void c_character_physics_mode_ground_datum::update_internal(s_character_physics_update_output_datum* output, s_character_physics_update_input_datum const* input, bool a2, vector3d const* a3)
+void c_character_physics_mode_ground_datum::update_internal(s_character_physics_update_output_datum* physics_output, s_character_physics_update_input_datum const* physics_input, bool localize_airborne, vector3d const* localized_velocity)
 {
-	INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, update_internal, output, input, a2, a3);
+	INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, update_internal, physics_output, physics_input, localize_airborne, localized_velocity);
 }
 
-bool c_character_physics_mode_ground_datum::update_localized_physics_space_linear_velocity(s_character_physics_update_input_datum const* input, vector3d* a1, vector3d* a2, matrix3x3* a3)
+bool c_character_physics_mode_ground_datum::update_localized_physics_space_linear_velocity(s_character_physics_update_input_datum const* physics_input, vector3d* linear_velocity, vector3d* space_acceleration, matrix3x3* rotation)
 {
-	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, update_localized_physics_space_linear_velocity, input, a1, a2, a3);
+	return INVOKE_CLASS_MEMBER(0x00724BB0, c_character_physics_mode_ground_datum, update_localized_physics_space_linear_velocity, physics_input, linear_velocity, space_acceleration, rotation);
 }
 
 //.text:00725A60
--- game/source/physics/character_physics_mode_ground.hpp
+++ game/source/physics/character_physics_mode_ground.hpp
@@ -1,5 +1,7 @@
 #include "cseries/cseries.hpp"
 
+#include "game/materials.hpp"
+
 struct s_character_physics_definition;
 struct c_animation_manager;
 struct hkShape;
@@ -96,15 +98,37 @@ struct c_character_physics_mode_ground_datum
 	long get_support_object_index() const;
 	long get_support_rigid_body_index() const;
 	void initialize();
-	void initialize_internal(bool a1);
-	void move(s_character_physics_move_output_datum* output, s_character_physics_move_input_datum const* input);
+	void initialize_internal(bool initialize_support_data);
+	void move(s_character_physics_move_output_datum* physics_output, s_character_physics_move_input_datum const* physics_input);
 	void notify_in_slip_surface_volume();
-	void set_contact(long a1, long a2, vector3d const* a3);
-	void set_ground_plane(vector3d const* a1);
-	void update(s_character_physics_update_output_datum* output, struct s_character_physics_update_input_datum const* input);
-	void update_internal(s_character_physics_update_output_datum* output, s_character_physics_update_input_datum const* input, bool a2, vector3d const* a3);
-	bool update_localized_physics_space_linear_velocity(s_character_physics_update_input_datum const* input, vector3d* a1, vector3d* a2, matrix3x3* a3);
+	void set_contact(long havok_component_index, s_havok_contact_point_reference const* primary_contact_point_reference, vector3d const* ground_plane);
+	void set_ground_plane(vector3d const* ground_plane);
+	void update(s_character_physics_update_output_datum* physics_output, s_character_physics_update_input_datum const* physics_input);
+	void update_internal(s_character_physics_update_output_datum* physics_output, s_character_physics_update_input_datum const* physics_input, bool localize_airborne, vector3d const* localized_velocity);
+	bool update_localized_physics_space_linear_velocity(s_character_physics_update_input_datum const* physics_input, vector3d* linear_velocity, vector3d* space_acceleration, matrix3x3* rotation);
 
 	bool __thiscall update_ground_velocity(s_character_physics_update_input_datum const* physics_input, vector3d* linear_velocity, matrix3x3* rotation);
+
+	vector3d m_last_localized_physics_velocity;
+	long m_last_time_animation_velocity;
+	vector3d m_last_animation_velocity;
+	long m_support_havok_component_index;
+	long m_support_havok_component_rigid_body_index;
+	real_matrix4x3 m_support_havok_component_matrix;
+	long m_surface_index;
+	long m_support_object_index;
+	c_global_material_type m_ground_material_type;
+	byte m_ground_physics_update_ticks;
+	byte_flags m_flags;
+	vector3d m_ground_plane;
+	real m_landing_velocity;
 };
+static_assert(sizeof(c_character_physics_mode_ground_datum) == 0x74);
+
+struct s_havok_contact_point_reference
+{
+	short rigid_body_index;
+	short contact_point_index;
+};
+static_assert(sizeof(s_havok_contact_point_reference) == 0x4);
 
