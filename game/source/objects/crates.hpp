7f4e45498900353482295db151506d3cb740ff83
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-19 20:05:31 +0000

    Added `crate_datum`

--- game/source/objects/crates.hpp
+++ game/source/objects/crates.hpp
@@ -1,2 +1,20 @@
 #pragma once
 
+#include "cseries/cseries.hpp"
+#include "objects/objects.hpp"
+
+struct _crate_datum
+{
+	dword_flags flags;
+	long self_destruction_timer;
+};
+static_assert(sizeof(_crate_datum) == 0x8);
+
+struct crate_datum
+{
+	long definition_index;
+	_object_datum object;
+	_crate_datum crate;
+};
+static_assert(sizeof(crate_datum) == sizeof(long) + sizeof(_object_datum) + sizeof(_crate_datum));
+
