702fb022d8a56944952dd82a3ffce83543c7b42f
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-19 19:12:57 +0000

    Updated `device_datum`

--- game/source/devices/devices.hpp
+++ game/source/devices/devices.hpp
@@ -9,43 +9,38 @@ struct device_group_datum :
 	s_datum_header
 {
 	word_flags flags;
-	real __unknown4; // desired value, actual value
-	real __unknown8; // desired value, actual value
+	real desired_value;
+	real initial_value;
 	dword __unknownC;
 };
 static_assert(sizeof(device_group_datum) == 0x10);
 
 struct s_device_animation_control
 {
-	real __unknown0;
-	real __unknown4;
-	real __unknown8;
-	real __unknownC;
-	real __unknown10;
-	real __unknown14;
-	real __unknown18;
-	real __unknown1C;
+	real current_position;
+	real current_velocity;
+	real time_remaining;
+	real cruise_start_time;
+	real cruise_end_time;
+	real cruise_velocity;
+	real exit_velocity;
+	real exit_pos;
 };
 static_assert(sizeof(s_device_animation_control) == 0x20);
 
 struct _device_datum
 {
 	dword_flags flags;
-
 	long power_group_index;
 	real power;
 	real power_velocity;
-
 	long position_group_index;
 	real position;
 	real position_velocity;
-
 	short delay_ticks;
-	short __unknown1E;
-
+	word pad;
 	s_device_animation_control position_track;
 	s_device_animation_control power_and_overlay_track;
-
 	c_animation_channel position_channel;
 	c_animation_channel power_and_overlay_track_channel;
 };
