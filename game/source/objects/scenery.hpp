74421b068806258774655ad9fefa23e22eb7b9e2
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-19 20:14:04 +0000

    Added `scenery_datum`

--- game/source/objects/scenery.hpp
+++ game/source/objects/scenery.hpp
@@ -1,2 +1,21 @@
 #pragma once
 
+#include "objects/objects.hpp"
+
+struct _scenery_datum
+{
+	long flags;
+	short pathfinding_policy;
+	long lightmap_object_index;
+	long animation_relative_object_index;
+};
+static_assert(sizeof(_scenery_datum) == 0x10);
+
+struct scenery_datum
+{
+	long definition_index;
+	_object_datum object;
+	_scenery_datum scenery;
+};
+static_assert(sizeof(scenery_datum) == sizeof(long) + sizeof(_object_datum) + sizeof(_scenery_datum));
+
