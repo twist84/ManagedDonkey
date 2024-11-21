8acdf274d301c1381ee3657db027539c373d0bc2
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-21 16:25:06 +0000

    Updated `s_game_sound_globals`

--- game/source/sound/game_sound.hpp
+++ game/source/sound/game_sound.hpp
@@ -10,9 +10,34 @@ struct game_looping_sound_datum :
 };
 static_assert(sizeof(game_looping_sound_datum) == 0x20);
 
+struct s_game_sound_listener
+{
+	long cluster_loop_index;
+	long active_fog_definition_index;
+	bool tracked_as_underwater;
+};
+static_assert(sizeof(s_game_sound_listener) == 0xC);
+
+struct s_sound_global_environment_parameters
+{
+	real direct_gain;
+	real direct_hf_gain;
+	real reverb_gain;
+	real reverb_hf_gain;
+	long speaker_filter_frequency;
+	real speaker_filter_gain;
+};
+static_assert(sizeof(s_sound_global_environment_parameters) == 0x18);
+
 struct s_game_sound_globals
 {
-	byte __data[0x154];
+	c_static_array<s_game_sound_listener, 4> listeners;
+	long dialog_ducker_stop_tick;
+	c_static_array<word, 128> portal_occlusions;
+	s_sound_global_environment_parameters global_environment_parameters;
+	bool allow_details_in_cinematics;
+	bool suppress_ambience_update_on_revert;
+	real additional_occlusion_interpolator_dt;
 };
 static_assert(sizeof(s_game_sound_globals) == 0x154);
 
