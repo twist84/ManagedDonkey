a537eb4410cbe8a47bdb5dce8744f0c8e16fb1b6
theTwister <6237734+twist84@users.noreply.github.com>
2024-11-27 20:45:43 +0000

    Added `content_item` commented functions

--- game/source/saved_games/content_item.cpp
+++ game/source/saved_games/content_item.cpp
@@ -1,2 +1,37 @@
 #include "saved_games/content_item.hpp"
 
+//.text:0074CB10 ; public: __cdecl s_content_item::s_content_item()
+//.text:0074CB40 ; public: __cdecl s_content_item::~s_content_item()
+//.text:0074CB50 ; public: void __cdecl s_content_item::block_until_finished()
+//.text:0074CB60 ; protected: void __cdecl s_content_item::create_from_enumeration(e_game_content_type, c_content_catalogue*, struct _XCONTENT_DATA const*)
+//.text:0074CBE0 ; protected: bool __cdecl s_content_item::create_new(e_game_content_type, c_content_catalogue*, wchar_t const*, long, long, bool)
+//.text:0074CBF0 ; protected: bool __cdecl s_content_item::flush(bool)
+//.text:0074CC00 ; public: wchar_t const* __cdecl s_content_item::get_container_full_path(wchar_t*, long, bool) const
+//.text:0074CC10 ; 
+//.text:0074CC20 ; 
+//.text:0074CC30 ; 
+//.text:0074CC40 ; 
+//.text:0074CC50 ; public: wchar_t* __cdecl s_content_item::get_dashboard_display_name(wchar_t*, long) const
+//.text:0074CCF0 ; public: bool __cdecl s_content_item::get_directory_reference(s_file_reference*)
+//.text:0074CD60 ; public: wchar_t* __cdecl s_content_item::get_display_name(wchar_t*, long) const
+//.text:0074CE00 ; public: bool __cdecl s_content_item::get_file_reference(s_file_reference*) const
+//.text:0074CE70 ; public: wchar_t const * __cdecl s_content_item::get_game_file_full_path(wchar_t*, long) const
+//.text:0074CEB0 ; public: bool __cdecl s_content_item::get_saved_game_metadata(s_saved_game_item_metadata*) const
+//.text:0074CF10 ; 
+//.text:0074CF20 ; 
+//.text:0074CF60 ; public: bool __cdecl s_content_item::is_open() const
+//.text:0074CF70 ; public: bool __cdecl s_content_item::is_valid() const
+//.text:0074CFB0 ; 
+//.text:0074CFC0 ; public: bool __cdecl s_content_item::metadata_is_valid() const
+//.text:0074D010 ; protected: bool __cdecl s_content_item::mount(bool, bool, long, bool)
+//.text:0074D020 ; 
+//.text:0074D060 ; protected: void __cdecl s_content_item::remove(bool)
+//.text:0074D070 ; public: bool __cdecl s_content_item::retrieve_saved_game_metadata_internal(s_file_reference*)
+//.text:0074D290 ; 
+//.text:0074D2B0 ; 
+//.text:0074D2C0 ; protected: void __cdecl s_content_item::set_content_catalogue(c_content_catalogue*)
+//.text:0074D300 ; 
+//.text:0074D310 ; protected: void __cdecl s_content_item::set_content_data(struct _XCONTENT_DATA const*)
+//.text:0074D350 ; public: void __cdecl s_content_item::set_saved_game_metadata(s_saved_game_item_metadata const*)
+//.text:0074D390 ; protected: void __cdecl s_content_item::unmount(bool)
+
