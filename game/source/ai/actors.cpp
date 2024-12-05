18f52773b364679b70834279a488fa95e94925f8
theTwister <6237734+twist84@users.noreply.github.com>
2024-12-05 15:13:56 +0000

    Added more `actors` function comments

--- game/source/ai/actors.cpp
+++ game/source/ai/actors.cpp
@@ -3,7 +3,22 @@
 #include "ai/swarms.hpp"
 #include "memory/thread_local.hpp"
 
-//.text:014289F0 ; 
+//.text:01428390 ; 
+//.text:014283B0 ; 
+//.text:014283D0 ; 
+//.text:014283F0 ; 
+//.text:01428400 ; 
+//.text:01428410 ; 
+//.text:01428470 ; real_argb_color const* __cdecl actor_activation_debug_color(long)
+//.text:014284B0 ; void __cdecl actor_attach_unit(long, long)
+//.text:01428590 ; bool __cdecl actor_attacking_target(long, vector3d*)
+//.text:01428640 ; void __cdecl actor_berserk_update(long)
+//.text:01428700 ; void __cdecl actor_braindead(long, bool)
+//.text:01428760 ; void __cdecl actor_break(long, bool)
+//.text:014287B0 ; void __cdecl actor_broken_update(long)
+//.text:01428820 ; bool __cdecl actor_bubbleshield_attached(long)
+//.text:014288D0 ; void __cdecl actor_change_squad(long, short)
+//.text:014289F0 ; void __cdecl actor_clear_orders(long)
 //.text:01428A50 ; void __cdecl actor_clear_output(long)
 //.text:01428B20 ; void __cdecl actor_clear_spawn_definition(actor_spawn_definition*)
 //.text:01428BC0 ; void __cdecl actor_cover_update(long)
@@ -12,7 +27,7 @@
 //.text:01428E80 ; bool __cdecl actor_create_kamikaze_grenades(long)
 //.text:01428EE0 ; void __cdecl actor_customize_unit(long, long, long, long, short, long)
 //.text:014291D0 ; void __cdecl actor_customize_weapon(long, long)
-//.text:01429350 ; 
+//.text:01429350 ; bool __cdecl actor_datum_available_to_current_thread()
 
 void __cdecl actor_delete(long actor_index, bool a2)
 {
@@ -68,7 +83,7 @@ void __cdecl actor_erase(long actor_index, bool delete_immediately)
 //.text:0142A9C0 ; bool __cdecl actor_get_running_blind_vector(long, vector3d*)
 //.text:0142AAF0 ; long __cdecl actor_get_secondary_weapon(long)
 //.text:0142AB50 ; short __cdecl actor_get_standing(long)
-//.text:0142ABD0 ; 
+//.text:0142ABD0 ; bool __cdecl actor_get_task_direction(long, vector3d*)
 //.text:0142AC90 ; void __cdecl actor_get_timeslices(long)
 //.text:0142ADB0 ; long __cdecl actor_get_weapon(long)
 //.text:0142AE50 ; void __cdecl actor_handle_change_internal(long, bool, bool)
@@ -79,7 +94,7 @@ void __cdecl actor_erase(long actor_index, bool delete_immediately)
 //.text:0142B560 ; void __cdecl actor_handle_get_attention(long, long)
 //.text:0142B600 ; bool __cdecl actor_handle_kill_volume(long)
 //.text:0142B6A0 ; void __cdecl actor_handle_order_change(long, orders_definition*)
-//.text:0142B6E0 ; 
+//.text:0142B6E0 ; void __cdecl actor_handle_projectile_attach(long, long)
 //.text:0142B770 ; void __cdecl actor_handle_stun(long, real, vector3d const*)
 //.text:0142B8E0 ; void __cdecl actor_handle_task_change(long, s_task*)
 //.text:0142B940 ; void __cdecl actor_handle_weapon_trade(long, long, long, long)
@@ -90,7 +105,7 @@ void __cdecl actor_erase(long actor_index, bool delete_immediately)
 //.text:0142BD10 ; void __cdecl actor_initiate_combat(long)
 //.text:0142BD90 ; void __cdecl actor_input_sample_position(long, long, actor_position_data*)
 //.text:0142BE50 ; void __cdecl actor_input_update(long)
-//.text:0142C690 ; 
+//.text:0142C690 ; bool __cdecl actor_is_active(actor_datum const*)
 //.text:0142C6A0 ; bool __cdecl actor_is_blind(long)
 //.text:0142C700 ; bool __cdecl actor_is_deaf(long)
 //.text:0142C750 ; bool __cdecl actor_is_fighting(long)
@@ -146,7 +161,7 @@ actor_datum* __cdecl actor_iterator_next(actor_iterator* iterator)
 //.text:0142CC80 ; actor_datum* __cdecl actor_nearby_actors_iterator_next(actor_nearby_actors_iterator*)
 //.text:0142CE20 ; long __cdecl actor_new(long)
 //.text:0142D590 ; long __cdecl actor_place(short, actor_spawn_definition const*)
-//.text:0142E080 ; 
+//.text:0142E080 ; void __cdecl actor_preupdate(long)
 //.text:0142E0B0 ; void __cdecl actor_randomly_control_unit(long, long)
 //.text:0142E170 ; void __cdecl actor_relinquish_pathfinding_timeslice(long)
 //.text:0142E1A0 ; bool __cdecl actor_runtime_within_structure_bsp(long)
@@ -154,7 +169,7 @@ actor_datum* __cdecl actor_iterator_next(actor_iterator* iterator)
 //.text:0142E410 ; void __cdecl actor_select_character_voice(long, long)
 //.text:0142E6E0 ; bool __cdecl actor_set_active(long, bool)
 //.text:0142E7F0 ; bool __cdecl actor_set_active_camo(long, bool)
-//.text:0142E810 ; 
+//.text:0142E810 ; bool __cdecl actor_set_active_camo(long, bool, real)
 //.text:0142E8B0 ; bool __cdecl actor_set_bring_forward(long, bool, real, real)
 //.text:0142E9C0 ; void __cdecl actor_set_covered(long, bool)
 //.text:0142EA10 ; void __cdecl actor_set_mission_critical(long, long)
@@ -164,7 +179,7 @@ actor_datum* __cdecl actor_iterator_next(actor_iterator* iterator)
 //.text:0142EDC0 ; void __cdecl actor_suppress_combat(long)
 //.text:0142EDF0 ; void __cdecl actor_suppress_combat_update(long)
 //.text:0142EED0 ; bool __cdecl actor_switch_bsp(long)
-//.text:0142EFC0 ; 
+//.text:0142EFC0 ; long __cdecl actor_target_unit_index(long)
 //.text:0142F010 ; bool __cdecl actor_test_activation_cluster_bit_vector(long, s_game_cluster_bit_vectors const*)
 //.text:0142F150 ; void __cdecl actor_unit_control(long)
 //.text:0142F2C0 ; void __cdecl actor_unit_control_boost(long, bool)
@@ -176,8 +191,8 @@ actor_datum* __cdecl actor_iterator_next(actor_iterator* iterator)
 //.text:0142F4B0 ; bool __cdecl actor_unit_is_mission_critical(long)
 //.text:0142F500 ; void __cdecl actor_update(long)
 //.text:0142F640 ; void __cdecl actor_update_goal_status(long)
-//.text:0142F6E0 ; 
-//.text:0142F6F0 ; 
+//.text:0142F6E0 ; bool __cdecl actor_uses_gravemind(long)
+//.text:0142F6F0 ; bool __cdecl actor_vehicle_charge_allowed(long)
 //.text:0142F860 ; void __cdecl actor_vehicle_update(long)
 //.text:0142FD10 ; void __cdecl actors_dispose()
 //.text:0142FD30 ; void __cdecl actors_dispose_from_old_map()
@@ -186,7 +201,7 @@ actor_datum* __cdecl actor_iterator_next(actor_iterator* iterator)
 //.text:0142FE80 ; void __cdecl actors_initialize_for_new_map()
 //.text:0142FEC0 ; void __cdecl actors_move_randomly()
 //.text:0142FF30 ; void __cdecl actors_update()
-//.text:014302E0 ; 
+//.text:014302E0 ; bool __cdecl ai_area_type_allowed(short, area_definition const*)
 //.text:01430340 ; 
 //.text:01430350 ; 
 //.text:01430390 ; 
