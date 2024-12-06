8f1e908d0f8546dab497919c8513ae0050517025
theTwister <6237734+twist84@users.noreply.github.com>
2024-12-06 08:11:09 +0000

    Updated `cinematics` function comments

--- game/source/cutscene/cinematics.cpp
+++ game/source/cutscene/cinematics.cpp
@@ -37,15 +37,15 @@ void __cdecl cinematic_game_pause_handler(bool paused)
 //.text:0067CF10 ; e_cinematic_channel_type __cdecl cinematic_get_channel_type()
 //.text:0067CF60 ; s_cinematic_clip_global const* __cdecl cinematic_get_clip_planes(long*)
 //.text:0067CF90 ; cinematic_get_debug_mode
-//.text:0067CFA0 ; 
-//.text:0067CFE0 ; 
+//.text:0067CFA0 ; void __cdecl cinematic_get_default_chapter_title_bounds(short_rectangle2d*)
+//.text:0067CFE0 ; short __cdecl cinematic_get_descriptor()
 //.text:0067D0A0 ; long __cdecl cinematic_get_early_exit()
-//.text:0067D0C0 ; 
+//.text:0067D0C0 ; double __cdecl cinematic_get_fade_amount()
 //.text:0067D130 ; double __cdecl cinematic_get_letterbox_coverage()
 //.text:0067D1C0 ; long __cdecl cinematic_get_scene_index(long, long)
 //.text:0067D220 ; 
 //.text:0067D230 ; 
-//.text:0067D250 ; 
+//.text:0067D250 ; long __cdecl cinematic_get_tag_index()
 
 bool __cdecl cinematic_in_progress()
 {
@@ -72,18 +72,18 @@ void __cdecl cinematic_initialize_for_new_structure_bsp(dword activating_structu
 	INVOKE(0x0067D410, cinematic_initialize_for_new_structure_bsp, activating_structure_bsp_mask);
 }
 
-//.text:0067D420 ; 
+//.text:0067D420 ; bool __cdecl cinematic_is_letterbox()
 //.text:0067D480 ; void __cdecl cinematic_move_attached_objects(long, long, long, long)
-//.text:0067D550 ; 
-//.text:0067D620 ; 
-//.text:0067D750 ; 
+//.text:0067D550 ; void __cdecl cinematic_new_globals_initialize()
+//.text:0067D620 ; long __cdecl cinematic_object_create(char const*, long, long, dword)
+//.text:0067D750 ; void __cdecl cinematic_object_create_cinematic_anchor(char const*, long)
 //.text:0067D820 ; 
-//.text:0067D950 ; 
+//.text:0067D950 ; void __cdecl cinematic_object_destroy(char const*)
 //.text:0067DA30 ; void __cdecl cinematic_object_destroy_internal(long)
-//.text:0067DAB0 ; 
-//.text:0067DB30 ; 
+//.text:0067DAB0 ; long __cdecl cinematic_object_get(char const*)
+//.text:0067DB30 ; bool __cdecl cinematic_object_get_name_from_object_index(long, char*, long)
 //.text:0067DBC0 ; long __cdecl cinematic_object_try_and_get_by_index(long)
-//.text:0067DC40 ; 
+//.text:0067DC40 ; void __cdecl cinematic_outro_start()
 //.text:0067DC60 ; void __cdecl cinematic_predict_shot(s_cinematic_scene*, long, long, c_tag_resource_collector*)
 
 void __cdecl cinematic_prepare_for_new_zone_set(dword old_active_structure_bsp_mask, dword new_active_structure_bsp_mask)
@@ -119,9 +119,9 @@ void __cdecl cinematic_render(bool a1, bool a2)
 //.text:0067E830 ; void __cdecl cinematic_set_title_delayed(short, real)
 //.text:0067E8B0 ; void __cdecl cinematic_show_letterbox(bool)
 //.text:0067E8D0 ; void __cdecl cinematic_show_letterbox_immediate(bool)
-//.text:0067E920 ; 
-//.text:0067E950 ; 
-//.text:0067E970 ; 
+//.text:0067E920 ; bool __cdecl cinematic_skip_in_progress()
+//.text:0067E950 ; void __cdecl cinematic_skip_start()
+//.text:0067E970 ; void __cdecl cinematic_skip_stop()
 //.text:0067E990 ; void __cdecl cinematic_start()
 
 void __cdecl cinematic_start_user_skip_fade_out()
@@ -134,7 +134,7 @@ void __cdecl cinematic_start_user_skip_fade_out()
 //.text:0067EAF0 ; long __cdecl cinematic_tag_reference_get_animation(long, long, long)
 //.text:0067EB70 ; long __cdecl cinematic_tag_reference_get_bink(long)
 //.text:0067EBB0 ; long __cdecl cinematic_tag_reference_get_cinematic(long)
-//.text:0067EBD0 ; 
+//.text:0067EBD0 ; long __cdecl cinematic_tag_reference_get_dialogue(long, long, long, long)
 //.text:0067EC60 ; long __cdecl cinematic_tag_reference_get_effect(long, long, long, long)
 //.text:0067ED00 ; long __cdecl cinematic_tag_reference_get_music(long, long, long, long)
 //.text:0067ED90 ; long __cdecl cinematic_tag_reference_get_music_looping(long, long, long, long)
@@ -171,7 +171,6 @@ void __cdecl cinematics_game_tick()
 }
 
 //.text:0067F140 ; long __cdecl cortana_tag_reference_get_scene(long)
-//.text:0067F140 ; 
 //.text:0067F160 ; 
 //.text:0067F170 ; 
 //.text:0067F1A0 ; 
