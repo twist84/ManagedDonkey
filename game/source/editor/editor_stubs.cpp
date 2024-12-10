2e140fb41f33ba1d07ddbdc2141a8a09cd589615
theTwister <6237734+twist84@users.noreply.github.com>
2024-12-10 17:12:19 +0000

    Updated `editor_stubs`

--- game/source/editor/editor_stubs.cpp
+++ game/source/editor/editor_stubs.cpp
@@ -9,101 +9,161 @@
 //HOOK_DECLARE(0x0042DFB0, editor_dispose_from_old_structure_bsp);
 HOOK_DECLARE(0x0042DFD0, editor_get_cluster_color);
 HOOK_DECLARE(0x0042DFE0, editor_get_map_name);
+HOOK_DECLARE(0x0042DFF0, editor_handle_tag_reload);
 HOOK_DECLARE(0x0042E000, sub_42E000);
 //HOOK_DECLARE(0x0042E010, editor_initialize);
 //HOOK_DECLARE(0x0042E020, editor_initialize_for_new_map);
 //HOOK_DECLARE(0x0042E030, editor_initialize_for_new_structure_bsp);
 HOOK_DECLARE(0x0042E040, editor_input_inhibited);
 HOOK_DECLARE(0x0042E060, editor_orphan_object);
+HOOK_DECLARE(0x0042E070, editor_prepare_for_tag_reload);
 HOOK_DECLARE(0x0042E080, editor_register_new_object);
 HOOK_DECLARE(0x0042E0C0, editor_save_progress);
 HOOK_DECLARE(0x0042E0D0, editor_should_render_object);
 HOOK_DECLARE(0x0042E0E0, editor_should_render_transparent_geometry);
+HOOK_DECLARE(0x0042E0F0, editor_should_resolve_block_references);
+HOOK_DECLARE(0x0042E100, editor_should_resolve_tag_reference);
 HOOK_DECLARE(0x0042E110, editor_switch_zone_set);
 HOOK_DECLARE(0x0042E120, editor_update);
 HOOK_DECLARE(0x0042E130, game_in_editor);
+HOOK_DECLARE(0x0042E140, game_in_editor_player_simulation);
 
+//.text:0042DF80 ; 
 void __cdecl editor_change_pvs(s_game_cluster_bit_vectors const* old_cluster_activation, s_game_cluster_bit_vectors const* new_cluster_activation, bool local)
 {
 }
 
+//.text:0042DF90 ; 
 void __cdecl editor_dispose()
 {
 }
 
+//.text:0042DFA0 ; 
 void __cdecl editor_dispose_from_old_map()
 {
 }
 
+//.text:0042DFB0 ; 
 void __cdecl editor_dispose_from_old_structure_bsp(dword deactivating_structure_bsp_mask)
 {
 }
 
+//.text:0042DFC0 ; 
+
+//.text:0042DFD0 ; 
 void __cdecl editor_get_cluster_color(s_cluster_reference const* cluster_reference, real_argb_color* cluster_color)
 {
 }
 
+//.text:0042DFE0 ; 
 char const* __cdecl editor_get_map_name()
 {
 	return "";
 }
 
+//.text:0042DFF0 ; 
+void __cdecl editor_handle_tag_reload(bool reloading_scenario)
+{
+}
+
 // used in `input_update`
 // something like `editor_can_update_input`?
+//.text:0042E000 ; 
 bool __cdecl sub_42E000()
 {
 	return false;
 }
 
+//.text:0042E010 ; 
 void __cdecl editor_initialize()
 {
 }
 
+//.text:0042E020 ; 
 void __cdecl editor_initialize_for_new_map()
 {
 }
 
+//.text:0042E030 ; 
 void __cdecl editor_initialize_for_new_structure_bsp(dword activating_structure_bsp_mask)
 {
 }
 
+//.text:0042E040 ; 
 bool __cdecl editor_input_inhibited()
 {
 	return true;
 }
 
-void __cdecl editor_orphan_object(e_object_type type, long a2)
+//.text:0042E050 ; 
+
+//.text:0042E060 ; 
+void __cdecl editor_orphan_object(e_object_type object_type, long scenario_datum_index)
 {
 }
 
-void __cdecl editor_register_new_object(e_object_type type, long a2, long a3)
+//.text:0042E070 ; 
+void __cdecl editor_prepare_for_tag_reload(bool reloading_scenario)
 {
 }
 
+//.text:0042E080 ; 
+void __cdecl editor_register_new_object(e_object_type object_type, long scenario_datum_index, long new_object_index)
+{
+}
+
+//.text:0042E090 ; 
+//.text:0042E0A0 ; 
+//.text:0042E0B0 ; 
+
+//.text:0042E0C0 ; 
 void __cdecl editor_save_progress()
 {
 }
 
+//.text:0042E0D0 ; 
 bool __cdecl editor_should_render_object(long object_index)
 {
 	return true;
 }
 
+//.text:0042E0E0 ; 
 bool __cdecl editor_should_render_transparent_geometry()
 {
 	return false;
 }
 
+//.text:0042E0F0 ; 
+bool __cdecl editor_should_resolve_block_references(s_tag_field const* block_field)
+{
+	return true;
+}
+
+//.text:0042E100 ; 
+bool __cdecl editor_should_resolve_tag_reference(s_tag_field const* reference_field)
+{
+	return true;
+}
+
+//.text:0042E110 ; 
 bool __cdecl editor_switch_zone_set(long zone_set_index)
 {
 	return scenario_switch_zone_set(zone_set_index);
 }
 
+//.text:0042E120 ; 
 void __cdecl editor_update()
 {
 }
 
+//.text:0042E130 ; 
 bool __cdecl game_in_editor()
+{
+	return false;
+}
+
+//.text:0042E140 ; 
+bool __cdecl game_in_editor_player_simulation()
 {
 	return false;
 }
--- game/source/editor/editor_stubs.hpp
+++ game/source/editor/editor_stubs.hpp
@@ -4,6 +4,7 @@
 #include "render/render_visibility_collection.hpp"
 
 enum e_object_type;
+struct s_tag_field;
 
 extern void __cdecl editor_change_pvs(s_game_cluster_bit_vectors const* old_cluster_activation, s_game_cluster_bit_vectors const* new_cluster_activation, bool local);
 extern void __cdecl editor_dispose();
@@ -11,17 +12,22 @@ extern void __cdecl editor_dispose_from_old_map();
 extern void __cdecl editor_dispose_from_old_structure_bsp(dword deactivating_structure_bsp_mask);
 extern void __cdecl editor_get_cluster_color(s_cluster_reference const* cluster_reference, real_argb_color* cluster_color);
 extern char const* __cdecl editor_get_map_name();
+extern void __cdecl editor_handle_tag_reload(bool reloading_scenario);
 extern bool __cdecl sub_42E000();
 extern void __cdecl editor_initialize();
 extern void __cdecl editor_initialize_for_new_map();
 extern void __cdecl editor_initialize_for_new_structure_bsp(dword activating_structure_bsp_mask);
 extern bool __cdecl editor_input_inhibited();
-extern void __cdecl editor_orphan_object(e_object_type type, long a2);
-extern void __cdecl editor_register_new_object(e_object_type type, long a2, long a3);
+extern void __cdecl editor_orphan_object(e_object_type object_type, long scenario_datum_index);
+extern void __cdecl editor_prepare_for_tag_reload(bool reloading_scenario);
+extern void __cdecl editor_register_new_object(e_object_type object_type, long scenario_datum_index, long new_object_index);
 extern void __cdecl editor_save_progress();
 extern bool __cdecl editor_should_render_object(long object_index);
 extern bool __cdecl editor_should_render_transparent_geometry();
+extern bool __cdecl editor_should_resolve_block_references(s_tag_field const* block_field);
+extern bool __cdecl editor_should_resolve_tag_reference(s_tag_field const* reference_field);
 extern bool __cdecl editor_switch_zone_set(long zone_set_index);
 extern void __cdecl editor_update();
 extern bool __cdecl game_in_editor();
+extern bool __cdecl game_in_editor_player_simulation();
 
