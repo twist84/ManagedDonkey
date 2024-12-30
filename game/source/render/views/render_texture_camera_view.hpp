f4684fd5d8f9308eb8d61b888c7c76b78ad47100
theTwister <6237734+twist84@users.noreply.github.com>
2024-12-30 20:04:34 +0000

    Updated `s_render_texture_camera_globals`

--- game/source/render/views/render_texture_camera_view.hpp
+++ game/source/render/views/render_texture_camera_view.hpp
@@ -2,35 +2,106 @@
 
 #include "cseries/cseries.hpp"
 
+enum e_texture_camera_render_mode
+{
+    _texture_camera_render_default = 0,
+    _texture_camera_render_target_only,
+
+    k_texture_camera_render_mode_count
+};
+
+enum e_texture_camera_type
+{
+    _texture_camera_type_marker_id = 0,
+    _texture_camera_type_position,
+
+    k_texture_camera_type_count
+};
+
+enum e_texture_camera_orientation_type
+{
+    _texture_camera_orientation_target_auto = 0,
+    _texture_camera_orientation_position_marker,
+    _texture_camera_orientation_target_marker,
+    _texture_camera_orientation_explicit,
+    _texture_camera_orientation_attached_object_orient,
+
+    k_texture_camera_orientation_type_count
+};
+
+enum e_texture_camera_projection_type
+{
+    _texture_camera_projection_projective = 0,
+
+    k_texture_camera_projection_type_count
+};
+
+enum e_texture_camera_fov_type
+{
+    _texture_camera_fov_fixed = 0,
+    _texture_camera_fov_frame_target,
+
+    k_texture_camera_fov_type_count
+};
+
 struct s_render_texture_camera_globals
 {
-	bool camera_enable;
-	bool camera_dynamic_lights_enable;
-	
-	short __unknown2;
+    bool active;
+    bool dynamic_lights;
+    e_texture_camera_render_mode render_mode;
+    e_texture_camera_type position_type;
+    e_texture_camera_type target_type;
+    e_texture_camera_orientation_type orientation_type;
+    e_texture_camera_projection_type projection_type;
+    e_texture_camera_fov_type fov_type;
 
-	long camera_render_mode;
+    union
+    {
+        struct
+        {
+            long object_index;
+            long marker_id;
+        };
 
-	long __unknown8;
-	long __unknownC;
-	long __unknown10;
-	long __unknown14;
-	long __unknown18;
+        real_point3d position;
 
-	long camera_object_handle;
-	long camera_marker_name;
+    } position;
 
-	long __unknown24;
+    real_vector3d position_offset;
 
-	real_point3d camera_position_world;
-	real_point3d camera_target_object;
+    union
+    {
+        struct
+        {
+            long object_index;
+            long marker_id;
+        };
 
-	byte __unknown40[0x1C];
+        real_point3d position;
 
-	real camera_fov;
-	real aspect_ratio;
-	long camera_resolution_width;
-	long camera_resolution_height;
+    } target;
+
+    long __unknown40_object_index;
+
+    union
+    {
+        struct
+        {
+            real_vector3d forward;
+            real_vector3d up;
+        };
+    } orientation;
+
+    union
+    {
+        real vertical_fov_radians;
+        real target_scale;
+
+    } fov;
+
+    real aspect_ratio;
+    long width;
+    long height;
 };
 static_assert(sizeof(s_render_texture_camera_globals) == 0x6C);
 
