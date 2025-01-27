e44efeefaa683bd2431addb6300d78c6b186213b
theTwister <6237734+twist84@users.noreply.github.com>
2025-01-27 19:34:56 +0000

    Updated `device_controls`

--- game/source/devices/device_controls.cpp
+++ game/source/devices/device_controls.cpp
@@ -1,2 +1,68 @@
 #include "devices/device_controls.hpp"
 
+#include "cache/cache_files.hpp"
+#include "devices/device_definitions.hpp"
+
+void __cdecl control_destroyed(long control_index)
+{
+	INVOKE(0x00BBFB20, control_destroyed, control_index);
+
+	//control_datum* control = control_get_datum(control_index);
+	//struct control_definition* control_definition = (struct control_definition*)tag_get(DEVICE_CONTROL_TAG, control->definition_index);
+	//if (control_definition->control.trigger == _control_trigger_destruction)
+	//{
+	//	control_toggle(control_index);
+	//}
+}
+
+control_datum* __cdecl control_get_datum(long control_index)
+{
+	control_datum* result = (control_datum*)object_get_and_verify_type(control_index, _object_type_control);
+	return result;
+}
+
+long __cdecl control_get_action_string(long control_index)
+{
+	return INVOKE(0x00BBFB70, control_get_action_string, control_index);
+
+	//control_datum* control = control_get_datum(control_index);
+	//struct control_definition* control_definition = (struct control_definition*)tag_get(DEVICE_CONTROL_TAG, control->definition_index);
+	//return control_definition->control.action_string;
+}
+
+void __cdecl control_place(long control_index, s_scenario_control const* scenario_control)
+{
+	INVOKE(0x00BBFBB0, control_place, control_index, scenario_control);
+
+	//control_datum* control = control_get_datum(control_index);
+	//if (TEST_BIT(scenario_control->control.flags, 0))
+	//	control->control.flags |= FLAG(_control_usable_from_both_sides_bit);
+	//if (TEST_BIT(scenario_control->control.flags, 4))
+	//	control->control.flags |= FLAG(_control_toggled_bit);
+	//control->control.flags &= ~FLAG(_control_toggled_bit);
+	//SET_BIT(control->control.flags, _control_toggle_only_once_bit, TEST_BIT(scenario_control->device.flags, 2));
+	//control->control.hud_override_index = scenario_control->control.hud_override_string_list_index - 1;
+}
+
+void __cdecl control_toggle(long control_index)
+{
+	INVOKE(0x00BBFC30, control_toggle, control_index);
+
+	//control_datum* control = control_get_datum(control_index);
+	//struct control_definition* control_definition = (struct control_definition*)tag_get(DEVICE_CONTROL_TAG, control->definition_index);
+	//long position_group_index = control->device.position_group_index;
+	//// $TODO: implement me
+}
+
+void __cdecl control_touched(long control_index, long unit_index)
+{
+	INVOKE(0x00BBFD50, control_touched, control_index, unit_index);
+
+	//control_datum* control = control_get_datum(control_index);
+	//struct control_definition* control_definition = (struct control_definition*)tag_get(DEVICE_CONTROL_TAG, control->definition_index);
+	//if (control_definition->control.trigger == _control_trigger_player)
+	//{
+	//	control_toggle(control_index);
+	//}
+}
+
--- game/source/devices/device_controls.hpp
+++ game/source/devices/device_controls.hpp
@@ -2,7 +2,15 @@
 
 #include "cseries/cseries.hpp"
 #include "devices/devices.hpp"
-#include "objects/objects.hpp"
+
+enum
+{
+	_control_usable_from_both_sides_bit = 0,
+	_control_toggled_bit,
+	_control_toggle_only_once_bit,
+
+	NUMBER_OF_CONTROL_DATUM_FLAGS
+};
 
 struct _control_datum
 {
@@ -20,4 +28,13 @@ struct control_datum
 	_control_datum control;
 };
 static_assert(sizeof(control_datum) == sizeof(long) + sizeof(_object_datum) + sizeof(_device_datum) + sizeof(_control_datum));
+
+struct s_scenario_control;
+
+extern void __cdecl control_destroyed(long control_index);
+extern long __cdecl control_get_action_string(long control_index);
+extern control_datum* __cdecl control_get_datum(long control_index);
+extern void __cdecl control_place(long control_index, s_scenario_control const* scenario_control);
+extern void __cdecl control_toggle(long control_index);
+extern void __cdecl control_touched(long control_index, long unit_index);
 
