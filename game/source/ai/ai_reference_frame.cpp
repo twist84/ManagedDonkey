93ce21a7c96a98ddd170131252278aa4a94b7935
theTwister <6237734+twist84@users.noreply.github.com>
2025-02-06 20:31:05 +0000

    Added `ai_refresh_reference_frames`
    Added `ai_reference_frame` function comments

--- game/source/ai/ai_reference_frame.cpp
+++ game/source/ai/ai_reference_frame.cpp
@@ -1,7 +1,32 @@
 #include "ai/ai_reference_frame.hpp"
 
+//.text:014516D0 ; real __cdecl ai_distance3d(c_ai_point3d const*, c_ai_point3d const*)
+//.text:01451710 ; real __cdecl ai_distance3d(c_ai_point3d const*, real_point3d const*)
+//.text:014517C0 ; real __cdecl ai_distance_squared3d(c_ai_point3d const*, c_ai_point3d const*)
+//.text:01451890 ; real __cdecl ai_distance_squared3d(c_ai_point3d const*, real_point3d const*)
+//.text:01451930 ; bool __cdecl ai_point3d_new(real_point3d const*, long, dword, c_ai_point3d*)
+//.text:014519C0 ; bool __cdecl ai_point_from_world_position(short, real_point3d const*, c_ai_point3d*)
+
 real_point3d* __cdecl ai_point_get_position(c_ai_point3d const* ai_point, real_point3d* position)
 {
     return INVOKE(0x01451A10, ai_point_get_position, ai_point, position);
+}
+
+//.text:01451A60 ; short __cdecl ai_reference_frame_create(long, short)
+//.text:01451A70 ; short __cdecl ai_reference_frame_get_by_bsp_reference(long, dword)
+//.text:01451AD0 ; short __cdecl ai_reference_frame_get_by_identifier_and_node_index(c_object_identifier*, short)
+//.text:01451B30 ; short __cdecl ai_reference_frame_get_by_node_index(long, short)
+//.text:01451B70 ; void __cdecl ai_reference_frame_handle_deleted_object(long)
+//.text:01451BD0 ; bool __cdecl ai_reference_frame_inverse_transform_point(short, real_point3d const*, real_point3d*)
+//.text:01451CD0 ; bool __cdecl ai_reference_frame_inverse_transform_vector(short, real_vector3d const*, real_vector3d*)
+//.text:01451DD0 ; bool __cdecl ai_reference_frame_transform_ai_point(short, c_ai_point3d const*, c_ai_point3d*)
+//.text:01451E70 ; bool __cdecl ai_reference_frame_transform_point(short, real_point3d const*, real_point3d*)
+//.text:01451F80 ; bool __cdecl ai_reference_frame_transform_vector(short, real_vector3d const*, real_vector3d*)
+//.text:01452090 ; bool __cdecl ai_reference_frame_verify(short)
+//.text:014520F0 ; bool __cdecl ai_reference_frames_isolated(short, short)
+
+void __cdecl ai_refresh_reference_frames()
+{
+    INVOKE(0x01452180, ai_refresh_reference_frames);
 }
 
--- game/source/ai/ai_reference_frame.hpp
+++ game/source/ai/ai_reference_frame.hpp
@@ -6,4 +6,5 @@ struct s_ai_reference_frame_definition;
 
 struct c_ai_point3d;
 real_point3d* __cdecl ai_point_get_position(c_ai_point3d const* ai_point, real_point3d* position);
+extern void __cdecl ai_refresh_reference_frames();
 
