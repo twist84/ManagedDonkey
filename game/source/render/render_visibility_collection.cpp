18df26063261f06a2f4d0bfc99b9fba42147d818
theTwister <6237734+twist84@users.noreply.github.com>
2025-03-03 13:49:35 +0000

    Added `eset_visible_clusters_for_frame`
    `render_visibility_reset_visible_clusters_for_frame`

---
+++ game/source/render/render_visibility_collection.cpp
@@ -0,0 +1,7 @@
+#include "render/render_visibility_collection.hpp"
+
+void __cdecl render_visibility_reset_visible_clusters_for_frame()
+{
+	INVOKE(0x00A53380, render_visibility_reset_visible_clusters_for_frame);
+}
+
