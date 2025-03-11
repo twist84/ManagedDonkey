de846e50ebc4d10ea8df7ee1b1302087dc454753
theTwister <6237734+twist84@users.noreply.github.com>
2025-03-11 22:17:41 +0000

    Patch to enable main menu leave game item

--- game/source/interface/gui_screens/main_menu/gui_screen_main_menu.cpp
+++ game/source/interface/gui_screens/main_menu/gui_screen_main_menu.cpp
@@ -2,4 +2,7 @@
 
 #include "memory/module.hpp"
 
+// patch to disable call for `data->set_disabled_element(STRING_ID(global, name), STRING_ID(global, leave_game))`
+byte const set_list_elements_patch_bytes[] = { 0x74, 0x25 };
+DATA_PATCH_DECLARE(0x00AE8163, set_list_elements, set_list_elements_patch_bytes);
 
