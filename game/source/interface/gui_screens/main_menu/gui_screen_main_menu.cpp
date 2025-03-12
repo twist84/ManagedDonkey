6ca81c3e2045193218c835728e278070d16d74ef
theTwister <6237734+twist84@users.noreply.github.com>
2025-03-12 13:12:54 +0000

    Implemented `set_list_elements`
    `c_main_menu_screen_widget::set_list_elements`

--- game/source/interface/gui_screens/main_menu/gui_screen_main_menu.cpp
+++ game/source/interface/gui_screens/main_menu/gui_screen_main_menu.cpp
@@ -1,8 +1,122 @@
 #include "interface/gui_screens/main_menu/gui_screen_main_menu.hpp"
 
+#include "interface/c_controller.hpp"
+#include "interface/user_interface.hpp"
+#include "interface/user_interface_data.hpp"
 #include "memory/module.hpp"
+#include "saved_games/saved_game_files.hpp"
 
-// patch to disable call for `data->set_disabled_element(STRING_ID(global, name), STRING_ID(global, leave_game))`
-byte const set_list_elements_patch_bytes[] = { 0x74, 0x25 };
-DATA_PATCH_DECLARE(0x00AE8163, set_list_elements, set_list_elements_patch_bytes);
+HOOK_DECLARE_CLASS_MEMBER(0x00AE80F0, c_main_menu_screen_widget, set_list_elements);
+
+//.text:00AE71E0 ; public: c_main_menu_screen_widget::c_main_menu_screen_widget(long)
+//.text:00AE72A0 ; 
+//.text:00AE72B0 ; 
+//.text:00AE72D0 ; public: virtual c_main_menu_screen_widget::~c_main_menu_screen_widget()
+//.text:00AE7320 ; public: virtual void* c_http_stored_buffer_downloader<4665>::`scalar deleting destructor'(unsigned int)
+//.text:00AE7380 ; public: virtual void* c_main_menu_screen_widget::`scalar deleting destructor'(unsigned int)
+//.text:00AE7410 ; private: void c_main_menu_screen_widget::compute_active_players(s_player_identifier* const)
+//.text:00AE7470 ; public: virtual void c_main_menu_screen_widget::dispose()
+//.text:00AE7480 ; // extras portal related
+//.text:00AE74E0 ; public: e_download_status c_http_blf_simple_downloader<s_message_of_the_day_popup>::get_data_no_update(s_message_of_the_day_popup const**, long*)
+//.text:00AE75A0 ; public: e_download_status c_http_blf_simple_downloader<s_message_of_the_day_popup>::get_download_status() const
+//.text:00AE75B0 ; 
+//.text:00AE75C0 ; public: virtual e_gui_location c_main_menu_screen_widget::get_gui_location() const
+
+long c_main_menu_screen_widget::get_in_use_controller_count(e_controller_index* out_first_in_use_controller)
+{
+	return INVOKE_CLASS_MEMBER(0x00AE75D0, c_main_menu_screen_widget, get_in_use_controller_count, out_first_in_use_controller);
+
+	long in_use_controller_count = 0;
+	if (out_first_in_use_controller)
+		*out_first_in_use_controller = k_no_controller;
+	for (e_controller_index controller = _controller0; controller < k_number_of_controllers; controller++)
+	{
+		if (controller_get(controller)->in_use())
+		{
+			if (out_first_in_use_controller && *out_first_in_use_controller == k_no_controller)
+				*out_first_in_use_controller = controller;
+
+			in_use_controller_count++;
+		}
+	}
+	return in_use_controller_count;
+}
+
+//.text:00AE7630 ; 
+//.text:00AE7640 ; 
+//.text:00AE7650 ; 
+//.text:00AE7660 ; public: virtual bool c_main_menu_screen_widget::handle_controller_input_message(c_controller_input_message const*)
+//.text:00AE7790 ; public: virtual bool c_main_menu_screen_widget::handle_dialog_result(c_dialog_result_message const*)
+//.text:00AE77D0 ; public: virtual bool c_main_menu_screen_widget::handle_list_item_chosen(c_controller_input_message const*, long, c_gui_list_item_widget*, c_gui_data*)
+//.text:00AE7E80 ; public: virtual void c_main_menu_screen_widget::initialize()
+//.text:00AE7EF0 ; public: bool c_http_blf_simple_downloader<s_message_of_the_day_popup>::is_submitting_to_cache() const
+//.text:00AE7F00 ; private: void c_main_menu_screen_widget::load_progress_ui()
+//.text:00AE7F10 ; // extras portal related
+//.text:00AE8000 ; public: virtual void c_main_menu_screen_widget::post_initialize()
+
+void c_main_menu_screen_widget::set_list_elements()
+{
+	//HOOK_INVOKE_CLASS_MEMBER(, c_main_menu_screen_widget, set_list_elements);
+
+	static bool x_enable_leave_game_element = true; // $TODO: make the menu taller?
+	static bool x_disable_theater_element = true;   // $TODO: enable when saved films actually work?
+
+	if (c_gui_data* data = get_data(STRING_ID(gui, main_menu), NULL))
+	{
+		bool has_saved_game_state_blocking = false;
+		e_controller_index saved_game_controller = k_no_controller;
+		if (get_in_use_controller_count(&saved_game_controller) == 1 /*&& !get_is_blue_disk()*/)
+			has_saved_game_state_blocking = saved_game_files_controller_has_saved_game_state_blocking(saved_game_controller);
+
+		data->clear_disabled_elements();
+
+		if (has_saved_game_state_blocking)
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, start_new_campaign));
+		else
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, resume_campaign));
+
+		if (get_alpha_is_internal_beta())
+		{
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, campaign));
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, mapeditor));
+		}
+		else
+		{
+			if (!x_enable_leave_game_element)
+				data->set_disabled_element(STRING_ID(global, name), STRING_ID(global, leave_game));
+		}
+
+		if (get_alpha_custom_games_disabled())
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(global, multiplayer));
+
+		if (get_alpha_is_locked_down())
+		{
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, matchmaking));
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, theater));
+		}
+		else
+		{
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(global, locked));
+		}
+
+		if (x_disable_theater_element)
+			data->set_disabled_element(STRING_ID(global, name), STRING_ID(gui, theater));
+	}
+
+	//if (!get_is_blue_disk())
+	{
+		if (c_gui_list_widget* child_list_widget = get_child_list_widget(STRING_ID(gui, main_menu)))
+		{
+			if (c_gui_widget* main_menu_list_item = get_first_child_widget_by_type(_gui_list_item))
+				main_menu_list_item->set_use_alternate_ambient_state(get_in_use_controller_count(NULL) > 1);
+		}
+	}
+}
+
+//.text:00AE8240 ; public: void c_http_blf_simple_downloader<s_message_of_the_day_popup>::set_url(c_url_string const*)
+//.text:00AE8250 ; private: void c_main_menu_screen_widget::show_motd_popup_if_needed()
+//.text:00AE8370 ; 
+//.text:00AE8380 ; public: void c_http_blf_simple_downloader<s_message_of_the_day_popup>::update()
+//.text:00AE8390 ; public: virtual void c_main_menu_screen_widget::update(dword)
+//.text:00AE8650 ; private: void c_main_menu_screen_widget::update_motd_popup()
 
--- game/source/interface/gui_screens/main_menu/gui_screen_main_menu.hpp
+++ game/source/interface/gui_screens/main_menu/gui_screen_main_menu.hpp
@@ -1,4 +1,39 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "game/players.hpp"
+#include "interface/c_gui_screen_widget.hpp"
+#include "interface/gui_screens/motd_popup/gui_screen_motd_popup.hpp"
+#include "networking/logic/storage/network_http_buffer_downloader.hpp"
+
+struct c_main_menu_screen_widget :
+	public c_gui_screen_widget
+{
+public:
+
+//private:
+	long get_in_use_controller_count(e_controller_index* out_first_in_use_controller);
+	void set_list_elements();
+
+//protected:
+	long m_last_ui_lockdown_level;
+	bool m_checked_for_film_autoplayback;
+	bool m_bink_was_active;
+	bool m_content_enumeration_active;
+	s_player_identifier m_last_active_players[4];
+	dword m_progress_dialog_start_time_milliseconds;
+	c_http_blf_simple_downloader<s_motd_popup_data, 4665> m_motd_popup_downloader;
+	bool m_motd_popup_shown;
+	long m_url_key;
+};
+static_assert(sizeof(c_main_menu_screen_widget) == 0x33B4);
+static_assert(0x1AA0 == OFFSETOF(c_main_menu_screen_widget, m_last_ui_lockdown_level));
+static_assert(0x1AA4 == OFFSETOF(c_main_menu_screen_widget, m_checked_for_film_autoplayback));
+static_assert(0x1AA5 == OFFSETOF(c_main_menu_screen_widget, m_bink_was_active));
+static_assert(0x1AA6 == OFFSETOF(c_main_menu_screen_widget, m_content_enumeration_active));
+static_assert(0x1AA7 == OFFSETOF(c_main_menu_screen_widget, m_last_active_players));
+static_assert(0x1AC8 == OFFSETOF(c_main_menu_screen_widget, m_progress_dialog_start_time_milliseconds));
+static_assert(0x1ACC == OFFSETOF(c_main_menu_screen_widget, m_motd_popup_downloader));
+static_assert(0x33AC == OFFSETOF(c_main_menu_screen_widget, m_motd_popup_shown));
+static_assert(0x33B0 == OFFSETOF(c_main_menu_screen_widget, m_url_key));
 
