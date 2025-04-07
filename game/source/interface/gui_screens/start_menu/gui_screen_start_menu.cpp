51e0c5f0d17f3b43d159d6f3bf3bfbca3657298c
theTwister <6237734+twist84@users.noreply.github.com>
2025-04-07 23:19:27 +0100

    Mapped `c_start_menu_screen_widget`

--- game/source/interface/gui_screens/start_menu/gui_screen_start_menu.cpp
+++ game/source/interface/gui_screens/start_menu/gui_screen_start_menu.cpp
@@ -9,13 +9,13 @@
 #include "networking/online/online_service_record.hpp"
 #include "simulation/simulation.hpp"
 
-HOOK_DECLARE_CLASS_MEMBER(0x00AE05B0, c_start_menu_screen_widget, handle_controller_input_message);
-HOOK_DECLARE_CLASS(0x00AE0720, c_start_menu_screen_widget, handle_global_start_button_press);
+HOOK_DECLARE_CLASS_MEMBER(0x00AE05B0, c_start_menu_screen_widget, handle_controller_input_message_);
+HOOK_DECLARE_CLASS(0x00AE0720, c_start_menu_screen_widget, handle_global_start_button_press_);
 
 byte const handle_global_start_button_press_patch_bytes[] = { 0xEB, 0x10 };
 DATA_PATCH_DECLARE(0x00AAC890, handle_global_start_button_press, handle_global_start_button_press_patch_bytes);
 
-bool __thiscall c_start_menu_screen_widget::handle_controller_input_message(c_controller_input_message* message)
+bool __thiscall c_start_menu_screen_widget::handle_controller_input_message_(c_controller_input_message* message)
 {
 	if (message->get_event_type() == _event_type_button_press)
 	{
@@ -32,28 +32,28 @@ bool __thiscall c_start_menu_screen_widget::handle_controller_input_message(c_co
 	}
 
 	bool result = false;
-	HOOK_INVOKE_CLASS_MEMBER(result =, c_start_menu_screen_widget, handle_controller_input_message, message);
+	HOOK_INVOKE_CLASS_MEMBER(result =, c_start_menu_screen_widget, handle_controller_input_message_, message);
 	return result;
 }
 
-bool __cdecl c_start_menu_screen_widget::handle_global_start_button_press(c_controller_input_message* message)
+bool __cdecl c_start_menu_screen_widget::handle_global_start_button_press_(c_controller_input_message* message)
 {
-	if (message->get_event_type() != _event_type_button_press || message->get_component() != _controller_component_button_start)
-		return false;
+	if (message->get_event_type() == _event_type_button_press && message->get_component() == _controller_component_button_start)
+	{
+		e_controller_index controller_index = message->get_controller();
+		c_controller_interface const* controller = controller_get(controller_index);
 
-	e_controller_index controller_index = message->get_controller();
-	c_controller_interface const* controller = controller_get(controller_index);
+		if (simulation_starting_up())
+		{
+			event(_event_status, "ui:start_menu: Can't show start menu -- simulation starting up.");
+			return false;
+		}
+		if (!controller->in_use() || controller->get_user_index() == NONE)
+		{
+			event(_event_error, "ui:start_menu: controller not in use.");
+			return false;
+		}
 
-	if (simulation_starting_up())
-	{
-		event(_event_status, "ui:start_menu: Can't show start menu -- simulation starting up.");
-	}
-	else if (!controller->in_use() || controller->get_user_index() == NONE)
-	{
-		event(_event_error, "ui:start_menu: controller not in use.");
-	}
-	else
-	{
 		s_player_identifier player_identifier{};
 		qword const player_xuid = controller->get_player_xuid();
 		return load_start_menu(controller_index, controller->get_player_identifier(&player_identifier), &player_xuid, NULL, NULL, 0);
--- game/source/interface/gui_screens/start_menu/gui_screen_start_menu.hpp
+++ game/source/interface/gui_screens/start_menu/gui_screen_start_menu.hpp
@@ -1,22 +1,40 @@
 #pragma once
 
 #include "cseries/cseries.hpp"
+#include "game/players.hpp"
+#include "interface/c_gui_screen_widget.hpp"
+#include "networking/online/online_service_record.hpp"
+
+enum e_controller_index;
+struct c_controller_input_message;
 
 struct s_start_menu_breadcrumb
 {
-	long __unknown0;
-	long __unknown4;
+	long screen_id;
+	long element_handle;
 };
 static_assert(sizeof(s_start_menu_breadcrumb) == 0x8);
 
-struct c_controller_input_message;
-enum e_controller_index;
-struct s_player_identifier;
-struct s_service_record_identity;
-
-struct c_start_menu_screen_widget
+struct c_start_menu_screen_widget :
+	public c_gui_screen_widget
 {
-	bool __thiscall handle_controller_input_message(c_controller_input_message* message);
-	static bool __cdecl handle_global_start_button_press(c_controller_input_message* message);
+public:
+	bool __thiscall handle_controller_input_message_(c_controller_input_message* message);
+	static bool __cdecl handle_global_start_button_press_(c_controller_input_message* message);
 	static bool __cdecl load_start_menu(e_controller_index controller_index, s_player_identifier const* player_identifier, qword const* player_xuid, s_service_record_identity const* identity, s_start_menu_breadcrumb const* breadcrumbs, long breadcrumb_count);
+
+protected:
+	s_player_identifier m_owner_player_id;
+	s_player_identifier m_target_player_id;
+	qword m_target_player_xuid;
+	s_service_record_identity m_service_record_identity;
+	real m_old_film_playback_speed;
+	c_static_stack<s_start_menu_breadcrumb, 8> m_breadcrumbs;
+	long m_requested_pane;
+	bool m_breadcrumb_to_load_valid;
+	s_start_menu_breadcrumb m_breadcrumb_to_load;
+	bool m_is_rooted;
+	bool m_wants_to_pause_game_time;
 };
+static_assert(sizeof(c_start_menu_screen_widget) == sizeof(c_gui_screen_widget) + 0x718);
+
