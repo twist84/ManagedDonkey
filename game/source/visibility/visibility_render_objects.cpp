a7c623b247cd8b30837efd1d82d2b6b925faeb67
theTwister <6237734+twist84@users.noreply.github.com>
2025-04-18 17:14:31 +0100

    Added `visibility_render_objects` functions

--- game/source/visibility/visibility_render_objects.cpp
+++ game/source/visibility/visibility_render_objects.cpp
@@ -12,6 +12,11 @@ bool object_has_cinematic_lod(long object_index)
 	return object && object->object.flags.test(_object_cinematic_lod_bit);
 }
 
+long __cdecl visibility_render_objects_compute_level_of_detail(long object_index, long player_window_index, s_model_level_of_detail const* level_of_detail_definition, real lod_distance, char last_lod)
+{
+	return INVOKE(0x00A19490, visibility_render_objects_compute_level_of_detail, object_index, player_window_index, level_of_detail_definition, lod_distance, last_lod);
+}
+
 void __cdecl visibility_render_objects_compute_lod_transparency(long object_index, real lod_distance, s_lod_transparency* desired_transparency)
 {
 	bool cinematic_lod = object_has_cinematic_lod(object_index);
@@ -22,5 +27,40 @@ void __cdecl visibility_render_objects_compute_lod_transparency(long object_inde
 
 	if (!cinematic_lod)
 		object_cinematic_lod(object_index, false);
+}
+
+bool __cdecl visibility_render_objects_find_geometry(long object_index, long render_model_index, long player_window_index, short* desired_level_of_detail, char last_level_of_detail, byte* mesh_indices, long* region_count, bool* using_old_permutation, word* a9)
+{
+	return INVOKE(0x00A19A40 , visibility_render_objects_find_geometry, object_index, render_model_index, player_window_index, desired_level_of_detail, last_level_of_detail, mesh_indices, region_count, using_old_permutation, a9);
+}
+
+short __cdecl visibility_render_objects_find_lightmap_object_index(long object_index)
+{
+	return INVOKE(0x00A19C20, visibility_render_objects_find_lightmap_object_index, object_index);
+}
+
+bool __cdecl visibility_render_objects_geometry_in_cache(long render_model_index, long region_count, char const* region_permutation_indices, char* last_region_permutation_indices, long level_of_detail, bool desired_geometry, byte* mesh_indices, bool* using_old_permutation)
+{
+	return INVOKE(0x00A19C60, visibility_render_objects_geometry_in_cache, render_model_index, region_count, region_permutation_indices, last_region_permutation_indices, level_of_detail, desired_geometry, mesh_indices, using_old_permutation);
+}
+
+char __cdecl visibility_render_objects_get_last_level_of_detail_rendered(long object_index, long player_window_index)
+{
+	return INVOKE(0x00A19D40, visibility_render_objects_get_last_level_of_detail_rendered, object_index, player_window_index);
+}
+
+bool __cdecl visibility_render_objects_get_model_info(long object_index, real lod_distance, real screen_pixels, long player_window_index, s_render_object_info* info)
+{
+	return INVOKE(0x00A19D60, visibility_render_objects_get_model_info, object_index, lod_distance, screen_pixels, player_window_index, info);
+}
+
+void __cdecl visibility_render_objects_get_region_zsort_offsets(long object_index, long model_definition_index, s_render_object_info* info)
+{
+	INVOKE(0x00A1A000, visibility_render_objects_get_region_zsort_offsets, object_index, model_definition_index, info);
+}
+
+void __cdecl visibility_render_objects_set_last_level_of_detail_rendered(long object_index, long player_window_index, char lod)
+{
+	INVOKE(0x00A1A0D0, visibility_render_objects_set_last_level_of_detail_rendered, object_index, player_window_index, lod);
 }
 
--- game/source/visibility/visibility_render_objects.hpp
+++ game/source/visibility/visibility_render_objects.hpp
@@ -3,6 +3,16 @@
 #include "cseries/cseries.hpp"
 
 struct s_lod_transparency;
+struct s_model_level_of_detail;
+struct s_render_object_info;
 
+extern long __cdecl visibility_render_objects_compute_level_of_detail(long object_index, long player_window_index, s_model_level_of_detail const* level_of_detail_definition, real lod_distance, char last_lod);
 extern void __cdecl visibility_render_objects_compute_lod_transparency(long object_index, real shadow_fade_distance, s_lod_transparency* desired_alpha);
+extern bool __cdecl visibility_render_objects_find_geometry(long object_index, long render_model_index, long player_window_index, short* desired_level_of_detail, char last_level_of_detail, byte* mesh_indices, long* region_count, bool* using_old_permutation, word* a9);
+extern short __cdecl visibility_render_objects_find_lightmap_object_index(long object_index);
+extern bool __cdecl visibility_render_objects_geometry_in_cache(long render_model_index, long region_count, char const* region_permutation_indices, char* last_region_permutation_indices, long level_of_detail, bool desired_geometry, byte* mesh_indices, bool* using_old_permutation);
+extern char __cdecl visibility_render_objects_get_last_level_of_detail_rendered(long object_index, long player_window_index);
+extern bool __cdecl visibility_render_objects_get_model_info(long object_index, real lod_distance, real screen_pixels, long player_window_index, s_render_object_info* info);
+extern void __cdecl visibility_render_objects_get_region_zsort_offsets(long object_index, long model_definition_index, s_render_object_info* info);
+extern void __cdecl visibility_render_objects_set_last_level_of_detail_rendered(long object_index, long player_window_index, char lod);
 
