8cda255ff2dc2d0d4bd61b07909a1f58ac50b107
theTwister <6237734+twist84@users.noreply.github.com>
2025-04-24 01:32:44 +0100

    Updated `hs_library_internal_compile`

---
+++ game/source/hs/hs_library_internal_compile.cpp
@@ -0,0 +1,300 @@
+#include "hs/hs_library_internal_compile.hpp"
+
+bool hs_get_parameter_indices(const char* function_name, short count, long* result_indices, long expression_index)
+{
+	hs_syntax_node* expr_node = hs_syntax_get(expression_index);
+	long next_node_index = hs_syntax_get(expr_node->long_value)->next_node_index;
+
+	bool match_count = false;
+
+	if (next_node_index == NONE)
+	{
+		match_count = (count == 0);
+	}
+	else
+	{
+		for (short i = 0; i < count; i++)
+		{
+			result_indices[i] = next_node_index;
+
+			if (hs_syntax_get(next_node_index))
+			{
+				next_node_index = hs_syntax_get(next_node_index)->next_node_index;
+			}
+			else
+			{
+				break;
+			}
+
+			if (next_node_index == NONE)
+			{
+				match_count = (i + 1 == count);
+				break;
+			}
+		}
+	}
+
+	if (match_count && next_node_index == NONE)
+		return true;
+
+	csnzprintf(hs_compile_globals.error_buffer, k_hs_compile_error_buffer_size,
+		"the %s call requires %d arguments.",
+		function_name,
+		count);
+
+	hs_compile_globals.error_message = hs_compile_globals.error_buffer;
+	hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+
+	return false;
+}
+
+bool hs_parse_begin(short function_index, long expression_index)
+{
+	bool parse_success = true;
+	hs_syntax_node* expression = hs_syntax_get(expression_index);
+	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
+
+	ASSERT(function_index == _hs_function_begin || function_index == _hs_function_begin_random);
+
+	short argument_count = 0;
+	while (parse_success && next_node_index != NONE)
+	{
+		long next_next_node_index = hs_syntax_get(next_node_index)->next_node_index;
+		if (function_index)
+		{
+			parse_success = hs_parse(next_node_index, expression->type);
+			if (!expression->type && parse_success)
+				expression->type = hs_syntax_get(next_node_index)->type;
+		}
+		else
+		{
+			short type = next_next_node_index == NONE ? expression->type.get() : _hs_type_void;
+			parse_success = hs_parse(next_node_index, type);
+			if (next_next_node_index == NONE && !expression->type && parse_success)
+				expression->type = hs_syntax_get(next_node_index)->type;
+		}
+		next_node_index = next_next_node_index;
+
+		argument_count++;
+	}
+
+	if (parse_success)
+	{
+		if (argument_count < 1)
+		{
+			hs_compile_globals.error_message = csnzprintf(hs_compile_globals.error_buffer, k_hs_compile_error_buffer_size,
+				"a statement block must contain at least one argument.",
+				hs_function_get_debug(function_index)->name);
+			hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+			return false;
+		}
+
+		if (argument_count > 32 && function_index == _hs_function_begin_random)
+		{
+			hs_compile_globals.error_message = "begin_random can take a maximum of 32 arguments (matt can increase this.)";
+			hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+			return false;
+		}
+	}
+
+	return parse_success;
+}
+
+bool hs_parse_if(short function_index, long expression_index)
+{
+	bool parse_success = false;
+	hs_syntax_node* expression = hs_syntax_get(expression_index);
+	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
+
+	ASSERT(function_index == _hs_function_if);
+
+	long next_next_expression = NONE;
+	long next_next_next_node_index = NONE;
+	if (next_node_index == NONE
+		|| (next_next_expression = hs_syntax_get(next_node_index)->next_node_index, next_next_expression == NONE)
+		|| (next_next_next_node_index = hs_syntax_get(next_next_expression)->next_node_index, next_next_next_node_index != NONE)
+		&& hs_syntax_get(next_next_next_node_index)->next_node_index != NONE)
+	{
+		hs_compile_globals.error_message = "i expected (if <condition> <then> [<else>]).";
+		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+	}
+	else if (hs_parse(next_node_index, _hs_type_boolean))
+	{
+		if (hs_parse(next_next_expression, expression->type))
+		{
+			if (!expression->type)
+				expression->type = hs_syntax_get(next_next_expression)->type;
+
+			parse_success = next_next_next_node_index == -1 || hs_parse(next_next_next_node_index, expression->type);
+		}
+		else if (!hs_compile_globals.error_message
+			&& !expression->type
+			&& next_next_next_node_index != NONE
+			&& hs_parse(next_next_next_node_index, expression->type))
+		{
+			expression->type = hs_syntax_get(next_next_next_node_index)->type;
+			parse_success = hs_parse(next_next_expression, expression->type);
+		}
+	}
+
+	return parse_success;
+}
+
+bool hs_parse_cond(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+}
+
+bool hs_parse_set(short function_index, long expression_index)
+{
+	bool parse_success = false;
+	hs_syntax_node* expression = hs_syntax_get(expression_index);
+	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
+
+	if (next_node_index == NONE)
+	{
+		hs_compile_globals.error_message = "i expected a variable to set and a value.";
+		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+		return false;
+	}
+
+	long next_next_node_index = hs_syntax_get(next_node_index)->next_node_index;
+	if (next_next_node_index == NONE)
+	{
+		hs_compile_globals.error_message = "i expected a variable to set and a value.";
+		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+		return false;
+	}
+
+	if (hs_syntax_get(next_next_node_index)->next_node_index != NONE)
+	{
+		hs_compile_globals.error_message = "i didn't expect this argument.";
+		hs_compile_globals.error_offset = hs_syntax_get(hs_syntax_get(next_next_node_index)->next_node_index)->source_offset;
+		return false;
+	}
+
+	hs_syntax_node* next_expression = hs_syntax_get(next_node_index);
+	short global_index = hs_find_global_by_name(&hs_compile_globals.compiled_source[next_expression->source_offset]);
+	if (global_index == NONE)
+	{
+		hs_compile_globals.error_message = "this is not a valid global variable.";
+		hs_compile_globals.error_offset = next_expression->source_offset;
+		return false;
+	}
+
+	next_expression->type = hs_global_get_type(global_index);
+	if (expression->type && !hs_can_cast(next_expression->type, expression->type))
+	{
+		hs_compile_globals.error_message = csnzprintf(hs_compile_globals.error_buffer, k_hs_compile_error_buffer_size,
+			"you cannot pass the result of this set (type %s) to a function that expects type %s.",
+			hs_type_names[next_expression->type.get()],
+			hs_type_names[expression->type.get()]);
+		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+		return false;
+	}
+
+	ASSERT(hs_parse_variable(next_node_index));
+	if (!expression->type)
+		expression->type = next_expression->type;
+
+	if (!hs_parse(next_next_node_index, next_expression->type))
+		return false;
+
+	return true;
+}
+
+bool hs_parse_logical(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_arithmetic(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_equality(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_inequality(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_sleep(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_sleep_for_ticks(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_sleep_forever(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_sleep_until(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_wake(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
+bool hs_parse_inspect(short function_index, long expression_index)
+{
+	ASSERT(function_index == _hs_function_inspect);
+
+	long argument_index;
+	if (!hs_get_parameter_indices(hs_function_table_debug[function_index]->name, 1, &argument_index, expression_index))
+	{
+		return false;
+	}
+
+	if (!hs_parse(argument_index, _hs_unparsed))
+	{
+		if (!hs_compile_globals.error_message)
+		{
+			hs_compile_globals.error_message = "this is not a global variable reference, function call, or script call.";
+			hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
+		}
+		return false;
+	}
+
+	return true;
+};
+
+bool hs_parse_object_cast_up(short function_index, long expression_index)
+{
+	// $TODO: implement
+
+	return false;
+};
+
--- game/source/hs/hs_library_internal_compile.hpp
+++ game/source/hs/hs_library_internal_compile.hpp
@@ -5,171 +5,57 @@
 #include "hs/hs_function.hpp"
 #include "hs/hs_runtime.hpp"
 
-enum e_hs_function
+enum
 {
 	_hs_function_begin = 0,
 	_hs_function_begin_random,
 	_hs_function_if,
 	_hs_function_cond,
 	_hs_function_set,
+	_hs_function_and,
+	_hs_function_or,
+	_hs_function_plus,
+	_hs_function_minus,
+	_hs_function_times,
+	_hs_function_divide,
+	_hs_function_modulo,
+	_hs_function_min,
+	_hs_function_max,
+	_hs_function_equal,
+	_hs_function_not_equal,
+	_hs_function_gt,
+	_hs_function_lt,
+	_hs_function_gte,
+	_hs_function_lte,
+	_hs_function_sleep,
+	_hs_function_sleep_for_ticks,
+	_hs_function_sleep_forever,
+	_hs_function_sleep_until,
+	_hs_function_wake,
+	_hs_function_inspect,
+	_hs_function_object_to_unit,
 
-	// ...
+	_hs_function_debug_string__ai_debug_communication_suppress = 27,
+	_hs_function_debug_string__ai_debug_communication_ignore = 28,
+	_hs_function_debug_string__ai_debug_communication_focus = 29,
+
+	_hs_function_debug_string__first = 27,
+	_hs_function_debug_string__last = 29,
 };
 
-bool hs_parse_begin(short function_index, long expression_index)
-{
-	bool parse_success = true;
-	hs_syntax_node* expression = hs_syntax_get(expression_index);
-	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
-
-	ASSERT(function_index == _hs_function_begin || function_index == _hs_function_begin_random);
-
-	short argument_count = 0;
-	while (parse_success && next_node_index != NONE)
-	{
-		long next_next_node_index = hs_syntax_get(next_node_index)->next_node_index;
-		if (function_index)
-		{
-			parse_success = hs_parse(next_node_index, expression->type);
-			if (!expression->type && parse_success)
-				expression->type = hs_syntax_get(next_node_index)->type;
-		}
-		else
-		{
-			short type = next_next_node_index == NONE ? expression->type.get() : _hs_type_void;
-			parse_success = hs_parse(next_node_index, type);
-			if (next_next_node_index == NONE && !expression->type && parse_success)
-				expression->type = hs_syntax_get(next_node_index)->type;
-		}
-		next_node_index = next_next_node_index;
-
-		argument_count++;
-	}
-
-	if (parse_success)
-	{
-		if (argument_count < 1)
-		{
-			hs_compile_globals.error_message = csnzprintf(hs_compile_globals.error_buffer, k_hs_compile_error_buffer_size,
-				"a statement block must contain at least one argument.",
-				hs_function_get_debug(function_index)->name);
-			hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-			return false;
-		}
-
-		if (argument_count > 32 && function_index == _hs_function_begin_random)
-		{
-			hs_compile_globals.error_message = "begin_random can take a maximum of 32 arguments (matt can increase this.)";
-			hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-			return false;
-		}
-	}
-
-	return parse_success;
-}
-
-bool hs_parse_if(short function_index, long expression_index)
-{
-	bool parse_success = false;
-	hs_syntax_node* expression = hs_syntax_get(expression_index);
-	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
-
-	ASSERT(function_index == _hs_function_if);
-
-	long next_next_expression = NONE;
-	long next_next_next_node_index = NONE;
-	if (next_node_index == NONE
-		|| (next_next_expression = hs_syntax_get(next_node_index)->next_node_index, next_next_expression == NONE)
-		|| (next_next_next_node_index = hs_syntax_get(next_next_expression)->next_node_index, next_next_next_node_index != NONE)
-		&& hs_syntax_get(next_next_next_node_index)->next_node_index != NONE)
-	{
-		hs_compile_globals.error_message = "i expected (if <condition> <then> [<else>]).";
-		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-	}
-	else if (hs_parse(next_node_index, _hs_type_boolean))
-	{
-		if (hs_parse(next_next_expression, expression->type))
-		{
-			if (!expression->type)
-				expression->type = hs_syntax_get(next_next_expression)->type;
-
-			parse_success = next_next_next_node_index == -1 || hs_parse(next_next_next_node_index, expression->type);
-		}
-		else if (!hs_compile_globals.error_message
-			&& !expression->type
-			&& next_next_next_node_index != NONE
-			&& hs_parse(next_next_next_node_index, expression->type))
-		{
-			expression->type = hs_syntax_get(next_next_next_node_index)->type;
-			parse_success = hs_parse(next_next_expression, expression->type);
-		}
-	}
-
-	return parse_success;
-}
-
-bool hs_parse_cond(short function_index, long expression_index)
-{
-	// $TODO: implement
-
-	return false;
-}
-
-bool hs_parse_set(short function_index, long expression_index)
-{
-	bool parse_success = false;
-	hs_syntax_node* expression = hs_syntax_get(expression_index);
-	long next_node_index = hs_syntax_get(expression->long_value)->next_node_index;
-
-	if (next_node_index == NONE)
-	{
-		hs_compile_globals.error_message = "i expected a variable to set and a value.";
-		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-		return false;
-	}
-
-	long next_next_node_index = hs_syntax_get(next_node_index)->next_node_index;
-	if (next_next_node_index == NONE)
-	{
-		hs_compile_globals.error_message = "i expected a variable to set and a value.";
-		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-		return false;
-	}
-
-	if (hs_syntax_get(next_next_node_index)->next_node_index != NONE)
-	{
-		hs_compile_globals.error_message = "i didn't expect this argument.";
-		hs_compile_globals.error_offset = hs_syntax_get(hs_syntax_get(next_next_node_index)->next_node_index)->source_offset;
-		return false;
-	}
-
-	hs_syntax_node* next_expression = hs_syntax_get(next_node_index);
-	short global_index = hs_find_global_by_name(&hs_compile_globals.compiled_source[next_expression->source_offset]);
-	if (global_index == NONE)
-	{
-		hs_compile_globals.error_message = "this is not a valid global variable.";
-		hs_compile_globals.error_offset = next_expression->source_offset;
-		return false;
-	}
-
-	next_expression->type = hs_global_get_type(global_index);
-	if (expression->type && !hs_can_cast(next_expression->type, expression->type))
-	{
-		hs_compile_globals.error_message = csnzprintf(hs_compile_globals.error_buffer, k_hs_compile_error_buffer_size,
-			"you cannot pass the result of this set (type %s) to a function that expects type %s.",
-			hs_type_names[next_expression->type.get()],
-			hs_type_names[expression->type.get()]);
-		hs_compile_globals.error_offset = hs_syntax_get(expression_index)->source_offset;
-		return false;
-	}
-
-	ASSERT(hs_parse_variable(next_node_index));
-	if (!expression->type)
-		expression->type = next_expression->type;
-
-	if (!hs_parse(next_next_node_index, next_expression->type))
-		return false;
-
-	return true;
-}
+extern bool hs_parse_begin(short function_index, long expression_index);
+extern bool hs_parse_if(short function_index, long expression_index);
+extern bool hs_parse_cond(short function_index, long expression_index);
+extern bool hs_parse_set(short function_index, long expression_index);
+extern bool hs_parse_logical(short function_index, long expression_index);
+extern bool hs_parse_arithmetic(short function_index, long expression_index);
+extern bool hs_parse_equality(short function_index, long expression_index);
+extern bool hs_parse_inequality(short function_index, long expression_index);
+extern bool hs_parse_sleep(short function_index, long expression_index);
+extern bool hs_parse_sleep_for_ticks(short function_index, long expression_index);
+extern bool hs_parse_sleep_forever(short function_index, long expression_index);
+extern bool hs_parse_sleep_until(short function_index, long expression_index);
+extern bool hs_parse_wake(short function_index, long expression_index);
+extern bool hs_parse_inspect(short function_index, long expression_index);
+extern bool hs_parse_object_cast_up(short function_index, long expression_index);
 
