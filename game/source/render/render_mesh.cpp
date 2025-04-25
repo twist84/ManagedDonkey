29fc5de94b0d722a6ba4e72f22b63873e549af23
theTwister <6237734+twist84@users.noreply.github.com>
2025-04-25 13:34:23 +0100

    Updated `render_mesh`
    Added `begin_render_mesh_part_only`
    Added `end_render_mesh_part_only`

---
+++ game/source/render/render_mesh.cpp
@@ -0,0 +1,26 @@
+#include "render/render_mesh.hpp"
+
+#include "cseries/cseries.hpp"
+
+void __cdecl begin_render_mesh_part_only()
+{
+	INVOKE(0x00A473F0, begin_render_mesh_part_only);
+
+	//g_render_mesh_part_only = !g_disable_render_state_cache_optimization;
+}
+
+void __cdecl end_render_mesh_part_only()
+{
+	INVOKE(0x00A47400, end_render_mesh_part_only);
+
+	//g_render_mesh_part_only = false;
+	//if (g_render_state_cache_should_be_invalidated)
+	//{
+	//	c_rasterizer::invalidate_render_state_cache();
+	//	render_method_submit_invalidate_cache();
+	//	g_render_state_cache_should_be_invalidated = false;
+	//}
+}
+
+//.text:00A47430 ; bool __cdecl part_is_renderable(s_part const*, geometry_material const*, long)
+
---
+++ game/source/render/render_mesh.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+extern void __cdecl begin_render_mesh_part_only();
+extern void __cdecl end_render_mesh_part_only();
+
