23fb855d4c6abd8ec9794907c7b1d06d1431a112
theTwister <6237734+twist84@users.noreply.github.com>
2025-05-05 00:31:47 +0100

    Updat `appearance_colors::handle_controller_input`
    `c_start_menu_settings_appearance_colors, handle_controller_input_message_`

--- game/source/interface/gui_screens/start_menu/panes/settings_appearance_colors/start_menu_settings_appearance_colors.cpp
+++ game/source/interface/gui_screens/start_menu/panes/settings_appearance_colors/start_menu_settings_appearance_colors.cpp
@@ -1,6 +1,7 @@
 #include "interface/gui_screens/start_menu/panes/settings_appearance_colors/start_menu_settings_appearance_colors.hpp"
 
 #include "cseries/cseries.hpp"
+#include "cseries/cseries_events.hpp"
 #include "interface/c_controller.hpp"
 #include "interface/c_gui_list_item_widget.hpp"
 #include "interface/c_gui_list_widget.hpp"
@@ -8,7 +9,7 @@
 #include "interface/user_interface_text_parser.hpp"
 #include "memory/module.hpp"
 
-//HOOK_DECLARE_CLASS_MEMBER(0x00AF9A30, c_start_menu_settings_appearance_colors, handle_controller_input_message_);
+HOOK_DECLARE_CLASS_MEMBER(0x00AF9A30, c_start_menu_settings_appearance_colors, handle_controller_input_message_);
 HOOK_DECLARE_CLASS_MEMBER(0x00AF9C20, c_start_menu_settings_appearance_colors, initialize_);
 HOOK_DECLARE_CLASS_MEMBER(0x00AF9F70, c_start_menu_settings_appearance_colors, set_color_values_from_profile);
 
@@ -63,151 +64,205 @@ bool __cdecl parse_xml_emblem3(void* this_ptr, wchar_t* buffer, int32 buffer_len
 	return parse_xml_color((c_gui_screen_widget*)this_ptr, buffer, buffer_length, STRING_ID(gui, color_emblem3));
 }
 
-//bool __thiscall c_start_menu_settings_appearance_colors::handle_controller_input_message_(c_controller_input_message const* message)
-//{
-//	//return INVOKE_CLASS_MEMBER(0x00AF9A30, c_start_menu_settings_appearance_colors*, handle_controller_input_message, message);
-//
-//	bool result = false;
-//
-//	e_event_type event_type = message->get_event_type();
-//	e_controller_component component = message->get_component();
-//
-//	c_gui_list_widget* focused_parent_list_widget = NULL;
-//	if (c_gui_widget* focused_widget = get_focused_widget())
-//		focused_parent_list_widget = focused_widget->get_parent_list();
-//
-//	c_gui_list_widget* group_selector_child_list = get_child_list_widget(STRING_ID(gui, group_selector));
-//	if (focused_parent_list_widget && group_selector_child_list)
-//	{
-//		if (focused_parent_list_widget == group_selector_child_list)
-//		{
-//			if ((event_type == _event_type_button_press && component == _controller_component_button_a) || event_type == _event_type_tab_right)
-//			{
-//				c_gui_list_item_widget* parent_list_item = NULL;
-//				if (c_gui_widget* focused_widget = get_focused_widget())
-//					parent_list_item = focused_widget->get_parent_list_item();
-//
-//				int32 parent_list_item_name = parent_list_item->m_name.get_value();
-//				if (c_gui_list_widget* child_list = get_child_list_widget(parent_list_item_name))
-//				{
-//					int32 focused_item_index = child_list->get_focused_item_index();
-//					for (c_gui_list_item_widget* child_list_item = (c_gui_list_item_widget*)child_list->get_first_child_widget_by_type(_gui_list_item);
-//						child_list_item;
-//						child_list_item = get_next_list_item_widget(true))
-//					{
-//						if (focused_item_index == child_list_item->get_list_item_index())
-//						{
-//							transfer_focus(child_list_item);
-//							result = true;
-//						}
-//					}
-//				}
-//
-//			}
-//			else if (event_type == _event_type_button_press && component == _controller_component_button_b)
-//			{
-//				close_current_subpane();
-//				return true;
-//			}
-//		}
-//		else if (event_type == _event_type_button_press && (component == _controller_component_button_a || component == _controller_component_button_b))
-//		{
-//			int32 parent_list_name = focused_parent_list_widget->m_name.get_value();
-//			for (c_gui_list_item_widget* child_list_item = (c_gui_list_item_widget*)group_selector_child_list->get_first_child_widget_by_type(_gui_list_item);
-//				child_list_item;
-//				child_list_item = get_next_list_item_widget(true))
-//			{
-//				if (child_list_item->m_name.get_value() == parent_list_name)
-//				{
-//					group_selector_child_list->set_focused_item_index(child_list_item->get_list_item_index(), false);
-//					transfer_focus(child_list_item);
-//					return true;
-//				}
-//			}
-//
-//			if (component != _controller_component_button_a)
-//			{
-//				set_color_values_from_profile();
-//			}
-//			else
-//			{
-//				e_controller_index controller_index = get_arbitrary_responding_controller();
-//				c_player_profile_interface* player_profile = controller_get(controller_index)->get_player_profile_interface();
-//				int32 focused_item_index = focused_parent_list_widget->get_focused_item_index();
-//
-//				s_emblem_info emblem_info = player_profile->m_emblem_info;
-//				switch (parent_list_name)
-//				{
-//				case STRING_ID(gui, color_armor1):
-//					player_profile->set_primary_change_color(focused_item_index, true);
-//					return true;
-//				case STRING_ID(gui, color_armor2):
-//					player_profile->set_secondary_change_color(focused_item_index, true);
-//					return true;
-//				//case STRING_ID(gui, color_armor3):
-//				//	//player_profile->set_tertiary_change_color(focused_item_index, true);
-//				//	return true;
-//				case STRING_ID(gui, color_emblem1):
-//					//emblem_info.emblem_primary_color = focused_item_index;
-//					//player_profile->set_emblem_info(&emblem_info, true);
-//					emblem_info.emblem_primary_color = focused_item_index;
-//					player_profile->m_emblem_info = emblem_info;
-//					return true;
-//				case STRING_ID(gui, color_emblem2):
-//					//emblem_info.emblem_secondary_color = focused_item_index;
-//					//player_profile->set_emblem_info(&emblem_info, true);
-//					emblem_info.emblem_secondary_color = focused_item_index;
-//					player_profile->m_emblem_info = emblem_info;
-//					return true;
-//				case STRING_ID(gui, color_emblem3):
-//					//emblem_info.emblem_background_color = focused_item_index;
-//					//player_profile->set_emblem_info(&emblem_info, true);
-//					emblem_info.emblem_background_color = focused_item_index;
-//					player_profile->m_emblem_info = emblem_info;
-//					return true;
-//				default:
-//				{
-//					if (parent_list_name == string_id_retrieve("color_armor3"))
-//					{
-//						//player_profile->set_tertiary_change_color(focused_item_index, true);
-//						return true;
-//					}
-//
-//					return true;
-//				}
-//				break;
-//				}
-//
-//				return true;
-//			}
-//		}
-//	}
-//
-//	if (event_type == _event_type_button_press)
-//	{
-//		e_controller_index controller_index = message->get_controller();
-//		switch (component)
-//		{
-//		case _controller_component_button_x:
-//			//player_emblem_toggle_alternate_emblem_foreground(controller_index);
-//			return true;
-//		case _controller_component_button_left_thumb:
-//			//player_emblem_toggle_flip_emblem_foreground(controller_index);
-//			return true;
-//		case _controller_component_button_right_thumb:
-//			//player_emblem_toggle_flip_emblem_background(controller_index);
-//			return true;
-//		}
-//	}
-//
-//	if (!result)
-//	{
-//		//result = c_start_menu_pane_screen_widget::handle_controller_input_message(message);
-//		result = INVOKE_CLASS_MEMBER(0x00B1F620, c_start_menu_pane_screen_widget, handle_controller_input_message, message);
-//	}
-//
-//	return result;
-//}
+void player_emblem_toggle_alternate_emblem_foreground(e_controller_index controller_index)
+{
+	c_controller_interface* controller = controller_get(controller_index);
+	if (TEST_BIT(controller->m_player_profile.m_flags, 8))
+	{
+		event(_event_message, "player_profile: settings change request ignored because player profile is busy with i/o");
+		return;
+	}
+
+	s_emblem_info emblem_info = controller->m_player_profile.get_emblem_info();
+	emblem_info.emblem_info_flags.set(_emblem_info_flag_alternate_foreground_channel_off, !emblem_info.emblem_info_flags.test(_emblem_info_flag_alternate_foreground_channel_off));
+	controller->m_player_profile.set_emblem_info(&emblem_info, true);
+}
+
+void player_emblem_toggle_flip_emblem_foreground(e_controller_index controller_index)
+{
+	//c_controller_interface* controller = controller_get(controller_index);
+	//if (TEST_BIT(controller->m_player_profile.m_flags, 8))
+	//{
+	//	event(_event_message, "player_profile: settings change request ignored because player profile is busy with i/o");
+	//	return;
+	//}
+	//
+	//s_emblem_info emblem_info = controller->m_player_profile.get_emblem_info();
+	//emblem_info.emblem_info_flags.set(_emblem_info_flag_flip_foreground, !emblem_info.emblem_info_flags.test(_emblem_info_flag_flip_foreground));
+	//controller->m_player_profile.set_emblem_info(&emblem_info, true);
+}
+
+void player_emblem_toggle_flip_emblem_background(e_controller_index controller_index)
+{
+	//c_controller_interface* controller = controller_get(controller_index);
+	//if (TEST_BIT(controller->m_player_profile.m_flags, 8))
+	//{
+	//	event(_event_message, "player_profile: settings change request ignored because player profile is busy with i/o");
+	//	return;
+	//}
+	//
+	//s_emblem_info emblem_info = controller->m_player_profile.get_emblem_info();
+	//emblem_info.emblem_info_flags.set(_emblem_info_flag_flip_background, !emblem_info.emblem_info_flags.test(_emblem_info_flag_flip_background));
+	//controller->m_player_profile.set_emblem_info(&emblem_info, true);
+}
+
+bool __thiscall c_start_menu_settings_appearance_colors::handle_controller_input_message_(c_controller_input_message const* message)
+{
+	//return INVOKE_CLASS_MEMBER(0x00AF9A30, c_start_menu_settings_appearance_colors*, handle_controller_input_message, message);
+
+	bool result = false;
+
+	e_event_type event_type = message->get_event_type();
+	e_controller_component component = message->get_component();
+
+	c_gui_list_widget* focused_parent_list_widget = NULL;
+	if (c_gui_widget* focused_widget = get_focused_widget())
+		focused_parent_list_widget = focused_widget->get_parent_list();
+
+	c_gui_list_widget* group_selector_child_list = get_child_list_widget(STRING_ID(gui, group_selector));
+	if (focused_parent_list_widget && group_selector_child_list)
+	{
+		if (focused_parent_list_widget == group_selector_child_list)
+		{
+			if ((event_type == _event_type_button_press && component == _controller_component_button_a) || event_type == _event_type_tab_right)
+			{
+				c_gui_list_item_widget* parent_list_item = NULL;
+				if (c_gui_widget* focused_widget = get_focused_widget())
+					parent_list_item = focused_widget->get_parent_list_item();
+
+				int32 parent_list_item_name = parent_list_item->m_name.get_value();
+				if (c_gui_list_widget* child_list = get_child_list_widget(parent_list_item_name))
+				{
+					int32 focused_item_index = child_list->get_focused_item_index();
+					for (c_gui_list_item_widget* child_list_item = (c_gui_list_item_widget*)child_list->get_first_child_widget_by_type(_gui_list_item);
+						child_list_item;
+						child_list_item = get_next_list_item_widget(true))
+					{
+						if (focused_item_index == child_list_item->get_list_item_index())
+						{
+							transfer_focus(child_list_item);
+							result = true;
+						}
+					}
+				}
+
+			}
+			else if (event_type == _event_type_button_press && component == _controller_component_button_b)
+			{
+				close_current_subpane();
+				return true;
+			}
+		}
+		else if (event_type == _event_type_button_press && (component == _controller_component_button_a || component == _controller_component_button_b))
+		{
+			int32 parent_list_name = focused_parent_list_widget->m_name.get_value();
+			for (c_gui_list_item_widget* child_list_item = (c_gui_list_item_widget*)group_selector_child_list->get_first_child_widget_by_type(_gui_list_item);
+				child_list_item;
+				child_list_item = get_next_list_item_widget(true))
+			{
+				if (child_list_item->m_name.get_value() == parent_list_name)
+				{
+					group_selector_child_list->set_focused_item_index(child_list_item->get_list_item_index(), false);
+					transfer_focus(child_list_item);
+					return true;
+				}
+			}
+
+			if (component != _controller_component_button_a)
+			{
+				set_color_values_from_profile();
+			}
+			else
+			{
+				e_controller_index controller_index = get_arbitrary_responding_controller();
+				c_player_profile_interface* player_profile = controller_get(controller_index)->get_player_profile_interface();
+				int32 focused_item_index = focused_parent_list_widget->get_focused_item_index();
+
+				s_emblem_info emblem_info = player_profile->get_emblem_info();
+				switch (parent_list_name)
+				{
+				case STRING_ID(gui, color_armor1):
+				{
+					player_profile->set_primary_change_color(focused_item_index, true);
+				}
+				return true;
+				case STRING_ID(gui, color_armor2):
+				{
+					player_profile->set_secondary_change_color(focused_item_index, true);
+				}
+				return true;
+				//case STRING_ID(gui, color_armor3):
+				//{
+				//	//player_profile->set_tertiary_change_color(focused_item_index, true);
+				//}
+				//return true;
+				case STRING_ID(gui, color_emblem1):
+				{
+					emblem_info.primary_color_index = focused_item_index;
+					player_profile->set_emblem_info(&emblem_info, true);
+				}
+				return true;
+				case STRING_ID(gui, color_emblem2):
+				{
+					emblem_info.secondary_color_index = focused_item_index;
+					player_profile->set_emblem_info(&emblem_info, true);
+				}
+				return true;
+				case STRING_ID(gui, color_emblem3):
+				{
+					emblem_info.background_color_index = focused_item_index;
+					player_profile->set_emblem_info(&emblem_info, true);
+				}
+				return true;
+				default:
+				{
+					if (parent_list_name == string_id_retrieve("color_armor3"))
+					{
+						//player_profile->set_tertiary_change_color(focused_item_index, true);
+						return true;
+					}
+
+					return true;
+				}
+				break;
+				}
+
+				return true;
+			}
+		}
+	}
+
+	if (event_type == _event_type_button_press)
+	{
+		e_controller_index controller_index = message->get_controller();
+		switch (component)
+		{
+		case _controller_component_button_x:
+		{
+			player_emblem_toggle_alternate_emblem_foreground(controller_index);
+		}
+		return true;
+		case _controller_component_button_left_thumb:
+		{
+			player_emblem_toggle_flip_emblem_foreground(controller_index);
+		}
+		return true;
+		case _controller_component_button_right_thumb:
+		{
+			player_emblem_toggle_flip_emblem_background(controller_index);
+		}
+		return true;
+		}
+	}
+
+	if (!result)
+	{
+		//result = c_start_menu_pane_screen_widget::handle_controller_input_message(message);
+		result = INVOKE_CLASS_MEMBER(0x00B1F620, c_start_menu_pane_screen_widget, handle_controller_input_message, message);
+	}
+
+	return result;
+}
 
 void __thiscall c_start_menu_settings_appearance_colors::initialize_()
 {
