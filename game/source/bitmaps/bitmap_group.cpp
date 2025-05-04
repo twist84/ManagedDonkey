0eaf25c2bd07a3948dff28c2d3fe41bd175bde3a
theTwister <6237734+twist84@users.noreply.github.com>
2025-05-04 21:35:34 +0100

    Add `bitmap_group_get_bounding_rect_from_sequence`

---
+++ game/source/bitmaps/bitmap_group.cpp
@@ -0,0 +1,9 @@
+#include "bitmaps/bitmap_group.hpp"
+
+#include "cseries/cseries.hpp"
+
+real_rectangle2d const* __cdecl bitmap_group_get_bounding_rect_from_sequence(int32 bitmap_group_index, int16 sequence_index, int16 frame_index)
+{
+	return INVOKE(0x00718E60, bitmap_group_get_bounding_rect_from_sequence, bitmap_group_index, sequence_index, frame_index);
+}
+
---
+++ game/source/bitmaps/bitmap_group.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+union real_rectangle2d;
+
+extern real_rectangle2d const* __cdecl bitmap_group_get_bounding_rect_from_sequence(int32 bitmap_group_index, int16 sequence_index, int16 frame_index);
+
