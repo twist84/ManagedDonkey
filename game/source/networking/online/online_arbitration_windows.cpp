9a089615e9b19b2bb9da616f01f098b3e9089365
theTwister <6237734+twist84@users.noreply.github.com>
2025-05-25 12:19:09 +0100

    Added `online_arbitration_windows`

---
+++ game/source/networking/online/online_arbitration_windows.cpp
@@ -0,0 +1,83 @@
+#include "networking/online/online_arbitration_windows.hpp"
+
+#include "cseries/cseries_events.hpp"
+#include "networking/logic/network_arbitration.hpp"
+#include "networking/online/online_guide_pc.hpp"
+
+//s_online_arbitration_globals g_online_arbitration_globals{};
+
+void online_arbitration_dispose()
+{
+	//overlapped_track_delete(g_online_arbitration_globals.arbitration_overlapped_task);
+	//csmemset(&g_online_arbitration_globals, 0, sizeof(s_online_arbitration_globals));
+}
+
+void online_arbitration_initialize()
+{
+	//csmemset(&g_online_arbitration_globals, 0, sizeof(s_online_arbitration_globals));
+	//g_online_arbitration_globals.arbitration_overlapped_task = (c_arbitration_overlapped_task*)overlapped_malloc_tracked(sizeof(c_arbitration_overlapped_task), __FILE__, __LINE__);
+	//
+	//g_online_arbitration_globals.arbitration_overlapped_task->constructor(__FILE__, __LINE__);
+	//
+	//ASSERT(g_online_arbitration_globals.arbitration_overlapped_task);
+	//g_online_arbitration_globals.initialized = true;
+}
+
+bool online_arbitration_initiate_registration(void* session_handle, uns64 session_nonce)
+{
+	//ASSERT(online_arbitration_registration_get_status() != _online_arbitration_registration_in_progress);
+	//
+	//g_online_arbitration_globals.arbitration_overlapped_task->m_session_handle = session_handle;
+	//g_online_arbitration_globals.arbitration_overlapped_task->m_session_nonce = session_nonce;
+	//
+	//bool started = overlapped_task_start_internal(g_online_arbitration_globals.arbitration_overlapped_task, __FILE__, __LINE__);
+	//if (!started)
+	//{
+	//	event(_event_warning, "networking:online:arbitration: failed to initiate arbitration for session 0x%08X", (uns32)session_handle);
+	//}
+	//
+	//return started;
+
+	return false;
+}
+
+bool online_arbitration_registration_get_result(s_arbitration_registration_result* arbitration_result_out)
+{
+	//ASSERT(arbitration_result_out);
+	//ASSERT(online_arbitration_registration_get_status() == _online_arbitration_registration_complete_success);
+	//
+	//csmemset(arbitration_result_out, 0, sizeof(s_arbitration_registration_result));
+	//arbitration_result_out->registrant_count = 0;
+	//
+	//// `XSESSION_REGISTRATION_RESULTS` loop
+
+	return false;
+}
+
+e_online_arbitration_registration_status online_arbitration_registration_get_status()
+{
+	//switch (g_online_arbitration_globals.arbitration_overlapped_task->get_task_state())
+	//{
+	//case _overlapped_task_state_none:
+	//{
+	//	return _online_arbitration_registration_none;
+	//}
+	//case _overlapped_task_state_starting:
+	//case _overlapped_task_state_pending:
+	//case _overlapped_task_state_completing:
+	//{
+	//	return _online_arbitration_registration_in_progress;
+	//}
+	//case _overlapped_task_state_succeeded:
+	//{
+	//	return _online_arbitration_registration_complete_success;
+	//}
+	//case _overlapped_task_state_failed:
+	//{
+	//	return _online_arbitration_registration_complete_failed;
+	//}
+	//}
+
+	return _online_arbitration_registration_none;
+}
+
---
+++ game/source/networking/online/online_arbitration_windows.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+//#include "cseries/async_xoverlapped.hpp"
+
+struct s_arbitration_registration_result;
+
+enum e_online_arbitration_registration_status
+{
+	_online_arbitration_registration_none = 0,
+	_online_arbitration_registration_in_progress,
+	_online_arbitration_registration_complete_success,
+	_online_arbitration_registration_complete_failed,
+
+	k_online_arbitration_registration_count,
+};
+
+//struct c_arbitration_overlapped_task :
+//	public c_overlapped_task
+//{
+//	byte m_registration_result[0x1000];
+//	void* m_session_handle;
+//	uns64 m_session_nonce;
+//};
+//static_assert(sizeof(c_arbitration_overlapped_task) == 0x1020);
+//
+//struct s_online_arbitration_globals
+//{
+//	bool initialized;
+//	c_arbitration_overlapped_task* arbitration_overlapped_task;
+//};
+//static_assert(sizeof(s_online_arbitration_globals) == 0x8);
+//
+//extern s_online_arbitration_globals g_online_arbitration_globals;
+
+extern void online_arbitration_dispose();
+extern void online_arbitration_initialize();
+extern bool online_arbitration_initiate_registration(void* session_handle, uns64 session_nonce);
+extern bool online_arbitration_registration_get_result(s_arbitration_registration_result* arbitration_result_out);
+extern e_online_arbitration_registration_status online_arbitration_registration_get_status();
+
