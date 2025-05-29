75ccd17f42ef02c425d5e4fb6197b72fabfd4a97
theTwister <6237734+twist84@users.noreply.github.com>
2025-05-29 17:25:20 +0100

    Added `interface_scale` functions

--- game/source/interface/interface_constants.cpp
+++ game/source/interface/interface_constants.cpp
@@ -59,14 +59,42 @@ void __cdecl interface_get_current_window_settings(rectangle2d* out_render_windo
 	INVOKE(0x00A9FAF0, interface_get_current_window_settings, out_render_window_bounds, out_render_window_title_safe_bounds, out_display_window_bounds, out_display_window_title_safe_bounds);
 }
 
+//.text:00A9FC70 ; 
+//.text:00A9FCB0 ; 
+//.text:00A9FCF0 ; 
+//.text:00A9FD30 ; 
+//.text:00A9FD40 ; bool __cdecl interface_is_split_screen()
+
+void __cdecl interface_scale_points_for_xenon_scaler(point2d* points, int32 point_count, real_vector2d const* aspect_ratio_scale)
+{
+	//INVOKE(0x00A9FD50, interface_scale_points_for_xenon_scaler, points, point_count, aspect_ratio_scale);
+	DECLFUNC(0x00A9FD50, void, __cdecl, point2d*, int32, real_vector2d const*)(points, point_count, aspect_ratio_scale);
+}
+
+void __cdecl interface_scale_points_for_xenon_scaler(real_point2d* points, int32 point_count, real_vector2d const* aspect_ratio_scale)
+{
+	//INVOKE(0x00A9FDA0, interface_scale_points_for_xenon_scaler, points, point_count, aspect_ratio_scale);
+	DECLFUNC(0x00A9FDA0, void, __cdecl, real_point2d*, int32, real_vector2d const*)(points, point_count, aspect_ratio_scale);
+}
+
+void __cdecl interface_scale_real_rectangle2d_for_xenon_scaler(real_rectangle2d* rectangle, real_vector2d const* aspect_ratio_scale)
+{
+	//INVOKE(0x00A9FDE0, interface_scale_real_rectangle2d_for_xenon_scaler, rectangle, aspect_ratio_scale);
+
+	rectangle->x0 *= aspect_ratio_scale->i;
+	rectangle->x1 *= aspect_ratio_scale->i;
+	rectangle->y0 *= aspect_ratio_scale->j;
+	rectangle->y1 *= aspect_ratio_scale->j;
+}
+
 void __cdecl interface_scale_rectangle2d_for_xenon_scaler(rectangle2d* rectangle, real_vector2d const* aspect_ratio_scale)
 {
 	//INVOKE(0x00A9FE30, interface_scale_rectangle2d_for_xenon_scaler, rectangle, aspect_ratio_scale);
 
-	rectangle->y0 = static_cast<int16>(rectangle->y0 * aspect_ratio_scale->i);
-	rectangle->y1 = static_cast<int16>(rectangle->y1 * aspect_ratio_scale->i);
-	rectangle->x0 = static_cast<int16>(rectangle->x0 * aspect_ratio_scale->j);
-	rectangle->x1 = static_cast<int16>(rectangle->x1 * aspect_ratio_scale->j);
+	rectangle->y0 *= int16(((real32)rectangle->y0 * aspect_ratio_scale->i) * 0.001f);
+	rectangle->y1 *= int16(((real32)rectangle->y1 * aspect_ratio_scale->i) * 0.001f);
+	rectangle->x0 *= int16(((real32)rectangle->x0 * aspect_ratio_scale->j) * 0.001f);
+	rectangle->x1 *= int16(((real32)rectangle->x1 * aspect_ratio_scale->j) * 0.001f);
 }
 
 void __cdecl interface_scale_screenspace_vertices_for_xenon_scaler(rasterizer_vertex_screen* vertices, int32 vertex_count, real_vector2d const* aspect_ratio_scale)
--- game/source/interface/interface_constants.hpp
+++ game/source/interface/interface_constants.hpp
@@ -13,6 +13,9 @@ extern int32 __cdecl interface_get_closest_authoring_screen_dimensions();
 extern void __cdecl interface_get_current_display_or_window_settings(rectangle2d* out_render_bounds, rectangle2d* out_render_title_safe_bounds, rectangle2d* out_display_bounds, rectangle2d* out_display_title_safe_bounds);
 extern int32 __cdecl interface_get_current_display_settings(rectangle2d* out_render_bounds, rectangle2d* out_render_title_safe_bounds, rectangle2d* out_display_bounds, rectangle2d* out_display_title_safe_bounds);
 extern void __cdecl interface_get_current_window_settings(rectangle2d* out_render_window_bounds, rectangle2d* out_render_window_title_safe_bounds, rectangle2d* out_display_window_bounds, rectangle2d* out_display_window_title_safe_bounds);
+extern void __cdecl interface_scale_points_for_xenon_scaler(point2d* points, int32 point_count, real_vector2d const* aspect_ratio_scale);
+extern void __cdecl interface_scale_points_for_xenon_scaler(real_point2d* points, int32 point_count, real_vector2d const* aspect_ratio_scale);
+extern void __cdecl interface_scale_real_rectangle2d_for_xenon_scaler(real_rectangle2d* rectangle, real_vector2d const* aspect_ratio_scale);
 extern void __cdecl interface_scale_rectangle2d_for_xenon_scaler(rectangle2d* rectangle, real_vector2d const* aspect_ratio_scale);
 extern void __cdecl interface_scale_screenspace_vertices_for_xenon_scaler(rasterizer_vertex_screen* vertices, int32 vertex_count, real_vector2d const* aspect_ratio_scale);
 
