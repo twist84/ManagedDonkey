7f3a29fc11d69b822326f44678b24348a6f7b144
theTwister <6237734+twist84@users.noreply.github.com>
2025-06-07 20:17:19 +0100

    Added `start_menu_game_campaign`

---
+++ game/source/interface/gui_screens/start_menu/panes/game_campaign/start_menu_game_campaign.cpp
@@ -0,0 +1,17 @@
+#include "interface/gui_screens/start_menu/panes/game_campaign/start_menu_game_campaign.hpp"
+
+//.text:00AE86E0 ; public: c_start_menu_game_campaign::c_start_menu_game_campaign(int32)
+//.text:00AE8700 ; public: virtual c_start_menu_game_campaign::~c_start_menu_game_campaign()
+//.text:00AE8710 ; 
+//.text:00AE8720 ; public: virtual void* c_start_menu_game_campaign::`scalar deleting destructor'(unsigned int)
+//.text:00AE8750 ; public: virtual bool c_start_menu_game_campaign::allow_pane_tab_change()
+//.text:00AE8760 ; public: virtual c_gui_bitmap_widget* c_start_menu_game_campaign::create_bitmap_widget(const s_runtime_bitmap_widget_definition*)
+//.text:00AE87E0 ; public: virtual bool c_start_menu_game_campaign::handle_dialog_result(const c_dialog_result_message*)
+//.text:00AE8930 ; public: virtual bool c_start_menu_game_campaign::handle_list_item_chosen(const c_controller_input_message*, int32, c_gui_list_item_widget*, c_gui_data*)
+//.text:00AE8B90 ; public: virtual void c_start_menu_game_campaign::initialize()
+//.text:00AE8C80 ; public: virtual void c_start_menu_game_campaign::initialize_datasource()
+//.text:00AE8D80 ; bool __cdecl parse_xml_primary_mission_objectives(void*, wchar_t*, int32)
+//.text:00AE8DD0 ; bool __cdecl parse_xml_secondary_mission_objectives(void*, wchar_t*, int32)
+//.text:00AE8E20 ; public: virtual void c_start_menu_game_campaign::post_initialize()
+//.text:00AE8E90 ; public: virtual void c_start_menu_game_campaign::update(uns32)
+
---
+++ game/source/interface/gui_screens/start_menu/panes/game_campaign/start_menu_game_campaign.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "interface/gui_screens/start_menu/start_menu_pane_screen_widget.hpp"
+
+class c_start_menu_game_campaign :
+	public c_start_menu_pane_screen_widget
+{
+public:
+};
+static_assert(sizeof(c_start_menu_game_campaign) == 0x2168);
+static_assert(sizeof(c_start_menu_game_campaign) == sizeof(c_start_menu_pane_screen_widget));
+
