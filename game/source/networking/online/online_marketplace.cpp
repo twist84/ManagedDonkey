e198016e4321d3eb0f6301791e3a4a550a479a4f
theTwister <6237734+twist84@users.noreply.github.com>
2025-06-07 15:49:46 +0100

    Updated `online_marketplace`
    Updated `c_online_marketplace_consume_asset_task`
    Updated `c_online_marketplace_enumerate_assets_task`
    Added `c_online_marketplace_enumerate_offers_task`
    Added `c_online_marketplace_purchase_task`

---
+++ game/source/networking/online/online_marketplace.cpp
@@ -0,0 +1,134 @@
+#include "networking/online/online_marketplace.hpp"
+
+#include "cseries/cseries_events.hpp"
+#include "networking/online/online_error.hpp"
+
+#include <winerror.h>
+
+c_online_marketplace_consume_asset_task::~c_online_marketplace_consume_asset_task()
+{
+}
+
+c_online_marketplace_enumerate_assets_task::~c_online_marketplace_enumerate_assets_task()
+{
+}
+
+c_online_marketplace_enumerate_offers_task::~c_online_marketplace_enumerate_offers_task()
+{
+}
+
+c_online_marketplace_purchase_task::~c_online_marketplace_purchase_task()
+{
+}
+
+void c_online_marketplace_enumerate_assets_task::complete()
+{
+	INVOKE_CLASS_MEMBER(0x014E2DE0, c_online_marketplace_enumerate_assets_task, complete);
+}
+
+void c_online_marketplace_enumerate_offers_task::complete()
+{
+	INVOKE_CLASS_MEMBER(0x014E2E20, c_online_marketplace_enumerate_offers_task, complete);
+}
+
+const char* c_online_marketplace_consume_asset_task::get_context_string() const
+{
+	//return INVOKE_CLASS_MEMBER(0x014E2E60, c_online_marketplace_consume_asset_task, get_context_string);
+
+	return "XMarketplaceConsumeAssets";
+}
+
+const char* c_online_marketplace_enumerate_assets_task::get_context_string() const
+{
+	//return INVOKE_CLASS_MEMBER(0x014E2E70, c_online_marketplace_enumerate_assets_task, get_context_string);
+
+	return "XMarketplaceCreateAssetEnumerator";
+}
+
+const char* c_online_marketplace_enumerate_offers_task::get_context_string() const
+{
+	//return INVOKE_CLASS_MEMBER(0x014E2E80, c_online_marketplace_enumerate_offers_task, get_context_string);
+
+	return "XMarketplaceCreateOfferEnumerator";
+}
+
+const char* c_online_marketplace_purchase_task::get_context_string() const
+{
+	//return INVOKE_CLASS_MEMBER(0x014E2E90, c_online_marketplace_purchase_task, get_context_string);
+
+	return "XShowMarketplaceDownloadItemsUI";
+}
+
+//.text:014E2EA0 ; public: void c_online_marketplace_consume_asset_task::initialize(const char* file, int32 line, e_controller_index controller_index, const int32* asset)
+//.text:014E2EC0 ; public: void c_online_marketplace_enumerate_assets_task::initialize(const char* file, int32 line, e_controller_index controller_index)
+//.text:014E2EE0 ; public: void c_online_marketplace_enumerate_offers_task::initialize(const char* file, int32 line, e_controller_index controller_index, uns32 offer_filter_flags)
+//.text:014E2F10 ; public: void c_online_marketplace_purchase_task::initialize(const char* file, int32 line, e_controller_index controller_index, c_online_marketplace_purchase_task::e_entry_point entry_point, const uns64* offer_ids, int32 offer_count)
+
+bool c_online_marketplace_enumerate_assets_task::is_result_successful(uns32 calling_result, uns32 overlapped_error, uns32 overlapped_extended_error)
+{
+	return INVOKE_CLASS_MEMBER(0x014E2F70, c_online_marketplace_enumerate_assets_task, is_result_successful, calling_result, overlapped_error, overlapped_extended_error);
+}
+
+bool c_online_marketplace_enumerate_offers_task::is_result_successful(uns32 calling_result, uns32 overlapped_error, uns32 overlapped_extended_error)
+{
+	return INVOKE_CLASS_MEMBER(0x014E2FB0, c_online_marketplace_enumerate_offers_task, is_result_successful, calling_result, overlapped_error, overlapped_extended_error);
+}
+
+void c_online_marketplace_enumerate_assets_task::reset()
+{
+	INVOKE_CLASS_MEMBER(0x014E2FF0, c_online_marketplace_enumerate_assets_task, reset);
+}
+
+void c_online_marketplace_enumerate_offers_task::reset()
+{
+	INVOKE_CLASS_MEMBER(0x014E3020, c_online_marketplace_enumerate_offers_task, reset);
+}
+
+uns32 c_online_marketplace_consume_asset_task::start(void* overlapped)
+{
+	//return INVOKE_CLASS_MEMBER(0x014E3050, c_online_marketplace_consume_asset_task, start, overlapped);
+
+	return E_FAIL;
+}
+
+uns32 c_online_marketplace_enumerate_assets_task::start(void* overlapped)
+{
+	//return INVOKE_CLASS_MEMBER(0x014E3060, c_online_marketplace_enumerate_assets_task, start, overlapped);
+
+	m_result_count = 0;
+	return E_FAIL;
+}
+
+uns32 c_online_marketplace_enumerate_offers_task::start(void* overlapped)
+{
+	//return INVOKE_CLASS_MEMBER(0x014E3080, c_online_marketplace_enumerate_offers_task, start, overlapped);
+
+	m_result_count = 0;
+	return E_FAIL;
+}
+
+uns32 c_online_marketplace_purchase_task::start(void* overlapped)
+{
+	//return INVOKE_CLASS_MEMBER(0x014E30A0, c_online_marketplace_purchase_task, start, overlapped);
+
+	return E_FAIL;
+}
+
+void c_online_marketplace_enumerate_assets_task::success(uns32 return_result)
+{
+	INVOKE_CLASS_MEMBER(0x014E30B0, c_online_marketplace_enumerate_assets_task, success, return_result);
+}
+
+void c_online_marketplace_enumerate_offers_task::success(uns32 return_result)
+{
+	INVOKE_CLASS_MEMBER(0x014E30D0, c_online_marketplace_enumerate_offers_task, success, return_result);
+}
+
+void c_online_marketplace_purchase_task::success(uns32 return_result)
+{
+	//INVOKE_CLASS_MEMBER(0x014E30F0, c_online_marketplace_purchase_task, success, return_result);
+
+	event(_event_message, "networking:online:marketplace: XShowMarketplaceDownloadItemsUI result: %s",
+		online_error_get_string(m_purchase_result).get_string());
+}
+
--- game/source/networking/online/online_marketplace.hpp
+++ game/source/networking/online/online_marketplace.hpp
@@ -2,19 +2,20 @@
 
 #include "cseries/async_xoverlapped.hpp"
 
-class c_online_marketplace_purchase_task :
+class c_online_marketplace_consume_asset_task :
 	public c_overlapped_task
 {
 public:
-	static unsigned int const k_max_offer_count = 4;
+	virtual ~c_online_marketplace_consume_asset_task();
+	virtual const char* get_context_string() const override;
+	virtual uns32 start(void* overlapped) override;
 
+public:
 	e_controller_index m_controller_index;
-	int32 m_entry_point;
-	uns64 m_offers[k_max_offer_count]; // is this the correct name
-	int32 m_offer_count;
-	int32 m_purchase_result;
+	int32 m_asset; // XMARKETPLACE_ASSET
 };
-static_assert(sizeof(c_online_marketplace_purchase_task) == 0x40);
+static_assert(sizeof(c_online_marketplace_consume_asset_task) == 0x18);
+static_assert(sizeof(c_online_marketplace_consume_asset_task) == sizeof(c_overlapped_task) + 0x8);
 
 class c_online_marketplace_enumerate_assets_task :
 	public c_overlapped_task
@@ -25,6 +26,16 @@ public:
 		k_maximum_number_of_assets_to_enumerate = 32,
 	};
 
+public:
+	virtual ~c_online_marketplace_enumerate_assets_task();
+	virtual const char* get_context_string() const override;
+	virtual uns32 start(void* overlapped) override;
+	virtual void success(uns32 return_result) override;
+	virtual void complete() override;
+	virtual void reset() override;
+	virtual bool is_result_successful(uns32 calling_result, uns32 overlapped_error, uns32 overlapped_extended_error) override;
+
+protected:
 	e_controller_index m_controller_index;
 	void* m_enumeration_handle;
 	void* m_overlapped_results_buffer;
@@ -32,13 +43,75 @@ public:
 	int32 m_result_count;
 };
 static_assert(sizeof(c_online_marketplace_enumerate_assets_task) == 0xA0);
+static_assert(sizeof(c_online_marketplace_enumerate_assets_task) == sizeof(c_overlapped_task) + 0x90);
 
-class c_online_marketplace_consume_asset_task :
+struct c_online_marketplace_enumerate_offers_task :
 	public c_overlapped_task
 {
 public:
+	enum
+	{
+		k_maximum_number_of_offers_to_enumerate = 32,
+	};
+
+	enum e_offer_filter_flags
+	{
+		_offer_filter_bnet_pro_subscription_bit = 0,
+	};
+
+	struct s_enumerated_offer
+	{
+		uns64 offer_id;
+	};
+	static_assert(sizeof(s_enumerated_offer) == 0x8);
+
+public:
+	virtual ~c_online_marketplace_enumerate_offers_task();
+	virtual const char* get_context_string() const override;
+	virtual uns32 start(void* overlapped) override;
+	virtual void success(uns32 return_result) override;
+	virtual void complete() override;
+	virtual void reset() override;
+	virtual bool is_result_successful(uns32 calling_result, uns32 overlapped_error, uns32 overlapped_extended_error) override;
+
+
+protected:
 	e_controller_index m_controller_index;
-	int32 m_asset; // XMARKETPLACE_ASSET
+	uns32 m_offer_filter_flags;
+	void* m_enumeration_handle;
+	void* m_overlapped_results_buffer;
+	s_enumerated_offer m_results[k_maximum_number_of_offers_to_enumerate];
+	int32 m_result_count;
 };
-static_assert(sizeof(c_online_marketplace_consume_asset_task) == 0x18);
+static_assert(sizeof(c_online_marketplace_enumerate_offers_task) == 0x128);
+static_assert(sizeof(c_online_marketplace_enumerate_offers_task) == sizeof(c_overlapped_task) + 0x118);
+
+class c_online_marketplace_purchase_task :
+	public c_overlapped_task
+{
+public:
+	enum
+	{
+		k_maximum_number_of_offers_to_purchase = 4,
+	};
+
+	enum e_entry_point
+	{
+		// $TODO: map me
+	};
+
+public:
+	virtual ~c_online_marketplace_purchase_task();
+	virtual const char* get_context_string() const override;
+	virtual uns32 start(void* overlapped) override;
+	virtual void success(uns32 return_result) override;
+
+	e_controller_index m_controller_index;
+	e_entry_point m_entry_point;
+	uns64 m_offers[k_maximum_number_of_offers_to_purchase]; // is this the correct name
+	int32 m_offer_count;
+	int32 m_purchase_result;
+};
+static_assert(sizeof(c_online_marketplace_purchase_task) == 0x40);
+static_assert(sizeof(c_online_marketplace_purchase_task) == sizeof(c_overlapped_task) + 0x30);
 
