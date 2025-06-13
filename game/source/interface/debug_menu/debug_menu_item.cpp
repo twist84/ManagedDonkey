5a3fe1621f3ebdcdd9c4a556cd31e7cfee38ac15
theTwister <6237734+twist84@users.noreply.github.com>
2025-06-13 16:40:11 +0100

    Updated `debug_menu_item`

--- game/source/interface/debug_menu/debug_menu_item.cpp
+++ game/source/interface/debug_menu/debug_menu_item.cpp
@@ -8,80 +8,9 @@
 
 #include <string.h>
 
-c_debug_menu_item::~c_debug_menu_item()
+void* c_debug_menu_item::operator new(unsigned int size)
 {
-	if (m_child_ref)
-		m_child_ref->~c_debug_menu();
-	m_child_ref = NULL;
-}
-
-void c_debug_menu_item::update()
-{
-}
-
-void c_debug_menu_item::render(c_font_cache_base* font_cache, const point2d& position)
-{
-	const real_argb_color* color = global_real_argb_black;
-	if (get_active() && get_menu()->get_enabled())
-		color = get_enabled_color();
-
-	c_rasterizer_draw_string draw_string{};
-
-	rectangle2d bounds{};
-	interface_get_current_display_settings(NULL, NULL, NULL, &bounds);
-	set_rectangle2d(&bounds, position.x, position.y, bounds.x1, bounds.y1);
-
-	draw_string.set_bounds(&bounds);
-	draw_string.set_color(color);
-	draw_string.draw(font_cache, get_name());
-}
-
-void c_debug_menu_item::notify_selected()
-{
-	if (get_child())
-		debug_menu_set_active_menu(get_child(), false);
-}
-
-void c_debug_menu_item::open()
-{
-	set_active(true);
-}
-
-void c_debug_menu_item::notify_left()
-{
-}
-
-void c_debug_menu_item::notify_right()
-{
-}
-
-const real_argb_color* c_debug_menu_item::get_enabled_color()
-{
-	if (get_child())
-		return debug_real_argb_tv_green;
-
-	return debug_real_argb_grey;
-}
-
-const real_argb_color* c_debug_menu_item::get_background_color()
-{
-	if (!get_is_selection())
-		return debug_real_argb_tv_white;
-
-	real32 v4 = (debug_menu_get_time() % 80) / 79.0f;
-	real32 v5 = v4 + v4;
-	if (v4 >= 0.5f)
-		v5 = 1.0f - ((v4 - 0.5f) + (v4 - 0.5f));
-
-	static real_argb_color background_color{};
-	background_color.alpha = 1.0f;
-	for (int32 i = 0; i < NUMBEROF(background_color.rgb.n); i++)
-	{
-		real32 v6 = (v5 * 0.4f) + 0.05f;
-		background_color.rgb.n[i] = (debug_real_argb_tv_white->rgb.n[i] * v6) + (global_real_argb_black->rgb.n[i] * (1.0f - v6));
-	}
-
-	return &background_color;
+	return debug_menu_malloc(size);
 }
 
 c_debug_menu_item::c_debug_menu_item(c_debug_menu* menu, const char* name, c_debug_menu* child, bool active) :
@@ -94,28 +23,13 @@ c_debug_menu_item::c_debug_menu_item(c_debug_menu* menu, const char* name, c_deb
 	set_data(NONE);
 }
 
-const char* c_debug_menu_item::get_name()
+c_debug_menu_item::~c_debug_menu_item()
 {
-	return m_name ? m_name : "";
-}
-
-void c_debug_menu_item::set_name(const char* name)
-{
-	int32 name_size = strlen(name) + 1;
-
-	ASSERT(name != NULL && m_name == NULL);
-	m_name = static_cast<char*>(debug_menu_malloc(name_size));
-	csstrnzcpy(m_name, name, name_size);
-}
-
-c_debug_menu* c_debug_menu_item::get_menu()
-{
-	return m_menu_ref;
-}
-
-c_debug_menu* c_debug_menu_item::get_child()
-{
-    return m_child_ref;
+	if (m_child_ref)
+	{
+		m_child_ref->~c_debug_menu();
+	}
+	m_child_ref = NULL;
 }
 
 bool c_debug_menu_item::get_active()
@@ -123,9 +37,34 @@ bool c_debug_menu_item::get_active()
     return m_active;
 }
 
-void c_debug_menu_item::set_active(bool active)
+const real_argb_color* c_debug_menu_item::get_background_color()
 {
-	m_active = active;
+	if (!get_is_selection())
+	{
+		return debug_real_argb_tv_white;
+	}
+
+	real32 v4 = (debug_menu_get_time() % 80) / 79.0f;
+	real32 v5 = v4 + v4;
+	if (v4 >= 0.5f)
+	{
+		v5 = 1.0f - ((v4 - 0.5f) + (v4 - 0.5f));
+	}
+
+	static real_argb_color background_color{};
+	background_color.alpha = 1.0f;
+	for (int32 i = 0; i < NUMBEROF(background_color.rgb.n); i++)
+	{
+		real32 v6 = (v5 * 0.4f) + 0.05f;
+		background_color.rgb.n[i] = (debug_real_argb_tv_white->rgb.n[i] * v6) + (global_real_argb_black->rgb.n[i] * (1.0f - v6));
+	}
+
+	return &background_color;
+}
+
+c_debug_menu* c_debug_menu_item::get_child()
+{
+	return m_child_ref;
 }
 
 bool c_debug_menu_item::get_data()
@@ -133,9 +72,14 @@ bool c_debug_menu_item::get_data()
 	return m_data;
 }
 
-void c_debug_menu_item::set_data(int32 data)
+const real_argb_color* c_debug_menu_item::get_enabled_color()
 {
-	m_data = data;
+	if (get_child())
+	{
+		return debug_real_argb_tv_green;
+	}
+
+	return debug_real_argb_grey;
 }
 
 int16 c_debug_menu_item::get_index()
@@ -143,7 +87,9 @@ int16 c_debug_menu_item::get_index()
 	for (int16 item_index = 0; item_index < get_menu()->get_num_items(); item_index++)
 	{
 		if (get_menu()->get_item(item_index) == this)
+		{
 			return item_index;
+		}
 	}
 
 	VASSERT("unreachable");
@@ -155,8 +101,76 @@ bool c_debug_menu_item::get_is_selection()
 	return get_index() == get_menu()->get_selection();
 }
 
-void* c_debug_menu_item::operator new(unsigned int size)
+c_debug_menu* c_debug_menu_item::get_menu()
 {
-	return debug_menu_malloc(size);
+	return m_menu_ref;
+}
+
+const char* c_debug_menu_item::get_name()
+{
+	return m_name ? m_name : "";
+}
+
+void c_debug_menu_item::notify_left()
+{
+}
+
+void c_debug_menu_item::notify_right()
+{
+}
+
+void c_debug_menu_item::notify_selected()
+{
+	if (get_child())
+	{
+		debug_menu_set_active_menu(get_child(), false);
+	}
+}
+
+void c_debug_menu_item::open()
+{
+	set_active(true);
+}
+
+void c_debug_menu_item::render(c_font_cache_base* font_cache, const point2d& position)
+{
+	const real_argb_color* color = global_real_argb_black;
+	if (get_active() && get_menu()->get_enabled())
+	{
+		color = get_enabled_color();
+	}
+
+	c_rasterizer_draw_string draw_string{};
+
+	rectangle2d bounds{};
+	interface_get_current_display_settings(NULL, NULL, NULL, &bounds);
+	set_rectangle2d(&bounds, position.x, position.y, bounds.x1, bounds.y1);
+
+	draw_string.set_bounds(&bounds);
+	draw_string.set_color(color);
+	draw_string.draw(font_cache, get_name());
+}
+
+void c_debug_menu_item::set_active(bool active)
+{
+	m_active = active;
+}
+
+void c_debug_menu_item::set_data(int32 data)
+{
+	m_data = data;
+}
+
+void c_debug_menu_item::set_name(const char* name)
+{
+	int32 name_size = strlen(name) + 1;
+
+	ASSERT(name != NULL && m_name == NULL);
+	m_name = static_cast<char*>(debug_menu_malloc(name_size));
+	csstrnzcpy(m_name, name, name_size);
+}
+
+void c_debug_menu_item::update()
+{
 }
 
