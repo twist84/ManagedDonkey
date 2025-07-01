d04df4b269e17cdff15ad9e869d2e287262e1a80
theTwister <6237734+twist84@users.noreply.github.com>
2025-07-01 18:47:53 +0100

    Automatic menu switching for HO mainmenu WIP

--- game/source/main/main.cpp
+++ game/source/main/main.cpp
@@ -891,6 +891,34 @@ void __cdecl sub_5077E0()
 void __cdecl sub_641A60(real32 shell_seconds_elapsed)
 {
 	INVOKE(0x00641A60, sub_641A60, shell_seconds_elapsed);
+
+	static bool x_switch_menu_on_player_model_chosen = false;
+	if (x_switch_menu_on_player_model_chosen && game_is_ui_shell())
+	{
+		c_controller_interface* controller_interface = controller_get(_controller0);
+		c_player_profile_interface* player_profile_interface = controller_interface->get_player_profile_interface();
+
+		static e_player_model_choice x_player_model_choice = _player_model_choice_spartan;
+		if (x_player_model_choice != player_profile_interface->get_player_model_choice())
+		{
+			x_player_model_choice = player_profile_interface->get_player_model_choice();
+			{
+				switch (x_player_model_choice)
+				{
+				case _player_model_choice_spartan:
+				{
+					user_interface_start_hs_script_by_name("humanhangar");
+				}
+				break;
+				case _player_model_choice_elite:
+				{
+					user_interface_start_hs_script_by_name("elitehangar");
+				}
+				break;
+				}
+			}
+		};
+	}
 }
 
 void __cdecl main_loop_body()
