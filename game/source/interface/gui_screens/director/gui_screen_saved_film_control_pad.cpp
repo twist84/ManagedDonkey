9a9c2ef74ddcbffc6f6c41ca2bb59c135fd7caab
theTwister <6237734+twist84@users.noreply.github.com>
2025-08-08 20:17:30 +0100

    Updated `handle_list_item_chosen`
    `c_saved_film_control_pad::handle_list_item_chosen`

--- game/source/interface/gui_screens/director/gui_screen_saved_film_control_pad.cpp
+++ game/source/interface/gui_screens/director/gui_screen_saved_film_control_pad.cpp
@@ -1,6 +1,7 @@
 #include "interface/gui_screens/director/gui_screen_saved_film_control_pad.hpp"
 
 #include "cseries/cseries_events.hpp"
+#include "game/game.hpp"
 #include "interface/c_controller.hpp"
 #include "interface/c_gui_bitmap_widget.hpp"
 #include "interface/c_gui_list_item_widget.hpp"
@@ -12,7 +13,7 @@
 #include "screenshots/screenshots_uploader.hpp"
 
 HOOK_DECLARE_CLASS_MEMBER(0x00AE4B10, c_saved_film_control_pad, handle_list_item_chosen_);
-//HOOK_DECLARE_CLASS_MEMBER(0x00AE4D80, c_saved_film_control_pad, update_);
+HOOK_DECLARE_CLASS_MEMBER(0x00AE4D80, c_saved_film_control_pad, update_);
 HOOK_DECLARE_CLASS_MEMBER(0x00AE4F90, c_saved_film_control_pad, update_enabled_menu_items_);
 
 // skip `STRING_ID(gui, play_film)` check in `c_saved_film_control_pad::update`
@@ -23,7 +24,6 @@ DATA_PATCH_DECLARE(0x00AE4F2D, gui_play_film, gui_play_film_jump.bytes);
 
 bool __thiscall c_saved_film_control_pad::handle_list_item_chosen_(const c_controller_input_message* message, int32 list_name, c_gui_list_item_widget* list_item_widget, c_gui_data* datasource)
 {
-	bool result = false;
 	if (list_name == STRING_ID(gui, button_list))
 	{
 		int32 item_name = NONE;
@@ -34,13 +34,13 @@ bool __thiscall c_saved_film_control_pad::handle_list_item_chosen_(const c_contr
 			case STRING_ID(gui, skip_back):
 			{
 				saved_film_manager_request_revert(_saved_film_revert_backwards);
-				result = true;
+				return true;
 			}
 			break;
 			case STRING_ID(gui, skip_forward):
 			{
 				saved_film_manager_request_revert(_saved_film_revert_forwards);
-				result = true;
+				return true;
 			}
 			break;
 			case STRING_ID(gui, take_screenshot):
@@ -76,61 +76,63 @@ bool __thiscall c_saved_film_control_pad::handle_list_item_chosen_(const c_contr
 					}
 				}
 
-				result = true;
+				return true;
 			}
 			break;
-			//case STRING_ID(gui, record_clip):
-			//{
-			//	// $TODO: implement me
-			//}
-			//return true;
+			case STRING_ID(gui, record_clip):
+			{
+				c_controller_interface* controller = controller_get(message->get_controller());
+
+				if (!controller->is_signed_in_to_machine())
+				{
+					c_gui_screen_widget::play_sound(_ui_global_sound_effect_failure);
+					break;
+				}
+
+				saved_film_manager_start_recording_snippet();
+			}
+			return true;
 			case STRING_ID(gui, play_film):
 			{
 				saved_film_manager_set_playback_game_speed(1.0f);
-				result = true;
+				return true;
 			}
 			break;
 			case STRING_ID(gui, pause_film):
 			{
 				saved_film_manager_set_playback_game_speed(0.0f);
-				result = true;
+				return true;
 			}
 			break;
-			//case STRING_ID(gui, stop_recording):
-			//{
-			//	if (saved_film_manager_get_snippet_state() == _saved_film_snippet_state_recording)
-			//	{
-			//		saved_film_manager_stop_recording_snippet();
-			//	}
-			//}
-			//return true;
+			case STRING_ID(gui, stop_recording):
+			{
+				if (saved_film_manager_get_snippet_state() == _saved_film_snippet_state_recording)
+				{
+					saved_film_manager_stop_recording_snippet();
+				}
+				return true;
+			}
+			break;
 			case STRING_ID(gui, replay_film):
 			{
 				saved_film_manager_replay_film();
-				result = true;
+				return true;
 			}
 			break;
-			//case STRING_ID(gui, end_preview):
-			//{
-			//	if (saved_film_manager_get_snippet_state() == _saved_film_snippet_state_previewing)
-			//	{
-			//		saved_film_manager_preview_snippet_stop();
-			//	}
-			//}
-			//return true;
+			case STRING_ID(gui, end_preview):
+			{
+				if (saved_film_manager_get_snippet_state() == _saved_film_snippet_state_previewing)
+				{
+					saved_film_manager_preview_snippet_stop();
+				}
+				return true;
+			}
+			break;
 			}
 		}
 	}
 
-	if (result)
-	{
-		return true;
-	}
-
-	//result c_gui_screen_widget::handle_list_item_chosen(message, list_name, list_item_widget, datasource);
-
-	HOOK_INVOKE_CLASS_MEMBER(result =, c_saved_film_control_pad, handle_list_item_chosen_, message, list_name, list_item_widget, datasource);
-	return result;
+	return c_gui_screen_widget::handle_list_item_chosen(message, list_name, list_item_widget, datasource);
 }
 
 void __thiscall c_saved_film_control_pad::update_(uns32 current_milliseconds)
@@ -145,7 +147,7 @@ void __thiscall c_saved_film_control_pad::update_(uns32 current_milliseconds)
 
 	if (data && button_list)
 	{
-		button_list->set_enabled(!!(snippet_state));
+		button_list->set_enabled(snippet_state == _saved_film_snippet_state_none);
 
 		for (c_gui_list_item_widget* child_button = (c_gui_list_item_widget*)button_list->get_first_child_widget_by_type(_gui_list_item);
 			child_button != NULL;
@@ -153,24 +155,97 @@ void __thiscall c_saved_film_control_pad::update_(uns32 current_milliseconds)
 		{
 			int32 element_handle = child_button->get_element_handle();
 
-			if (element_handle != NONE)
+			if (element_handle == NONE)
 			{
-				int32 button_name = NONE;
-				if (data->get_string_id_value(element_handle, STRING_ID(gui, gui_item), &button_name))
+				continue;
+			}
+
+			int32 button_name = NONE;
+			if (data->get_string_id_value(element_handle, STRING_ID(gui, gui_item), &button_name))
+			{
+				for (c_gui_bitmap_widget* child_button_bitmap = (c_gui_bitmap_widget*)child_button->get_first_child_widget_by_type(_gui_bitmap);
+					child_button_bitmap != NULL;
+					child_button_bitmap = child_button_bitmap->get_next_bitmap_widget())
 				{
-					for (c_gui_bitmap_widget* child_button_bitmap = (c_gui_bitmap_widget*)child_button->get_first_child_widget_by_type(_gui_bitmap);
-						child_button_bitmap != NULL;
-						child_button_bitmap = child_button_bitmap->get_next_bitmap_widget())
+					if (child_button_bitmap->get_name() != STRING_ID(gui, hilite))
 					{
-						if (child_button_bitmap->get_name() != STRING_ID(gui, hilite))
-						{
-							child_button_bitmap->set_visible(false);
-						}
+						child_button_bitmap->set_visible(false);
 					}
+				}
 
-					child_button->set_child_visible(_gui_bitmap, button_name, true);
+				child_button->set_child_visible(_gui_bitmap, button_name, true);
+				
+				switch (snippet_state)
+				{
+				case _saved_film_snippet_state_none:
+				{
+					if (button_name == STRING_ID(gui, take_screenshot))
+					{
+						bool enabled = !screenshots_uploader_try_and_get() ||
+							screenshots_uploader_try_and_get()->m_screenshots_uploader_task == c_screenshots_uploader::_screenshots_uploader_task_none;
+						child_button->set_enabled(enabled);
+					}
+				}
+				break;
+				case _saved_film_snippet_state_recording_waiting_for_seek:
+				case _saved_film_snippet_state_recording_waiting_for_start:
+				case _saved_film_snippet_state_recorded_and_ready:
+				case _saved_film_snippet_state_previewing_waiting_for_seek:
+				case _saved_film_snippet_state_resetting:
+				{
+					child_button->set_enabled(false);
+				}
+				break;
+				case _saved_film_snippet_state_recording:
+				{
+					bool enabled = button_name == STRING_ID(gui, stop_recording);
+					child_button->set_enabled(enabled);
+				}
+				break;
+				case _saved_film_snippet_state_previewing:
+				{
+					bool enabled = button_name == STRING_ID(gui, end_preview) &&
+						saved_film_manager_get_current_tick_estimate() > saved_film_manager_get_snippet_start_tick();
+					child_button->set_enabled(enabled);
+				}
+				break;
+				}
 
-					// $TODO: implement the rest
+				if (button_name == STRING_ID(gui, skip_back) &&
+					!saved_film_manager_can_revert(_saved_film_revert_backwards))
+				{
+					child_button->set_enabled(false);
+				}
+					
+				if (button_name == STRING_ID(gui, skip_forward) &&
+					!saved_film_manager_can_revert(_saved_film_revert_forwards))
+				{
+					child_button->set_enabled(false);
+				}
+
+				if (button_name == STRING_ID(gui, record_clip) &&
+					(game_in_progress() &&
+						game_is_multiplayer() &&
+						game_playback_get() == _game_playback_film &&
+						player_mapping_output_user_active_count() == 1 &&
+						saved_film_manager_get_snippet_start_tick() == NONE &&
+						saved_film_manager_get_ticks_remaining() &&
+						controller_get_first_non_guest_signed_in_controller() != k_no_controller))
+				{
+					child_button->set_enabled(false);
+				}
+
+				if (button_name == STRING_ID(gui, take_screenshot) &&
+					(!game_in_progress() || 
+						game_playback_get() != _game_playback_film || 
+						player_mapping_output_user_active_count() != 1))
+				{
+					child_button->set_enabled(false);
+				}
+
+				if (button_name == STRING_ID(gui, replay_film) && !game_is_campaign())
+				{
+					child_button->set_enabled(false);
 				}
 			}
 		}
@@ -178,10 +253,18 @@ void __thiscall c_saved_film_control_pad::update_(uns32 current_milliseconds)
 
 	c_gui_screen_widget::set_text_widget_string_id_from_focused_list_item(STRING_ID(gui, status), STRING_ID(gui, button_list), STRING_ID(gui, gui_item));
 
-	if (m_previous_snippet_state != _saved_film_snippet_state_recorded_and_ready &&
-		snippet_state == _saved_film_snippet_state_recorded_and_ready)
+	if (m_previous_snippet_state != _saved_film_snippet_state_recorded_and_ready && snippet_state == _saved_film_snippet_state_recorded_and_ready)
 	{
-		// $TODO: implement me
+		c_load_screen_message* screen_message = new c_load_screen_message(
+			STRING_ID(gui, saved_film_snippet),
+			k_any_controller,
+			c_gui_screen_widget::get_render_window(),
+			STRING_ID(gui, top_most));
+		if (screen_message)
+		{
+			user_interface_messaging_post(screen_message);
+			c_gui_screen_widget::transition_out(_transition_out_normal);
+		}
 	}
 
 	if (saved_film_manager_film_is_ended(NULL))
