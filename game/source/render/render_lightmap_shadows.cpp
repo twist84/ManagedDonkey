fdbe1e3c7eeb5c1b23a65734971e5f4218c98a28
theTwister <6237734+twist84@users.noreply.github.com>
2025-09-03 11:33:14 +0100

    Updated `render_lightmap_shadows` references

--- game/source/render/render_lightmap_shadows.cpp
+++ game/source/render/render_lightmap_shadows.cpp
@@ -10,19 +10,19 @@
 #include "render/views/render_view.hpp"
 #include "visibility/visibility_collection_objects.hpp"
 
-REFERENCE_DECLARE(0x01918160, bool, c_lightmap_shadows_view::g_debug_shadow_screenspace);
-REFERENCE_DECLARE(0x01918164, real32, shadow_fade_distance_scale);
-REFERENCE_DECLARE(0x01918168, real32, shadow_drastic_distance_scale);
-REFERENCE_DECLARE(0x0191816C, real32, shadow_reduce_distance_scale);
-REFERENCE_DECLARE(0x01918170, real32, shadow_decrease_distance_blend);
-REFERENCE_DECLARE(0x05115B3E, bool, c_lightmap_shadows_view::g_debug_shadow_bounds);
-REFERENCE_DECLARE(0x05115B3F, bool, c_lightmap_shadows_view::g_debug_shadow_bounds_solid);
-REFERENCE_DECLARE(0x05115B40, bool, c_lightmap_shadows_view::g_debug_shadow_opaque);            // unused
-REFERENCE_DECLARE(0x05115B41, bool, c_lightmap_shadows_view::g_debug_shadow_histencil);
-REFERENCE_DECLARE(0x05115B42, bool, c_lightmap_shadows_view::g_debug_shadow_force_hi_res);
-REFERENCE_DECLARE(0x05115B43, bool, c_lightmap_shadows_view::g_debug_objectspace_stencil_clip);
-REFERENCE_DECLARE(0x05115B44, bool, c_lightmap_shadows_view::g_debug_force_fancy_shadows);      // unused
-REFERENCE_DECLARE(0x05115B45, bool, c_lightmap_shadows_view::g_debug_force_old_shadows);        // unused
+REFERENCE_DECLARE(0x01918160, bool, c_lightmap_shadows_view::g_debug_shadow_screenspace) = true;
+REFERENCE_DECLARE(0x01918164, real32, shadow_fade_distance_scale) = 1.0f;
+REFERENCE_DECLARE(0x01918168, real32, shadow_drastic_distance_scale) = 1.1f;
+REFERENCE_DECLARE(0x0191816C, real32, shadow_reduce_distance_scale) = 1.01f;
+REFERENCE_DECLARE(0x01918170, real32, shadow_decrease_distance_blend) = 0.99f;
+REFERENCE_DECLARE(0x05115B3E, bool, c_lightmap_shadows_view::g_debug_shadow_bounds) = true;
+REFERENCE_DECLARE(0x05115B3F, bool, c_lightmap_shadows_view::g_debug_shadow_bounds_solid) = true;
+REFERENCE_DECLARE(0x05115B40, bool, c_lightmap_shadows_view::g_debug_shadow_opaque) = false;            // unused
+REFERENCE_DECLARE(0x05115B41, bool, c_lightmap_shadows_view::g_debug_shadow_histencil) = false;
+REFERENCE_DECLARE(0x05115B42, bool, c_lightmap_shadows_view::g_debug_shadow_force_hi_res) = false;
+REFERENCE_DECLARE(0x05115B43, bool, c_lightmap_shadows_view::g_debug_objectspace_stencil_clip) = false;
+REFERENCE_DECLARE(0x05115B44, bool, c_lightmap_shadows_view::g_debug_force_fancy_shadows) = false;      // unused
+REFERENCE_DECLARE(0x05115B45, bool, c_lightmap_shadows_view::g_debug_force_old_shadows) = false;        // unused
 
 HOOK_DECLARE_CLASS_MEMBER(0x00A68C70, c_lightmap_shadows_view, render);
 
