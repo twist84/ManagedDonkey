676b960114ac0db4b1c546bf200f3b80d542d050
theTwister <6237734+twist84@users.noreply.github.com>
2025-09-03 13:15:03 +0100

    Update `user_interface_network_search_globals` ref

--- game/source/interface/user_interface_network_search.cpp
+++ game/source/interface/user_interface_network_search.cpp
@@ -4,15 +4,15 @@
 #include "memory/module.hpp"
 #include "networking/logic/network_search.hpp"
 
-REFERENCE_DECLARE(0x05270A90, bool, g_available_squads_ready);
+REFERENCE_DECLARE(0x05270A90, s_user_interface_network_search_globals, user_interface_network_search_globals);
 
 HOOK_DECLARE(0x00AD4380, user_interface_available_squads_initialize);
 
 bool __cdecl user_interface_available_squads_initialize(int32 controller_index, uns32 available_squad_search_flags, char category)
 {
 	c_allocation_base* allocation = user_interface_memory_allocation();
-	g_available_squads_ready = network_search_start(controller_index, category != 0, 4 /* default is `25`, this causes an issue not having enough memory allocate */, available_squad_search_flags, allocation);
+	user_interface_network_search_globals.network_search_valid = network_search_start(controller_index, category != 0, 4 /* default is `25`, this causes an issue not having enough memory allocate */, available_squad_search_flags, allocation);
 
-	return g_available_squads_ready;
+	return user_interface_network_search_globals.network_search_valid;
 }
 
--- game/source/interface/user_interface_network_search.hpp
+++ game/source/interface/user_interface_network_search.hpp
@@ -2,7 +2,12 @@
 
 #include "cseries/cseries.hpp"
 
-extern bool& g_available_squads_ready;
+struct s_user_interface_network_search_globals
+{
+	bool network_search_valid;
+};
+
+extern s_user_interface_network_search_globals& user_interface_network_search_globals;
 
 extern bool __cdecl user_interface_available_squads_initialize(int32 controller_index, uns32 available_squad_search_flags, char category);
 
