c57f65b89264d5631670398c64b83f9dfe80b2e4
theTwister <6237734+twist84@users.noreply.github.com>
2025-09-03 11:32:28 +0100

    Updated `g_bounce_light_scale` value

--- game/source/geometry/geometry_sampling.cpp
+++ game/source/geometry/geometry_sampling.cpp
@@ -6,21 +6,13 @@
 #include "render/views/render_view.hpp"
 #include "scenario/scenario.hpp"
 
-// g_bounce_light_scale scales `s_geometry_sample::m_diffuse` 
+// `c_geometry_sampler::g_bounce_light_scale` scales `s_geometry_sample::m_diffuse` 
 // as part of a call to `sh_eval_directional_light` from within the `c_geometry_sampler::sample`
 // - halo 3: 1.0f
 // - halo reach: 1.5f
 // - halo online: 1.5f
-REFERENCE_DECLARE(0x018F2FFC, real32, c_geometry_sampler::g_bounce_light_scale);
+REFERENCE_DECLARE(0x018F2FFC, real32, c_geometry_sampler::g_bounce_light_scale) = 0.0f;
 REFERENCE_DECLARE(0x018F3000, s_geometry_sample, c_geometry_sampler::m_default_lightprobe_sample);
-
-c_geometry_sampler override_geometry_sampler = {};
-
-c_geometry_sampler::c_geometry_sampler()
-{
-	// set `g_bounce_light_scale` to `0.0f` to "fix random colored lighting"
-	c_geometry_sampler::g_bounce_light_scale = 0.0f;
-}
 
 // Halo Online uses the index for the name
 //#define USE_INDEX
