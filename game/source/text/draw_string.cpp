713c54b5b4ffa7b21b51836bed915bae7cbd726a
theTwister <6237734+twist84@users.noreply.github.com>
2025-09-04 17:38:19 +0100

    Updated `c_draw_string`

--- game/source/text/draw_string.cpp
+++ game/source/text/draw_string.cpp
@@ -1,5 +1,6 @@
 #include "text/draw_string.hpp"
 
+#include "interface/interface_constants.hpp"
 #include "math/color_math.hpp"
 #include "text/font_group.hpp"
 #include "text/font_loading.hpp"
@@ -52,16 +53,50 @@ void c_draw_string::set_align_bottom_vertically(bool align_bottom)
 void c_draw_string::set_bounds(const real_rectangle2d* bounds, const real_rectangle2d* clip)
 {
 	DECLFUNC(0x00658B10, void, __thiscall, c_draw_string*, const real_rectangle2d*, const real_rectangle2d*)(this, bounds, clip);
+
+	// $IMPLEMENT
 }
 
 void c_draw_string::set_bounds(const real_rectangle2d* bounds)
 {
-	DECLFUNC(0x00658BC0, void, __thiscall, c_draw_string*, const real_rectangle2d*)(this, bounds);
+	//DECLFUNC(0x00658BC0, void, __thiscall, c_draw_string*, const real_rectangle2d*)(this, bounds);
+
+	real32 x0 = 0.0f;
+	real32 x1 = 0.0f;
+	real32 y0 = 0.0f;
+	real32 y1 = 0.0f;
+	if (bounds)
+	{
+		if ((bounds->x1 - bounds->x0) >= 0.0f && (bounds->y1 - bounds->y0) >= 0.0f)
+		{
+			x0 = bounds->x0;
+			x1 = bounds->x1;
+			y0 = bounds->y0;
+			y1 = bounds->y1;
+		}
+	}
+	set_real_rectangle2d(&m_bounds, x0, x1, y0, y1);
 }
 
 void c_draw_string::set_bounds(const rectangle2d* bounds)
 {
-	DECLFUNC(0x00658D20, void, __thiscall, c_draw_string*, const rectangle2d*)(this, bounds);
+	//DECLFUNC(0x00658D20, void, __thiscall, c_draw_string*, const rectangle2d*)(this, bounds);
+
+	real32 x0 = 0.0f;
+	real32 x1 = 0.0f;
+	real32 y0 = 0.0f;
+	real32 y1 = 0.0f;
+	if (bounds)
+	{
+		if ((bounds->x1 - bounds->x0) >= 0 && (bounds->y1 - bounds->y0) >= 0)
+		{
+			x0 = (real32)bounds->x0;
+			x1 = (real32)bounds->x1;
+			y0 = (real32)bounds->y0;
+			y1 = (real32)bounds->y1;
+		}
+	}
+	set_real_rectangle2d(&m_bounds, x0, x1, y0, y1);
 }
 
 void c_draw_string::set_center_vertically(bool center_vertically)
@@ -107,10 +142,14 @@ void c_draw_string::set_font(e_font_id font)
 	}
 	else
 	{
-		if (font < 0)
+		if (font < _terminal_font)
+		{
 			font = _terminal_font;
-		if (font > 10)
+		}
+		if (font > _main_menu_font)
+		{
 			font = _main_menu_font;
+		}
 		m_font_id = font;
 		m_styled_font_header = font_get_header(font);
 	}
@@ -179,11 +218,15 @@ void c_draw_string::set_tab_stops(const int16* tab_stops, int16 count)
 	{
 		int16 tab_stop_count = count;
 		if (count > NUMBEROF(m_tab_stops))
+		{
 			tab_stop_count = NUMBEROF(m_tab_stops);
+		}
 
 		m_tab_stop_count = tab_stop_count;
 		if (tab_stop_count > 0)
+		{
 			csmemcpy(m_tab_stops, tab_stops, sizeof(*m_tab_stops) * tab_stop_count);
+		}
 	}
 	else
 	{
@@ -202,40 +245,46 @@ void c_draw_string::text_bounds_draw_character(real32 screen_left, real32 screen
 }
 
 c_draw_string::c_draw_string() :
-	__vftable(reinterpret_cast<decltype(__vftable)>(0x0165DB98)),
+	__vftable(reinterpret_cast<decltype(__vftable)>(0x0165FC44)),
 	m_flags(),
-	m_font_id(),
-	m_styled_font_header(),
-	m_style(),
-	m_justification(),
-	m_drop_shadow_style(),
-	m_color(),
-	m_shadow_color(),
-	m_scale(),
-	m_display_resolution_scale_adjustment(),
-	m_height_adjust(),
-	m_tab_stop_count(),
+	m_font_id(_terminal_font),
+	m_styled_font_header(font_get_header(_terminal_font)),
+	m_style(_text_style_plain),
+	m_justification(_text_justification_left),
+	m_drop_shadow_style(_shadow_drop),
+	m_color(*global_real_argb_white),
+	m_shadow_color(*global_real_argb_black),
+	m_scale(1.0f),
+	m_display_resolution_scale_adjustment(draw_string_get_glyph_scaling_for_display_settings()),
+	m_height_adjust(0),
+	m_tab_stop_count(0),
 	m_tab_stops(),
 	m_bounds(),
 	m_text_bounds(),
 	m_clip(),
 	m_cursor(),
-	m_permutation_proc(),
-	m_permutation_context(),
-	m_initial_indent(),
-	m_paragraph_indent(),
+	m_permutation_proc(NULL),
+	m_permutation_context(NULL),
+	m_initial_indent(0),
+	m_paragraph_indent(0),
 	m_saved_parse_state(),
-	m_saved_tab_stop_index(),
-	m_saved_line_count(),
-	m_saved_tab_stop_line_count(),
-	m_saved_maximum_tab_stop_line_count()
+	m_saved_tab_stop_index(0),
+	m_saved_line_count(0),
+	m_saved_tab_stop_line_count(0),
+	m_saved_maximum_tab_stop_line_count(0)
 {
-	DECLFUNC(0x00657010, void, __thiscall, c_draw_string*)(this);
+	//DECLFUNC(0x00657010, void, __thiscall, c_draw_string*)(this);
+
+	rectangle2d display_bounds{};
+	rectangle2d display_title_safe_pixel_bounds{};
+	interface_get_current_display_settings(NULL, NULL, &display_bounds, &display_title_safe_pixel_bounds);
+	c_draw_string::set_bounds(&display_title_safe_pixel_bounds);
+	csmemset(&m_saved_parse_state, 0, sizeof(m_saved_parse_state));
 }
 
 c_draw_string::~c_draw_string()
 {
-	DECLFUNC(0x006571D0, void, __thiscall, c_draw_string*)(this);
+	//DECLFUNC(0x006571D0, void, __thiscall, c_draw_string*)(this);
 }
 
 c_hardware_draw_string::c_hardware_draw_string() : 
