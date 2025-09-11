9c727e032725e1b63c5b0ed78ebe16357b2faa8d
theTwister <6237734+twist84@users.noreply.github.com>
2025-09-11 18:49:42 +0100

    Added `structure_bsp_compute_cluster_active_pvs`

---
+++ game/source/structures/structure_bsp_definitions.cpp
@@ -0,0 +1,7 @@
+#include "structures/structure_bsp_definitions.hpp"
+
+void __cdecl structure_bsp_compute_cluster_active_pvs(s_cluster_reference cluster_reference, s_game_cluster_bit_vectors* destination_pvs)
+{
+	INVOKE(0x005F37B0, structure_bsp_compute_cluster_active_pvs, cluster_reference, destination_pvs);
+}
+
--- game/source/structures/structure_bsp_definitions.hpp
+++ game/source/structures/structure_bsp_definitions.hpp
@@ -297,3 +297,5 @@ struct structure_instanced_geometry_instance
 };
 static_assert(sizeof(structure_instanced_geometry_instance) == 0x74);
 
+extern void __cdecl structure_bsp_compute_cluster_active_pvs(s_cluster_reference cluster_reference, s_game_cluster_bit_vectors* destination_pvs);
+
