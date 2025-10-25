0f4f98f068461cbf80ac7280c1d120966b319583
theTwister <6237734+twist84@users.noreply.github.com>
2025-10-25 01:23:16 +0100

    Update edit_text.cpp

--- game/source/dialogs/edit_text.cpp
+++ game/source/dialogs/edit_text.cpp
@@ -18,11 +18,15 @@ void __cdecl edit_text_fix_selection(edit_text* edit)
 	int16 text_len = int16(strlen(edit->buffer));
 
 	if (edit->insertion_point_index <= 0)
+	{
 		edit->insertion_point_index = 0;
+	}
 
 	int16 text_len11 = text_len;
 	if (edit->insertion_point_index < text_len)
+	{
 		text_len11 = edit->insertion_point_index;
+	}
 
 	uns16 selection_start_index = edit->selection_start_index;
 	bool v8 = edit->selection_start_index <= NONE;
@@ -30,17 +34,23 @@ void __cdecl edit_text_fix_selection(edit_text* edit)
 
 	int16 v5 = NONE;
 	if (!v8)
+	{
 		v5 = selection_start_index;
+	}
 	if (v5 < text_len)
 	{
 		text_len = NONE;
 		if (selection_start_index > NONE)
+		{
 			text_len = selection_start_index;
+		}
 	}
 
 	edit->selection_start_index = text_len;
 	if (text_len11 == text_len)
+	{
 		edit->selection_start_index = NONE;
+	}
 }
 
 void __cdecl edit_text_selection_reset(edit_text* edit)
@@ -58,13 +68,15 @@ uns8 __cdecl edit_text_get_selection_indices(edit_text* edit, int16* start_index
 
 	edit_text_fix_selection(edit);
 
-	if (edit->selection_start_index == NONE)
-		return 0;
+	uns8 selection_valid = 0;
+	if (edit->selection_start_index != NONE)
+	{
+		*start_index = (edit->selection_start_index <= edit->insertion_point_index) ? edit->selection_start_index : edit->insertion_point_index;
+		*end_index = (edit->selection_start_index <= edit->insertion_point_index) ? edit->insertion_point_index : edit->selection_start_index;
 
-	*start_index = (edit->selection_start_index <= edit->insertion_point_index) ? edit->selection_start_index : edit->insertion_point_index;
-	*end_index = (edit->selection_start_index <= edit->insertion_point_index) ? edit->insertion_point_index : edit->selection_start_index;
-
-	return 1;
+		selection_valid = 1;
+	}
+	return selection_valid;
 }
 
 void __cdecl edit_text_handle_key(edit_text* edit, const s_key_state* key)
