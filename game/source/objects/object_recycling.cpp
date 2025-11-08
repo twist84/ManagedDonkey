7d9d79ba451b24cffa0dfb6e45a73f0a10be21da
theTwister <6237734+twist84@users.noreply.github.com>
2025-11-08 18:35:17 +0000

    Updated `object_recycling`

--- game/source/objects/object_recycling.cpp
+++ game/source/objects/object_recycling.cpp
@@ -1,58 +1,58 @@
 #include "objects/object_recycling.hpp"
 
-//.text:00BB9260 ; int32 __cdecl recycle_worst_object_available(int32, uns8*, int32, bool*, char*, int32)
-//.text:00BB9330 ; int32 __cdecl setup_recycling_workspace(int32, uns8*, int32, bool*, char*, int32)
+//.text:00BB9260 ; int32 __cdecl recycle_worst_object_available(int32 filter_flags, uns8* working_memory, int32 working_memory_size, bool* more_to_release, char* description, int32 description_length)
+//.text:00BB9330 ; int32 __cdecl setup_recycling_workspace(int32 filter_flags, uns8* working_memory, int32 working_memory_size, bool* more_to_release, char* description, int32 description_length)
 //.text:00BB9360 ; 
-//.text:00BB9370 ; 
-//.text:00BB9390 ; 
-//.text:00BB93B0 ; 
-//.text:00BB93C0 ; 
-//.text:00BB93D0 ; void __cdecl __tls_set_g_recycling_groups_allocator(void*)
-//.text:00BB9400 ; void __cdecl __tls_set_g_recycling_volumes_allocator(void*)
-//.text:00BB9420 ; 
-//.text:00BB9460 ; 
-//.text:00BB94A0 ; 
+//.text:00BB9370 ; tls
+//.text:00BB9390 ; tls
+//.text:00BB93B0 ; tls
+//.text:00BB93C0 ; tls
+//.text:00BB93D0 ; void __cdecl __tls_set_g_recycling_groups_allocator(void* new_address)
+//.text:00BB9400 ; void __cdecl __tls_set_g_recycling_volumes_allocator(void* new_address)
+//.text:00BB9420 ; tls
+//.text:00BB9460 ; tls
+//.text:00BB94A0 ; public: void c_data_iterator<s_recycling_group>::begin(s_data_array* data)
 //.text:00BB94C0 ; 
-//.text:00BB94D0 ; 
-//.text:00BB9550 ; 
-//.text:00BB9580 ; 
-//.text:00BB95B0 ; int32 __cdecl find_worst_object_in_group(int32, const s_recycling_group*, int32)
+//.text:00BB94D0 ; real32 __cdecl closest_player_distance_squared(const real_point3d* test_point, const real_point3d* player_heads, int32 player_count)
+//.text:00BB9550 ; tls
+//.text:00BB9580 ; tls
+//.text:00BB95B0 ; int32 __cdecl find_worst_object_in_group(int32 filter_flags, const s_recycling_group* group, int32 current_time)
 //.text:00BB96B0 ; 
-//.text:00BB96C0 ; 
-//.text:00BB96F0 ; 
+//.text:00BB96C0 ; tls
+//.text:00BB96F0 ; tls
 //.text:00BB9720 ; 
 //.text:00BB9730 ; 
-//.text:00BB9740 ; int32 __cdecl get_player_head_positions(real_point3d*, int32)
+//.text:00BB9740 ; int32 __cdecl get_player_head_positions(real_point3d* player_heads, int32 max_player_heads)
 //.text:00BB97D0 ; 
 //.text:00BB97E0 ; 
 //.text:00BB97F0 ; 
 //.text:00BB9800 ; 
 //.text:00BB9820 ; 
-//.text:00BB9830 ; void __cdecl move_recycling_object_between_groups(int32, int32, int32, int32)
-//.text:00BB98F0 ; 
+//.text:00BB9830 ; void __cdecl move_recycling_object_between_groups(int32 object_index, int32 previous_object_index, int32 source_group_index, int32 dest_group_index)
+//.text:00BB98F0 ; public: bool c_data_iterator<struct s_recycling_group>::next()
 //.text:00BB9910 ; void __cdecl object_recycling_clear_history()
-//.text:00BB9920 ; void __cdecl object_recycling_group_remove_object(int32)
+//.text:00BB9920 ; void __cdecl object_recycling_group_remove_object(int32 object_index)
 //.text:00BB99B0 ; void __cdecl object_recycling_reset_volume_list()
-//.text:00BB9A40 ; void __cdecl object_recycling_switch_lists(int32, int32*, int32*)
-//.text:00BB9AD0 ; void __cdecl object_recycling_system_add_volume(int32, int32, int32)
+//.text:00BB9A40 ; void __cdecl object_recycling_switch_lists(int32 volume_index, int32* top_of_source_list, int32* top_of_dest_list)
+//.text:00BB9AD0 ; void __cdecl object_recycling_system_add_volume(int32 trigger_volume_index, int32 max_object_count, int32 duration)
 //.text:00BB9BA0 ; void __cdecl objects_recycling_dispose()
 //.text:00BB9BB0 ; void __cdecl objects_recycling_dispose_from_old_map()
-//.text:00BB9BF0 ; void __cdecl objects_recycling_group_process(s_recycling_workspace*, int32)
+//.text:00BB9BF0 ; void __cdecl objects_recycling_group_process(s_recycling_workspace* recycling_workspace, int32 list_index)
 //.text:00BBA020 ; void __cdecl objects_recycling_initialize()
 //.text:00BBA0B0 ; void __cdecl objects_recycling_initialize_for_new_map()
-//.text:00BBA0F0 ; void __cdecl objects_recycling_render(s_recycling_workspace*)
-//.text:00BBA100 ; void __cdecl objects_recycling_update(e_recycling_methods)
-//.text:00BBA330 ; void __cdecl objects_recycling_update_membership(s_recycling_workspace*)
-//.text:00BBA860 ; void __cdecl prepare_recycling_group_list(s_recycling_workspace*)
+//.text:00BBA0F0 ; void __cdecl objects_recycling_render(s_recycling_workspace* recycling_workspace)
+//.text:00BBA100 ; void __cdecl objects_recycling_update(e_recycling_methods method)
+//.text:00BBA330 ; void __cdecl objects_recycling_update_membership(s_recycling_workspace* recycling_workspace)
+//.text:00BBA860 ; void __cdecl prepare_recycling_group_list(s_recycling_workspace* recycling_workspace)
 //.text:00BBAA00 ; uns32 __cdecl recycling_get_target_flags(uns32)
 //.text:00BBAA90 ; uns32 __cdecl recycling_get_tolerance_flags()
 //.text:00BBAB00 ; void __cdecl recycling_release_object(int32)
-//.text:00BBAB30 ; 
-//.text:00BBAB70 ; 
+//.text:00BBAB30 ; tls
+//.text:00BBAB70 ; tls
 //.text:00BBABB0 ; 
-//.text:00BBABE0 ; int __cdecl sort_recycling_entry_descending(const void*, const void*)
-//.text:00BBAC00 ; void __cdecl sort_recycling_entry_list(s_recycling_workspace*)
+//.text:00BBABE0 ; int __cdecl sort_recycling_entry_descending(const void* p1, const void* p2)
+//.text:00BBAC00 ; void __cdecl sort_recycling_entry_list(s_recycling_workspace* recycling_workspace)
 //.text:00BBAC30 ; 
-//.text:00BBAC50 ; void __cdecl update_active_recycling_volumes(s_recycling_workspace*)
-//.text:00BBAF80 ; void __cdecl update_recycling_group_list(s_recycling_workspace*)
+//.text:00BBAC50 ; void __cdecl update_active_recycling_volumes(s_recycling_workspace* recycling_workspace)
+//.text:00BBAF80 ; void __cdecl update_recycling_group_list(s_recycling_workspace* recycling_workspace)
 
--- game/source/objects/object_recycling.hpp
+++ game/source/objects/object_recycling.hpp
@@ -2,16 +2,105 @@
 
 #include "cseries/cseries.hpp"
 
+enum e_recycling_constants
+{
+	k_recycling_history_count = 64,
+	k_recycling_max_volumes = 16,
+	k_recycling_max_initial_groups = 112,
+	k_recycling_max_volume_groups = 16,
+	k_recycling_max_groups = 128,
+};
+
+enum e_recycling_methods
+{
+	_recycle_normally = 0,
+	_recycle_everything_ready_excluding_visible = 1,
+	_recycle_everything_ready_including_visible = 2,
+};
+
+enum e_recycling_object_flag_bits
+{
+	_recycling_object_waiting = 0,
+	_recycling_object_onscreen,
+	_recycling_object_active_only,
+	k_recycling_object_flag_bits,
+};
+
+enum e_object_recycling_flags
+{
+	_object_recycling_ignore = 0,
+	_object_recycling_candidate,
+	_object_recycling_active_candidate,
+	_object_recycling_filtered,
+	_object_recycling_filter_waiting,
+	_object_recycling_filter_visible,
+
+	k_object_recycling_flags_count,
+};
+typedef c_flags<e_object_recycling_flags, uns8, k_object_recycling_flags_count> c_object_recycling_flags;
+
+enum e_recycling_tolerance_flag_bits
+{
+	_recycling_reduce_memory = 0,
+	_recycling_reduce_objects,
+	_recycling_reduce_activity,
+	_recycling_tolerances_critical,
+	k_recycling_tolerance_flag_bits_count,
+
+	_recycling_mask_reduce_activity = FLAG(_recycling_reduce_activity),
+	_recycling_mask_reduce_overhead = FLAG(_recycling_reduce_memory) | FLAG(_recycling_reduce_objects),
+	_recycling_mask_critical = FLAG(_recycling_tolerances_critical),
+	_recycling_mask_any = NONE,
+};
+typedef c_flags<e_recycling_tolerance_flag_bits, int32, k_recycling_tolerance_flag_bits_count> c_recycling_tolerance_flags;
+
+struct s_recycling_volume
+{
+	int32 previous_volume;
+	int32 next_volume;
+	int32 trigger_volume_index;
+	int32 max_object_count;
+	int32 ending_tick;
+};
+static_assert(sizeof(s_recycling_volume) == 0x14);
+
 struct s_recycling_volumes
 {
-	byte __data[0x148];
+	int32 recycling_volume_list_top;
+	int32 recycling_volume_free_list_top;
+	s_recycling_volume recycling_volume_members[k_recycling_max_volumes];
 };
 static_assert(sizeof(s_recycling_volumes) == 0x148);
 
 struct s_recycling_group :
 	s_datum_header
 {
-	byte __data[0x12];
+	int16 member_count;
+	int32 first_member;
+	real_point3d world_position;
 };
 static_assert(sizeof(s_recycling_group) == 0x14);
+
+struct s_recycling_entry
+{
+	int32 score;
+	int32 index;
+	real_rectangle3d bounding_box;
+};
+static_assert(sizeof(s_recycling_entry) == 0x20);
+
+struct s_recycling_workspace
+{
+	bool membership_updated;
+	int32 group_count;
+	s_recycling_entry group_list[k_recycling_max_groups];
+};
+static_assert(sizeof(s_recycling_workspace) == 0x1008);
+
+struct s_recycling_history_item
+{
+	real_point3d world_center;
+	real32 radius;
+};
+static_assert(sizeof(s_recycling_history_item) == 0x10);
 
