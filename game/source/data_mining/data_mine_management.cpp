cef677457b9401164386cbe0b71234a236cb120a
theTwister <6237734+twist84@users.noreply.github.com>
2025-11-21 21:18:45 +0000

    Added `data_mine_management` functions

--- game/source/data_mining/data_mine_management.cpp
+++ game/source/data_mining/data_mine_management.cpp
@@ -7,23 +7,41 @@ void __cdecl data_mine_dispose()
 	INVOKE(0x005AC740, data_mine_dispose);
 }
 
-//.text:005AC750 ; void __cdecl data_mine_enable(bool)
-//.text:005AC760 ; bool __cdecl data_mine_enabled()
+void __cdecl data_mine_enable(bool setting)
+{
+	INVOKE(0x005AC750, data_mine_enable, setting);
+}
+
+bool __cdecl data_mine_enabled()
+{
+	return INVOKE(0x005AC760, data_mine_enabled);
+}
 
 void __cdecl data_mine_flush()
 {
 	INVOKE(0x005AC770, data_mine_flush);
 }
 
-//.text:005AC780 ; const char* __cdecl data_mine_get_tag_name(int32)
+const char* __cdecl data_mine_get_tag_name(int32 tag_index)
+{
+	return INVOKE(0x005AC780, data_mine_get_tag_name, tag_index);
+}
 
 void __cdecl data_mine_initialize()
 {
 	INVOKE(0x005AC790, data_mine_initialize);
 }
 
-//.text:005AC7A0 ; 
-//.text:005AC7B0 ; void __cdecl data_mine_insert_text_and_camera_position(const char*)
+void __cdecl data_mine_insert(const void* data, int32 data_length)
+{
+	INVOKE(0x005AC7A0, data_mine_insert, data, data_length);
+}
+
+void __cdecl data_mine_insert_text_and_camera_position(const char* text)
+{
+	INVOKE(0x005AC7B0, data_mine_insert_text_and_camera_position, text);
+}
+
 //.text:005AC7C0 ; 
 
 void __cdecl data_mine_update()
@@ -31,7 +49,18 @@ void __cdecl data_mine_update()
 	INVOKE(0x005AC7D0, data_mine_update);
 }
 
-//.text:005AC7E0 ; void __cdecl data_mine_upload()
-//.text:005AC7F0 ; void __cdecl data_mine_upload_no_dialog()
-//.text:005AC800 ; bool __cdecl data_mine_uploading_files()
+void __cdecl data_mine_upload()
+{
+	INVOKE(0x005AC7E0, data_mine_upload);
+}
+
+void __cdecl data_mine_upload_no_dialog()
+{
+	INVOKE(0x005AC7F0, data_mine_upload_no_dialog);
+}
+
+bool __cdecl data_mine_uploading_files()
+{
+	return INVOKE(0x005AC800, data_mine_uploading_files);
+}
 
--- game/source/data_mining/data_mine_management.hpp
+++ game/source/data_mining/data_mine_management.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
 extern void __cdecl data_mine_dispose();
+extern void __cdecl data_mine_enable(bool setting);
+extern bool __cdecl data_mine_enabled();
 extern void __cdecl data_mine_flush();
+extern const char* __cdecl data_mine_get_tag_name(int32 tag_index);
 extern void __cdecl data_mine_initialize();
+extern void __cdecl data_mine_insert(const void* data, int32 data_length);
+extern void __cdecl data_mine_insert_text_and_camera_position(const char* text);
 extern void __cdecl data_mine_update();
+extern void __cdecl data_mine_upload();
+extern void __cdecl data_mine_upload_no_dialog();
+extern bool __cdecl data_mine_uploading_files();
 
