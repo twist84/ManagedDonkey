409da3b662d9cdaf8893ca1e37aafd10eece9814
theTwister <6237734+twist84@users.noreply.github.com>
2025-12-03 21:09:03 +0000

    Update main.cpp

--- game/source/main/main.cpp
+++ game/source/main/main.cpp
@@ -1187,36 +1187,40 @@ void __cdecl main_loop_body()
 									sound_render();
 								}
 
-								//static uns32 main_loop_network_time_since_abort = 0;
-								//if (simulation_aborted())
-								//{
-								//	uns32 network_time_since_abort = simulation_get_network_time_since_abort();
-								//	if (network_time_since_abort > 1000)
-								//	{
-								//		network_time_since_abort /= 1000;
-								//	}
-								//	else
-								//	{
-								//		network_time_since_abort = 0;
-								//	}
-								//
-								//	if (network_time_since_abort >= 5)
-								//	{
-								//		if (network_time_since_abort >= main_loop_network_time_since_abort + 5)
-								//		{
-								//			main_loop_network_time_since_abort = network_time_since_abort;
-								//			console_printf("simulation aborted %i seconds ago [%s]. Waiting for instructions...", network_time_since_abort, simulation_get_abort_reason_string());
-								//		}
-								//	}
-								//	else
-								//	{
-								//		main_loop_network_time_since_abort = 0;
-								//	}
-								//}
-								//else
-								//{
-								//	main_loop_network_time_since_abort = 0;
-								//}
+#if 0
+								static uns32 main_loop_network_time_since_abort = 0;
+								if (simulation_aborted())
+								{
+									uns32 network_time_since_abort = simulation_get_network_time_since_abort();
+									if (network_time_since_abort > 1000)
+									{
+										network_time_since_abort /= 1000;
+									}
+									else
+									{
+										network_time_since_abort = 0;
+									}
+								
+									if (network_time_since_abort >= 5)
+									{
+										if (network_time_since_abort >= main_loop_network_time_since_abort + 5)
+										{
+											main_loop_network_time_since_abort = network_time_since_abort;
+											console_printf("simulation aborted %i seconds ago [%s]. Waiting for instructions...",
+												network_time_since_abort,
+												simulation_get_abort_reason_string());
+										}
+									}
+									else
+									{
+										main_loop_network_time_since_abort = 0;
+									}
+								}
+								else
+								{
+									main_loop_network_time_since_abort = 0;
+								}
+#endif
 							}
 						}
 
