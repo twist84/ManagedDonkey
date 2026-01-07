4f0a1366c9e4c6937de83c840d6ea06114342d84
theTwister <6237734+twist84@users.noreply.github.com>
2026-01-07 12:10:58 +0000

    `start_menu_settings.cpp` cleanup

--- game/source/interface/gui_screens/start_menu/panes/settings/start_menu_settings.cpp
+++ game/source/interface/gui_screens/start_menu/panes/settings/start_menu_settings.cpp
@@ -31,22 +31,26 @@ bool c_start_menu_settings::handle_list_item_chosen(const c_controller_input_mes
 {
 	//return INVOKE_CLASS_MEMBER(0x00AF73B0, c_start_menu_settings, handle_list_item_chosen, message, list_name, list_item_widget, datasource);
 
+	bool handled = false;
+
+	bool game_is_in_ui_shell = game_in_progress() && game_is_ui_shell();
 	if (list_name == STRING_ID(gui, sidebar))
 	{
-		int32 item_name = NONE;
+		string_id item_name = NONE;
 		if (datasource->get_string_id_value(list_item_widget->get_element_handle(), STRING_ID(global, name), &item_name)
 			&& item_name == STRING_ID(gui, appearance)
-			&& (!game_in_progress() || !game_is_ui_shell()))
+			&& !game_is_in_ui_shell)
 		{
-			user_interface_error_manager_get()->post_error(
-				STRING_ID(gui_alert, can_not_view_in_game),
-				message->get_controller(),
-				false);
-			return true;
+			user_interface_error_manager_get()->post_error(STRING_ID(gui_alert, can_not_view_in_game), message->get_controller(), false);
+			handled = true;
 		}
 	}
 
-	return c_start_menu_pane_screen_widget::handle_list_item_chosen(message, list_name, list_item_widget, datasource);
+	if (!handled)
+	{
+		handled = c_start_menu_pane_screen_widget::handle_list_item_chosen(message, list_name, list_item_widget, datasource);
+	}
+	return handled;
 }
 
 void c_start_menu_settings::initialize()
@@ -56,9 +60,9 @@ void c_start_menu_settings::initialize()
 	c_gui_screen_widget::initialize();
 
 #if defined(TOP_MENU_CAP)
-	bool is_ui_shell = game_is_ui_shell();
-	c_gui_widget::set_child_visible(_gui_group, STRING_ID(gui, top_menu), is_ui_shell);
-	c_gui_widget::set_child_visible(_gui_group, STRING_ID(gui, top_menu_cap), !is_ui_shell);
+	bool ui_shell = game_is_ui_shell();
+	c_gui_widget::set_child_visible(_gui_group, STRING_ID(gui, top_menu), ui_shell);
+	c_gui_widget::set_child_visible(_gui_group, STRING_ID(gui, top_menu_cap), !ui_shell);
 #endif
 }
 
@@ -66,13 +70,13 @@ void c_start_menu_settings::update(uns32 current_milliseconds)
 {
 	//INVOKE_CLASS_MEMBER(0x00AF7470, c_start_menu_settings, update, current_milliseconds);
 
-	int32 pane_name = c_start_menu_screen_widget::get_current_game_pane_name();
-	if (pane_name == NONE)
+	string_id header_string_id = c_start_menu_screen_widget::get_current_game_pane_name();
+	if (header_string_id == NONE)
 	{
-		pane_name = STRING_ID(gui, top_menu_display_item0);
+		header_string_id = STRING_ID(gui, top_menu_display_item0);
 	}
 
-	c_gui_text_widget* pane_text_widget = c_gui_widget::get_child_text_widget(pane_name);
+	c_gui_text_widget* header = c_gui_widget::get_child_text_widget(header_string_id);
 	c_gui_widget::set_child_visible(_gui_text, STRING_ID(gui, top_menu_display_item0), false);
 	c_gui_widget::set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_campaign), false);
 	c_gui_widget::set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_saved_film), false);
@@ -80,21 +84,21 @@ void c_start_menu_settings::update(uns32 current_milliseconds)
 	c_gui_widget::set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_multiplayer), false);
 
 #if defined(TOP_MENU_CAP)
-	c_gui_group_widget* top_menu_cap_group_widget = c_gui_widget::get_child_group_widget(STRING_ID(gui, top_menu_cap));
-	if (top_menu_cap_group_widget && top_menu_cap_group_widget->get_visible())
+	c_gui_group_widget* top_menu_cap = c_gui_widget::get_child_group_widget(STRING_ID(gui, top_menu_cap));
+	if (top_menu_cap && top_menu_cap->get_visible())
 	{
-		pane_text_widget = top_menu_cap_group_widget->get_child_text_widget(pane_name);
-		top_menu_cap_group_widget->set_child_visible(_gui_text, STRING_ID(gui, top_menu_display_item0), false);
-		top_menu_cap_group_widget->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_campaign), false);
-		top_menu_cap_group_widget->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_saved_film), false);
-		top_menu_cap_group_widget->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_editor), false);
-		top_menu_cap_group_widget->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_multiplayer), false);
+		header = top_menu_cap->get_child_text_widget(header_string_id);
+		top_menu_cap->set_child_visible(_gui_text, STRING_ID(gui, top_menu_display_item0), false);
+		top_menu_cap->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_campaign), false);
+		top_menu_cap->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_saved_film), false);
+		top_menu_cap->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_editor), false);
+		top_menu_cap->set_child_visible(_gui_text, STRING_ID(gui, start_menu_game_multiplayer), false);
 	}
 #endif
 
-	if (pane_text_widget)
+	if (header)
 	{
-		pane_text_widget->set_visible(true);
+		header->set_visible(true);
 	}
 
 	c_start_menu_pane_screen_widget::update(current_milliseconds);
